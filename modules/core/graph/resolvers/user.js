d2db31381bafedc14f2aa330d51b014f86136b18
Matteo Cominetti <matteo@speckle.systems>
2020-07-28 14:43:01 +0100

    refractor: fixes indentation

--- modules/core/graph/resolvers/user.js
+++ modules/core/graph/resolvers/user.js
@@ -8,90 +8,88 @@ const setupCheck = require( `${appRoot}/setupcheck` )
 const zxcvbn = require( 'zxcvbn' )
 
 module.exports = {
-    Query: {
+  Query: {
 
-        async _() {
-            return `Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn.`
-        },
-
-        async user( parent, args, context, info ) {
-
-            await validateServerRole( context, 'server:user' )
-
-            if ( !args.id )
-                await validateScopes( context.scopes, 'profile:read' )
-            else
-                await validateScopes( context.scopes, 'users:read' )
-
-            if ( !args.id && !context.userId ) {
-                throw new UserInputError( 'You must provide an user id.' )
-            }
-
-            return await getUser( args.id || context.userId )
-        },
-
-        async userSearchResults( parent, args, context, info ) {
-
-            await validateServerRole( context, 'server:user' )
-            await validateScopes( context.scopes, 'profile:read' )
-            await validateScopes( context.scopes, 'users:read' )
-
-            if ( !args.query ) {
-                throw new UserInputError( 'You must provide a search query.' )
-            }
-
-            if ( args.query.length < 3 ) {
-                throw new UserInputError( 'Search query must be at least 3 carachters.' )
-            }
-
-            if ( args.limit  && args.limit > 100 ) {
-                throw new UserInputError( 'Cannot return more than 100 results.' )
-            }
-
-            return await searchUsers( args.query, args.limit )
-        },
-
-        async userPwdStrength( parent, args, context, info ) {
-            let res = zxcvbn( args.pwd )
-            return { score: res.score, feedback: res.feedback }
-        }
-
+    async _() {
+      return `Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn.`
     },
 
-    User: {
+    async user( parent, args, context, info ) {
+      await validateServerRole( context, 'server:user' )
 
-        async email( parent, args, context, info ) {
-            // NOTE: we're redacting the field (returning null) rather than throwing a full error which would invalidate the request.
-            if ( context.userId === parent.id ) {
-                try {
-                    await validateScopes( context.scopes, 'profile:email' )
-                    return parent.email
-                } catch ( err ) {
-                    return null
-                }
-            }
+      if ( !args.id )
+        await validateScopes( context.scopes, 'profile:read' )
+      else
+        await validateScopes( context.scopes, 'users:read' )
 
-            try {
-                await validateScopes( context.scopes, 'users:email' )
-                return parent.email
-            } catch ( err ) {
-                return null
-            }
-        },
+      if ( !args.id && !context.userId ) {
+        throw new UserInputError( 'You must provide an user id.' )
+      }
 
-        async role( parent, args, context, info ) {
-            return await getUserRole( parent.id )
-        }
-
+      return await getUser( args.id || context.userId )
     },
 
+    async userSearchResults( parent, args, context, info ) {
+      await validateServerRole( context, 'server:user' )
+      await validateScopes( context.scopes, 'profile:read' )
+      await validateScopes( context.scopes, 'users:read' )
 
+      if ( !args.query ) {
+        throw new UserInputError( 'You must provide a search query.' )
+      }
 
-    Mutation: {
-        async userEdit( parent, args, context, info ) {
-            await validateServerRole( context, 'server:user' )
-            await updateUser( context.userId, args.user )
-            return true
-        }
+      if ( args.query.length < 3 ) {
+        throw new UserInputError( 'Search query must be at least 3 carachters.' )
+      }
+
+      if ( args.limit  && args.limit > 100 ) {
+        throw new UserInputError( 'Cannot return more than 100 results.' )
+      }
+
+      return await searchUsers( args.query, args.limit )
+    },
+
+    async userPwdStrength( parent, args, context, info ) {
+      let res = zxcvbn( args.pwd )
+      return { score: res.score, feedback: res.feedback }
     }
+
+  },
+
+  User: {
+
+    async email( parent, args, context, info ) {
+      // NOTE: we're redacting the field (returning null) rather than throwing a full error which would invalidate the request.
+      if ( context.userId === parent.id ) {
+        try {
+          await validateScopes( context.scopes, 'profile:email' )
+          return parent.email
+        } catch ( err ) {
+          return null
+        }
+      }
+
+      try {
+        await validateScopes( context.scopes, 'users:email' )
+        return parent.email
+      } catch ( err ) {
+        return null
+      }
+    },
+
+    async role( parent, args, context, info ) {
+      return await getUserRole( parent.id )
+    }
+
+  },
+
+
+
+  Mutation: {
+    async userEdit( parent, args, context, info ) {
+      await validateServerRole( context, 'server:user' )
+      await updateUser( context.userId, args.user )
+      return true
+    }
+  }
 }
--- modules/core/services/users.js
+++ modules/core/services/users.js
@@ -9,98 +9,97 @@ const ServerRoles = () => knex( 'server_acl' )
 
 module.exports = {
 
-    /*
+  /*
 
         Users
 
      */
 
-    async createUser( user ) {
-        let [ {count} ] = await ServerRoles().where( {role: 'server:admin'} ).count()
+  async createUser( user ) {
+    let [ {count} ] = await ServerRoles().where( {role: 'server:admin'} ).count()
 
-        user.id = crs( {length: 10} )
+    user.id = crs( {length: 10} )
 
-        if ( user.password ) {
-            user.passwordDigest = await bcrypt.hash( user.password, 10 )
-        }
-        delete user.password
-
-        let usr = await Users().select( 'id' ).where( {email: user.email} ).first()
-        if ( usr ) throw new Error( 'Email taken. Try logging in?' )
-
-        let res = await Users().returning( 'id' ).insert( user )
-
-        if ( parseInt( count ) === 0 ) {
-            await ServerRoles().insert( {userId: res[0], role: 'server:admin'} )
-        } else {
-            await ServerRoles().insert( {userId: res[0], role: 'server:user'} )
-        }
-
-        return res[0]
-    },
-
-    async findOrCreateUser( {user, rawProfile} ) {
-        let existingUser = await Users().select( 'id' ).where( {email: user.email} ).first()
-
-        if ( existingUser )
-            return existingUser
-
-        user.password = crs( {length: 20} )
-        user.verified = true // because we trust the external identity provider, no?
-        return {id: await module.exports.createUser( user )}
-    },
-
-    async getUserById( {userId} ) {
-        let user = await Users().where( {id: userId} ).select( '*' ).first()
-        delete user.passwordDigest
-        return user
-    },
-
-    // TODO: deprecate
-    async getUser( id ) {
-        let user = await Users().where( {id: id} ).select( '*' ).first()
-        delete user.passwordDigest
-        return user
-    },
-
-    async getUserByEmail( {email} ) {
-        let user = await Users().where( {email: email} ).select( '*' ).first()
-        delete user.passwordDigest
-        return user
-    },
-
-    async getUserRole( id ) {
-        let {role} = await ServerRoles().where( {userId: id} ).select( 'role' ).first()
-        return role
-    },
-
-    async updateUser( id, user ) {
-        delete user.id
-        delete user.passwordDigest
-        delete user.password
-        delete user.email
-        await Users().where( {id: id} ).update( user )
-    },
-
-    async searchUsers( query, limit ) {
-        
-        limit = limit || 100
-        let likeQuery = "%" + query + "%"
-        let users = await Users()
-            .where( {email: query} ) //match full email or partial username / name
-            .orWhere( 'username', 'like', likeQuery )
-            .orWhere( 'name', 'like', likeQuery )
-            .limit( limit )
-
-        return users
-    },
-
-    async validatePasssword( {email, password} ) {
-        let {passwordDigest} = await Users().where( {email: email} ).select( 'passwordDigest' ).first()
-        return bcrypt.compare( password, passwordDigest )
-    },
-
-    async deleteUser( id ) {
-        throw new Error( 'not implemented' )
+    if ( user.password ) {
+      user.passwordDigest = await bcrypt.hash( user.password, 10 )
     }
+    delete user.password
+
+    let usr = await Users().select( 'id' ).where( {email: user.email} ).first()
+    if ( usr ) throw new Error( 'Email taken. Try logging in?' )
+
+    let res = await Users().returning( 'id' ).insert( user )
+
+    if ( parseInt( count ) === 0 ) {
+      await ServerRoles().insert( {userId: res[0], role: 'server:admin'} )
+    } else {
+      await ServerRoles().insert( {userId: res[0], role: 'server:user'} )
+    }
+
+    return res[0]
+  },
+
+  async findOrCreateUser( {user, rawProfile} ) {
+    let existingUser = await Users().select( 'id' ).where( {email: user.email} ).first()
+
+    if ( existingUser )
+      return existingUser
+
+    user.password = crs( {length: 20} )
+    user.verified = true // because we trust the external identity provider, no?
+    return {id: await module.exports.createUser( user )}
+  },
+
+  async getUserById( {userId} ) {
+    let user = await Users().where( {id: userId} ).select( '*' ).first()
+    delete user.passwordDigest
+    return user
+  },
+
+  // TODO: deprecate
+  async getUser( id ) {
+    let user = await Users().where( {id: id} ).select( '*' ).first()
+    delete user.passwordDigest
+    return user
+  },
+
+  async getUserByEmail( {email} ) {
+    let user = await Users().where( {email: email} ).select( '*' ).first()
+    delete user.passwordDigest
+    return user
+  },
+
+  async getUserRole( id ) {
+    let {role} = await ServerRoles().where( {userId: id} ).select( 'role' ).first()
+    return role
+  },
+
+  async updateUser( id, user ) {
+    delete user.id
+    delete user.passwordDigest
+    delete user.password
+    delete user.email
+    await Users().where( {id: id} ).update( user )
+  },
+
+  async searchUsers( query, limit ) {
+    limit = limit || 100
+    let likeQuery = "%" + query + "%"
+    let users = await Users()
+      .where( {email: query} ) //match full email or partial username / name
+      .orWhere( 'username', 'like', likeQuery )
+      .orWhere( 'name', 'like', likeQuery )
+      .limit( limit )
+
+    return users
+  },
+
+  async validatePasssword( {email, password} ) {
+    let {passwordDigest} = await Users().where( {email: email} ).select( 'passwordDigest' ).first()
+    return bcrypt.compare( password, passwordDigest )
+  },
+
+  async deleteUser( id ) {
+    throw new Error( 'not implemented' )
+  }
 }
