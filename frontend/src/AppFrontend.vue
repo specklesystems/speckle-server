33b5704cb421ae5ab1bf7a6e3587ff84ca1c084d
Matteo Cominetti <matteo@cominetti.org>
2020-10-02 12:52:51 +0100

    style(ui): adds vscode workspace settings

--- frontend/src/AppFrontend.vue
+++ frontend/src/AppFrontend.vue
@@ -2,18 +2,26 @@
   <v-app id="inspire">
     <v-app-bar app color="white" flat>
       <v-container class="py-0 fill-height">
-        
-
-
         <v-btn text to="/">
-          <v-img contain max-height="30" max-width="30" src="./assets/logo.svg" />
+          <v-img
+            contain
+            max-height="30"
+            max-width="30"
+            src="./assets/logo.svg"
+          />
           <div class="mt-1">
-            <span class="primary--text"><b>SPECKLE</b></span>&nbsp;<span class="font-weight-light">ADMIN</span>
+            <span class="primary--text"><b>SPECKLE</b></span
+            >&nbsp;<span class="font-weight-light">ADMIN</span>
           </div>
-          
         </v-btn>
 
-        <v-btn v-for="link in navLinks" :key="link.name" text class="text-uppercase" :to="link.link">
+        <v-btn
+          v-for="link in navLinks"
+          :key="link.name"
+          text
+          class="text-uppercase"
+          :to="link.link"
+        >
           {{ link.name }}
         </v-btn>
 
@@ -62,9 +70,9 @@
 
           <v-col>
             <!-- <v-sheet min-height="70vh" rounded="lg"> -->
-              <v-main class="pt-0">
-                <router-view :user="user"></router-view>
-              </v-main>
+            <v-main class="pt-0">
+              <router-view :user="user"></router-view>
+            </v-main>
             <!-- </v-sheet> -->
           </v-col>
         </v-row>
@@ -110,24 +118,25 @@ export default {
             }
           }
         }
-      `,
-    },
+      `
+    }
   },
   components: {},
 
-  data: () => ( {
+  data: () => ({
     user: {},
-    navLinks: [ 
-      {link: "/streams", name:"streams"}, 
-      {link: "/teams", name:"teams"}, 
-      {link: "/profile", name:"profile"}, 
-      {link: "/settings", name:"settings"}, 
-      {link: "/help", name:"help"}
-    ] } ),
-  
-  watch:{
-    user( val ){
-      console.log( val )
+    navLinks: [
+      { link: "/streams", name: "streams" },
+      { link: "/teams", name: "teams" },
+      { link: "/profile", name: "profile" },
+      { link: "/settings", name: "settings" },
+      { link: "/help", name: "help" }
+    ]
+  }),
+
+  watch: {
+    user(val) {
+      console.log(val)
     }
   }
 }
