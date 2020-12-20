a4aa7afa853c0c75e5400b19a97db282cfa5cf27
Dimitrie Stefanescu <didimitrie@gmail.com>
2020-12-20 02:20:27 +0200

    feat(frontend): various little hacks

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,110 +1,118 @@
-import Vue from "vue"
-import VueRouter from "vue-router"
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "home",
+    path: '/',
+    name: 'home',
     meta: {
-      title: "Home | Speckle"
+      title: 'Home | Speckle'
     },
-    component: () => import("../views/Home.vue")
+    component: () => import('../views/Home.vue')
   },
   {
-    path: "/streams",
-    name: "streams",
+    path: '/streams',
+    name: 'streams',
     meta: {
-      title: "Streams | Speckle"
+      title: 'Streams | Speckle'
     },
-    component: () => import("../views/Streams.vue")
+    component: () => import('../views/Streams.vue')
   },
   {
-    path: "/streams/:streamId",
+    path: '/streams/:streamId',
     meta: {
-      title: "Stream | Speckle"
+      title: 'Stream | Speckle'
     },
-    component: () => import("../views/Stream.vue"),
+    component: () => import('../views/Stream.vue'),
     children: [
       {
-        path: "",
-        name: "stream",
+        path: '',
+        name: 'stream',
         meta: {
-          title: "Stream | Speckle"
+          title: 'Stream | Speckle'
         },
-        component: () => import("../views/StreamMain.vue")
+        component: () => import('../views/StreamMain.vue')
       },
       {
-        path: "branches/:branchName",
-        name: "branch",
+        path: 'branches/:branchName',
+        name: 'branch',
         meta: {
-          title: "Branch | Speckle"
+          title: 'Branch | Speckle'
         },
-        component: () => import("../views/Branch.vue")
+        component: () => import('../views/Branch.vue')
       },
       {
-        path: "commits/:commitId",
-        name: "commit",
+        path: 'commits/:commitId',
+        name: 'commit',
         meta: {
-          title: "Commit | Speckle"
+          title: 'Commit | Speckle'
         },
-        component: () => import("../views/Commit.vue")
+        component: () => import('../views/Commit.vue')
       },
       {
-        path: "objects/:objectId",
-        name: "objects",
+        path: 'objects/:objectId',
+        name: 'objects',
         meta: {
-          title: "Object | Speckle"
+          title: 'Object | Speckle'
         },
-        component: () => import("../views/Object.vue")
+        component: () => import('../views/Object.vue')
       }
     ]
   },
   {
-    path: "/profile",
-    name: "profile",
+    path: '/profile',
+    name: 'profile',
     meta: {
-      title: "Your Profile | Speckle"
+      title: 'Your Profile | Speckle'
     },
-    component: () => import("../views/Profile.vue")
+    component: () => import('../views/Profile.vue')
   },
   {
-    path: "/profile/:userId",
-    name: "user profile",
+    path: '/profile/:userId',
+    name: 'user profile',
     meta: {
-      title: "User Profile | Speckle"
+      title: 'User Profile | Speckle'
     },
-    component: () => import("../views/ProfileUser.vue")
+    component: () => import('../views/ProfileUser.vue')
   },
   {
-    path: "/help",
-    name: "help",
+    path: '/help',
+    name: 'help',
     meta: {
-      title: "Help | Speckle"
+      title: 'Help | Speckle'
     },
-    component: () => import("../views/Help.vue")
+    component: () => import('../views/Help.vue')
   },
   {
-    path: "/about",
-    name: "about",
+    path: '/about',
+    name: 'about',
     meta: {
-      title: "About | Speckle"
+      title: 'About | Speckle'
     },
-    component: () => import("../views/About.vue")
+    component: () => import('../views/About.vue')
   },
   {
-    path: "*",
-    name: "notfound",
+    path: '/error',
+    name: 'error',
     meta: {
-      title: "Not Found | Speckle"
+      title: 'Error | Speckle'
     },
-    component: () => import("../views/NotFound.vue")
+    component: () => import('../views/Error.vue')
+  },
+  {
+    path: '*',
+    name: 'notfound',
+    meta: {
+      title: 'Not Found | Speckle'
+    },
+    component: () => import('../views/NotFound.vue')
   }
 ]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
@@ -112,7 +120,7 @@ const router = new VueRouter({
 //TODO: include stream name in page title eg `My Cool Stream | Speckle`
 router.afterEach((to, from) => {
   Vue.nextTick(() => {
-    document.title = (to.meta && to.meta.title) || "Speckle"
+    document.title = (to.meta && to.meta.title) || 'Speckle'
   })
 })
 
