6f61a36cd5d3bce57a9f53f297599abb196509eb
Dimitrie Stefanescu <didimitrie@gmail.com>
2020-12-22 10:53:20 +0200

    feat(server): added migrations for totalChildrenCount and sourceApplication for commit table

---
+++ modules/core/migrations/20201222100048_add_sourceapp_to_commits.js
@@ -0,0 +1,12 @@
+
+exports.up = async ( knex ) => {
+  await knex.schema.alterTable( 'commits', table => {
+    table.string( 'sourceApplication', 1024 )
+  } )
+}
+
+exports.down = async ( knex ) => {
+  await knex.schema.alterTable( 'commits', table => {
+    table.dropColumn( 'sourceApplication' )
+  } )
+}
---
+++ modules/core/migrations/20201222101522_add_totalchildrencount_to_commits.js
@@ -0,0 +1,12 @@
+
+exports.up = async ( knex ) => {
+  await knex.schema.alterTable( 'commits', table => {
+    table.integer( 'totalChildrenCount' )
+  } )
+}
+
+exports.down = async ( knex ) => {
+  await knex.schema.alterTable( 'commits', table => {
+    table.dropColumn( 'totalChildrenCount' )
+  } )
+}
