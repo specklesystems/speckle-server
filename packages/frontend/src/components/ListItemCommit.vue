56f3b3fb79916aed56a2f988dadb56e912106224
Dimitrie Stefanescu <didimitrie@gmail.com>
2021-01-23 22:41:58 +0000

    feat(frontend): adds source app and branch to commit list item

--- packages/frontend/src/components/ListItemCommit.vue
+++ packages/frontend/src/components/ListItemCommit.vue
@@ -9,8 +9,14 @@
       />
     </v-list-item-icon>
     <v-list-item-content>
-      <v-list-item-title>
+      <v-list-item-title class="my-2 py-1">
         {{ commit.message }}
+        <span class="caption" v-if="commit.branchName">
+          <v-chip small style="top:-3px">
+            <v-icon small class="mr-2">mdi-source-branch</v-icon>
+            {{ commit.branchName }}
+          </v-chip>
+        </span>
       </v-list-item-title>
       <v-list-item-subtitle class="caption">
         <b>{{ commit.authorName }}</b>
@@ -19,13 +25,17 @@
         ({{ commitDate }})
       </v-list-item-subtitle>
     </v-list-item-content>
+    <v-list-item-action>
+      <source-app-avatar :application-name="commit.sourceApplication" />
+    </v-list-item-action>
   </v-list-item>
 </template>
 <script>
 import UserAvatar from './UserAvatar'
+import SourceAppAvatar from './SourceAppAvatar'
 
 export default {
-  components: { UserAvatar },
+  components: { UserAvatar, SourceAppAvatar },
   props: ['commit', 'streamId'],
   computed: {
     commitDate() {
@@ -34,6 +44,15 @@ export default {
       let options = { year: 'numeric', month: 'long', day: 'numeric' }
 
       return date.toLocaleString(undefined, options)
+    },
+    branchUrl() {
+      if (!this.commit) return null
+      return `${window.location.origin}/streams/${this.$route.params.streamId}/branches/${this.commit.branchName}`
+    }
+  },
+  methods: {
+    goToBranch() {
+      this.$router.push(this.branchUrl)
     }
   }
 }
