cfd58e3de594f10c5071b0f349fce68a00a65ee2
Dimitrie Stefanescu <didimitrie@gmail.com>
2021-03-03 18:04:33 +0000

    feat(server): scaffolds invites and pwd reset modules

---
+++ packages/server/modules/invites/index.js
@@ -0,0 +1,11 @@
+'use strict'
+let debug = require( 'debug' )
+const appRoot = require( 'app-root-path' )
+
+exports.init = async ( app, options ) => {
+  debug( 'speckle:modules' )( '💌 Init invites module' )
+}
+
+exports.finalize = async () => {
+
+}
---
+++ packages/server/modules/pwdreset/index.js
@@ -0,0 +1,11 @@
+'use strict'
+let debug = require( 'debug' )
+const appRoot = require( 'app-root-path' )
+
+exports.init = async ( app, options ) => {
+  debug( 'speckle:modules' )( '♻️ Init pwd reset module' )
+}
+
+exports.finalize = async () => {
+
+}
