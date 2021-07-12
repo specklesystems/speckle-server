9a379421382e1db03e50c770e28110e76266cfee
izzy lyseggen <izzy.lyseggen@gmail.com>
2021-07-12 18:31:55 +0100

    test(webhooks): add gql tests

--- packages/server/modules/webhooks/tests/webhooks.spec.js
+++ packages/server/modules/webhooks/tests/webhooks.spec.js
@@ -4,18 +4,22 @@ const chaiHttp = require( 'chai-http' )
 const assert = require( 'assert' )
 
 const appRoot = require( 'app-root-path' )
-const { init } = require( `${appRoot}/app` )
+const { init, startHttp } = require( `${appRoot}/app` )
 const knex = require( `${appRoot}/db/knex` )
+const { createPersonalAccessToken } = require( '../../core/services/tokens' )
+const { createWebhook, getStreamWebhooks, getLastWebhookEvents, getWebhook, updateWebhook, deleteWebhook, dispatchStreamEvent } = require( '../services/webhooks' )
+const { createUser } = require( '../../core/services/users' )
+const { createStream, getStream, grantPermissionsStream } = require( '../../core/services/streams' )
 
 const expect = chai.expect
 chai.use( chaiHttp )
 
+const serverAddress = `http://localhost:${process.env.PORT || 3000}`
 
-const { createWebhook, getStreamWebhooks, getLastWebhookEvents, getWebhook, updateWebhook, deleteWebhook, dispatchStreamEvent } = require( '../services/webhooks' )
-const { createUser } = require( '../../core/services/users' )
-const { createStream, getStream } = require( '../../core/services/streams' )
 
-describe( 'Webhooks', ( ) => {
+describe( 'Webhooks @webhooks', () => {
+  let testServer
+
   let userOne = {
     name: 'User',
     email: 'user@gmail.com',
@@ -23,7 +27,7 @@ describe( 'Webhooks', ( ) => {
   }
 
   let streamOne = {
-    name: 'stream',
+    name: 'streamOne',
     description: 'stream',
     isPublic: true
   }
@@ -40,16 +44,20 @@ describe( 'Webhooks', ( ) => {
   before( async ( ) => {
     await knex.migrate.rollback( )
     await knex.migrate.latest( )
-    await init()
+    let { app } = await init()
+    let { server } = await startHttp( app )
+    testServer = server
 
     userOne.id = await createUser( userOne )
     streamOne.ownerId = userOne.id
     streamOne.id = await createStream( streamOne )
+
     webhookOne.streamId = streamOne.id
   } )
 
   after( async ( ) => {
     // await knex.migrate.rollback( )
+    testServer.close( )
   } )
 
   describe( 'Create, Read, Update, Delete Webhooks', ( ) => {
@@ -81,22 +89,118 @@ describe( 'Webhooks', ( ) => {
       expect( webhook ).to.be.undefined
     } )
 
-    it( 'Should get webooks for stream', async ( ) => {
+    it( 'Should get webhooks for stream', async ( ) => {
       let streamWebhooks = await getStreamWebhooks( { streamId: streamOne.id } )
       expect( streamWebhooks ).to.have.lengthOf( 0 )
-      
+
       webhookOne.id = await createWebhook( webhookOne )
       streamWebhooks = await getStreamWebhooks( { streamId: streamOne.id } )
       expect( streamWebhooks ).to.have.lengthOf( 1 )
-      expect( streamWebhooks[0] ).to.have.property( 'url' )
-      expect( streamWebhooks[0].url ).to.equal( webhookOne.url )
+      expect( streamWebhooks[ 0 ] ).to.have.property( 'url' )
+      expect( streamWebhooks[ 0 ].url ).to.equal( webhookOne.url )
     } )
 
-    it( 'Should dispatch and get events', async () => {      
+    it( 'Should dispatch and get events', async () => {
       await dispatchStreamEvent( { streamId: streamOne.id, event: 'commit_create', eventPayload: 'payload123' } )
       let lastEvents = await getLastWebhookEvents( { webhookId: webhookOne.id } )
       expect( lastEvents ).to.have.lengthOf( 1 )
-      expect( lastEvents[0].payload ).to.equal( 'payload123' )
+      expect( lastEvents[ 0 ].payload ).to.equal( 'payload123' )
+    } )
+  } )
+
+  describe( 'GraphQL API Webhooks @webhooks-api', () => {
+    let userTwo = {
+      name: 'User2',
+      email: 'user2@gmail.com',
+      password: 'jdsadjsadasfdsa'
+    }
+
+    let webhookTwo = {
+      streamId: null,
+      url: 'http://localhost:42/non-existent-two',
+      description: 'test wh no 2',
+      secret: 'secret',
+      enabled: true,
+      events: {
+        'commit_create': true,
+        'commit_update': true
+      }
+    }
+
+    let streamTwo = {
+      name: 'streamTwo',
+      description: 'stream',
+      isPublic: true
+    }
+
+    before( async () => {
+      userTwo.id = await createUser( userTwo )
+      streamTwo.ownerId = userOne.id
+      streamTwo.id = await createStream( streamTwo )
+      webhookTwo.streamId = streamTwo.id
+
+
+      userOne.token = `Bearer ${( await createPersonalAccessToken( userOne.id, 'userOne test token', [ 'streams:read', 'streams:write' ] ) )}`
+      userTwo.token = `Bearer ${( await createPersonalAccessToken( userTwo.id, 'userTwo test token', [ 'streams:read', 'streams:write' ] ) )}`
+      await grantPermissionsStream( { streamId: streamTwo.id, userId: userTwo.id, role: 'stream:contributor' } )
+    } )
+
+    it( 'Should create a webhook', async () => {
+      const res = await sendRequest( userOne.token, { query: 'mutation createWebhook($webhook: WebhookCreateInput!) { webhookCreate( webhook: $webhook ) }', variables: { webhook: webhookTwo } } )
+      expect( noErrors( res ) )
+      expect( res.body.data.webhookCreate ).to.not.be.null
+      webhookTwo.id = res.body.data.webhookCreate
+    } )
+
+    it( 'Should get stream webhooks and the previous events', async () => {
+      await dispatchStreamEvent( { streamId: streamTwo.id, event: 'commit_create', eventPayload: 'payload321' } )
+      const res = await sendRequest( userOne.token, { query: `query {
+        stream(id: "${streamTwo.id}") {
+          webhooks { totalCount items { id url enabled
+            history { totalCount items { status statusInfo payload } } }
+          }
+        }
+      }` } )
+      expect( noErrors( res ) )
+      let webhooks = res.body.data.stream.webhooks
+
+      expect( webhooks.totalCount ).to.equal( 1 )
+      expect( webhooks.items[ 0 ].url ).to.equal( webhookTwo.url )
+      expect( webhooks.items[ 0 ].history.totalCount ).to.equal( 1 )
+      expect( webhooks.items[ 0 ].history.items[ 0 ].payload ).to.equal( 'payload321' )
+    } )
+
+    it( 'Should update a webhook', async () => {
+      const res = await sendRequest( userOne.token, { query: `mutation { webhookUpdate(webhook: { id: "${webhookTwo.id}", description: "updated webhook", enabled: false })
+    }` } )
+      let webhook = await getWebhook( { id: webhookTwo.id } )
+      expect( noErrors( res ) )
+      expect( res.body.data.webhookUpdate ).to.equal( 'true' )
+      expect( webhook.description ).to.equal( 'updated webhook' )
+      expect( webhook.enabled ).to.equal( false )
+    } )
+
+    it( 'Should delete a webhook', async () => {
+      const res = await sendRequest( userOne.token, {
+        query: `mutation { webhookDelete(id: "${webhookTwo.id}") }`
+      } )
+      expect( noErrors( res ) )
+      expect( res.body.data.webhookDelete ).to.equal( 'true' )
+    } )
+
+    it( 'Should *not* create a webhook if user is not a stream owner', async () => {
+      delete webhookTwo.id
+      const res = await sendRequest( userTwo.token, { query: 'mutation createWebhook($webhook: WebhookCreateInput!) { webhookCreate( webhook: $webhook ) }', variables: { webhook: webhookTwo } } )
+      expect( res.body.errors ).to.exist
+      expect( res.body.errors[ 0 ].extensions.code ).to.equal( 'FORBIDDEN' )
+    } )
+
+    it( 'Should *not* get a webhook if the user is not a stream owner', async () => {
+      const res = await sendRequest( userTwo.token, { query: `query {
+        stream(id: "${streamTwo.id}") { webhooks { totalCount items { id url enabled } } }
+      }` } )
+      expect( res.body.errors ).to.exist
+      expect( res.body.errors[ 0 ].extensions.code ).to.equal( 'FORBIDDEN' )
     } )
 
     it( 'Should have a webhook limit for streams', async ( ) => {
@@ -127,3 +231,23 @@ describe( 'Webhooks', ( ) => {
     } )
   } )
 } )
+
+
+/**
+ * Sends a graphql request. Convenience wrapper.
+ * @param  {string} auth the user's token
+ * @param  {string} obj  the query/mutation to send
+ * @return {Promise}      the awaitable request
+ */
+function sendRequest( auth, obj, address = serverAddress ) {
+  return chai.request( address ).post( '/graphql' ).set( 'Authorization', auth ).send( obj )
+}
+
+/**
+ * Checks the response body for errors. To be used in expect assertions.
+ * Will throw an error if 'errors' exist.
+ * @param {*} res
+ */
+function noErrors( res ) {
+  if ( 'errors' in res.body ) throw new Error( `Failed GraphQL request: ${res.body.errors[ 0 ].message}` )
+}
