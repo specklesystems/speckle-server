eedd39debe12d3bfda4ed94cf15fafab3b2ecbff
cristi8 <cristi8@gmail.com>
2021-07-23 13:43:39 +0300

    webhook service build in ci

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -35,6 +35,13 @@ echo "$K8S_CLUSTER_CERTIFICATE" | base64 --decode > k8s_cert.crt
   --token=$K8S_TOKEN \
   set image deployment/$TARGET_SPECKLE_DEPLOYMENT-preview-service main=$DOCKER_IMAGE_TAG-preview-service:$IMAGE_VERSION_TAG
 
+./kubectl \
+  --kubeconfig=/dev/null \
+  --server=$K8S_SERVER \
+  --certificate-authority=k8s_cert.crt \
+  --token=$K8S_TOKEN \
+  set image deployment/$TARGET_SPECKLE_DEPLOYMENT-webhook-service main=$DOCKER_IMAGE_TAG-webhook-service:$IMAGE_VERSION_TAG
+
 
 # Wait for rollout to complete
 ./kubectl \
@@ -57,3 +64,10 @@ echo "$K8S_CLUSTER_CERTIFICATE" | base64 --decode > k8s_cert.crt
   --certificate-authority=k8s_cert.crt \
   --token=$K8S_TOKEN \
   rollout status -w deployment/$TARGET_SPECKLE_DEPLOYMENT-preview-service --timeout=3m
+
+./kubectl \
+  --kubeconfig=/dev/null \
+  --server=$K8S_SERVER \
+  --certificate-authority=k8s_cert.crt \
+  --token=$K8S_TOKEN \
+  rollout status -w deployment/$TARGET_SPECKLE_DEPLOYMENT-webhook-service --timeout=3m
