a61751f9612301f6931ab405d0ad13743e29dd8a
Matteo Cominetti <matteo@speckle.systems>
2021-08-03 10:44:00 +0200

    feat(frontend): updates sourceApp names & colors

--- packages/frontend/src/components/SourceAppAvatar.vue
+++ packages/frontend/src/components/SourceAppAvatar.vue
@@ -35,16 +35,18 @@ export default {
 
       let appname = this.applicationName.toLowerCase()
 
-      if (appname.includes('dynamo')) return 'purple darken-1'
+      if (appname.includes('dynamo')) return 'purple'
       if (appname.includes('revit')) return 'blue darken-1'
       if (appname.includes('autocad')) return 'red lighten-1'
       if (appname.includes('civil')) return 'blue lighten-1'
-      if (appname.includes('blender')) return 'orange'
+      if (appname.includes('blender')) return 'orange darken-1'
       if (appname.includes('rhino')) return 'black'
       if (appname.includes('grasshopper')) return 'green darken-2'
       if (appname.includes('excel')) return 'green lighten-1'
-      if (appname.includes('unity')) return 'yellow darken-1'
+      if (appname.includes('unity')) return 'teal'
       if (appname.includes('unreal')) return 'brown'
+      if (appname.includes('python')) return 'yellow darken-1'
+      if (appname.includes('.net')) return 'purple darken-2'
       return 'grey'
     },
     shortName() {
@@ -56,12 +58,14 @@ export default {
       if (appname.includes('revit')) return 'RVT'
       if (appname.includes('autocad')) return 'ACAD'
       if (appname.includes('civil')) return 'C3D'
-      if (appname.includes('blender')) return 'BLEND'
+      if (appname.includes('blender')) return 'BLNDR'
       if (appname.includes('rhino')) return 'RH'
       if (appname.includes('grasshopper')) return 'GH'
       if (appname.includes('excel')) return 'XLSX'
       if (appname.includes('unity')) return 'UNITY'
       if (appname.includes('unreal')) return 'UE'
+      if (appname.includes('python')) return 'PY'
+      if (appname.includes('.net')) return '.NET'
       return appname
     }
   }
