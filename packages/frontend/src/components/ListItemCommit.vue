4ad3e5fb64a5a4ac4251837d5c4cfe77e8d1f84d
KatKatKateryna <kateryna@speckle.systems>
2021-09-14 01:18:55 +0800

    Update ListItemCommit.vue

--- packages/frontend/src/components/ListItemCommit.vue
+++ packages/frontend/src/components/ListItemCommit.vue
@@ -11,6 +11,7 @@
         :size="40"
       />
     </v-list-item-icon>
+
     <v-list-item-content>
       <v-list-item-title class="mt-0 pt-0 py-1">
         {{ commit.message }}
@@ -21,6 +22,30 @@
         ({{ commitDate }})
       </v-list-item-subtitle>
     </v-list-item-content>
+
+    <v-list-item-action >
+      <p class="text-end mt-2 mb-0">
+      <v-list-item-title >
+        {{ "Received" }} 
+      </v-list-item-title>
+      </p>
+      <v-list-item-subtitle class="mt-2 caption">
+        <p> {{"by"}} &nbsp;
+        <b>{{ commit.authorName }}</b>&nbsp;
+        <timeago :datetime="commit.createdAt"></timeago>
+        </p>
+      </v-list-item-subtitle>
+    </v-list-item-action>
+
+    <v-list-item-action class="pl-4 pt-0 mt-0">
+      <user-avatar
+        :id="commit.authorId"
+        :avatar="commit.authorAvatar"
+        :name="commit.authorName"
+        :size="40"
+      />
+    </v-list-item-action>
+
     <v-list-item-action>
       <div>
         <span v-if="commit.branchName" class="caption">
@@ -32,6 +57,7 @@
         <source-app-avatar :application-name="commit.sourceApplication" />
       </div>
     </v-list-item-action>
+    
   </v-list-item>
 </template>
 <script>
