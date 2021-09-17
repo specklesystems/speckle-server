9a7e33773677a60fccbcb4cc55641036e04158b1
Gergő Jedlicska <gergo@jedlicska.com>
2021-09-17 20:56:06 +0200

    feat(scripts): add script to seed db with dummy users

---
+++ packages/server/scripts/seedUsers.js
@@ -0,0 +1,20 @@
+const appRoot = require( 'app-root-path' )
+const { createUser } = require( `${appRoot}/modules/core/services/users` )
+const https = require( 'https' )
+const axios = require( 'axios' )
+
+const main = async () => {
+  let userInputs = ( await axios.get( 'https://randomuser.me/api/?results=250' ) ).data.results.map( user => {
+    return {
+      name: `${user.name.first} ${user.name.last}`,
+      email: user.email,
+      password: `${user.login.password}${user.login.password}`
+    }
+  } )
+  await Promise.all( userInputs.map( userInput => createUser( userInput ) ) )
+}
+
+
+main()
+  .then( console.log( 'created' ) )
+  .catch( console.log( 'failed' ) )
