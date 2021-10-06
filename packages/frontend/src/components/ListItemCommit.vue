c87767d0037cb6e791fe3b91aeb5a3136ab8295d
Dimitrie Stefanescu <didimitrie@gmail.com>
2021-10-06 18:25:14 +0100

    feat(received receipts): minor fixes/fiddles for the layout

--- packages/frontend/src/components/ListItemCommit.vue
+++ packages/frontend/src/components/ListItemCommit.vue
@@ -1,8 +1,5 @@
 <template>
-  <v-list-item
-    :to="route ? route : `/streams/${streamId}/commits/${commit.id}`"
-    class="xxx-elevation-10"
-  >
+  <v-list-item class="xxx-elevation-10">
     <v-list-item-icon class="pl-4">
       <user-avatar
         :id="commit.authorId"
@@ -14,7 +11,12 @@
 
     <v-list-item-content>
       <v-list-item-title class="mt-0 pt-0 py-1">
-        {{ commit.message }}
+        <router-link
+          class="text-decoration-none"
+          :to="route ? route : `/streams/${streamId}/commits/${commit.id}`"
+        >
+          {{ commit.message }}
+        </router-link>
       </v-list-item-title>
       <v-list-item-subtitle class="caption">
         <b>{{ commit.authorName }}</b>
@@ -24,36 +26,41 @@
       </v-list-item-subtitle>
     </v-list-item-content>
 
-    <v-list-item-action v-if="receivedUsersAll.length>0">
+    <v-list-item-action
+      v-if="receivedUsersAll.length > 0"
+      style="cursor: pointer"
+      @click="showAllActivityDialog = true"
+      v-tooltip="'Click to see all commit receives.'"
+    >
       <p class="text-end mt-2 mb-0">
         <!-- Can be also "Received X times" -->
-        <v-list-item-title>Last received</v-list-item-title>
+        <v-list-item-title class="caption">
+          received by {{ receivedUsersAll[0].message.split('received by')[1] }}
+        </v-list-item-title>
       </p>
       <v-list-item-subtitle class="mt-2 caption">
         <p>
-          by
-          <b>{{ receivedUsersAll[0].userId }}</b>
-          &nbsp;
           <timeago :datetime="receivedUsersAll[0].time"></timeago>
         </p>
       </v-list-item-subtitle>
     </v-list-item-action>
 
-    <v-list-item-action class="pl-4 pt-0 mt-0">
+    <v-list-item-action class="pl-4 pt-0 mt-0" @click="showAllActivityDialog = true" v-tooltip="'Click to see all commit receives.'">
       <div>
-      <!-- Fix avatar size for small views (e.g. Stream home) -->
-      <user-avatar
+        <!-- Fix avatar size for small views (e.g. Stream home) -->
+        <user-avatar
           v-for="user in receivedUsersUnique.slice(0, 4)"
           :id="user"
           :key="user"
           :avatar="user.avatar"
-          :size="40" 
+          :size="30"
           :name="user.name"
+          :show-hover="false"
         />
         <v-avatar v-if="receivedUsersUnique.length > 4" size="30" color="grey">
           <span class="white--text">+{{ receivedUsersUnique.length - 4 }}</span>
         </v-avatar>
-        </div>
+      </div>
     </v-list-item-action>
 
     <v-list-item-action>
@@ -74,7 +81,18 @@
       </div>
     </v-list-item-action>
     <br />
-    
+    <v-dialog
+      v-model="showAllActivityDialog"
+      max-width="400"
+      :fullscreen="$vuetify.breakpoint.xsOnly"
+    >
+      <v-card>
+        <v-toolbar>
+          <v-toolbar-title>Full commit activity</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>{{ activity }}</v-card-text>
+      </v-card>
+    </v-dialog>
   </v-list-item>
 </template>
 <script>
@@ -124,7 +142,8 @@ export default {
       activity: null,
       receivedUsersAll: [],
       receivedNamesAll: [],
-      receivedUsersUnique: []
+      receivedUsersUnique: [],
+      showAllActivityDialog: false
     }
   },
   computed: {
@@ -147,7 +166,7 @@ export default {
       //console.log(val.items)
       let set = new Set()
       val.items.forEach((item) => set.add(item.userId))
-      this.receivedUsersUnique = Array.from( set )
+      this.receivedUsersUnique = Array.from(set)
       this.receivedUsersAll = val.items
       let arr = new Array()
     }
--- packages/frontend/src/components/UserAvatar.vue
+++ packages/frontend/src/components/UserAvatar.vue
@@ -9,7 +9,6 @@
           :size="size"
           v-bind="attrs"
           v-on="on"
-         
         >
           <v-img v-if="avatar" :src="avatar" />
           <v-img v-else :src="`https://robohash.org/` + id + `.png?size=40x40`" />
@@ -18,13 +17,13 @@
           <v-img contain src="/logo.svg"></v-img>
         </v-avatar>
       </template>
-      <v-card v-if="userById" style="width: 200px" :to="isSelf ? '/profile' : '/profile/' + id">
+      <v-card v-if="userById && showHover" style="width: 200px" :to="isSelf ? '/profile' : '/profile/' + id">
         <v-card-text v-if="!$apollo.loading" class="text-center">
           <v-avatar class="my-4" color="grey lighten-3" :size="40">
             <v-img v-if="avatar" :src="avatar" />
             <v-img v-else :src="`https://robohash.org/` + id + `.png?size=40x40`" />
           </v-avatar>
-          
+
           <!-- Uncomment when email verification is in place -->
           <!-- <div v-if="userById.verified" class="mb-1">
             <v-chip color="primary" small>
@@ -37,13 +36,13 @@
             <b>{{ userById.name }}</b>
           </div>
           <div class="caption">
-          {{ userById.company }}
-          <br/>
-          {{ userById.bio ? 'Bio: ' + userById.bio : ''}}
+            {{ userById.company }}
+            <br />
+            {{ userById.bio ? 'Bio: ' + userById.bio : '' }}
           </div>
         </v-card-text>
       </v-card>
-      <v-card v-else>
+      <v-card v-else-if="showHover">
         <v-card-text class="text-xs">
           <b>Speckle Ghost</b>
           <br />
@@ -51,7 +50,7 @@
         </v-card-text>
       </v-card>
     </v-menu>
-    <v-avatar v-else class="ma-1" color="grey lighten-3" :size="size">
+    <v-avatar v-else-if="showHover" class="ma-1" color="grey lighten-3" :size="size">
       <v-img v-if="avatar" :src="avatar" />
       <v-img v-else :src="`https://robohash.org/` + id + `.png?size=40x40`" />
     </v-avatar>
@@ -64,6 +63,10 @@ export default {
   props: {
     avatar: String,
     name: String,
+    showHover: {
+      type: Boolean,
+      default: true
+    },
     size: {
       type: Number,
       default: 42
