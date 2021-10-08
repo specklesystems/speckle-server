a9a8f7d32b1b24eb603b03edbf44918cfaf12da4
Dimitrie Stefanescu <didimitrie@gmail.com>
2021-10-08 09:56:51 +0100

    feat(ifc/server): adds comments on status meanings

--- packages/server/modules/fileuploads/migrations/20210915130000-fileuploads.js
+++ packages/server/modules/fileuploads/migrations/20210915130000-fileuploads.js
@@ -12,7 +12,7 @@ exports.up = async knex => {
     table.integer( 'fileSize' )
     table.boolean( 'uploadComplete' ).notNullable( ).defaultTo( false )
     table.timestamp( 'uploadDate' ).notNullable( ).defaultTo( knex.fn.now( ) )
-
+    // 0 = queued, 1 = in progress, 2 = success, 3 = error
     table.integer( 'convertedStatus' ).notNullable( ).defaultTo( 0 )
     table.timestamp( 'convertedLastUpdate' ).notNullable( ).defaultTo( knex.fn.now( ) )
     table.string( 'convertedMessage' )
