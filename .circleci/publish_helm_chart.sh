7ca54002d7be979967f0fb8ff2c749f1eb4262ce
cristi8 <cristi8@gmail.com>
2021-11-29 15:31:28 +0200

    ci helm

--- .circleci/publish_helm_chart.sh
+++ .circleci/publish_helm_chart.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+git config --global user.email "devops+circleci@speckle.systems"
+git config --global user.name "CI"
+
 git clone git@github.com:specklesystems/helm.git ../helm
 echo "test" > ../helm/test
 
