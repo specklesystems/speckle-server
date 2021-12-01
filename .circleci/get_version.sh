b79c20719061a6d8e0dd69b47bb6d04c898c3a2e
cristi8 <cristi8@gmail.com>
2021-12-01 12:05:22 +0200

    ci helm

--- .circleci/get_version.sh
+++ .circleci/get_version.sh
@@ -3,15 +3,10 @@
 set -e
 
 LAST_RELEASE=$(git describe --always --tags `git rev-list --tags` | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' | head -n 1)
->&2 echo "Last release: $LAST_RELEASE"
-git describe --always --tags `git rev-list --tags` | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' | >&2 head -n 10
+NEXT_RELEASE=$(echo ${LAST_RELEASE} | python -c "parts = input().split('.'); parts[-1] = str(int(parts[-1])+1); print('.'.join(parts))")
 
-
-NEXT_RELEASE=$(echo ${LAST_RELEASE} | awk -F. -v OFS=. '{$NF++;print}')
->&2 echo "Next release: $NEXT_RELEASE"
-
-if [[ "$CIRCLE_TAG" == "$LAST_RELEASE" ]]; then
-    echo $LAST_RELEASE
+if [[ "$CIRCLE_TAG" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo $CIRCLE_TAG
     exit 0
 fi
 
