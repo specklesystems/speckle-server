b8c9098c609795ac413afcc398f31da8cc107797
cristi8 <cristi8@gmail.com>
2021-12-01 11:42:22 +0200

    ci helm

--- .circleci/get_version.sh
+++ .circleci/get_version.sh
@@ -3,7 +3,12 @@
 set -e
 
 LAST_RELEASE=$(git describe --always --tags `git rev-list --tags` | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' | head -n 1)
+>&2 echo "Last release: $LAST_RELEASE"
+git describe --always --tags `git rev-list --tags` | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' | >&2 head -n 10
+
+
 NEXT_RELEASE=$(echo ${LAST_RELEASE} | awk -F. -v OFS=. '{$NF++;print}')
+>&2 echo "Next release: $NEXT_RELEASE"
 
 if [[ "$CIRCLE_TAG" == "$LAST_RELEASE" ]]; then
     echo $LAST_RELEASE
