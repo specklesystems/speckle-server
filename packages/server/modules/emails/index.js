afceeae48af0f04d6cc844c6fef6c354f805c886
Gergő Jedlicska <gergo@jedlicska.com>
2022-01-19 22:03:32 +0100

    refactor(server): refactor email sending service, fix small race condition

--- packages/server/modules/emails/index.js
+++ packages/server/modules/emails/index.js
@@ -1,73 +1,90 @@
 /* istanbul ignore file */
 'use strict'
-let debug = require( 'debug' )
-const appRoot = require( 'app-root-path' )
+const debug = require('debug')('speckle')
 
-const nodemailer = require( 'nodemailer' )
-let account, transporter
+const nodemailer = require('nodemailer')
+const modulesDebug = debug.extend('modules')
+const errorDebug = debug.extend('errors')
 
-exports.init = async ( app, options ) => {
-  debug( 'speckle:modules' )( '📧 Init emails module' )
+let transporter
 
-  if ( process.env.NODE_ENV === 'test' ) {
-    account = await nodemailer.createTestAccount()
-    transporter = nodemailer.createTransport( {
-      host: 'smtp.ethereal.email',
-      port: 587,
-      secure: false,
-      auth: {
-        user: account.user,
-        pass: account.pass
-      }
-    } )
+const initTestSmtpTransporter = async () => {
+  const account = await nodemailer.createTestAccount()
+  return nodemailer.createTransport({
+    'host': 'smtp.ethereal.email',
+    'port': 587,
+    'secure': false,
+    'auth': {
+      'user': account.user,
+      'pass': account.pass,
+    },
+  })
+}
 
-    return
+const createJsonEchoTransporter = () => nodemailer.createTransport({
+  'jsonTransport': true,
+})
+
+const initSmtpTransporter = async () => {
+  try {
+    const smtpTransporter = nodemailer.createTransport({
+      'host': process.env.EMAIL_HOST,
+      'port': process.env.EMAIL_PORT || 587,
+      'secure': process.env.EMAIL_SECURE === 'true',
+      'auth': {
+        'user': process.env.EMAIL_USERNAME,
+        'pass': process.env.EMAIL_PASSWORD,
+      },
+    })
+    await smtpTransporter.verify()
+    return smtpTransporter
+  } catch {
+    errorDebug('📧 Email provider is misconfigured, check config variables.')
   }
+}
 
-  if ( process.env.EMAIL === 'true' ) {
-    try {
-      transporter = nodemailer.createTransport( {
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT || 587,
-        secure: process.env.EMAIL_SECURE === 'true',
-        auth: {
-          user: process.env.EMAIL_USERNAME,
-          pass: process.env.EMAIL_PASSWORD
-        }
-      } )
-    } catch ( e ) {
-      debug( 'speckle:modules' )( '📧 Failed to initialise email provider.' )
-    }
-  } else {
-    debug( 'speckle:modules' )( '📧 Failed to initialise email provider. Server functionality will be limited.' )
-  }
+const initTransporter = async () => {
+  if (process.env.NODE_ENV === 'test') return createJsonEchoTransporter()
+  // if (process.env.NODE_ENV === 'test') return await initTestSmtpTransporter()
+  if (process.env.EMAIL === 'true') return await initSmtpTransporter()
+
+  modulesDebug(
+    `
+    📧 Email provider is not configured.
+     Server functionality will be limited.
+    `,
+  )
+}
+
+const transporterPromise = initTransporter()
+transporterPromise.then((value) => { transporter = value })
+
+exports.init = async (__, ___) => {
+  modulesDebug('📧 Init emails module')
 }
 
 exports.finalize = async () => {
   // Nothing to do here.
 }
 
-exports.transporter = transporter
+exports.sendEmail = async ({ from, to, subject, text, html }) => {
+  // if the transport takes a while to resolve, it is possible to
+  // trigger a false error
+  await transporterPromise
 
-exports.sendEmail = async( { from, to, subject, text, html } ) => {
-  if ( !transporter ) {
-    debug( 'speckle:errors' )( 'No email transport present. Cannot send emails.' )
-    return false
+  if (!transporter) {
+    errorDebug('No email transport present. Cannot send emails.')
   }
-
   try {
-    let email_from = process.env.EMAIL_FROM || 'no-reply@speckle.systems'
-    let info = await transporter.sendMail( {
-      from: from || `"Speckle" <${email_from}>`,
+    const emailFrom = process.env.EMAIL_FROM || 'no-reply@speckle.systems'
+    return await transporter.sendMail({
+      'from': from || `"Speckle" <${emailFrom}>`,
       to,
       subject,
       text,
-      html
-    } )
-    if ( process.env.NODE_ENV === 'test' ) {
-      debug( 'speckle:test' )( nodemailer.getTestMessageUrl( info ) )
-    }
-  } catch ( e ) {
-    debug( 'speckle:errors' )( e )
+      html,
+    })
+  } catch (error) {
+    errorDebug(error)
   }
 }
