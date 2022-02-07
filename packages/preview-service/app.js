abdc403a38337b9103e8853d49460fce69322b9f
cristi8 <cristi8@gmail.com>
2022-02-07 13:18:18 +0200

    dep updates

--- packages/preview-service/app.js
+++ packages/preview-service/app.js
@@ -18,8 +18,9 @@ prometheusClient.collectDefaultMetrics()
 var app = express()
 
 app.use( logger( 'dev' ) )
-app.use( express.json() )
-app.use( express.urlencoded( { extended: false } ) )
+
+app.use( express.json( { limit: '100mb' } ) )
+app.use( express.urlencoded( { limit: '100mb', extended: false } ) )
 app.use( cookieParser() )
 app.use( express.static( path.join( __dirname, 'public' ) ) )
 
