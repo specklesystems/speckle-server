36f4274a3933748081fae2308804b17f1ff82335
Dimitrie Stefanescu <didimitrie@gmail.com>
2022-03-12 20:29:45 +0000

    feat(comments): scaffolds stream comments page

---
+++ packages/frontend/src/main/pages/stream/Comments.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="">test the best</div>
+</template>
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  name: 'Branch',
+  components: {},
+  data() {
+    return {}
+  },
+  apollo: {},
+  methods: {}
+}
+</script>
+<style scoped></style>
