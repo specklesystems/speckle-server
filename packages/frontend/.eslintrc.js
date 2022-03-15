953bcf7205887e17dbfd637b5c2a3cc4e5f461b5
Kristaps Fabians Geikins <fabians@speckle.systems>
2022-03-15 11:56:16 +0200

    chore: Grammar fixes

--- packages/frontend/.eslintrc.js
+++ packages/frontend/.eslintrc.js
@@ -2,7 +2,7 @@
 
 /**
  * Extends repo root config, only put changes here that are scoped to this specific package
- * (if you're already are - evaluate whether you really need package scoped linting rules)
+ * (if you already are - evaluate whether you really need package scoped linting rules)
  */
 
 /** @type {import("eslint").Linter.Config} */
--- packages/preview-service/.eslintrc.js
+++ packages/preview-service/.eslintrc.js
@@ -1,6 +1,6 @@
 /**
  * Extends repo root config, only put changes here that are scoped to this specific package
- * (if you're already are - evaluate whether you really need package scoped linting rules)
+ * (if you already are - evaluate whether you really need package scoped linting rules)
  */
 
 /** @type {import("eslint").Linter.Config} */
