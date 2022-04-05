22920f98c9695e4d6c8915383ee2b6b884544a3e
Dimitrie Stefanescu <didimitrie@gmail.com>
2022-04-05 11:04:00 +0100

    feat(frontend): minor changes to fav navbar

--- packages/frontend/src/main/components/stream/favorites/FavoriteStreamsPlaceholder.vue
+++ packages/frontend/src/main/components/stream/favorites/FavoriteStreamsPlaceholder.vue
@@ -5,15 +5,15 @@
       Once you mark any streams as favorite, they're going to appear here.
     </p>
     <template #actions>
-      <v-list rounded class="transparent">
+      <v-list rounded class="transparent" dark>
         <v-list-item link class="primary mb-4" to="/streams">
           <v-list-item-icon>
-            <v-icon>mdi-format-list-bulleted</v-icon>
+            <v-icon>mdi-folder-outline</v-icon>
           </v-list-item-icon>
           <v-list-item-content>
             <v-list-item-title>Go to your streams</v-list-item-title>
             <v-list-item-subtitle class="caption">
-              Favorite streams to make them appear here
+              Favorite streams to make them appear here!
             </v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
