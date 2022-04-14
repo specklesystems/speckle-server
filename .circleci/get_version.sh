90883353b2389b1893b6fd3e3397484ab0f52d43
Gergő Jedlicska <gergo@jedlicska.com>
2022-04-14 20:09:42 +0200

    ci(circle): revert to prod tagging scheme

--- .circleci/get_version.sh
+++ .circleci/get_version.sh
@@ -5,8 +5,7 @@ set -e
 LAST_RELEASE=$(git describe --always --tags `git rev-list --tags` | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' | head -n 1)
 NEXT_RELEASE=$(echo ${LAST_RELEASE} | python -c "parts = input().split('.'); parts[-1] = str(int(parts[-1])+1); print('.'.join(parts))")
 
-if [[ "$CIRCLE_TAG" =~ ^[0-9]+\.[0-9]+\.[0-9]+-alpha\.[0-9]+$ ]]; then
-# if [[ "$CIRCLE_TAG" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+if [[ "$CIRCLE_TAG" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     echo $CIRCLE_TAG
     exit 0
 fi
