2d78f3c314f096ed429aea8ae4a6fc710543c849
Fabians <fabis94@live.com>
2022-04-27 10:58:29 +0300

    added eslint

--- packages/viewer-sandbox/src/main.ts
+++ packages/viewer-sandbox/src/main.ts
@@ -1,8 +1,10 @@
 import './style.css'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const app = document.querySelector<HTMLDivElement>('#app')
 
-app.innerHTML = `
+if (app) {
+  app.innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+}
