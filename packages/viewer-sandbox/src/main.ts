effe63bd3c28cdfb32a5fcf0f470b43785a5f773
Tomo <tomo.lapautre@gmail.com>
2022-05-14 03:50:08 +0100

    animation feature

--- packages/viewer-sandbox/src/main.ts
+++ packages/viewer-sandbox/src/main.ts
@@ -31,19 +31,62 @@ pane.addInput(PARAMS, 'factor')
 pane.addInput(PARAMS, 'title')
 pane.addInput(PARAMS, 'color')
 
+
+
 // Load demo object
+// async function loaddata() {
+//   viewer.loadObject(
+//   'https://speckle.xyz/streams/7395e94d5c/objects/5c7cec6958a88e671c4b2812bcbb5a7d'
+// )
+// }
+
+// async function filter() {
+//   await loaddata();
+//   const stages = Object.keys(viewer.getObjectsProperties().Stage.uniqueValues)
+//   let timer = 2000;
+
+//   for (let i=0; i<stages.length; i++){
+//   setTimeout(() => { 
+//   viewer.applyFilter({filterBy:{'Stage': stages[i]}, ghostOthers: true });
+//   }, 3000);
+//   timer += 2000;
+// }
+// }
 viewer.loadObject(
-  'https://speckle.xyz/streams/0d3cb7cb52/objects/f833afec2e17457f17e7f6429a106187'
-)
+'https://speckle.xyz/streams/7395e94d5c/objects/5c7cec6958a88e671c4b2812bcbb5a7d')
 
-
-viewer.applyFilter({filterBy: {'level.name': ['3FL', '4FL', '7FL']}, 
-colorBy: { property: 'level.name', 
-type: 'category', values: {'3FL': '#F0FFFF', '4FL': '#6495ED', '7FL': '#7B68EE'} }, 
-ghostOthers: true } )
 
 viewer.on<{ progress: number; id: string; url: string }>('load-progress', (a) => {
   if (a.progress >= 1) {
     viewer.onWindowResize()
   }})
+  
+Window.viewer = viewer
 
+
+function animate_stages() {
+  const stages = Object.keys(viewer.getObjectsProperties().Stage.uniqueValues)
+  range.max = stages.length -1
+  let timer = 1000; 
+
+  for (let i=0; i<stages.length; i++){
+  setTimeout(() => { 
+    range.value = i
+    viewer.applyFilter({filterBy:{'Stage': stages[i]}, ghostOthers: true });
+  }, timer);
+  timer += 800; //step between stages in millisecond
+  }
+}
+
+const button = document.getElementById('animate_button');
+button.addEventListener('click', () => {
+  animate_stages();
+});
+
+const range = document.getElementById('myRange');
+range.addEventListener('mouseup', () => {
+  const stages = Object.keys(viewer.getObjectsProperties().Stage.uniqueValues)
+  if (range.value < stages.length){
+  viewer.applyFilter({filterBy:{'Stage': stages[range.value]}, ghostOthers: true });
+  }
+});
