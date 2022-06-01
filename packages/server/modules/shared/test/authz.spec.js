eeeaf0f94efbdd8e7d5471ed3fb5d60e6f9a0e45
Gergő Jedlicska <gergo@jedlicska.com>
2022-06-01 10:17:46 +0200

    test(server authz): add unittests for the new server authz workflow

--- packages/server/modules/shared/test/authz.spec.js
+++ packages/server/modules/shared/test/authz.spec.js
@@ -1,15 +1,19 @@
+const expect = require('chai').expect
 const {
   authPipelineCreator,
   authFailed,
   authSuccess,
-  validateRole
+  validateRole,
+  validateScope,
+  contextRequiresStream,
+  SpeckleContextError
 } = require('@/modules/shared/authz')
-const expect = require('chai').expect
+const { SpeckleForbiddenError: SFE } = require('@/modules/shared/errors')
 
 describe('AuthZ @shared', () => {
   it('Empty pipeline returns no authorization', async () => {
     const pipeline = authPipelineCreator([])
-    const authResult = await pipeline({ context: { foo: 'bar' } })
+    const { authResult } = await pipeline({ context: { foo: 'bar' } })
     expect(authResult.authorized).to.equal(false)
   })
 
@@ -17,6 +21,7 @@ describe('AuthZ @shared', () => {
     const roles = [
       { name: '1', weight: 1 },
       { name: '2', weight: 2 },
+      { name: '3', weight: 3 },
       { name: 'goku', weight: 9001 },
       { name: '42', weight: 42 }
     ]
@@ -26,25 +31,179 @@ describe('AuthZ @shared', () => {
         name: 'Having lower privileged role than required results auth failed',
         requiredRole: '2',
         context: { auth: true, role: '1' },
-        expectedResult: authFailed()
+        expectedResult: authFailed(null, new SFE('You do not have the required role'))
+      },
+      {
+        name: 'Not having auth fails role validation',
+        requiredRole: '2',
+        context: { auth: false },
+        expectedResult: authFailed(null, new SFE('Cannot validate role without auth'))
+      },
+      {
+        name: 'Requiring a junk role fails auth',
+        requiredRole: 'knock knock...',
+        context: { auth: true, role: '1' },
+        expectedResult: authFailed(null, new SFE('Invalid role requirement specified'))
+      },
+      {
+        name: 'Having a junk role fails auth',
+        requiredRole: '2',
+        context: { auth: true, role: 'iddqd' },
+        expectedResult: authFailed(null, new SFE('Your role is not valid'))
+      },
+      {
+        name: 'Not having the required level fails',
+        requiredRole: 'goku',
+        context: { auth: true, role: '3' },
+        expectedResult: authFailed(null, new SFE('You do not have the required role'))
       },
       {
         name: 'Having the god mode role defeats even higher privilege requirement',
         requiredRole: 'goku',
         context: { auth: true, role: '42' },
         expectedResult: authSuccess()
+      },
+      {
+        name: 'Having equal role weight to required succeeds',
+        requiredRole: '3',
+        context: { auth: true, role: '3' },
+        expectedResult: authSuccess()
+      },
+      {
+        name: 'Having bigger role weight than required succeeds',
+        requiredRole: '3',
+        context: { auth: true, role: 'goku' },
+        expectedResult: authSuccess()
       }
     ]
 
     testData.forEach((testCase) =>
       it(`${testCase.name}`, async () => {
-        const step = validateRole({ requiredRole: 'goku', roles, iddqd: '42' })
-        const authResult = await step({
+        const step = validateRole({
+          requiredRole: testCase.requiredRole,
+          roles,
+          iddqd: '42'
+        })
+        const { authResult, context } = await step({
           context: testCase.context,
           authResult: authFailed()
         })
-        expect(authResult).to.deep.equal(testCase.expectedResult)
+        expect(authResult.authorized).to.exist
+        expect(authResult.authorized).to.equal(
+          testCase.expectedResult.authResult.authorized
+        )
+        // this also needs to check for the error type... is this how do you do that in JS????
+        expect(authResult.error?.name).to.equal(
+          testCase.expectedResult.authResult.error?.name
+        )
+        expect(authResult.error?.message).to.equal(
+          testCase.expectedResult.authResult.error?.message
+        )
+        expect(context).to.deep.equal(testCase.context)
       })
     )
+    it('Role validation fails if input authResult is already in an error state', async () => {
+      const step = validateRole({ requiredRole: 'goku', roles, iddqd: '42' })
+      const { authResult } = await step({ context: {}, authResult: { error: true } })
+      expect(authResult.authorized).to.equal(false)
+      expect(authResult.error.message).to.equal(
+        new SFE("Role validation doesn't rescue the auth pipeline").message
+      )
+    })
+  })
+
+  describe('Validate scopes', () => {
+    it('Scope validation fails if input authResult is already in an error state', async () => {
+      const step = validateScope({ requiredScope: 'play mahjong' })
+      const { authResult } = await step({ context: {}, authResult: { error: true } })
+      expect(authResult.authorized).to.equal(false)
+      const expectedError = new SFE("Scope validation doesn't rescue the auth pipeline")
+      expect(authResult.error.message).to.equal(expectedError.message)
+      expect(authResult.error.name).to.equal(expectedError.name)
+    })
+    it('Without having any scopes on the context cannot validate scopes', async () => {
+      const step = validateScope({ requiredScope: 'play mahjong' })
+      const { authResult } = await step({ context: {}, authResult: {} })
+      expect(authResult.authorized).to.equal(false)
+      const expectedError = new SFE('You do not have the required privileges.')
+      expect(authResult.error.message).to.equal(expectedError.message)
+      expect(authResult.error.name).to.equal(expectedError.name)
+    })
+    it('Not having the right scopes results auth failed', async () => {
+      const step = validateScope({ requiredScope: 'play mahjong' })
+      const { authResult } = await step({
+        context: { scopes: ['sit around and wait', 'try to be cool'] },
+        authResult: {}
+      })
+      expect(authResult.authorized).to.equal(false)
+      const expectedError = new SFE('You do not have the required privileges.')
+
+      expect(authResult.error.message).to.equal(expectedError.message)
+      expect(authResult.error.name).to.equal(expectedError.name)
+    })
+    it('Having the right scopes results auth success', async () => {
+      const step = validateScope({ requiredScope: 'play mahjong' })
+      const { authResult } = await step({
+        context: { scopes: ['sit around and wait', 'try to be cool', 'play mahjong'] },
+        authResult: {}
+      })
+      expect(authResult.authorized).to.equal(true)
+      expect(authResult.error).to.not.exist
+    })
+  })
+
+  describe('Context requires stream', () => {
+    const expectAuthError = (expectedError, authResult) => {
+      expect(authResult.authorized).to.be.false
+      expect(authResult.error).to.exist
+      expect(authResult.error.message).to.equal(expectedError.message)
+      expect(authResult.error.name).to.equal(expectedError.name)
+    }
+    it('Without streamId in the params it raises context error', async () => {
+      const step = contextRequiresStream(async () => ({ ur: 'bamboozled' }))
+      const { authResult } = await step({ params: {} })
+      expectAuthError(
+        new SpeckleContextError("The context doesn't have a streamId"),
+        authResult
+      )
+    })
+    it('If params is not defined it raises context error', async () => {
+      const step = contextRequiresStream(async () => ({ ur: 'bamboozled' }))
+      const { authResult } = await step({})
+      expectAuthError(
+        new SpeckleContextError("The context doesn't have a streamId"),
+        authResult
+      )
+    })
+    it('Stream is added to the returned context object', async () => {
+      const demoStream = {
+        id: 'foo',
+        name: 'bar'
+      }
+      const step = contextRequiresStream(async () => demoStream)
+      const { context } = await step({
+        context: {},
+        params: { streamId: 'this is fake and its fine' }
+      })
+      expect(context.stream).to.deep.equal(demoStream)
+    })
+    it('If context is not defined return auth failure', async () => {
+      const step = contextRequiresStream(async () => {})
+      const { authResult } = await step({ params: { streamId: 'the need for stream' } })
+
+      expectAuthError(new SpeckleContextError('The context is not defined'), authResult)
+    })
+    it('If stream getter raises, the error is handled', async () => {
+      const errorMessage = 'oh dangit'
+      const step = contextRequiresStream(async () => {
+        throw new Error(errorMessage)
+      })
+      const { authResult } = await step({
+        context: {},
+        params: { streamId: 'the need for stream' }
+      })
+
+      expectAuthError(new SpeckleContextError(errorMessage), authResult)
+    })
   })
 })
