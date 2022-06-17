b8a43b061a34db0d461050217634f892420f2c29
Gergő Jedlicska <gergo@jedlicska.com>
2022-06-17 09:30:25 +0200

    fix(server knex): parse env variable to int

--- packages/server/knexfile.js
+++ packages/server/knexfile.js
@@ -49,7 +49,7 @@ if (env.POSTGRES_USER && env.POSTGRES_PASSWORD) {
 // this is why the new datetime columns are created like this
 // table.specificType('createdAt', 'TIMESTAMPTZ(3)').defaultTo(knex.fn.now())
 
-const postgresMaxConnections = env.POSTGRES_MAX_CONNECTIONS_SERVER || 4
+const postgresMaxConnections = parseInt(env.POSTGRES_MAX_CONNECTIONS_SERVER) || 4
 
 const commonConfig = {
   client: 'pg',
