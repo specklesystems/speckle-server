aafa5cb759800784cdd5b85a5206dd58723fbead
Gergő Jedlicska <57442769+gjedlicska@users.noreply.github.com>
2022-06-21 14:05:52 +0200

    fix(backend services'): remove query timeout setting, it causes streaming queries to crash (#813)

--- packages/preview-service/knex.js
+++ packages/preview-service/knex.js
@@ -6,9 +6,7 @@ module.exports = require('knex')({
   connection: {
     application_name: 'speckle_preview_service',
     connectionString:
-      process.env.PG_CONNECTION_STRING ||
-      'postgres://speckle:speckle@localhost/speckle',
-    query_timeout: 4.32e7
+      process.env.PG_CONNECTION_STRING || 'postgres://speckle:speckle@localhost/speckle'
   },
   pool: { min: 0, max: 2 }
   // migrations are in managed in the server package
