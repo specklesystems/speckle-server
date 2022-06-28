f2cfe2439f875e6deda4a310bb43142513404b51
Fabians <fabis94@live.com>
2022-06-28 17:04:38 +0300

    cleaned up unnecessary type augmentations

--- packages/frontend/src/type-augmentations/window.d.ts
+++ packages/frontend/src/type-augmentations/window.d.ts
@@ -6,10 +6,5 @@ declare global {
      * Initialized in SpeckleViewer.vue
      */
     __viewer?: import('@speckle/viewer').Viewer
-
-    /**
-     * Set, if SpeckleLocalStorage used without available LocalStorage
-     */
-    fakeLocalStorage?: Storage
   }
 }
