3ec2b3ca625702f55a9199f7592990e22c7efd2e
Fabians <fabis94@live.com>
2022-07-19 10:45:35 +0300

    made apollo read version from SPECKLE_SERVER_VERSION

--- packages/server/modules/core/helpers/envHelper.js
+++ packages/server/modules/core/helpers/envHelper.js
@@ -10,4 +10,23 @@ function isProdEnv() {
   return process.env.NODE_ENV === 'production'
 }
 
-module.exports = { isTestEnv, isDevEnv, isProdEnv }
+function getServerVersion() {
+  return process.env.SPECKLE_SERVER_VERSION || 'dev'
+}
+
+function isApolloMonitoringEnabled() {
+  return [true, 'true'].includes(process.env.APOLLO_SCHEMA_REPORTING)
+}
+
+function getApolloServerVersion() {
+  return process.env.APOLLO_SERVER_USER_VERSION
+}
+
+module.exports = {
+  isTestEnv,
+  isDevEnv,
+  isProdEnv,
+  getServerVersion,
+  isApolloMonitoringEnabled,
+  getApolloServerVersion
+}
