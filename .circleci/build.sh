2abd2870bb6d5c62bc9ab45697d43d748f77808e
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2022-08-01 18:42:30 +0100

    Resolved shellcheck issues in existing files

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -10,17 +10,17 @@ DOCKER_IMAGE_TAG=speckle/speckle-$SPECKLE_SERVER_PACKAGE
 
 # IMAGE_VERSION_TAG=$(./.circleci/get_version.sh)
 IMAGE_VERSION_TAG="${IMAGE_VERSION_TAG:-0}"
-echo $IMAGE_VERSION_TAG
+echo "${IMAGE_VERSION_TAG}"
 
 export DOCKER_BUILDKIT=1
 
-docker build --build-arg SPECKLE_SERVER_VERSION=$IMAGE_VERSION_TAG -t $DOCKER_IMAGE_TAG:latest . -f $FOLDER/$SPECKLE_SERVER_PACKAGE/Dockerfile
+docker build --build-arg SPECKLE_SERVER_VERSION="${IMAGE_VERSION_TAG}" -t "${DOCKER_IMAGE_TAG}:latest" . -f "${FOLDER}/${SPECKLE_SERVER_PACKAGE}/Dockerfile"
 
-docker tag $DOCKER_IMAGE_TAG:latest $DOCKER_IMAGE_TAG:$IMAGE_VERSION_TAG
+docker tag "${DOCKER_IMAGE_TAG}:latest" "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}"
 
-if [[ "$IMAGE_VERSION_TAG" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-  docker tag $DOCKER_IMAGE_TAG:latest $DOCKER_IMAGE_TAG:2
+if [[ "${IMAGE_VERSION_TAG}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  docker tag "${DOCKER_IMAGE_TAG}:latest" "${DOCKER_IMAGE_TAG}:2"
 fi
 
-echo "$DOCKER_REG_PASS" | docker login -u "$DOCKER_REG_USER" --password-stdin $DOCKER_REG_URL
-docker push -a $DOCKER_IMAGE_TAG
+echo "${DOCKER_REG_PASS}" | docker login -u "${DOCKER_REG_USER}" --password-stdin "${DOCKER_REG_URL}"
+docker push -a "${DOCKER_IMAGE_TAG}"
