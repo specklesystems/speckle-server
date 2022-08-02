d716b798781325ff08dbf62fc73af77a5124b539
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2022-08-02 15:09:22 +0100

    build(circleci): builds images on every commit

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euo pipefail
+set -eo pipefail
 
 # enables building the test-deployment container with the same script
 # defaults to packages for minimal intervention in the ci config
@@ -9,20 +9,22 @@ PUBLISH_IMAGES="${PUBLISH_IMAGES:-false}"
 DOCKER_IMAGE_TAG="speckle/speckle-${SPECKLE_SERVER_PACKAGE}"
 
 # IMAGE_VERSION_TAG=$(./.circleci/get_version.sh)
-IMAGE_VERSION_TAG="${IMAGE_VERSION_TAG:-0}"
+# if there is not image version tag, uses the SHA1 of the last git commit of the branch that triggered this build
+IMAGE_VERSION_TAG="${IMAGE_VERSION_TAG:-${CIRCLE_SHA1}}"
 echo "${IMAGE_VERSION_TAG}"
 
 export DOCKER_BUILDKIT=1
 
-docker build --build-arg SPECKLE_SERVER_VERSION="${IMAGE_VERSION_TAG}" -t "${DOCKER_IMAGE_TAG}:latest" . -f "${FOLDER}/${SPECKLE_SERVER_PACKAGE}/Dockerfile"
+docker build --build-arg SPECKLE_SERVER_VERSION="${IMAGE_VERSION_TAG}" -t "${DOCKER_IMAGE_TAG}:${CIRCLE_SHA1}" . --file "${FOLDER}/${SPECKLE_SERVER_PACKAGE}/Dockerfile"
 
 if [[ "${PUBLISH_IMAGES}" == "true" ]]; then
-  docker tag "${DOCKER_IMAGE_TAG}:latest" "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}"
+  docker tag "${DOCKER_IMAGE_TAG}:${CIRCLE_SHA1}" "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}"
+  docker tag "${DOCKER_IMAGE_TAG}:${CIRCLE_SHA1}" "${DOCKER_IMAGE_TAG}:latest"
 
   if [[ "${IMAGE_VERSION_TAG}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    docker tag "${DOCKER_IMAGE_TAG}:latest" "${DOCKER_IMAGE_TAG}:2"
+    docker tag "${DOCKER_IMAGE_TAG}:${CIRCLE_SHA1}" "${DOCKER_IMAGE_TAG}:2"
   fi
 
   echo "${DOCKER_REG_PASS}" | docker login -u "${DOCKER_REG_USER}" --password-stdin "${DOCKER_REG_URL}"
-  docker push -a "${DOCKER_IMAGE_TAG}"
+  docker push --all-tags "${DOCKER_IMAGE_TAG}"
 fi
