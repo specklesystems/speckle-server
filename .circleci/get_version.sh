69829c8fa418a95d65874c099f826ff4efa016fa
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2022-08-03 15:04:43 +0100

    Bug fix: remove quotation marks

--- .circleci/get_version.sh
+++ .circleci/get_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 
-LAST_RELEASE="$(git describe --always --tags "$(git rev-list --tags)" | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' | head -n 1)"
+LAST_RELEASE="$(git describe --always --tags $(git rev-list --tags) | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' | head -n 1)"
 NEXT_RELEASE="$(echo "${LAST_RELEASE}" | python -c "parts = input().split('.'); parts[-1] = str(int(parts[-1])+1); print('.'.join(parts))")"
 
 if [[ "${CIRCLE_TAG}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
