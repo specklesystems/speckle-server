6d9086bfd9148249c8921b190e501bd7f9bd1771
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2022-08-09 11:23:12 +0100

    build: Images should be built on every branch & commit with a PR (#856)
    * allow images to be built on every branch & commit
    * use scripts to determine whether to build and whether to publish
    * all commits to `main`, `^hotfix.*`, and `^alpha.*` branches are published
    * semver tags are published
    * include instructions on publishing and releasing speckle-server

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
+set -eo pipefail
 
-set -e
+SHOULD_BUILD="${SHOULD_BUILD:-false}"
+if [[ "${SHOULD_BUILD}" != "true" ]]; then
+  echo "Not building as the SHOULD_BUILD environment variable is not 'true'."
+  exit 0
+fi
 
 # enables building the test-deployment container with the same script
 # defaults to packages for minimal intervention in the ci config
 FOLDER="${FOLDER:-packages}"
+SHOULD_PUBLISH="${SHOULD_PUBLISH:-false}"
 
-DOCKER_IMAGE_TAG=speckle/speckle-$SPECKLE_SERVER_PACKAGE
+DOCKER_IMAGE_TAG="speckle/speckle-${SPECKLE_SERVER_PACKAGE}"
 
 # IMAGE_VERSION_TAG=$(./.circleci/get_version.sh)
-IMAGE_VERSION_TAG="${IMAGE_VERSION_TAG:-0}"
-echo $IMAGE_VERSION_TAG
+# if there is not image version tag, uses the SHA1 of the last git commit of the branch that triggered this build
+IMAGE_VERSION_TAG="${IMAGE_VERSION_TAG:-${CIRCLE_SHA1}}"
+echo "${IMAGE_VERSION_TAG}"
 
 export DOCKER_BUILDKIT=1
 
-docker build --build-arg SPECKLE_SERVER_VERSION=$IMAGE_VERSION_TAG -t $DOCKER_IMAGE_TAG:latest . -f $FOLDER/$SPECKLE_SERVER_PACKAGE/Dockerfile
+docker build --build-arg SPECKLE_SERVER_VERSION="${IMAGE_VERSION_TAG}" -t "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}" . --file "${FOLDER}/${SPECKLE_SERVER_PACKAGE}/Dockerfile"
 
-docker tag $DOCKER_IMAGE_TAG:latest $DOCKER_IMAGE_TAG:$IMAGE_VERSION_TAG
+if [[ "${SHOULD_PUBLISH}" == "true" ]]; then
+  echo "publishing images"
+  docker tag "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}" "${DOCKER_IMAGE_TAG}:latest"
 
-if [[ "$IMAGE_VERSION_TAG" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-  docker tag $DOCKER_IMAGE_TAG:latest $DOCKER_IMAGE_TAG:2
+  if [[ "${IMAGE_VERSION_TAG}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    docker tag "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}" "${DOCKER_IMAGE_TAG}:2"
+  fi
+
+  echo "${DOCKER_REG_PASS}" | docker login -u "${DOCKER_REG_USER}" --password-stdin "${DOCKER_REG_URL}"
+  docker push --all-tags "${DOCKER_IMAGE_TAG}"
 fi
-
-echo "$DOCKER_REG_PASS" | docker login -u "$DOCKER_REG_USER" --password-stdin $DOCKER_REG_URL
-docker push -a $DOCKER_IMAGE_TAG
