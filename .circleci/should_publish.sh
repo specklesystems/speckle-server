427050a15df940aba6ef2f3a8c5a84ef241c2df2
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2022-08-09 15:00:51 +0100

    chore(pre-commit): Add shellcheck to pre-commit hook (#850)
    * chore(pre-commit): adds shellcheck
    * Resolves shellcheck linting issues in existing files

--- .circleci/should_publish.sh
+++ .circleci/should_publish.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-IFS='£' read -a PUB_TAGS <<< "${PUBLISHABLE_TAGS}"
+IFS='£' read -r -a PUB_TAGS <<< "${PUBLISHABLE_TAGS}"
+# shellcheck disable=SC2068
 for item in ${PUB_TAGS[@]}; do
     [[ "${CIRCLE_TAG}" =~ ${item} ]] && echo "true" && exit 0
 done
 
-IFS='£' read -a PUB_BRANCHES <<< "${PUBLISHABLE_BRANCHES}"
+IFS='£' read -r -a PUB_BRANCHES <<< "${PUBLISHABLE_BRANCHES}"
+# shellcheck disable=SC2068
 for item in ${PUB_BRANCHES[@]}; do
     [[ "${CIRCLE_BRANCH}" =~ ${item} ]] && echo "true" && exit 0
 done
