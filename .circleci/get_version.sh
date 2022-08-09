2e4ac5d104afff41d2d57379a49e40907b6b894a
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2022-08-09 12:42:55 +0100

    Fix shellcheck issues in newly merged scripts

--- .circleci/get_version.sh
+++ .circleci/get_version.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 
+# shellcheck disable=SC2068,SC2046
 LAST_RELEASE="$(git describe --always --tags $(git rev-list --tags) | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' | head -n 1)"
 NEXT_RELEASE="$(echo "${LAST_RELEASE}" | python -c "parts = input().split('.'); parts[-1] = str(int(parts[-1])+1); print('.'.join(parts))")"
 
