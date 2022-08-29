74ce7f8d03b49d6331a8817f49e6ff3c9b53aab5
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2022-08-29 12:17:00 +0100

    Fix typescript types

--- packages/server/modules/shared/helpers/secretsHelper.ts
+++ packages/server/modules/shared/helpers/secretsHelper.ts
@@ -2,73 +2,74 @@ import { MisconfiguredSecretError } from '@/modules/shared/errors'
 import { existsSync, readFile } from 'fs'
 import { join } from 'path'
 
-let redisUrl: string | undefined
-let postgresUrl: string | undefined
-let sessionSecret: string | undefined
-let emailPassword: string | undefined
-let s3SecretKey: string | undefined
-let googleClientSecret: string | undefined
-let githubClientSecret: string | undefined
-let azureADClientSecret: string | undefined
-let apolloKey: string | undefined
+let redisUrl: string
+let postgresUrl: string
+let sessionSecret: string
+let emailPassword: string
+let s3SecretKey: string
+let googleClientSecret: string
+let githubClientSecret: string
+let azureADClientSecret: string
+let apolloKey: string
 
-export function getRedisUrl(): string | undefined {
+export function getRedisUrl(): string {
   if (!redisUrl) redisUrl = getSecret('redis_url')
   return redisUrl
 }
 
-export function getPostgresUrl(): string | undefined {
+export function getPostgresUrl(): string {
   if (!postgresUrl) postgresUrl = getSecret('postgres_url')
   return postgresUrl
 }
 
-export function getSessionSecret(): string | undefined {
+export function getSessionSecret(): string {
   if (!sessionSecret) sessionSecret = getSecret('session_secret')
   return sessionSecret
 }
 
-export function getEmailPassword(): string | undefined {
+export function getEmailPassword(): string {
   if (!emailPassword) emailPassword = getSecret('email_password')
   return emailPassword
 }
 
-export function getS3SecretKey(): string | undefined {
+export function getS3SecretKey(): string {
   if (!s3SecretKey) s3SecretKey = getSecret('s3_secret_key')
   return s3SecretKey
 }
 
-export function getGoogleClientSecret(): string | undefined {
+export function getGoogleClientSecret(): string {
   if (!googleClientSecret) googleClientSecret = getSecret('google_client_secret')
   return googleClientSecret
 }
 
-export function getGitHubClientSecret(): string | undefined {
+export function getGitHubClientSecret(): string {
   if (!githubClientSecret) githubClientSecret = getSecret('github_client_secret')
   return githubClientSecret
 }
 
-export function getAzureADClientSecret(): string | undefined {
+export function getAzureADClientSecret(): string {
   if (!azureADClientSecret) azureADClientSecret = getSecret('azure_ad_client_secret')
   return azureADClientSecret
 }
 
-export function getApolloKey(): string | undefined {
+export function getApolloKey(): string {
   if (!apolloKey) apolloKey = getSecret('apollo_key')
   return apolloKey
 }
 
-function getSecret(secretName: string): string | undefined {
+function getSecret(secretName: string): string {
   const secretPath = join('/etc/secrets', secretName)
   if (existsSync(secretPath)) {
     readFile(secretPath, { encoding: 'utf-8' }, function (err, data) {
-      if (err) throw new MisconfiguredSecretError(`${secretPath} could not be read`)
-      return data
+      if (!err) return data
     })
-  } else if (process.env[secretName.toUpperCase()]) {
-    return process.env[secretName.toUpperCase()]
-  } else {
-    throw new MisconfiguredSecretError(
-      `Neither ${secretPath} or ${secretName.toUpperCase()} env var were configured`
-    )
   }
+
+  if (process.env[secretName.toUpperCase()]) {
+    return process.env[secretName.toUpperCase()] || ''
+  }
+
+  throw new MisconfiguredSecretError(
+    `Neither ${secretPath} or ${secretName.toUpperCase()} env var were configured`
+  )
 }
