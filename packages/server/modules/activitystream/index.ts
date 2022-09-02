1878400049a1f75821a8e1a3879f4fcd61bce98a
Gergő Jedlicska <gergo@jedlicska.com>
2022-09-02 10:05:59 +0200

    feat(server digest emails): set prod ready cron tab and timespan

--- packages/server/modules/activitystream/index.ts
+++ packages/server/modules/activitystream/index.ts
@@ -14,18 +14,25 @@ const activityModule: SpeckleModule = {
       initializeEventListener()
     }
 
-    const cronExpression = '*/1000 * * * * *'
+    // just to test stuff
+    // every 1000 seconds
+    // const cronExpression = '*/1000 * * * * *'
+    // at 00 minutest, 10 (am) hours, every month, every year,
+    // every 1st day of the week (monday)
+    // cheat sheet https://crontab.guru
+    const cronExpression = '00 10 * * 1'
+    // configure the number of days, the activities are scraped for
+    const numberOfDays = 7
     cron.validate(cronExpression)
     cron.schedule(cronExpression, async () => {
       activitiesDebug('Sending weekly email digests.')
-      const numberOfDays = 365
       const end = new Date()
       const start = new Date(end.getTime())
       start.setDate(start.getDate() - numberOfDays)
-      // const sendResult = await sendSummaryEmails(start, end, SendingService.sendEmail)
-      // sendResult
-      //   ? activitiesDebug('Successfully sent all summaries')
-      //   : activitiesDebug('Sending some email summaries failed')
+      const sendResult = await sendSummaryEmails(start, end, SendingService.sendEmail)
+      sendResult
+        ? activitiesDebug('Successfully sent all summaries')
+        : activitiesDebug('Sending some email summaries failed')
     })
   }
 }
