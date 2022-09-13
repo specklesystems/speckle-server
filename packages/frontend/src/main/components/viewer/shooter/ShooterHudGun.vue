c20607ce094c959d54316c2f850c6cc6a7a4606a
Kristaps Fabians Geikins <fabis94@live.com>
2022-09-13 23:49:08 +0300

    gun fire

--- packages/frontend/src/main/components/viewer/shooter/ShooterHudGun.vue
+++ packages/frontend/src/main/components/viewer/shooter/ShooterHudGun.vue
@@ -1,9 +1,33 @@
 <template>
   <div class="hud-gun">
-    <div class="hud-gun__inner" />
+    <div class="hud-gun__inner" :style="style" />
   </div>
 </template>
-<script lang="ts"></script>
+<script lang="ts">
+import { computed, defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'ShooterHudGun',
+  setup() {
+    const showFire = ref(false)
+
+    const style = computed(() => ({
+      backgroundPosition: showFire.value ? '0 -172px' : '0 -65px'
+    }))
+
+    document.addEventListener('click', () => {
+      showFire.value = true
+      setTimeout(() => {
+        showFire.value = false
+      }, 100)
+    })
+
+    return {
+      style
+    }
+  }
+})
+</script>
 <style lang="scss">
 .hud-gun {
   position: absolute;
@@ -16,12 +40,15 @@
   justify-content: center;
 
   &__inner {
+    // background-color: red;
     background-image: url('~~@/assets/viewer/shooter/guns.png');
     background-repeat: no-repeat;
-    background-position: 0 -65px;
+
+    position: relative;
+    bottom: 133px;
 
     width: 130px;
-    height: 85px;
+    height: 101px;
 
     transform: scale(5);
   }
