013c9635effc733df8186c50330e32b56fab9615
Gergő Jedlicska <gergo@jedlicska.com>
2022-10-04 16:13:45 +0200

    feat(server weekly digest): switch weekly digest cron trigger to the prod ready value

--- packages/server/modules/activitystream/index.ts
+++ packages/server/modules/activitystream/index.ts
@@ -13,11 +13,11 @@ let scheduledTask: cron.ScheduledTask | null = null
 const scheduleWeeklyActivityNotifications = () => {
   // just to test stuff
   // every 1000 seconds
-  const cronExpression = '*/1000 * * * * *'
+  // const cronExpression = '*/1000 * * * * *'
   // at 00 minutest, 10 (am) hours, every month, every year,
   // every 1st day of the week (monday)
   // cheat sheet https://crontab.guru
-  // const cronExpression = '00 13 * * 5'
+  const cronExpression = '00 10 * * 1'
   // configure the number of days, the activities are scraped for
   const numberOfDays = 7
   return scheduleExecution(
