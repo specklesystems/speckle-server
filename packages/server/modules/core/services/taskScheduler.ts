d2baeb35986916d4e167b7f31945ad0232c23db2
Gergő Jedlicska <gergo@jedlicska.com>
2022-10-04 15:52:30 +0200

    fix(server task scheduler): fix not catching lock acquire function errors

--- packages/server/modules/core/services/taskScheduler.ts
+++ packages/server/modules/core/services/taskScheduler.ts
@@ -18,17 +18,19 @@ export const scheduledCallbackWrapper = async (
 ) => {
   // try to acquire the task lock with the function name and a new expiration date
   const lockExpiresAt = new Date(scheduledTime.getTime() + lockTimeout)
-  const lock = await acquireLock({ taskName, lockExpiresAt })
-
-  // if couldn't acquire it, stop execution
-  if (!lock) {
-    activitiesDebug(`Could not acquire task lock for ${taskName}, stopping execution.`)
-    return null
-  }
-
-  // else continue executing the callback...
-  activitiesDebug(`Executing scheduled function ${taskName} at ${scheduledTime}`)
   try {
+    const lock = await acquireLock({ taskName, lockExpiresAt })
+
+    // if couldn't acquire it, stop execution
+    if (!lock) {
+      activitiesDebug(
+        `Could not acquire task lock for ${taskName}, stopping execution.`
+      )
+      return null
+    }
+
+    // else continue executing the callback...
+    activitiesDebug(`Executing scheduled function ${taskName} at ${scheduledTime}`)
     await callback(scheduledTime)
     // update lock as succeeded
     const finishDate = new Date()
