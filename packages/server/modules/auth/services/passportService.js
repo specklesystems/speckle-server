61eac5e9f7dc8d2f332dbfe036813295274b47a8
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2022-10-11 17:28:44 +0100

    refactor(server passport): log debugging output in passport authentication step
    Not intended for production; for debugging only. To be reverted.

--- packages/server/modules/auth/services/passportService.js
+++ packages/server/modules/auth/services/passportService.js
@@ -12,7 +12,13 @@ function passportAuthenticate(strategy, options = undefined) {
   return (req, res, next) =>
     passport.authenticate(strategy, options, (err, user, info) => {
       if (err) debug('speckle:error')(err)
+      console.log(
+        `passport.authenticate() callback. err: ${err}; user: ${user}; info: ${info}`
+      )
       if (!user) {
+        console.log(
+          `passport.authenticate() callback>!user. err: ${err}; user: ${user}; info: ${info}`
+        )
         const errMsg = info?.message || 'Failed to authenticate, contact server admins'
         return res.redirect(`/error?message=${errMsg}`)
       }
