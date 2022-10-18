3ce2b1fb8f74f7f9476d5f34d0e3f3cb5c901858
Kristaps Fabians Geikins <fabis94@live.com>
2022-10-18 18:01:03 +0300

    added some useful config regarding build optimization

--- packages/frontend-2/nuxt.config.ts
+++ packages/frontend-2/nuxt.config.ts
@@ -5,5 +5,18 @@ export default defineNuxtConfig({
     strict: true
   },
 
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+
+  alias: {
+    // Rewriting all lodash calls to lodash-es for proper tree-shaking & chunk splitting
+    lodash: 'lodash-es'
+  },
+  vite: {
+    resolve: {
+      alias: [{ find: /^lodash$/, replacement: 'lodash-es' }]
+    }
+  },
+  build: {
+    transpile: ['@apollo/client', 'ts-invariant/process', '@vue/apollo-composable']
+  }
 })
