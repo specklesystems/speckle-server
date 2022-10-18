8340a6fc7972d05291d1f934e8cfb31d95f6399e
Kristaps Fabians Geikins <fabis94@live.com>
2022-10-18 16:37:01 +0300

    new package, nuxt, eslint

---
+++ packages/frontend-2/.eslintrc.js
@@ -0,0 +1,109 @@
+const mainExtends = [
+  'plugin:nuxt/recommended',
+  'plugin:vue/vue3-recommended',
+  'prettier'
+]
+
+/** @type {import('eslint').Linter.Config} */
+const config = {
+  env: {
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  extends: [...mainExtends],
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['**/templates/*.{js,ts}', 'coverage'],
+  rules: {
+    camelcase: [
+      'error',
+      {
+        properties: 'always'
+      }
+    ],
+    'no-alert': 'error',
+    eqeqeq: ['error', 'always', { null: 'always' }],
+    'no-console': 'off',
+    'no-var': 'error'
+  },
+  overrides: [
+    {
+      files: '*.test.{ts,js}',
+      env: {
+        jest: true
+      }
+    },
+    {
+      files: './{components|pages|store|lib}/*.{js,ts,vue}',
+      env: {
+        node: false,
+        browser: true
+      }
+    },
+    {
+      files: '*.{ts,tsx,vue}',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json', './.nuxt/tsconfig.json'],
+        extraFileExtensions: ['.vue']
+      },
+      extends: ['plugin:@typescript-eslint/recommended', ...mainExtends],
+      rules: {
+        '@typescript-eslint/no-explicit-any': ['error'],
+        '@typescript-eslint/no-unsafe-argument': ['error'],
+        '@typescript-eslint/no-unsafe-assignment': 'error',
+        '@typescript-eslint/no-unsafe-call': 'error',
+        '@typescript-eslint/no-unsafe-member-access': 'error',
+        '@typescript-eslint/no-unsafe-return': 'error',
+        '@typescript-eslint/no-for-in-array': ['error'],
+        '@typescript-eslint/restrict-template-expressions': ['error'],
+        '@typescript-eslint/restrict-plus-operands': ['error'],
+        '@typescript-eslint/await-thenable': ['warn'],
+        '@typescript-eslint/ban-types': ['warn'],
+        'require-await': 'off',
+        '@typescript-eslint/require-await': 'error',
+        'no-undef': 'off'
+      }
+    },
+    {
+      files: '*.vue',
+      plugins: ['vuejs-accessibility'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        ...mainExtends,
+        'plugin:vuejs-accessibility/recommended'
+      ],
+      rules: {
+        'vue/component-tags-order': [
+          'error',
+          { order: ['docs', 'template', 'script', 'style'] }
+        ],
+        'vue/multi-word-component-names': 'off',
+        'vue/component-name-in-template-casing': [
+          'error',
+          'PascalCase',
+          { registeredComponentsOnly: false }
+        ],
+        'vuejs-accessibility/label-has-for': [
+          'error',
+          {
+            required: {
+              some: ['nesting', 'id']
+            }
+          }
+        ]
+      }
+    },
+    {
+      files: '*.d.ts',
+      rules: {
+        'no-var': 'off'
+      }
+    }
+  ]
+}
+
+module.exports = config
---
+++ packages/frontend-2/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="speckle">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+<script setup lang="ts">
+useHead({
+  // Title suffix
+  titleTemplate: (titleChunk) => (titleChunk ? `${titleChunk} - Speckle` : 'Speckle')
+})
+</script>
