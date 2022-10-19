309c7e7f6d68d43f6959e1f8c769069cc7a4aa77
Kristaps Fabians Geikins <fabis94@live.com>
2022-10-19 13:50:27 +0300

    tailwind theme setup

--- packages/frontend-2/pages/index.vue
+++ packages/frontend-2/pages/index.vue
@@ -1,8 +1,9 @@
 <template>
-  <div>
-    <div class="my-8 text-7xl text-black">Heyy TW test</div>
-    <div>
-      <FormButton>Test</FormButton>
+  <div class="p-8">
+    <div class="my-8 text-7xl text-foreground">Heyy TW test</div>
+    <div class="flex flex-col space-y-2 items-start">
+      <FormButton>Test button</FormButton>
+      <FormTextInput name="test1" label="Test input:" />
     </div>
   </div>
 </template>
