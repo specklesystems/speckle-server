d961f356ef85eb91a59e846563a2a1751dc1f7af
Kristaps Fabians Geikins <fabis94@live.com>
2022-10-19 15:53:26 +0300

    logo adjustment

---
+++ packages/frontend-2/components/header/LogoBlock.vue
@@ -0,0 +1,19 @@
+<template>
+  <NuxtLink class="flex items-center" to="/">
+    <img
+      class="block h-8"
+      :class="{ 'mr-2': !minimal }"
+      src="~~/assets/images/speckle_logo_big.png"
+      alt="Speckle"
+    />
+    <div v-if="!minimal" class="text-2xl text-primary-lighter font-bold">Speckle</div>
+  </NuxtLink>
+</template>
+<script setup lang="ts">
+defineProps({
+  minimal: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
--- packages/frontend-2/layouts/default.vue
+++ packages/frontend-2/layouts/default.vue
@@ -5,16 +5,7 @@
         <div class="flex h-16 justify-between">
           <div class="flex">
             <div class="flex flex-shrink-0 items-center">
-              <img
-                class="block h-8 w-auto lg:hidden"
-                src="https://tailwindui.com/img/logos/mark.svg?color=blue&shade=600"
-                alt="Your Company"
-              />
-              <img
-                class="hidden h-8 w-auto lg:block"
-                src="https://tailwindui.com/img/logos/mark.svg?color=blue&shade=600"
-                alt="Your Company"
-              />
+              <HeaderLogoBlock minimal />
             </div>
             <div class="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
               <a
