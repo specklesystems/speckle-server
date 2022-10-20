0c38766d3eada9717d7af282573a603411a7a8df
Kristaps Fabians Geikins <fabis94@live.com>
2022-10-20 13:31:50 +0300

    WIP login page

--- packages/frontend-2/pages/login.vue
+++ packages/frontend-2/pages/login.vue
@@ -1,25 +1,33 @@
 <template>
-  <LayoutPanel class="mx-auto max-w-screen-md">
+  <LayoutPanel form class="mx-auto max-w-screen-md" @submit="onSubmit">
     <template #header>
       <span class="h5">Log in</span>
     </template>
     <template #default>
       <div class="flex flex-col space-y-8">
-        <FormTextInput type="email" name="email" label="E-mail" />
-        <FormTextInput type="password" name="password" label="Password" />
+        <FormTextInput type="email" name="email" label="E-mail" :rules="emailRules" />
+        <FormTextInput
+          type="password"
+          name="password"
+          label="Password"
+          :rules="passwordRules"
+        />
       </div>
     </template>
     <template #footer>
-      <FormButton>Submit</FormButton>
+      <FormButton submit>Submit</FormButton>
     </template>
   </LayoutPanel>
 </template>
 <script setup lang="ts">
 import { useForm } from 'vee-validate'
+import { isEmail, isRequired } from '~~/lib/common/helpers/validation'
 
 type FormValues = { email: string; password: string }
 
 const { handleSubmit } = useForm<FormValues>()
+const emailRules = [isEmail]
+const passwordRules = [isRequired]
 
 const onSubmit = handleSubmit((values) => {
   console.log(values)
