107fd71a70a97bcac596d42e585d813465cb7303
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2022-10-26 15:11:21 +0100

    Clean up image tagging logic - tags as latest when version is semver or semver-alpha.build_number - other commits aren't tagged as latest, and instead tagged with their branch name - move naming logic to common.sh script

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -5,17 +5,24 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # shellcheck disable=SC1090,SC1091
 source "${SCRIPT_DIR}/common.sh"
 
-echo "Publishing: ${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}"
+echo "Starting tagging & publishing of image: ${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}"
 
 echo "💾 Loading image"
 docker load --input "/tmp/ci/workspace/${DOCKER_FILE_NAME}"
 
-echo "🐳 Publishing image"
-docker tag "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}" "${DOCKER_IMAGE_TAG}:latest"
+if [[ "${IMAGE_VERSION_TAG}" =~ ^[0-9]+\.[0-9]+\.[0-9]+(?:-alpha\.[0-9]+)?$ ]]; then
+  echo "🏷 Tagging image as '${DOCKER_IMAGE_TAG}:latest'"
+  docker tag "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}" "${DOCKER_IMAGE_TAG}:latest"
 
-if [[ "${IMAGE_VERSION_TAG}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-  docker tag "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}" "${DOCKER_IMAGE_TAG}:2"
+  if [[ "${IMAGE_VERSION_TAG}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "🏷 Tagging image as '${DOCKER_IMAGE_TAG}:2'"
+    docker tag "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}" "${DOCKER_IMAGE_TAG}:2"
+  fi
+else
+  echo "🏷 Tagging image as '${DOCKER_IMAGE_TAG}:$NEXT_RELEASE-branch.${BRANCH_NAME_TRUNCATED}'"
+  docker tag "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}" "$NEXT_RELEASE-branch.${BRANCH_NAME_TRUNCATED}"
 fi
 
+echo "🐳 Publishing image"
 echo "${DOCKER_REG_PASS}" | docker login -u "${DOCKER_REG_USER}" --password-stdin "${DOCKER_REG_URL}"
 docker push --all-tags "${DOCKER_IMAGE_TAG}"
