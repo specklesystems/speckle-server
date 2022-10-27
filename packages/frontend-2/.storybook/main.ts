54a2a4d0db6923c27e8aa82a2bff42f31795b5bd
Kristaps Fabians Geikins <fabis94@live.com>
2022-10-27 13:13:54 +0300

    more addons & config for storybook

--- packages/frontend-2/.storybook/main.ts
+++ packages/frontend-2/.storybook/main.ts
@@ -1,11 +1,10 @@
+import { flatten } from 'lodash-es'
 import type { StorybookConfig } from '@storybook/builder-vite'
 import { mergeConfig, InlineConfig } from 'vite'
 import jiti from 'jiti'
 
 // having to use jiti cause of weird transpilation stuff going on during the storybook build
-// debugger
 const jitiImport = jiti(import.meta.url, {
-  // debug: true,
   cache: false,
   esmResolve: true
 })
@@ -13,12 +12,20 @@ const nuxtViteConfigUtil = jitiImport(
   './lib/fake-nuxt-env/utils/nuxtViteConfig.mjs'
 ) as typeof import('~~/lib/fake-nuxt-env/utils/nuxtViteConfig.mjs')
 
+const storyPaths = ['stories', 'components', 'pages', 'lib', 'layouts']
+const storiesPairs = storyPaths.map((p) => [
+  `../${p}/**/*.stories.mdx`,
+  `../${p}/**/*.stories.@(js|ts)`
+])
+const stories = flatten(storiesPairs)
+
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories,
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y'
   ],
   framework: {
     name: '@storybook/vue3-vite',
