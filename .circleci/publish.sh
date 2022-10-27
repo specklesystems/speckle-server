e49b8eb76e67fb83f8a9c6d9d42b65b9b76faad7
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2022-10-27 12:35:24 +0100

    Push each tag individually to avoid pollution - docker engine used is shared amongst all runs of speckle-server pipeline, so pushing all tags indiscrimanately pushes images built on other CircleCI runs 😬

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -10,21 +10,23 @@ echo "Starting tagging & publishing of image: ${DOCKER_IMAGE_TAG}:${IMAGE_VERSIO
 echo "💾 Loading image"
 docker load --input "/tmp/ci/workspace/${DOCKER_FILE_NAME}"
 
+echo "🐳 Logging into Docker"
+echo "${DOCKER_REG_PASS}" | docker login -u "${DOCKER_REG_USER}" --password-stdin "${DOCKER_REG_URL}"
+docker push "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}"
+
 if [[ "${IMAGE_VERSION_TAG}" =~ ^[0-9]+\.[0-9]+\.[0-9]+(?:-alpha\.[0-9]+)?$ ]]; then
   echo "🏷 Tagging image as '${DOCKER_IMAGE_TAG}:latest'"
   docker tag "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}" "${DOCKER_IMAGE_TAG}:latest"
+  docker push "${DOCKER_IMAGE_TAG}:latest"
 
   if [[ "${IMAGE_VERSION_TAG}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     echo "🏷 Tagging image as '${DOCKER_IMAGE_TAG}:2'"
     docker tag "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}" "${DOCKER_IMAGE_TAG}:2"
+    docker push "${DOCKER_IMAGE_TAG}:2"
   fi
 else
-  echo "🏷 Tagging image as '${DOCKER_IMAGE_TAG}:$NEXT_RELEASE-branch.${BRANCH_NAME_TRUNCATED}'"
-  docker tag "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}" "${DOCKER_IMAGE_TAG}:$NEXT_RELEASE-branch.${BRANCH_NAME_TRUNCATED}"
+  BRANCH_TAG="${NEXT_RELEASE}-branch.${BRANCH_NAME_TRUNCATED}""
+  echo "🏷 Tagging image as '${DOCKER_IMAGE_TAG}:${BRANCH_TAG}'"
+  docker tag "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}" "${DOCKER_IMAGE_TAG}:${BRANCH_TAG}"
+  docker push "${DOCKER_IMAGE_TAG}:${BRANCH_TAG}"
 fi
-echo "📜 Listing all image tags"
-docker image ls
-
-echo "🐳 Publishing image"
-# echo "${DOCKER_REG_PASS}" | docker login -u "${DOCKER_REG_USER}" --password-stdin "${DOCKER_REG_URL}"
-# docker push --all-tags "${DOCKER_IMAGE_TAG}"
