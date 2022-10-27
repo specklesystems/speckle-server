e2c7f710e547e22ae1c572c9b60bc1f7d413e51e
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2022-10-27 12:47:57 +0100

    Improve logging

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -12,21 +12,24 @@ docker load --input "/tmp/ci/workspace/${DOCKER_FILE_NAME}"
 
 echo "🐳 Logging into Docker"
 echo "${DOCKER_REG_PASS}" | docker login -u "${DOCKER_REG_USER}" --password-stdin "${DOCKER_REG_URL}"
+echo "⏫ Pushing loaded image: '${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}'"
 docker push "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}"
 
 if [[ "${IMAGE_VERSION_TAG}" =~ ^[0-9]+\.[0-9]+\.[0-9]+(?:-alpha\.[0-9]+)?$ ]]; then
-  echo "🏷 Tagging image as '${DOCKER_IMAGE_TAG}:latest'"
+  echo "🏷 Tagging and pushing image as '${DOCKER_IMAGE_TAG}:latest'"
   docker tag "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}" "${DOCKER_IMAGE_TAG}:latest"
   docker push "${DOCKER_IMAGE_TAG}:latest"
 
   if [[ "${IMAGE_VERSION_TAG}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    echo "🏷 Tagging image as '${DOCKER_IMAGE_TAG}:2'"
+    echo "🏷 Tagging and pushing image as '${DOCKER_IMAGE_TAG}:2'"
     docker tag "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}" "${DOCKER_IMAGE_TAG}:2"
     docker push "${DOCKER_IMAGE_TAG}:2"
   fi
 else
   BRANCH_TAG="${NEXT_RELEASE}-branch.${BRANCH_NAME_TRUNCATED}"
-  echo "🏷 Tagging image as '${DOCKER_IMAGE_TAG}:${BRANCH_TAG}'"
+  echo "🏷 Tagging and pushing image as '${DOCKER_IMAGE_TAG}:${BRANCH_TAG}'"
   docker tag "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}" "${DOCKER_IMAGE_TAG}:${BRANCH_TAG}"
   docker push "${DOCKER_IMAGE_TAG}:${BRANCH_TAG}"
 fi
+
+echo "✅ Publishing completed."
