8202be3d34c7999beff0305cf985067a79a2ebef
Kristaps Fabians Geikins <fabis94@live.com>
2022-11-03 11:17:24 +0200

    demo of async loaders

--- packages/frontend-2/lib/common/generated/gql/gql.ts
+++ packages/frontend-2/lib/common/generated/gql/gql.ts
@@ -3,11 +3,13 @@ import * as types from './graphql';
 import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 
 const documents = {
+    "\n  query IntegrationStoryDemoServerInfo {\n    serverInfo {\n      blobSizeLimitBytes\n      name\n      company\n      description\n      adminContact\n      canonicalUrl\n      termsOfService\n      inviteOnly\n      version\n    }\n  }\n": types.IntegrationStoryDemoServerInfoDocument,
     "\n  query TestData {\n    testNumber\n    testList {\n      foo\n      bar\n    }\n  }\n": types.TestDataDocument,
     "\n  query IsLoggedIn {\n    activeUser {\n      id\n    }\n  }\n": types.IsLoggedInDocument,
     "\n  query GetActiveUser {\n    activeUser {\n      id\n      name\n      role\n    }\n  }\n": types.GetActiveUserDocument,
 };
 
+export function graphql(source: "\n  query IntegrationStoryDemoServerInfo {\n    serverInfo {\n      blobSizeLimitBytes\n      name\n      company\n      description\n      adminContact\n      canonicalUrl\n      termsOfService\n      inviteOnly\n      version\n    }\n  }\n"): (typeof documents)["\n  query IntegrationStoryDemoServerInfo {\n    serverInfo {\n      blobSizeLimitBytes\n      name\n      company\n      description\n      adminContact\n      canonicalUrl\n      termsOfService\n      inviteOnly\n      version\n    }\n  }\n"];
 export function graphql(source: "\n  query TestData {\n    testNumber\n    testList {\n      foo\n      bar\n    }\n  }\n"): (typeof documents)["\n  query TestData {\n    testNumber\n    testList {\n      foo\n      bar\n    }\n  }\n"];
 export function graphql(source: "\n  query IsLoggedIn {\n    activeUser {\n      id\n    }\n  }\n"): (typeof documents)["\n  query IsLoggedIn {\n    activeUser {\n      id\n    }\n  }\n"];
 export function graphql(source: "\n  query GetActiveUser {\n    activeUser {\n      id\n      name\n      role\n    }\n  }\n"): (typeof documents)["\n  query GetActiveUser {\n    activeUser {\n      id\n      name\n      role\n    }\n  }\n"];
