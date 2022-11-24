e3152ecc1fe254d30fdf33f85e67261a513b3156
Kristaps Fabians Geikins <fabis94@live.com>
2022-11-24 14:58:44 +0200

    added back link

--- packages/frontend-2/components/auth/OnboardingPanel.vue
+++ packages/frontend-2/components/auth/OnboardingPanel.vue
@@ -8,7 +8,21 @@
     />
     <LayoutPanel class="max-w-3xl w-full">
       <div class="flex flex-col items-center">
-        <CommonSteps v-model="bottomStep" :steps="bottomSteps" basic class="mb-11" />
+        <div class="w-full relative">
+          <CommonSteps v-model="bottomStep" :steps="bottomSteps" basic class="mb-11" />
+          <CommonTextLink
+            v-if="bottomStep === 1"
+            class="absolute top-0 left-0"
+            foreground-link
+            size="sm"
+            @click="goToFirstStep"
+          >
+            <div class="inline-flex items-center space-x-1.5">
+              <ArrowLeftIcon class="w-4 h-4 mt-0.5" />
+              <span>Back</span>
+            </div>
+          </CommonTextLink>
+        </div>
         <AuthOnboardingIndustryStep v-if="bottomStep === 0" v-model="state" />
         <AuthOnboardingRoleStep v-if="bottomStep === 1" v-model="state" />
         <CommonTextLink
@@ -24,6 +38,7 @@
   </div>
 </template>
 <script setup lang="ts">
+import { ArrowLeftIcon } from '@heroicons/vue/20/solid'
 import { OnboardingState } from '~~/lib/auth/helpers/onboarding'
 import { StepType } from '~~/lib/common/helpers/components'
 
@@ -33,11 +48,14 @@ const topStep = computed(() => 1) // read-only
 const bottomSteps: StepType[] = [{ name: 'Industry' }, { name: 'Role' }]
 const bottomStep = ref(0)
 
+const goToFirstStep = () => (bottomStep.value = 0)
+const goToSecondStep = () => (bottomStep.value = 1)
+
 const state = ref<OnboardingState>({ industry: undefined, role: undefined })
 
 watch(state, (newState, oldState) => {
   if (newState.industry !== oldState.industry) {
-    bottomStep.value = 1
+    goToSecondStep()
   } else if (newState.role !== oldState.role) {
     finish()
   }
