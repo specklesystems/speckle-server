90bae53ad5701042e6d6eb61a2ac75a4f8e6dc54
Kristaps Fabians Geikins <fabis94@live.com>
2022-11-24 14:33:17 +0200

    responsive

--- packages/frontend-2/components/auth/onboarding/IndustryStep.vue
+++ packages/frontend-2/components/auth/onboarding/IndustryStep.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="h3 mb-8">Which industry are you in?</div>
-  <div class="w-full grid grid-cols-3 gap-4">
+  <div class="h3 mb-8 text-center">Which industry are you in?</div>
+  <div class="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
     <FormCardButton
       v-for="(val, title) in OnboardingIndustry"
       :key="val"
