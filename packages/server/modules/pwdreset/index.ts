09f95dcf7207a31c0a593161203cea0430df0492
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2022-11-25 15:48:46 +0000

    Amend indentation of a couple of log messages to align with others

--- packages/server/modules/pwdreset/index.ts
+++ packages/server/modules/pwdreset/index.ts
@@ -4,7 +4,7 @@ import { SpeckleModule } from '@/modules/shared/helpers/typeHelper'
 import { noop } from 'lodash'
 
 export const init: SpeckleModule['init'] = (app) => {
-  moduleLogger.info('♻️  Init pwd reset module')
+  moduleLogger.info('♻️ Init pwd reset module')
   RestSetup(app)
 }
 
