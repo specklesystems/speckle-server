5ac043ac6ac1f407479188e90167910859c41a84
Alexandru Popovici <alexandrupopoviciioan@gmail.com>
2022-11-25 16:31:45 +0200

    Fixed an issue in the frontend which was causing the views not to work. Fixed an issue with object selection camera animation where the dolly lerp factor was much too high for smooth animation (#1225)

--- packages/viewer-sandbox/src/main.ts
+++ packages/viewer-sandbox/src/main.ts
@@ -82,7 +82,7 @@ sandbox.makeFilteringUI()
 await sandbox.loadUrl(
   // 'https://speckle.xyz/streams/da9e320dad/commits/5388ef24b8?c=%5B-7.66134,10.82932,6.41935,-0.07739,-13.88552,1.8697,0,1%5D'
   // Revit sample house (good for bim-like stuff with many display meshes)
-  'https://speckle.xyz/streams/da9e320dad/commits/5388ef24b8'
+  // 'https://speckle.xyz/streams/da9e320dad/commits/5388ef24b8'
   // 'Super' heavy revit shit
   // 'https://speckle.xyz/streams/e6f9156405/commits/0694d53bb5'
   // IFC building (good for a tree based structure)
@@ -134,4 +134,7 @@ await sandbox.loadUrl(
   // 'https://speckle.xyz/streams/0208ffb67b/commits/a980292728'
   // 'https://latest.speckle.dev/streams/1c2b3db9fb/commits/f12861736e'
   // 'https://latest.speckle.dev/streams/1c2b3db9fb/commits/1015d417ea'
+  // Jedd's views
+  // 'https://latest.speckle.dev/streams/c1faab5c62/commits/e6632fe057'
+  'https://latest.speckle.dev/streams/7d051a6449/commits/7632757a33'
 )
