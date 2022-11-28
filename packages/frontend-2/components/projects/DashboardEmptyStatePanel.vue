6919b41fadcba94952a977e920f93540c01012d6
Kristaps Fabians Geikins <fabis94@live.com>
2022-11-28 12:32:49 +0200

    empty state done

---
+++ packages/frontend-2/components/projects/DashboardEmptyStatePanel.vue
@@ -0,0 +1,27 @@
+<template>
+  <LayoutPanel class="w-full max-w-md" rounded-border-size="base">
+    <div class="flex flex-col space-y-9 items-center h-full">
+      <Component :is="icon" class="h-16 w-16" />
+      <div class="text-center grow">
+        <h3 class="h4 font-bold leading-8 text-foreground"><slot name="title" /></h3>
+        <h4 class="h5 font-normal leading-7 text-foreground-2">
+          <slot name="subtitle" />
+        </h4>
+      </div>
+      <FormButton size="lg" full-width @click="$emit('click', $event)">
+        <slot name="ctaText" />
+      </FormButton>
+    </div>
+  </LayoutPanel>
+</template>
+<script setup lang="ts">
+import { ConcreteComponent } from 'vue'
+
+defineEmits<{
+  (e: 'click', v: MouseEvent): void
+}>()
+
+defineProps<{
+  icon: ConcreteComponent
+}>()
+</script>
