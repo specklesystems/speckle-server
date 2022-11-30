84cecc9d68dd545932a7a80a1fa898001f6ba614
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2022-11-30 19:33:54 +0000

    Use correct reference to knex and do not instantiate HttpLogger prematurely

--- packages/preview-service/observability/expressLogging.js
+++ packages/preview-service/observability/expressLogging.js
@@ -1,5 +1,5 @@
 const { logger } = require('./logging')
-const HttpLogger = require('pino-http')()
+const HttpLogger = require('pino-http')
 
 module.exports.LoggingExpressMiddleware = HttpLogger({
   logger,
