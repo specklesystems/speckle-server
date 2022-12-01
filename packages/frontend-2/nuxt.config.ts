c66f09bd74c1422a9faaef824fdd4e8b6a716165
Gergő Jedlicska <gergo@jedlicska.com>
2022-12-01 13:21:04 +0100

    chore(frontend-2): add meaningful  default config values

--- packages/frontend-2/nuxt.config.ts
+++ packages/frontend-2/nuxt.config.ts
@@ -17,9 +17,9 @@ export default defineNuxtConfig({
   ],
   runtimeConfig: {
     public: {
-      apiOrigin: 'foo',
-      mixpanelApiHost: 'bar',
-      mixpanelTokenId: 'baz'
+      apiOrigin: 'UNDEFINED',
+      mixpanelApiHost: 'UNDEFINED',
+      mixpanelTokenId: 'UNDEFINED'
     }
   },
 
