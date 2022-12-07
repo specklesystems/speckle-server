a7dc667e5e598b86e268b5d44808dd6c67ee2b7c
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2022-12-07 11:17:52 +0000

    WIP: refactor based on PR review comments

---
+++ packages/frontend-2/server/meta/v1/healthz/readiness.get.ts
@@ -0,0 +1,28 @@
+import { useApolloClient } from '@vue/apollo-composable'
+import { graphql } from '~~/lib/common/generated/gql'
+
+const serverInfoVersionQuery = graphql(`
+  query serverInfo {
+    version
+  }
+`)
+
+export default defineEventHandler(async () => {
+  const client = useApolloClient().client
+  const { data, error } = await client.query({ query: serverInfoVersionQuery })
+  if (error) {
+    throw createError({
+      statusCode: 500,
+      name: 'InternalServerError',
+      message:
+        'Frontend is unable to reach the Graphql server, or the server is unable to query the database',
+      statusMessage: 'Internal Server Error'
+    })
+  }
+
+  if (data) {
+    return {
+      status: 'ready'
+    }
+  }
+})
