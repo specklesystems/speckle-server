cce6eb3c3f3448dd5dcb19eed0e794a4fc8f2afc
Dimitrie Stefanescu <didimitrie@gmail.com>
2022-12-09 09:55:22 +0000

    feat(fe2): adds simple story

---
+++ packages/frontend-2/components/tour/ViewerOnboarding.stories.ts
@@ -0,0 +1,22 @@
+import { Meta, Story } from '@storybook/vue3'
+import DesignSystem from '~~/components/DesignSystem.vue'
+import Viewer from '~~/components/tour/Viewer.vue'
+
+export default {
+  title: 'Onboarding With Viewer',
+  component: Viewer,
+  parameters: {
+    layout: 'fullscreen'
+  }
+} as Meta
+
+export const Default: Story = {
+  render: () => ({
+    components: { Viewer },
+    template: `
+    <Suspense>
+      <Viewer />
+    </Suspense>
+    `
+  })
+}
