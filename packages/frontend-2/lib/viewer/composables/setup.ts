e668e7dd24f1cab84a0a5dfe8f93e78e629438fb
Kristaps Fabians Geikins <fabis94@live.com>
2023-01-17 17:29:36 +0200

    minor fixes

--- packages/frontend-2/lib/viewer/composables/setup.ts
+++ packages/frontend-2/lib/viewer/composables/setup.ts
@@ -570,6 +570,8 @@ function useViewerObjectAutoLoading(state: InjectableViewerState) {
 }
 
 function useViewerSelectionEventHandler(state: InjectableViewerState) {
+  if (process.server) return
+
   useSelectionEvents(
     {
       singleClickCallback: () => {
--- packages/shared/src/viewer/helpers/route.ts
+++ packages/shared/src/viewer/helpers/route.ts
@@ -58,6 +58,7 @@ export class ViewerModelFolderResource implements ViewerResource {
 }
 
 export function parseUrlParameters(resourceGetParam: string) {
+  if (!resourceGetParam?.length) return []
   const parts = resourceGetParam.toLowerCase().split(',').sort()
   const resources: ViewerResource[] = []
   for (const part of parts) {
