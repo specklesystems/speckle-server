a94039e9348bf7c466c488683f64821a5733fe78
Dimitrie Stefanescu <didimitrie@gmail.com>
2023-01-24 09:56:16 +0000

    feat(fe2): object info - handles most cases

--- packages/frontend-2/components/viewer/selection/Object.vue
+++ packages/frontend-2/components/viewer/selection/Object.vue
@@ -1,24 +1,35 @@
 <template>
   <div>
-    <div class="flex items-center mb-2">
-      <div>
-        <div class="overflow-hidden flex-shrink-0">
-          <button
-            class="h-full hover:bg-primary-muted hover:text-primary rounded flex items-center space-x-2"
-            @click="unfold = !unfold"
-          >
-            <ChevronDownIcon
-              :class="`w-3 h-3 transition ${!unfold ? '-rotate-90' : 'rotate-0'}`"
-            />
-            <div class="text-xs font-bold">{{ headerAndSubheader.header }}</div>
-          </button>
-        </div>
+    <div class="flex items-center mb-1">
+      <div class="overflow-hidden flex-shrink-0">
+        <button
+          class="h-full hover:bg-primary-muted hover:text-primary rounded flex items-center space-x-1"
+          @click="unfold = !unfold"
+        >
+          <ChevronDownIcon
+            :class="`w-3 h-3 transition ${!unfold ? '-rotate-90' : 'rotate-0'}`"
+          />
+          <div class="text-xs font-bold">
+            {{ title || headerAndSubheader.header }}
+          </div>
+        </button>
       </div>
+
       <!-- <div class="text-xs font-bold">{{ title }}</div> -->
     </div>
-    <div v-if="unfold" class="ml-2 space-y-1">
-      <div v-for="(kvp, index) in categorisedValuePairs.primitives" :key="index">
-        <div class="grid grid-cols-3">
+    <div v-if="unfold" class="ml-1 space-y-1">
+      <div
+        v-for="(kvp, index) in [
+          ...categorisedValuePairs.primitives,
+          ...categorisedValuePairs.nulls
+        ]"
+        :key="index"
+      >
+        <div
+          :class="`grid grid-cols-3 ${
+            kvp.value === null || kvp.value === undefined ? 'text-foreground-2' : ''
+          }`"
+        >
           <div
             class="truncate text-xs col-span-1 font-bold"
             :title="(kvp.key as string)"
@@ -26,7 +37,8 @@
             {{ kvp.key }}
           </div>
           <div class="truncate text-xs col-span-2" :title="(kvp.value as string)">
-            {{ kvp.value }}
+            <!-- NOTE: can't do kvp.value || 'null' because 0 || 'null' = 'null' -->
+            {{ kvp.value === null ? 'null' : kvp.value }}
           </div>
         </div>
       </div>
@@ -37,17 +49,43 @@
           :unfold="false"
         />
       </div>
-      <div v-for="(kvp, index) in categorisedValuePairs.arrays" :key="index">
-        {{ kvp.key }}:{{ kvp.type }}
+      <div
+        v-for="(kvp, index) in categorisedValuePairs.nonPrimitiveArrays"
+        :key="index"
+        class="text-xs"
+      >
+        {{ kvp.key }}:{{ kvp.innerType }} (array todo)
+      </div>
+      <div v-for="(kvp, index) in categorisedValuePairs.primitiveArrays" :key="index">
+        <div class="grid grid-cols-3">
+          <div
+            class="truncate text-xs col-span-1 font-bold"
+            :title="(kvp.key as string)"
+          >
+            {{ kvp.key }}
+          </div>
+          <div
+            class="truncate text-xs col-span-2 flex min-w-0 w-full"
+            :title="(kvp.value as string)"
+          >
+            <div class="truncate flex-grow">{{ kvp.arrayPreview }}</div>
+            <div class="text-foreground-2">({{ kvp.arrayLength }})</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { XMarkIcon, ChevronDownIcon } from '@heroicons/vue/24/solid'
-import { onKeyStroke } from '@vueuse/core'
-import { useInjectedViewerInterfaceState } from '~~/lib/viewer/composables/setup'
+import { ChevronDownIcon } from '@heroicons/vue/24/solid'
+import {
+  useInjectedViewer,
+  useInjectedViewerInterfaceState
+} from '~~/lib/viewer/composables/setup'
 import { getHeaderAndSubheaderForSpeckleObject } from '~~/lib/object-sidebar/helpers'
+
+const { instance: viewer } = useInjectedViewer()
+const viewerTree = viewer.getWorldTree()
 
 const props = withDefaults(
   defineProps<{
@@ -68,8 +106,8 @@ const headerAndSubheader = computed(() => {
 const ignoredProps = [
   '__closure',
   // 'displayMesh',
-  // 'displayValue',
-  // 'totalChildrenCount',
+  'displayValue',
+  'totalChildrenCount',
   '__importedUrl',
   '__parents'
 ]
@@ -83,16 +121,22 @@ const keyValuePairs = computed(() => {
     const type = Array.isArray(props.object[key]) ? 'array' : typeof props.object[key]
     let innerType = null
     let arrayLength = null
+    let arrayPreview = null
     if (type === 'array') {
       const arr = props.object[key] as unknown[]
       arrayLength = arr.length
-      if (arr.length > 0) innerType = Array.isArray(arr[0]) ? 'array' : typeof arr[0]
+      if (arr.length > 0) {
+        innerType = Array.isArray(arr[0]) ? 'array' : typeof arr[0]
+        arrayPreview = arr.slice(0, 3).join(', ')
+        if (arr.length > 10) arrayPreview += ' ...' // in case truncate doesn't hit
+      }
     }
     kvps.push({
       key,
       type,
       innerType,
       arrayLength,
+      arrayPreview,
       value: props.object[key]
     })
   }
@@ -108,9 +152,19 @@ const categorisedValuePairs = computed(() => {
     objects: keyValuePairs.value.filter(
       (item) => item.type === 'object' && item.value !== null
     ),
-    arrays: keyValuePairs.value.filter(
-      (item) => item.type === 'array' && item.value !== null
-    )
+    nonPrimitiveArrays: keyValuePairs.value.filter(
+      (item) =>
+        item.type === 'array' &&
+        item.value !== null &&
+        (item.innerType === 'object' || item.innerType === 'array')
+    ),
+    primitiveArrays: keyValuePairs.value.filter(
+      (item) =>
+        item.type === 'array' &&
+        item.value !== null &&
+        !(item.innerType === 'object' || item.innerType === 'array')
+    ),
+    nulls: keyValuePairs.value.filter((item) => item.value === null)
   }
 })
 </script>
--- packages/frontend-2/components/viewer/selection/Sidebar.vue
+++ packages/frontend-2/components/viewer/selection/Sidebar.vue
@@ -8,16 +8,36 @@
   >
     <div class="mb-2">
       <div class="flex items-center">
-        <!-- <div class="font-bold">Selected Objects ({{ objects.length }})</div> -->
+        <div class="font-bold text-xs flex items-center space-x-1">
+          <CubeIcon class="w-3 h-3" />
+          <span>{{ objects.length }}</span>
+        </div>
+        <FormButton size="xs" :icon-right="EyeIcon" text @click="clearSelection()">
+          &nbsp;
+        </FormButton>
+        <FormButton size="xs" :icon-right="FunnelIcon" text @click="clearSelection()">
+          &nbsp;
+        </FormButton>
+        <FormButton
+          size="xs"
+          :icon-right="ArrowTopRightOnSquareIcon"
+          text
+          title="open selection in new tab"
+          @click="clearSelection()"
+        >
+          &nbsp;
+        </FormButton>
         <FormButton size="xs" :icon-right="XMarkIcon" text @click="clearSelection()">
-          Clear Selection
+          &nbsp;
         </FormButton>
       </div>
     </div>
+    <div class="w-full my-2 border-b border-outline-3"></div>
 
     <div>
       <div v-for="object in objects.reverse()" :key="(object.id as string)">
         <ViewerSelectionObject :object="object" :unfold="unfold" />
+        <div class="w-full my-2 border-b border-outline-3"></div>
       </div>
     </div>
     <div v-if="objects.length === 1" class="text-xs text-foreground-2 mt-2">
@@ -26,7 +46,13 @@
   </div>
 </template>
 <script setup lang="ts">
-import { XMarkIcon } from '@heroicons/vue/24/solid'
+import {
+  XMarkIcon,
+  ArrowTopRightOnSquareIcon,
+  EyeIcon,
+  FunnelIcon,
+  CubeIcon
+} from '@heroicons/vue/24/solid'
 import { onKeyStroke } from '@vueuse/core'
 import { useInjectedViewerInterfaceState } from '~~/lib/viewer/composables/setup'
 
