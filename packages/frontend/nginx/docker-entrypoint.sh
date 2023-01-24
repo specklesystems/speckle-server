02727baccc2a5da610d5a60fdb7459406b9b547c
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2023-01-24 19:23:26 +0000

    Need to include mime.types

--- packages/frontend/nginx/docker-entrypoint.sh
+++ packages/frontend/nginx/docker-entrypoint.sh
@@ -5,6 +5,7 @@ defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
 
 echo Starting nginx environment template rendering with "${defined_envs}"
 
+cp /opt/bitnami/openresty/nginx/mime.types /opt/bitnami/openresty/nginx/conf/mime.types
 envsubst "${defined_envs}" < /opt/bitnami/openresty/nginx/templates/nginx.conf.template > /opt/bitnami/openresty/nginx/conf/nginx.conf
 
 echo Nginx conf rendered, starting server...
