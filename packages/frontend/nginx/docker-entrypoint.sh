5a23af78e639bce19c3980fb6f4a562ac62b0c39
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2023-01-24 11:26:39 +0000

    Nginx for bitnami/openresty is in a different directory than openresty/openresty
    html files are in /app
    nginx.conf is in /opt/bitnami/openresty/nginx/conf/nginx.conf

--- packages/frontend/nginx/docker-entrypoint.sh
+++ packages/frontend/nginx/docker-entrypoint.sh
@@ -5,7 +5,7 @@ defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
 
 echo Starting nginx environment template rendering with "${defined_envs}"
 
-envsubst "${defined_envs}" < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
+envsubst "${defined_envs}" < /opt/bitnami/openresty/nginx/templates/nginx.conf.template > /opt/bitnami/openresty/nginx/conf/nginx.conf
 
 echo Nginx conf rendered, starting server...
 exec "$@"
