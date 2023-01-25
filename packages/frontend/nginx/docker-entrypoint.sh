68fd86b754d71b88b53dac37da113347bedb268c
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2023-01-25 19:06:48 +0000

    chore(frontend): use bitnami/openresty as base image for frontend Dockerfile (#1335)
    * chore(frontend): use bitnami/openresty as base image for frontend Dockerfile
    
    openresty/openresty was not being patched as frequently as we would like, resulting in numerous
    vulnerabilities without resolution. bitnami/openresty is being patched more frequently.
    
    Some additional changes were necessary when porting our frontend between these distributions:
    - html files are in /app
    - nginx.conf is in /opt/bitnami/openresty/nginx/conf/nginx.conf
    - envsubst is not available by default in bitnami/openresty and needs to be copied in
    - Nginx.conf - we wrap the server block in http block and overwrite root nginx.conf
        - using the existing bitnami/openresty nginx.conf as a server block alone causes issues with bitnami/openresty, as bitnami/openresty provides a root nginx.conf which conflicts with directives in Speckle's server block
    - we copy the directives from openresty/openresty (which are known to work with Speckle's server block), and apply them alongside Speckle's server block. This creates a new root nginx.conf which we can overwrite the default on the image.
    - nginx should use a port available to non sudo/root user, we have selected 8080 instead of previous 80
    - need to explicitly output nginx logs to stderr / stdout
    
    Created a readonly root file system on Kubernetes. This requires the following changes:
    - emptyDir volumes are mounted in kubernetes to allow bitnami/openresty to write to specific locations
    - explicitly include and copy mime.types file to nginx configuration directory
    
    Due to the change to non-privileged port number (8080), the following subsequent changes were required:
    - Update 1-click deployment script to match frontend at port 8080
    - Updates docker-compose-speckle.yaml file
    
    Co-authored-by: Gergő Jedlicska <gergo@jedlicska.com>

--- packages/frontend/nginx/docker-entrypoint.sh
+++ packages/frontend/nginx/docker-entrypoint.sh
@@ -5,7 +5,8 @@ defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
 
 echo Starting nginx environment template rendering with "${defined_envs}"
 
-envsubst "${defined_envs}" < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
+cp /opt/bitnami/openresty/nginx/mime.types /opt/bitnami/openresty/nginx/conf/mime.types
+envsubst "${defined_envs}" < /opt/bitnami/openresty/nginx/templates/nginx.conf.template > /opt/bitnami/openresty/nginx/conf/nginx.conf
 
 echo Nginx conf rendered, starting server...
 exec "$@"
