70c8d836d5c256983931693802435ea7933c6c7f
Kristaps Fabians Geikins <fabis94@live.com>
2023-03-15 17:31:44 +0200

    disabling onboarding redirect

--- packages/frontend-2/middleware/onboarding.global.ts
+++ packages/frontend-2/middleware/onboarding.global.ts
@@ -7,8 +7,8 @@ import { homeRoute, onboardingRoute } from '~~/lib/common/helpers/route'
  * Redirect user to /onboarding, if they haven't done it yet
  */
 export default defineNuxtRouteMiddleware(async (to) => {
-  // // TODO: Re-enable once we figure out how to set up clonable stream on latest
-  // return
+  // TODO: Re-enable once we figure out how to set up clonable stream on latest
+  return
 
   const { $apollo } = useNuxtApp()
   const client = ($apollo as { default: ApolloClient<unknown> }).default
