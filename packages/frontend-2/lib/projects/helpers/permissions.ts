896b5220a3d8feba8a92e3d8f3874a48c10bb955
Kristaps Fabians Geikins <fabis94@live.com>
2023-03-21 09:00:55 +0200

    fix: project metadata can only be updated by owner

--- packages/frontend-2/lib/projects/helpers/permissions.ts
+++ packages/frontend-2/lib/projects/helpers/permissions.ts
@@ -1,10 +1,11 @@
 import { MaybeNullOrUndefined, Roles } from '@speckle/shared'
 
 export const canEditProject = (project: { role?: MaybeNullOrUndefined<string> }) =>
+  ([Roles.Stream.Owner] as Array<MaybeNullOrUndefined<string>>).includes(project.role)
+
+export const canModifyModels = (project: { role?: MaybeNullOrUndefined<string> }) =>
   (
     [Roles.Stream.Contributor, Roles.Stream.Owner] as Array<
       MaybeNullOrUndefined<string>
     >
   ).includes(project.role)
-
-export const canModifyModels = canEditProject
