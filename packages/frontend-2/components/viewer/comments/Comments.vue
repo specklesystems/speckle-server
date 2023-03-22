b55939b62b986a95ec0e79f4947c0c02fd5a0538
Dimitrie Stefanescu <didimitrie@gmail.com>
2023-03-22 19:13:51 +0000

    feat(fe2): refactors comment visibility options

--- packages/frontend-2/components/viewer/comments/Comments.vue
+++ packages/frontend-2/components/viewer/comments/Comments.vue
@@ -2,21 +2,55 @@
   <ViewerLayoutPanel @close="$emit('close')">
     <template #actions>
       <FormButton
-        size="xs"
-        :icon-left="includeArchived ? CheckCircleIcon : CheckCircleIconOutlined"
         text
-        :disabled="commentThreadsMetadata?.totalArchivedCount === 0"
-        @click="includeArchived = includeArchived ? undefined : 'includeArchived'"
+        size="xs"
+        :icon-right="showVisibilityOptions ? XMarkIcon : EllipsisVerticalIcon"
+        @click="showVisibilityOptions = !showVisibilityOptions"
       >
-        {{ includeArchived ? 'Hide' : 'Show' }} Resolved ({{
-          commentThreadsMetadata?.totalArchivedCount
-        }})
-      </FormButton>
-      <FormButton size="xs" text @click="hideBubbles = !hideBubbles">
-        {{ !hideBubbles ? 'Hide' : 'Show' }} Threads
+        Comment Visibility Options
       </FormButton>
     </template>
     <div class="flex flex-col px-1">
+      <div
+        v-show="showVisibilityOptions"
+        class="sticky top-10 px-0 py-2 flex flex-col justify-start text-left border-b-2 border-primary-muted bg-foundation"
+      >
+        <div>
+          <FormButton
+            size="xs"
+            :icon-left="!hideBubbles ? CheckCircleIcon : CheckCircleIconOutlined"
+            text
+            @click="hideBubbles = !hideBubbles"
+          >
+            Show In 3D Model
+          </FormButton>
+        </div>
+        <div>
+          <FormButton
+            size="xs"
+            :icon-left="includeArchived ? CheckCircleIcon : CheckCircleIconOutlined"
+            text
+            :disabled="commentThreadsMetadata?.totalArchivedCount === 0"
+            @click="includeArchived = includeArchived ? undefined : 'includeArchived'"
+          >
+            {{ includeArchived ? 'Hide' : 'Show' }} Resolved ({{
+              commentThreadsMetadata?.totalArchivedCount
+            }})
+          </FormButton>
+        </div>
+        <div>
+          <FormButton
+            size="xs"
+            :icon-left="loadedVersionsOnly ? CheckCircleIcon : CheckCircleIconOutlined"
+            text
+            @click="
+              loadedVersionsOnly = loadedVersionsOnly ? undefined : 'loadedVersionsOnly'
+            "
+          >
+            Exclude Other Versions
+          </FormButton>
+        </div>
+      </div>
       <ViewerCommentsListItem
         v-for="thread in commentThreads"
         :key="thread.id"
@@ -28,7 +62,11 @@
 </template>
 <script setup lang="ts">
 import { graphql } from '~~/lib/common/generated/gql'
-import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+import {
+  CheckCircleIcon,
+  EllipsisVerticalIcon,
+  XMarkIcon
+} from '@heroicons/vue/24/solid'
 import { CheckCircleIcon as CheckCircleIconOutlined } from '@heroicons/vue/24/outline'
 import {
   useInjectedViewerInterfaceState,
@@ -74,12 +112,15 @@ const {
   threads: { hideBubbles }
 } = useInjectedViewerInterfaceState()
 
+const showVisibilityOptions = ref(false)
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const loadedVersionsOnly = computed({
   get: () =>
     threadFilters.value.loadedVersionsOnly || false ? 'loadedVersionsOnly' : undefined,
   set: (newVal) => (threadFilters.value.loadedVersionsOnly = !!newVal)
 })
+
 const includeArchived = computed({
   get: () =>
     threadFilters.value.includeArchived || false ? 'includeArchived' : undefined,
