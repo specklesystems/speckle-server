d88e3741661bb033fc0468f4b203ad2e322554a1
Dimitrie Stefanescu <didimitrie@gmail.com>
2023-04-10 17:05:07 +0100

    fix(fe2): prevents search text input focus loss on all models page

--- packages/frontend-2/components/project/models-page/Header.vue
+++ packages/frontend-2/components/project/models-page/Header.vue
@@ -24,7 +24,6 @@
           placeholder="Search"
           class="bg-foundation shadow w-full md:w-60"
           :show-clear="search !== ''"
-          :disabled="disabled"
           size="lg"
           @change="($event) => updateSearchImmediately($event.value)"
           @update:model-value="updateDebouncedSearch"
