c836ce0b8106a6d50425493e13e70270483f5d38
Dimitrie Stefanescu <didimitrie@gmail.com>
2023-04-10 17:04:32 +0100

    fix(fe2): fixes precommit checks

--- packages/frontend-2/middleware/dashboard-redirect.ts
+++ packages/frontend-2/middleware/dashboard-redirect.ts
@@ -1,7 +1,7 @@
 import { ApolloClient } from '@apollo/client/core'
 import { activeUserQuery } from '~~/lib/auth/composables/activeUser'
 import { convertThrowIntoFetchResult } from '~~/lib/common/helpers/graphql'
-import { homeRoute, loginRoute, onboardingRoute } from '~~/lib/common/helpers/route'
+import { loginRoute } from '~~/lib/common/helpers/route'
 
 /**
  * Redirect user to /onboarding, if they haven't done it yet
