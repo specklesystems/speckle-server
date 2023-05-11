d48d51ce8853ed681b6f905d58f7b7562d6dcc7e
Kristaps Fabians Geikins <fabis94@live.com>
2023-05-11 09:40:37 +0300

    fixed type output

--- packages/ui-components/vite.config.ts
+++ packages/ui-components/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [dts(), vue()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/lib.ts'),
