95fdaed868d6c69ea74ba96780f087a54340a484
Kristaps Fabians Geikins <fabis94@live.com>
2023-05-18 15:44:16 +0300

    added base layouts n stuff

--- packages/dui3/app.vue
+++ packages/dui3/app.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div id="speckle" class="bg-foundation-page text-foreground">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+<script setup lang="ts">
+useHead({
+  // Title suffix
+  titleTemplate: (titleChunk) =>
+    titleChunk ? `${titleChunk} - Speckle DUIv3` : 'Speckle DUIv3',
+  htmlAttrs: {
+    lang: 'en'
+  },
+  bodyAttrs: {
+    class: 'simple-scrollbar bg-foundation-page text-foreground'
+  }
+})
+</script>
---
+++ packages/dui3/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    Hello world
+    <Portal to="navigation">
+      <HeaderNavLink :to="'/'" :name="'Home'"></HeaderNavLink>
+    </Portal>
+  </div>
+</template>
