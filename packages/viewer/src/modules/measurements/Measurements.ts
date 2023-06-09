79eb69b6ea29e4f1c45de3e762750feec94df523
AlexandruPopovici <alexandrupopoviciioan@gmail.com>
2023-06-09 11:45:20 +0300

    When double clicking, Input will not ignore the second click and will not send a click event. It will send the first click event, then a double click event. Implemented auto perpendicular measurement by using double click

--- packages/viewer/src/modules/measurements/Measurements.ts
+++ packages/viewer/src/modules/measurements/Measurements.ts
@@ -2,15 +2,21 @@ import SpeckleRenderer, { ObjectLayers } from '../SpeckleRenderer'
 
 import { ViewerEvent } from '../../IViewer'
 import { Measurement, MeasurementState } from './Measurement'
+import { Ray, Vector3 } from 'three'
 
 export class Measurements {
   private renderer: SpeckleRenderer = null
   private measurement: Measurement = null
+  private clickTimeout
 
   public constructor(renderer: SpeckleRenderer) {
     this.renderer = renderer
     this.renderer.input.on('pointer-move', this.onPointerMove.bind(this))
     this.renderer.input.on(ViewerEvent.ObjectClicked, this.onPointerClick.bind(this))
+    this.renderer.input.on(
+      ViewerEvent.ObjectDoubleClicked,
+      this.onPointerDoubleClick.bind(this)
+    )
   }
 
   private onPointerMove(data) {
@@ -48,9 +54,51 @@ export class Measurements {
 
   private onPointerClick(data) {
     if (!data.event.ctrlKey) return
-    if (this.measurement.state === MeasurementState.DANGLING_START)
+    this.clickTimeout = setTimeout(() => {
+      if (this.measurement.state === MeasurementState.DANGLING_START)
+        this.measurement.state = MeasurementState.DANGLING_END
+      else if (this.measurement.state === MeasurementState.DANGLING_END) {
+        this.measurement.state = MeasurementState.COMPLETE
+        this.measurement.update()
+        this.measurement = null
+      }
+    }, 300)
+  }
+
+  private onPointerDoubleClick(data) {
+    if (!data.event.ctrlKey) return
+    clearTimeout(this.clickTimeout)
+
+    if (this.measurement.state === MeasurementState.DANGLING_START) {
+      const result = this.renderer.intersections.intersect(
+        this.renderer.scene,
+        this.renderer.camera,
+        data,
+        true,
+        undefined,
+        [ObjectLayers.STREAM_CONTENT_MESH]
+      )
+      if (!result || !result.length) return
+
+      const startPoint = new Vector3().copy(result[0].point)
+      const startNormal = new Vector3().copy(result[0].face.normal)
+
+      const perpResult = this.renderer.intersections.intersectRay(
+        this.renderer.scene,
+        this.renderer.camera,
+        new Ray(startPoint, startNormal),
+        true,
+        undefined,
+        [ObjectLayers.STREAM_CONTENT_MESH]
+      )
+      if (!perpResult || !perpResult.length) return
+
+      this.measurement.startPoint.copy(startPoint)
+      this.measurement.startNormal.copy(startNormal)
+      this.measurement.endPoint.copy(perpResult[0].point)
+      this.measurement.endNormal.copy(perpResult[0].face.normal)
       this.measurement.state = MeasurementState.DANGLING_END
-    else if (this.measurement.state === MeasurementState.DANGLING_END) {
+      this.measurement.update()
       this.measurement.state = MeasurementState.COMPLETE
       this.measurement.update()
       this.measurement = null
