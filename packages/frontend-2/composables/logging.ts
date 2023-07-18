21ee0ee23032bcc675b586f25b04e1e1c3376220
Kristaps Fabians Geikins <fabis94@live.com>
2023-07-18 12:55:18 +0300

    undoing logging bit

--- packages/frontend-2/composables/logging.ts
+++ packages/frontend-2/composables/logging.ts
@@ -1,14 +1,3 @@
-import { NuxtApp } from '#app'
-import { Optional } from '@speckle/shared'
-
 export const useLogger = () => {
-  let nuxtApp: Optional<NuxtApp>
-  try {
-    nuxtApp = useNuxtApp()
-  } catch (e) {
-    console.error('Nuxt app for logger not found! Skipping...')
-    // suppress
-  }
-
-  return nuxtApp?.$logger
+  return useNuxtApp().$logger
 }
