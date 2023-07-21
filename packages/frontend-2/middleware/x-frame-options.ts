eaac563a4043661115011669d2fbe96c39239b19
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2023-07-21 23:53:36 +0100

    fix(middleware): place in vue page instead of using global middleware

--- packages/frontend-2/middleware/x-frame-options.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware((to) => {
-  if (to.path.startsWith('/authn')) {
-    const { ssrContext } = useNuxtApp()
-    if (ssrContext) {
-      ssrContext.event.node.res.setHeader('x-frame-options', 'deny')
-    }
-  }
-})
