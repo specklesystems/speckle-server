c689e4c69c571413f3c4176f3266d82c66f461f9
Kristaps Fabians Geikins <fabis94@live.com>
2023-09-20 14:05:44 +0300

    fix(fe2): made onboarding bar load in SSR

--- packages/frontend-2/components/projects/Dashboard.vue
+++ packages/frontend-2/components/projects/Dashboard.vue
@@ -4,11 +4,9 @@
     <div
       class="w-[calc(100vw-8px)] ml-[calc(50%-50vw+4px)] mr-[calc(50%-50vw+4px)] -mt-6 mb-10 rounded-b-xl bg-foundation transition shadow-md hover:shadow-xl divide-y divide-outline-3"
     >
-      <ClientOnly>
-        <div v-if="showChecklist">
-          <OnboardingChecklistV1 show-intro />
-        </div>
-      </ClientOnly>
+      <div v-if="showChecklist">
+        <OnboardingChecklistV1 show-intro />
+      </div>
       <ProjectsInviteBanners
         v-if="projectsPanelResult?.activeUser?.projectInvites?.length"
         :invites="projectsPanelResult?.activeUser"
