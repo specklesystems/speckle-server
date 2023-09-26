0c37aa146691e9d5b46eac68742c005ac6ed1c64
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2023-09-26 13:03:48 +0200

    Use consistent story formatting

--- packages/ui-components/src/components/form/Switch.stories.ts
+++ packages/ui-components/src/components/form/Switch.stories.ts
@@ -1,8 +1,21 @@
 import { Meta, StoryObj } from '@storybook/vue3'
 import Switch from './Switch.vue'
 
+type SwitchStoryType = StoryObj<
+  Record<string, unknown> & {
+    'update:modelValue'?: (val: boolean) => void
+    modelValue?: boolean
+  }
+>
+
 export default {
   component: Switch,
+  argTypes: {
+    'update:modelValue': {
+      action: 'update:modelValue',
+      type: 'function'
+    }
+  },
   parameters: {
     docs: {
       description: {
@@ -12,8 +25,8 @@ export default {
   }
 } as Meta
 
-export const Default: StoryObj = {
-  render: (args) => ({
+export const Default: SwitchStoryType = {
+  render: (args, ctx) => ({
     components: { Switch },
     setup() {
       return { args }
@@ -22,10 +35,16 @@ export const Default: StoryObj = {
       <div>
         <Switch 
           v-bind="args" 
-          @update:modelValue="args.modelValue = $event"
+          @update:modelValue="onModelUpdate"
         />
       </div>
-    `
+    `,
+    methods: {
+      onModelUpdate(value: boolean) {
+        args['update:modelValue'] && args['update:modelValue'](value)
+        ctx.updateArgs({ ...args, modelValue: value })
+      }
+    }
   }),
   args: {
     modelValue: false
