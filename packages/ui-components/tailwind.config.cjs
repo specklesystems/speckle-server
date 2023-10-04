57de2d2b886c85ffdec1d9fe5020895496d2b0d1
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2023-10-04 11:35:47 +0200

    Updates to Dialog after conversations with Agi

--- packages/ui-components/tailwind.config.cjs
+++ packages/ui-components/tailwind.config.cjs
@@ -10,5 +10,16 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,vue}',
     tailwindContentEntry(require)
   ],
+  theme: {
+    extend: {
+      // Standard Tailwind Shadows add shadows below the element, 'shadow-t" variant adds shadow to the top of the element.
+      boxShadow: {
+        t: '0 -1px 3px 0 rgb(0 0 0 / 10%), 0 -1px 2px -1px rgb(0 0 0 / 6%)'
+      },
+      backdropBlur: {
+        xs: '1px'
+      }
+    }
+  },
   plugins: [speckleTheme.default, formsPlugin]
 }
