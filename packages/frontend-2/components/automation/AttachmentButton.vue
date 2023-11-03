99a381617e4408ab363453011001838c2a56593c
Dimitrie Stefanescu <didimitrie@gmail.com>
2023-11-03 17:14:52 +0000

    feat(fe2/automate): prettier :/

--- packages/frontend-2/components/automation/AttachmentButton.vue
+++ packages/frontend-2/components/automation/AttachmentButton.vue
@@ -1,10 +1,10 @@
 <template>
-	<FormButton v-tippy="fileInfo.name" text @click="onDownloadClick">
-		<span :class="`${restrictWidth ? 'max-w-[5rem]' : ''} truncate`">
-			{{ fileInfo.name }}
-		</span>
-		<PaperClipIcon class="w-3 h-3 text-primary" />
-	</FormButton>
+  <FormButton v-tippy="fileInfo.name" text @click="onDownloadClick">
+    <span :class="`${restrictWidth ? 'max-w-[5rem]' : ''} truncate`">
+      {{ fileInfo.name }}
+    </span>
+    <PaperClipIcon class="w-3 h-3 text-primary" />
+  </FormButton>
 </template>
 <script setup lang="ts">
 import { PaperClipIcon } from '@heroicons/vue/20/solid'
@@ -18,42 +18,42 @@ const { download } = useFileDownload()
 const { triggerNotification } = useGlobalToast()
 
 const props = withDefaults(
-	defineProps<{
-		blobId: string
-		projectId: string
-		restrictWidth: boolean
-	}>(),
-	{
-		restrictWidth: true
-	}
+  defineProps<{
+    blobId: string
+    projectId: string
+    restrictWidth: boolean
+  }>(),
+  {
+    restrictWidth: true
+  }
 )
 
 const { result } = useQuery(blobInfoQuery, () => ({
-	streamId: props.projectId,
-	blobId: props.blobId
+  streamId: props.projectId,
+  blobId: props.blobId
 }))
 
 const fileInfo = computed(() => {
-	return {
-		name: result.value?.stream?.blob?.fileName,
-		type: result.value?.stream?.blob?.fileType,
-		size: result.value?.stream?.blob?.fileSize
-	}
+  return {
+    name: result.value?.stream?.blob?.fileName,
+    type: result.value?.stream?.blob?.fileType,
+    size: result.value?.stream?.blob?.fileSize
+  }
 })
 
 const onDownloadClick = async () => {
-	try {
-		await download({
-			blobId: props.blobId,
-			fileName: fileInfo.value.name as string,
-			projectId: props.projectId
-		})
-	} catch (e) {
-		triggerNotification({
-			type: ToastNotificationType.Danger,
-			title: 'Download failed',
-			description: ensureError(e).message
-		})
-	}
+  try {
+    await download({
+      blobId: props.blobId,
+      fileName: fileInfo.value.name as string,
+      projectId: props.projectId
+    })
+  } catch (e) {
+    triggerNotification({
+      type: ToastNotificationType.Danger,
+      title: 'Download failed',
+      description: ensureError(e).message
+    })
+  }
 }
 </script>
--- packages/frontend-2/components/automation/ViewerResultRowItem.vue
+++ packages/frontend-2/components/automation/ViewerResultRowItem.vue
@@ -1,45 +1,45 @@
 <template>
-	<button
-		:class="`block transition text-left hover:bg-primary-muted hover:shadow-md rounded-md p-1 cursor-pointer border-l-2  ${
-			isIsolated ? 'border-primary bg-primary-muted shadow-md' : 'border-transparent'
-		}`"
-		@click="isolateOrUnisolateObjects()"
-	>
-		<div class="flex items-center space-x-1">
-			<div>
-				<Component :is="iconAndColor.icon" :class="`w-4 h-4 ${iconAndColor.color}`" />
-			</div>
-			<div :class="`text-xs ${iconAndColor.color}`">
-				{{ result.category }}: {{ result.objectIds.length }} affected elements
-			</div>
-		</div>
-		<div class="text-xs text-foreground-2 pl-5">{{ result.message }}</div>
-	</button>
+  <button
+    :class="`block transition text-left hover:bg-primary-muted hover:shadow-md rounded-md p-1 cursor-pointer border-l-2  ${
+      isIsolated ? 'border-primary bg-primary-muted shadow-md' : 'border-transparent'
+    }`"
+    @click="isolateOrUnisolateObjects()"
+  >
+    <div class="flex items-center space-x-1">
+      <div>
+        <Component :is="iconAndColor.icon" :class="`w-4 h-4 ${iconAndColor.color}`" />
+      </div>
+      <div :class="`text-xs ${iconAndColor.color}`">
+        {{ result.category }}: {{ result.objectIds.length }} affected elements
+      </div>
+    </div>
+    <div class="text-xs text-foreground-2 pl-5">{{ result.message }}</div>
+  </button>
 </template>
 <script setup lang="ts">
 import {
-	XMarkIcon,
-	InformationCircleIcon,
-	ExclamationTriangleIcon
+  XMarkIcon,
+  InformationCircleIcon,
+  ExclamationTriangleIcon
 } from '@heroicons/vue/24/outline'
 import { useInjectedViewerState } from '~~/lib/viewer/composables/setup'
 import { useFilterUtilities } from '~~/lib/viewer/composables/ui'
 
 type ObjectResult = {
-	category: string
-	objectIds: string[]
-	message: string
-	level: 'ERROR' | 'WARNING' | 'INFO'
+  category: string
+  objectIds: string[]
+  message: string
+  level: 'ERROR' | 'WARNING' | 'INFO'
 }
 
 const props = defineProps<{
-	result: ObjectResult
+  result: ObjectResult
 }>()
 
 const {
-	viewer: {
-		metadata: { filteringState }
-	}
+  viewer: {
+    metadata: { filteringState }
+  }
 } = useInjectedViewerState()
 const { isolateObjects, unIsolateObjects, resetFilters } = useFilterUtilities()
 
@@ -47,43 +47,43 @@ import { containsAll } from '~~/lib/common/helpers/utils'
 
 const isolatedObjects = computed(() => filteringState.value?.isolatedObjects)
 const isIsolated = computed(() => {
-	if (!isolatedObjects.value) return false
-	const ids = props.result.objectIds
-	return containsAll(ids, isolatedObjects.value)
+  if (!isolatedObjects.value) return false
+  const ids = props.result.objectIds
+  return containsAll(ids, isolatedObjects.value)
 })
 
 const isolateOrUnisolateObjects = () => {
-	const ids = props.result.objectIds
-	if (!isIsolated.value) {
-		resetFilters()
-		isolateObjects(ids)
-		return
-	}
+  const ids = props.result.objectIds
+  if (!isIsolated.value) {
+    resetFilters()
+    isolateObjects(ids)
+    return
+  }
 
-	unIsolateObjects(ids)
+  unIsolateObjects(ids)
 }
 
 const iconAndColor = computed(() => {
-	switch (props.result.level) {
-		case 'ERROR':
-			return {
-				icon: XMarkIcon,
-				color: 'text-danger'
-			}
-		case 'WARNING':
-			return {
-				icon: ExclamationTriangleIcon,
-				color: 'text-warning'
-			}
-		case 'INFO':
-			return {
-				icon: InformationCircleIcon,
-				color: 'text-foreground'
-			}
-	}
-	return {
-		icon: XMarkIcon,
-		color: 'text-danger'
-	}
+  switch (props.result.level) {
+    case 'ERROR':
+      return {
+        icon: XMarkIcon,
+        color: 'text-danger'
+      }
+    case 'WARNING':
+      return {
+        icon: ExclamationTriangleIcon,
+        color: 'text-warning'
+      }
+    case 'INFO':
+      return {
+        icon: InformationCircleIcon,
+        color: 'text-foreground'
+      }
+  }
+  return {
+    icon: XMarkIcon,
+    color: 'text-danger'
+  }
 })
 </script>
