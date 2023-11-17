e98725936f7401a7ff3a7e7210beb8a5486818f8
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2023-11-17 10:06:41 +0000

    Double limit of branches to ensure all are visible

--- packages/frontend/src/graphql/branch.js
+++ packages/frontend/src/graphql/branch.js
@@ -10,7 +10,7 @@ export const streamNavBranchesQuery = gql`
   query StreamAllBranches($streamId: String!, $cursor: String) {
     stream(id: $streamId) {
       id
-      branches(limit: 100, cursor: $cursor) {
+      branches(limit: 200, cursor: $cursor) {
         totalCount
         cursor
         items {
