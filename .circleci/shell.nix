4bd1cbe2d586e3dc54e6d350d60f77b83a61dd7e
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2023-11-21 16:36:46 +0000

    Remove unexpected in statement from nix-shell

--- .circleci/shell.nix
+++ .circleci/shell.nix
@@ -1,6 +1,6 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/5b7cd5c39befee629be284970415b6eb3b0ff000.tar.gz") {} }:
 
-in pkgs.mkShell {
+pkgs.mkShell {
   buildInputs = [
     pkgs.docker
   ];
