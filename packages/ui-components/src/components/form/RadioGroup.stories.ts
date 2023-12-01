3a707113ea4ac6d38b99a958cde151883cde3962
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2023-12-01 10:43:38 +0000

    Updates to RadioGroup

--- packages/ui-components/src/components/form/RadioGroup.stories.ts
+++ packages/ui-components/src/components/form/RadioGroup.stories.ts
@@ -1,7 +1,22 @@
 import type { Meta, StoryObj } from '@storybook/vue3'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
+import { action } from '@storybook/addon-actions'
 import RadioGroup from '~~/src/components/form/RadioGroup.vue'
 import { MegaphoneIcon, MagnifyingGlassIcon } from '@heroicons/vue/24/solid'
+
+interface RadioGroupArgs {
+  radioGroupName: string
+  options: {
+    id: string
+    title: string
+    description?: string
+    icon?: typeof MagnifyingGlassIcon | typeof MegaphoneIcon
+  }[]
+  modelValue: string
+  required?: boolean
+  helpText?: string
+  showTitle?: boolean
+}
 
 export default {
   component: RadioGroup,
@@ -10,33 +25,79 @@ export default {
       description: {
         component: 'Custom Stacked Card Radio Group'
       }
+    },
+    actions: {
+      handles: ['update:modelValue RadioGroup']
     }
   }
 } as Meta
 
-export const Default: StoryObj = {
+export const Default: StoryObj<RadioGroupArgs> = {
   render: (args) => ({
     components: { RadioGroup },
     setup() {
-      const selectedOption = ref(null)
+      const selectedOption = ref<string>(args.modelValue)
+
+      watch(selectedOption, (newValue) => {
+        args.modelValue = newValue
+        action('update:modelValue')(newValue)
+      })
 
       return { args, selectedOption }
     },
     template: `
       <div>
-        <RadioGroup v-bind="args" @update:selected="selectedOption = $event" />
+        <RadioGroup 
+          v-model="selectedOption" 
+          v-bind="args" 
+          @update:modelValue="args.modelValue = $event"
+        />
       </div>`
   }),
   args: {
-    label: 'Example Label:',
+    radioGroupName: 'Example Label:',
     options: [
       {
+        id: 'option1',
         title: 'Option 1 Title',
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
       },
       {
+        id: 'option2',
         title: 'Option 2 Title',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      }
+    ],
+    modelValue: 'option1'
+  }
+}
+
+export const PreselectedOption: StoryObj<RadioGroupArgs> = {
+  ...Default,
+  args: {
+    ...Default.args,
+    modelValue: 'option2'
+  }
+}
+
+export const WithIcons: StoryObj<RadioGroupArgs> = {
+  ...Default,
+  args: {
+    ...Default.args,
+    options: [
+      {
+        id: 'option1',
+        title: 'Option 1 Title',
+        icon: MagnifyingGlassIcon,
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      {
+        id: 'option2',
+        title: 'Option 2 Title',
+        icon: MegaphoneIcon,
         description:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
       }
@@ -44,22 +105,22 @@ export const Default: StoryObj = {
   }
 }
 
-export const WithIcons: StoryObj = {
+export const WithRequiredBadge: StoryObj<RadioGroupArgs> = {
   ...Default,
   args: {
-    options: [
-      {
-        title: 'Option 1 Title',
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        icon: MagnifyingGlassIcon
-      },
-      {
-        title: 'Option 2 Title',
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        icon: MegaphoneIcon
-      }
-    ]
+    ...Default.args,
+    required: true,
+    radioGroupName: 'Your Title',
+    showTitle: true
+  }
+}
+
+export const WithHelpTextAndTitle: StoryObj<RadioGroupArgs> = {
+  ...Default,
+  args: {
+    ...Default.args,
+    helpText: 'Additional information here',
+    radioGroupName: 'Your Title',
+    showTitle: true
   }
 }
--- packages/ui-components/src/components/form/RadioGroup.vue
+++ packages/ui-components/src/components/form/RadioGroup.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <div class="sr-only">{{ label }}</div>
+    <div class="block label text-foreground-2 mb-2" :class="!showTitle && 'sr-only'">
+      {{ radioGroupName }}
+      <span v-if="required">*</span>
+    </div>
     <div class="flex flex-col gap-2">
       <div
         v-for="(option, index) in options"
@@ -8,46 +11,55 @@
         class="flex items-center gap-3"
       >
         <input
-          :id="`option-${index}`"
-          v-model="selected"
+          :id="`option-${option.id}`"
+          v-model="selectedOption"
           type="radio"
-          :value="option"
+          :value="option.id"
+          :name="radioGroupName"
           class="cursor-pointer h-3 w-3 bg-foundation border-foundation ring-[1.5px] ring-primary checked:ring-primary-focus focus:ring-offset-foundation focus:outline-primary-focus"
         />
         <label
           class="flex items-center gap-1.5 text-sm text-foreground cursor-pointer"
-          :for="`option-${index}`"
+          :for="`option-${option.id}`"
         >
           <Component :is="option.icon" v-if="option.icon" class="h-9 w-9" />
           {{ option.title }}
         </label>
       </div>
+      <p v-if="helpText" class="text-xs sm:text-sm text-foreground-2">
+        {{ helpText }}
+      </p>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watchEffect } from 'vue'
+import { watch } from 'vue'
 import type { ConcreteComponent } from 'vue'
 
 interface Option {
+  id: string
   title: string
   icon?: ConcreteComponent
 }
 
 const props = defineProps<{
-  label: string
   options: Option[]
+  radioGroupName: string
+  required?: boolean
+  helpText?: string
+  showTitle?: boolean
 }>()
 
-const emit = defineEmits(['update:selected'])
+const selectedOption = defineModel<string>()
 
-const selected = ref<Option | null>(props.options[0] || null)
-
-watchEffect(() => {
-  if (props.options.length > 0 && !selected.value) {
-    selected.value = props.options[0]
-  }
-  emit('update:selected', selected.value)
-})
+watch(
+  () => props.options,
+  (newOptions) => {
+    if (newOptions.length > 0 && !selectedOption.value) {
+      selectedOption.value = newOptions[0].id
+    }
+  },
+  { immediate: true }
+)
 </script>
