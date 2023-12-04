1f141e7f3c02bfc60275df5b1c677100bab35066
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2023-12-04 17:18:49 +0000

    Merge

--- packages/frontend-2/components/header/NavBar.vue
+++ packages/frontend-2/components/header/NavBar.vue
@@ -1,29 +1,34 @@
 <template>
-  <nav class="fixed z-20 top-0 h-14 bg-foundation shadow hover:shadow-md transition">
-    <div class="flex items-center justify-between h-full w-screen px-4">
-      <div class="flex items-center truncate">
-        <HeaderLogoBlock :active="false" />
-        <HeaderNavLink
-          to="/"
-          name="Dashboard"
-          :separator="true"
-          class="hidden md:inline-block"
-        />
-        <PortalTarget name="navigation"></PortalTarget>
-      </div>
-      <div class="flex items-center">
-        <div class="hidden sm:flex">
-          <PortalTarget name="secondary-actions"></PortalTarget>
-          <PortalTarget name="primary-actions"></PortalTarget>
+  <div>
+    <div class="px-4">
+      <nav
+        class="fixed z-20 top-0 h-14 bg-foundation shadow hover:shadow-md transition"
+      >
+        <div class="flex items-center justify-between h-full w-screen px-4">
+          <div class="flex items-center truncate">
+            <HeaderLogoBlock :active="false" />
+            <HeaderNavLink
+              to="/"
+              name="Dashboard"
+              :separator="true"
+              class="hidden md:inline-block"
+            />
+            <PortalTarget name="navigation"></PortalTarget>
+          </div>
+          <div class="flex items-center">
+            <PortalTarget name="secondary-actions"></PortalTarget>
+            <PortalTarget name="primary-actions"></PortalTarget>
+            <!-- Notifications dropdown -->
+            <HeaderNavNotifications />
+            <!-- Profile dropdown -->
+            <HeaderNavUserMenu />
+          </div>
         </div>
-        <!-- Notifications dropdown -->
-        <HeaderNavNotifications />
-        <!-- Profile dropdown -->
-        <HeaderNavUserMenu />
-      </div>
+        <PopupsSignIn v-if="!activeUser" />
+      </nav>
     </div>
-    <PopupsSignIn v-if="!activeUser" />
-  </nav>
+    <div class="h-14"></div>
+  </div>
 </template>
 <script setup lang="ts">
 import { useActiveUser } from '~~/lib/auth/composables/activeUser'
