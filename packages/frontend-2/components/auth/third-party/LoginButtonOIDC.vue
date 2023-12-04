37c515d8dbd1ab7a7a65458ae8cdf1db36896825
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2023-12-04 15:01:37 +0000

    Get OIDC name and add to button

--- packages/frontend-2/components/auth/third-party/LoginButtonOIDC.vue
+++ packages/frontend-2/components/auth/third-party/LoginButtonOIDC.vue
@@ -1,13 +1,23 @@
 <template>
   <AuthThirdPartyLoginButtonBase :to="to" class="dark:bg-[#2F2F2F]">
     <IdentificationIcon class="h-4" />
-    <div>OIDC</div>
+    <div class="ml-3">{{ oidcName }}</div>
   </AuthThirdPartyLoginButtonBase>
 </template>
 <script setup lang="ts">
 import { IdentificationIcon } from '@heroicons/vue/24/outline'
+import { useQuery } from '@vue/apollo-composable'
+import { loginServerInfoQuery } from '~~/lib/auth/graphql/queries'
 
 defineProps<{
   to: string
 }>()
+
+const { result } = useQuery(loginServerInfoQuery)
+const authStrategies = computed(() => result.value?.serverInfo.authStrategies)
+
+const oidcName = computed(() => {
+  const oidcStrategy = authStrategies.value?.find((strategy) => strategy.id === 'oidc')
+  return oidcStrategy ? oidcStrategy.name : 'Log in'
+})
 </script>
