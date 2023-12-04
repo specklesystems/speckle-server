44049314a3266135ffae60ea68633368564e8d54
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2023-12-04 17:20:22 +0000

    Remove unneeded padding

--- packages/frontend-2/components/header/NavBar.vue
+++ packages/frontend-2/components/header/NavBar.vue
@@ -1,32 +1,28 @@
 <template>
   <div>
-    <div class="px-4">
-      <nav
-        class="fixed z-20 top-0 h-14 bg-foundation shadow hover:shadow-md transition"
-      >
-        <div class="flex items-center justify-between h-full w-screen px-4">
-          <div class="flex items-center truncate">
-            <HeaderLogoBlock :active="false" />
-            <HeaderNavLink
-              to="/"
-              name="Dashboard"
-              :separator="true"
-              class="hidden md:inline-block"
-            />
-            <PortalTarget name="navigation"></PortalTarget>
-          </div>
-          <div class="flex items-center">
-            <PortalTarget name="secondary-actions"></PortalTarget>
-            <PortalTarget name="primary-actions"></PortalTarget>
-            <!-- Notifications dropdown -->
-            <HeaderNavNotifications />
-            <!-- Profile dropdown -->
-            <HeaderNavUserMenu />
-          </div>
+    <nav class="fixed z-20 top-0 h-14 bg-foundation shadow hover:shadow-md transition">
+      <div class="flex items-center justify-between h-full w-screen px-4">
+        <div class="flex items-center truncate">
+          <HeaderLogoBlock :active="false" />
+          <HeaderNavLink
+            to="/"
+            name="Dashboard"
+            :separator="true"
+            class="hidden md:inline-block"
+          />
+          <PortalTarget name="navigation"></PortalTarget>
         </div>
-        <PopupsSignIn v-if="!activeUser" />
-      </nav>
-    </div>
+        <div class="flex items-center">
+          <PortalTarget name="secondary-actions"></PortalTarget>
+          <PortalTarget name="primary-actions"></PortalTarget>
+          <!-- Notifications dropdown -->
+          <HeaderNavNotifications />
+          <!-- Profile dropdown -->
+          <HeaderNavUserMenu />
+        </div>
+      </div>
+      <PopupsSignIn v-if="!activeUser" />
+    </nav>
     <div class="h-14"></div>
   </div>
 </template>
