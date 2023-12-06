c8bdf01cdda8bf88bbf7942ec35011a90d5f211d
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2023-12-06 09:56:22 +0000

    FE2 Viewer - Add Measure Mode (#1889)
    * RadioGroup & Initial UI for Measure
    
    * Add option to Panel to allow actions to move to bottom
    
    * Typo
    
    * Add count to precision
    
    * Add enable, snap and type api integrations
    
    * Update Units WIP
    
    * Add precision update
    
    * Update v-tippy name
    
    * Updates
    
    * New design
    
    * Better darkmode radio. Keystrokes.
    
    * Styling fixes. Fix select mount-menu-on-body
    
    * Fix ts bug
    
    * Show label in Select for units
    
    * Update shortcut to D
    
    * Small design changes
    
    * Small tidy ups
    
    * WIP New Measurements Helper State
    
    * Fix build erros
    
    * Remove viewer import from shared
    
    * Delete WIP
    
    * Fix delete
    
    * Fix close button on measure mode
    
    * Measurement nullable
    
    * Updates from PR
    
    * Seperate measurements into measurementsEnabled & measurementOptions
    
    * Update state.ts
    
    * Update ts bugs
    
    * Updates to RadioGroup
    
    * Use ctx.updateArgs
    
    * Replace RadioGroup with Radio - More consistent with existing inputs
    
    * Update FE2 to use new Radio
    
    * Fix circleci fail
    
    * Fix build
    
    * Fix wrong initial state for vertexSnap
    
    * Adjust type to measurement
    
    * Use Lodash isEqual
    
    * Fix bug where units don't update
    
    * Remove double input
    
    * Fix server error in data.ts
    
    * Revert change around useEqual

--- packages/frontend-2/components/viewer/layout/Panel.vue
+++ packages/frontend-2/components/viewer/layout/Panel.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-foundation sm:rounded-lg overflow-hidden shadow">
-    <div class="sticky top-0 z-50 flex flex-col bg-foundation shadow-md">
+  <div class="bg-foundation sm:rounded-lg overflow-hidden shadow flex flex-col">
+    <div class="sticky top-0 z-50 flex flex-col bg-foundation">
       <div v-if="!hideClose" class="absolute top-2 right-2 sm:right-0 z-10">
         <FormButton size="sm" color="secondary" text @click="$emit('close')">
           <XMarkIcon class="h-4 w-4 sm:h-3 sm:w-3 text-primary sm:text-foreground" />
@@ -8,7 +8,7 @@
       </div>
       <div
         v-if="$slots.title"
-        class="flex items-center h-10 px-3 border-b border-outline dark:border-foundation-2 bg-foundation rounded-t"
+        class="flex items-center h-10 px-3 border-b border-outline-3 dark:border-foundation-2 bg-foundation rounded-t"
       >
         <div
           class="flex items-center h-full w-full pr-8 font-semibold sm:font-bold text-sm text-primary"
@@ -18,11 +18,19 @@
           </span>
         </div>
       </div>
-      <div v-if="$slots.actions" class="flex items-center h-8 sm:h-10 gap-2 px-2">
-        <slot name="actions"></slot>
-      </div>
     </div>
-    <div>
+    <div
+      v-if="$slots.actions"
+      class="flex items-center h-8 sm:h-10 gap-2 px-2"
+      :class="
+        moveActionsToBottom
+          ? 'order-3 border-t border-outline-3 mt-2'
+          : 'order-2 shadow-md'
+      "
+    >
+      <slot name="actions"></slot>
+    </div>
+    <div :class="moveActionsToBottom ? 'order-2' : 'order-3'">
       <slot></slot>
     </div>
   </div>
@@ -32,6 +40,10 @@ import { XMarkIcon } from '@heroicons/vue/24/solid'
 defineEmits(['close'])
 defineProps({
   hideClose: {
+    type: Boolean,
+    default: false
+  },
+  moveActionsToBottom: {
     type: Boolean,
     default: false
   }
---
+++ packages/ui-components/src/components/form/Radio.vue
@@ -0,0 +1,228 @@
+<template>
+  <div
+    class="relative flex gap-2 mb-2 last:mb-0"
+    :class="description ? 'items-start' : 'items-center'"
+  >
+    <div class="flex h-6 items-center">
+      <!-- eslint-disable-next-line vuejs-accessibility/form-control-has-label -->
+      <input
+        :id="finalId"
+        :checked="coreChecked"
+        :aria-describedby="descriptionId"
+        :name="name"
+        :disabled="disabled"
+        :value="radioValue"
+        type="radio"
+        class="h-4 w-4 rounded-full text-primary focus:ring-primary bg-foundation disabled:cursor-not-allowed disabled:bg-disabled disabled:text-disabled-2"
+        :class="computedClasses"
+        v-bind="$attrs"
+        @change="onChange"
+      />
+    </div>
+    <div v-if="icon" class="text-sm">
+      <component :is="icon" class="h-10 w-10"></component>
+    </div>
+    <div class="text-sm">
+      <label :for="finalId" class="text-foreground" :class="{ 'sr-only': hideLabel }">
+        <span>{{ title }}</span>
+        <span v-if="showRequired" class="text-danger ml-1">*</span>
+      </label>
+      <p v-if="descriptionText" :id="descriptionId" :class="descriptionClasses">
+        {{ descriptionText }}
+      </p>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import { useField } from 'vee-validate'
+import type { RuleExpression } from 'vee-validate'
+import { computed, onMounted, ref } from 'vue'
+import type { PropType, ConcreteComponent } from 'vue'
+import type { Optional } from '@speckle/shared'
+import { nanoid } from 'nanoid'
+
+/**
+ * Troubleshooting:
+ * - If clicking on the radio doesn't do anything, check if any of its ancestor elements
+ * have a @click.prevent on them anywhere.
+ * - If you're not using the radio in a group, it's suggested that you set :value="true",
+ * so that a v-model attached to the radio will be either 'true' or 'undefined' depending on the
+ * checked state
+ */
+
+type ValueType = Optional<string | true> | string[]
+
+defineOptions({
+  inheritAttrs: false
+})
+
+const props = defineProps({
+  /**
+   * Input name/id. In a radio group, all radios must have the same name and different values.
+   */
+  name: {
+    type: String,
+    required: true
+  },
+  /**
+   * Whether the input is disabled
+   */
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+  /**
+   * Set label text
+   */
+  label: {
+    type: String as PropType<Optional<string>>,
+    default: undefined
+  },
+  /**
+   * Help text
+   */
+  description: {
+    type: String as PropType<Optional<string>>,
+    default: undefined
+  },
+  /**
+   * Whether to inline the help description
+   */
+  inlineDescription: {
+    type: Boolean,
+    default: false
+  },
+  /**
+   * Optional Icon
+   */
+  icon: {
+    type: Object as PropType<ConcreteComponent>,
+    default: undefined
+  },
+  /**
+   * vee-validate validation rules
+   */
+  rules: {
+    type: [String, Object, Function, Array] as PropType<RuleExpression<ValueType>>,
+    default: undefined
+  },
+  /**
+   * vee-validate validation() on component mount
+   */
+  validateOnMount: {
+    type: Boolean,
+    default: false
+  },
+  /**
+   * Whether to show the red "required" asterisk
+   */
+  showRequired: {
+    type: Boolean,
+    default: false
+  },
+  /**
+   * Radio group's value
+   */
+  modelValue: {
+    type: [String, Boolean] as PropType<ValueType | false>,
+    default: undefined
+  },
+  /**
+   * Radio's own value. If it is checked, modelValue will include this value (amongst any other checked values from the same group).
+   * If not set will default to 'name' value.
+   */
+  value: {
+    type: [String, Boolean] as PropType<Optional<string | true>>,
+    default: true
+  },
+  /**
+   * HTML ID to use, must be globally unique. If not specified, a random ID will be generated. One is necessary to properly associate the label and radio.
+   */
+  id: {
+    type: String as PropType<Optional<string>>,
+    default: undefined
+  },
+  hideLabel: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const generateRandomId = (prefix: string) => `${prefix}-${nanoid()}`
+
+defineEmits<{
+  (e: 'update:modelValue', val: ValueType): void
+}>()
+
+const radioValue = computed(() => props.value || props.name)
+
+const {
+  checked: coreChecked,
+  errorMessage,
+  handleChange,
+  value: coreValue
+} = useField<ValueType>(props.name, props.rules, {
+  validateOnMount: props.validateOnMount,
+  type: 'radio',
+  checkedValue: radioValue,
+  initialValue: props.modelValue || undefined
+})
+
+const title = computed(() => props.label || props.name)
+
+const computedClasses = computed((): string => {
+  return errorMessage.value ? 'border-danger-lighter' : 'border-foreground-4 '
+})
+
+const descriptionText = computed(() => props.description || errorMessage.value)
+const descriptionId = computed(() => `${props.name}-description`)
+const descriptionClasses = computed((): string => {
+  const classParts: string[] = []
+
+  if (props.inlineDescription) {
+    classParts.push('inline ml-2')
+  } else {
+    classParts.push('block')
+  }
+
+  if (errorMessage.value) {
+    classParts.push('text-danger')
+  } else {
+    classParts.push('text-foreground-2')
+  }
+
+  return classParts.join(' ')
+})
+
+const implicitId = ref<Optional<string>>(generateRandomId('radio'))
+const finalId = computed(() => props.id || implicitId.value)
+
+const onChange = (e: unknown) => {
+  if (props.disabled) return
+  handleChange(e)
+}
+
+/**
+ * Bugfix for strange issue where radio appears checked even tho it shouldnt be.
+ * It's not clear why this happens, but for some reason coreValue.value shows that the radio
+ * is checked, even tho props.modelValue is undefined.
+ */
+onMounted(() => {
+  const newModelValue = props.modelValue
+  const newCoreValue = coreValue.value
+
+  const shouldBeChecked = Array.isArray(newModelValue)
+    ? newModelValue.includes(props.value as any)
+    : newModelValue === props.value
+
+  const isCoreChecked = Array.isArray(newCoreValue)
+    ? newCoreValue.includes(props.value as any)
+    : newCoreValue === props.value
+
+  if (shouldBeChecked !== isCoreChecked) {
+    handleChange(newModelValue)
+  }
+})
+</script>
