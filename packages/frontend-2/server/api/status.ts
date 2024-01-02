31e3c1fc1b79db06ecc31c047943fcf4ed724e77
Kristaps Fabians Geikins <fabis94@live.com>
2024-01-02 17:21:30 +0200

    added correlation ids to upstream calls

--- packages/frontend-2/server/api/status.ts
+++ packages/frontend-2/server/api/status.ts
@@ -1,3 +1,6 @@
-export default defineEventHandler(() => {
-  return { hello: 'world' }
+import { useRequestId } from '~/lib/core/composables/server'
+
+export default defineEventHandler((event) => {
+  const reqId = useRequestId({ event })
+  return { status: 'ok', reqId }
 })
