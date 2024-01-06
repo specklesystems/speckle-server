b04a7b53c366dcd4df2cbb4c802b6832c4d55502
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2024-01-06 21:16:18 +0000

    Min Height of Clipboard Input multiline to 3 lines

--- packages/ui-components/src/components/form/ClipboardInput.vue
+++ packages/ui-components/src/components/form/ClipboardInput.vue
@@ -6,7 +6,7 @@
       name="contentArea"
       readonly
       :model-value="value"
-      class="relative z-10 text-sm text-foreground font-mono"
+      class="relative z-10 text-sm text-foreground font-mono min-h-[76px]"
       :rows="rows"
     />
     <FormTextInput
