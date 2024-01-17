b5c2089e309ee015aeda860160b9ae1809f016bd
Kristaps Fabians Geikins <fabis94@live.com>
2024-01-17 17:36:35 +0200

    preload plugin

---
+++ packages/frontend-2/plugins/003-dataPreload.ts
@@ -0,0 +1,42 @@
+import type { Optional } from '@speckle/shared'
+import { activeUserQuery } from '~/lib/auth/composables/activeUser'
+import { usePreloadApolloQueries } from '~/lib/common/composables/graphql'
+import { mainServerInfoDataQuery } from '~/lib/core/composables/server'
+import { projectAccessCheckQuery } from '~/lib/projects/graphql/queries'
+
+/**
+ * Prefetches data for specific routes to avoid the problem of serial API requests
+ * (e.g. in the case of multiple middlewares)
+ */
+export default defineNuxtPlugin(async (ctx) => {
+  const logger = useLogger()
+  const route = ctx._route
+  const preload = usePreloadApolloQueries()
+
+  if (!route) {
+    logger.info('No route obj found, skipping data preload...')
+    return
+  }
+
+  const path = route.path
+  const idParam = route.params.id as Optional<string>
+  const promises: Promise<unknown>[] = []
+
+  // Standard/global
+  promises.push(
+    preload({
+      queries: [{ query: activeUserQuery }, { query: mainServerInfoDataQuery }]
+    })
+  )
+
+  // Preload project data
+  if (idParam && path.startsWith('/projects/')) {
+    promises.push(
+      preload({
+        queries: [{ query: projectAccessCheckQuery, variables: { id: idParam } }]
+      })
+    )
+  }
+
+  await Promise.all(promises)
+})
