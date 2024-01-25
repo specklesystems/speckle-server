52770ec7ce35cf0562208ea8ad9ec761d3716f6f
Kristaps Fabians Geikins <fabis94@live.com>
2024-01-25 11:41:26 +0200

    feat(fe2): more robust redis cache

--- packages/frontend-2/plugins/004-redis.server.ts
+++ packages/frontend-2/plugins/004-redis.server.ts
@@ -12,27 +12,32 @@ export default defineNuxtPlugin(async () => {
   const { redisUrl } = useRuntimeConfig()
   const logger = useLogger()
 
-  const hasValidStatus =
-    redis && ['ready', 'connecting', 'reconnecting'].includes(redis.status)
-  if (!redis || !hasValidStatus) {
-    if (redis) {
-      await redis.quit()
+  try {
+    const hasValidStatus =
+      redis && ['ready', 'connecting', 'reconnecting'].includes(redis.status)
+    if (!redis || !hasValidStatus) {
+      if (redis) {
+        await redis.quit()
+      }
+
+      redis = new Redis(redisUrl)
+
+      redis.on('error', (err) => {
+        logger.error(err, 'Redis error')
+      })
+
+      redis.on('end', () => {
+        logger.info('Redis disconnected from server')
+      })
     }
-
-    redis = new Redis(redisUrl)
-
-    redis.on('error', (err) => {
-      logger.error(err, 'Redis error')
-    })
-
-    redis.on('end', () => {
-      logger.info('Redis disconnected from server')
-    })
+  } catch (e) {
+    logger.error(e, 'Redis setup failure')
   }
 
+  const isValid = redis && redis.status === 'ready'
   return {
     provide: {
-      redis
+      redis: isValid ? redis : undefined
     }
   }
 })
