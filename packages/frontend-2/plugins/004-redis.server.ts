f3e8bf70456b13c905553cdc280c1961a0e31b99
Kristaps Fabians Geikins <fabis94@live.com>
2024-01-25 11:02:55 +0200

    chore(fe2): more redis logging

--- packages/frontend-2/plugins/004-redis.server.ts
+++ packages/frontend-2/plugins/004-redis.server.ts
@@ -8,10 +8,26 @@ let redis: InstanceType<typeof Redis> | undefined = undefined
 /**
  * Provide redis (only in SSR)
  */
-export default defineNuxtPlugin(() => {
+export default defineNuxtPlugin(async () => {
   const { redisUrl } = useRuntimeConfig()
-  if (!redis) {
-    redis = redisUrl?.length ? new Redis(redisUrl) : undefined
+  const logger = useLogger()
+
+  const hasValidStatus =
+    redis && ['ready', 'connecting', 'reconnecting'].includes(redis.status)
+  if (!redis || !hasValidStatus) {
+    if (redis) {
+      await redis.quit()
+    }
+
+    redis = new Redis(redisUrl)
+
+    redis.on('error', (err) => {
+      logger.error(err, 'Redis error')
+    })
+
+    redis.on('end', () => {
+      logger.info('Redis disconnected from server')
+    })
   }
 
   return {
