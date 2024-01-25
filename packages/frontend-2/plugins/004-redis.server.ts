ff5c965df9ad97754c4ac9aae1daa8128a9ad89f
Kristaps Fabians Geikins <fabis94@live.com>
2024-01-25 10:50:04 +0200

    fix(fe2): redis connection leakage

--- packages/frontend-2/plugins/004-redis.server.ts
+++ packages/frontend-2/plugins/004-redis.server.ts
@@ -1,11 +1,18 @@
 import { Redis } from 'ioredis'
 
 /**
+ * Re-using the same client for all SSR reqs (shouldn't be a problem)
+ */
+let redis: InstanceType<typeof Redis> | undefined = undefined
+
+/**
  * Provide redis (only in SSR)
  */
 export default defineNuxtPlugin(() => {
   const { redisUrl } = useRuntimeConfig()
-  const redis = redisUrl?.length ? new Redis(redisUrl) : undefined
+  if (!redis) {
+    redis = redisUrl?.length ? new Redis(redisUrl) : undefined
+  }
 
   return {
     provide: {
