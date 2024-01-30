6c311113d53518da61eb29b82b6c8e45783ec49b
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2024-01-30 11:44:18 +0000

    Remove nesting of ManualLoad

--- packages/frontend-2/components/viewer/embed/ManualLoad.vue
+++ packages/frontend-2/components/viewer/embed/ManualLoad.vue
@@ -1,18 +1,20 @@
 <template>
-  <button
-    class="group flex items-center justify-center absolute inset-0"
-    @click="$emit('play')"
-  >
-    <div v-if="previewUrl" class="absolute inset-0">
-      <PreviewImage :preview-url="previewUrl" />
-    </div>
-    <div
-      class="relative z-10 pointer-events-none group-hover:scale-110 group-hover:shadow-xl shadow h-14 w-14 rounded-full border border-foundation bg-primary flex items-center justify-center transition -mt-10"
+  <div>
+    <button
+      class="group flex items-center justify-center absolute inset-0"
+      @click="$emit('play')"
     >
-      <PlayIcon class="h-6 w-6 ml-[3px] text-foundation" />
-    </div>
+      <div v-if="previewUrl" class="absolute inset-0">
+        <PreviewImage :preview-url="previewUrl" />
+      </div>
+      <div
+        class="relative z-10 pointer-events-none group-hover:scale-110 group-hover:shadow-xl shadow h-14 w-14 rounded-full border border-foundation bg-primary flex items-center justify-center transition -mt-10"
+      >
+        <PlayIcon class="h-6 w-6 ml-[3px] text-foundation" />
+      </div>
+    </button>
     <ViewerEmbedFooter :url="projectUrl" name="View in Speckle" />
-  </button>
+  </div>
 </template>
 
 <script setup lang="ts">
