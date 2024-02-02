eae121396d7c44bece2cbe47cddfeced6547eeb1
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2024-02-02 16:50:53 +0000

    Fix webhook being in Settings dialog

--- packages/frontend-2/components/project/page/stats-block/Settings.vue
+++ packages/frontend-2/components/project/page/stats-block/Settings.vue
@@ -4,7 +4,7 @@
       <div class="flex items-center">
         <div class="flex items-center justify-between w-full">
           <div
-            v-if="isLoggedIn"
+            v-if="activeUser"
             class="flex items-center gap-0.5 flex-grow select-none"
           >
             <Cog6ToothIcon class="h-5 w-5" />
@@ -23,7 +23,7 @@
     <template #bottom>
       <div class="flex items-center justify-between mt-3">
         <UserAvatarGroup :users="teamUsers" class="max-w-[104px]" />
-        <div v-if="isLoggedIn">
+        <div v-if="activeUser">
           <FormButton class="ml-2" @click="dialogOpen = true">
             {{ project.role === 'stream:owner' ? 'Manage' : 'View' }}
           </FormButton>
@@ -40,7 +40,7 @@ import { Cog6ToothIcon } from '@heroicons/vue/24/outline'
 import { useActiveUser } from '~~/lib/auth/composables/activeUser'
 import { graphql } from '~~/lib/common/generated/gql'
 import type { ProjectPageStatsBlockTeamFragment } from '~~/lib/common/generated/gql/graphql'
-const { isLoggedIn } = useActiveUser()
+const { activeUser } = useActiveUser()
 
 graphql(`
   fragment ProjectPageStatsBlockTeam on Project {
