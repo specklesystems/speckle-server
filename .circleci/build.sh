9658b17966826c23537764e52f8da3c97fa4e1d7
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2024-02-03 17:56:09 +0000

    chore(circleci): build a multiarch image

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -13,7 +13,6 @@ echo "Building image: ${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}"
 
 export DOCKER_BUILDKIT=1
 
-docker build --build-arg SPECKLE_SERVER_VERSION="${IMAGE_VERSION_TAG}" --tag "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}" --file "${FOLDER}/${SPECKLE_SERVER_PACKAGE}/Dockerfile" .
-
+docker buildx build --build-arg SPECKLE_SERVER_VERSION="${IMAGE_VERSION_TAG}" --tag "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}" --file "${FOLDER}/${SPECKLE_SERVER_PACKAGE}/Dockerfile" --platform "linux/arm64/v8,darwin/arm64" .
 echo " Saving image: ${DOCKER_FILE_NAME}"
 docker save --output "/tmp/ci/workspace/${DOCKER_FILE_NAME}" "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}"
