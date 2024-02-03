2813b8b5c6dff8770a65165166e1bad386087ee7
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2024-02-03 18:05:52 +0000

    Use a docker-container driver

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -13,6 +13,15 @@ echo "Building image: ${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}"
 
 export DOCKER_BUILDKIT=1
 
-docker buildx build --build-arg SPECKLE_SERVER_VERSION="${IMAGE_VERSION_TAG}" --tag "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}" --file "${FOLDER}/${SPECKLE_SERVER_PACKAGE}/Dockerfile" --platform "linux/arm64/v8,darwin/arm64" .
+docker buildx create \
+  --name container \
+  --driver docker-container
+docker buildx build \
+  --builder container \
+  --build-arg SPECKLE_SERVER_VERSION="${IMAGE_VERSION_TAG}" \
+  --tag "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}" \
+  --file "${FOLDER}/${SPECKLE_SERVER_PACKAGE}/Dockerfile" \
+  --platform "linux/arm64/v8,darwin/arm64" \
+  .
 echo " Saving image: ${DOCKER_FILE_NAME}"
 docker save --output "/tmp/ci/workspace/${DOCKER_FILE_NAME}" "${DOCKER_IMAGE_TAG}:${IMAGE_VERSION_TAG}"
