e797c2a3392a6c45fb683337efcd6b6ae208f758
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2024-02-06 18:29:58 +0000

    Add extra condition

--- packages/frontend-2/components/auth/RegisterPanel.vue
+++ packages/frontend-2/components/auth/RegisterPanel.vue
@@ -33,7 +33,7 @@
         />
         <div>
           <div
-            v-if="hasLocalStrategy"
+            v-if="hasThirdPartyStrategies && hasLocalStrategy"
             class="text-center label text-foreground-2 mb-3 text-xs font-normal"
           >
             Or sign up with your email
