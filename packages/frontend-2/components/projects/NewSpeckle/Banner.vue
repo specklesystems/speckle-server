c3f0974729aa87d7450d6d1bd3b0468935a74748
Benjamin Ottensten <benjamin.ottensten@gmail.com>
2024-03-12 13:05:20 +0100

    Update announcement banner
    Updated icons, copy, spacings

--- packages/frontend-2/components/projects/NewSpeckle/Banner.vue
+++ packages/frontend-2/components/projects/NewSpeckle/Banner.vue
@@ -1,26 +1,20 @@
 <template>
   <div>
     <div
-      class="flex flex-col gap-4 justify-between sm:items-center sm:flex-row px-4 py-4 sm:py-2 transition hover:bg-primary-muted"
+      class="flex flex-col gap-3 justify-between sm:items-center sm:flex-row px-4 pt-3 pb-4 sm:py-2 transition hover:bg-primary-muted"
     >
-      <div class="flex gap-3 items-center">
-        <div class="bg-primary rounded h-8 w-8 flex items-center justify-center">
-          <SpeakerWaveIcon class="h-5 w-5 text-white" />
+      <div class="flex gap-2 items-center">
+        <div class="h-8 w-8 flex items-center justify-center">
+          <SparklesIcon class="h-5 w-5 text-primary" />
         </div>
-        <div class="text-foreground text-sm">New Speckle Experience</div>
+        <div class="text-foreground text-sm">Announcing the New Speckle Web App</div>
       </div>
-      <div class="flex gap-2">
+      <div class="flex gap-3">
         <FormButton size="sm" color="default" text @click="emitDismissedOrOpened">
           Skip
         </FormButton>
-        <FormButton
-          size="sm"
-          class="px-4"
-          :icon-right="SparklesIcon"
-          to="/"
-          @click="showDialog = true"
-        >
-          Show me
+        <FormButton size="sm" class="px-4" to="/" @click="showDialog = true">
+          See What's New
         </FormButton>
       </div>
     </div>
@@ -33,7 +27,6 @@
 
 <script setup lang="ts">
 import { SparklesIcon } from '@heroicons/vue/24/solid'
-import { SpeakerWaveIcon } from '@heroicons/vue/24/outline'
 
 const emit = defineEmits<{ 'new-speckle-dismissed-or-opened': [] }>()
 
