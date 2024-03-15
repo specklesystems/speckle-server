93401a4784d1246e83c1e2383e7db08e0cc7d8a5
Gergő Jedlicska <gergo@jedlicska.com>
2024-03-15 10:03:12 +0100

    feat: add automate module with feature flag

--- packages/server/modules/automate/index.ts
+++ packages/server/modules/automate/index.ts
@@ -1,8 +1,10 @@
 import { moduleLogger } from '@/logging/logging'
 import { SpeckleModule } from '@/modules/shared/helpers/typeHelper'
+import { ENABLE_AUTOMATE_MODULE } from '../shared/helpers/envHelper'
 
 const automateModule: SpeckleModule = {
   async init() {
+    if (!ENABLE_AUTOMATE_MODULE) return
     moduleLogger.info('⚙️ Init automate module')
   }
 }
