2511a136e28cc3de92d4773791239e928132797c
Kristaps Fabians Geikins <fabis94@live.com>
2024-03-21 11:27:42 +0200

    a few viewer bugfixes

--- packages/frontend-2/components/viewer/sun/Menu.vue
+++ packages/frontend-2/components/viewer/sun/Menu.vue
@@ -1,90 +1,92 @@
 <template>
-  <Popover as="div" class="relative z-30">
-    <PopoverButton v-slot="{ open }" as="template">
-      <ViewerControlsButtonToggle flat secondary :active="open">
-        <SunIcon class="w-5 h-5" />
-      </ViewerControlsButtonToggle>
-    </PopoverButton>
-    <Transition
-      enter-active-class="transform ease-out duration-300 transition"
-      enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-      enter-to-class="translate-y-0 opacity-100 sm:translate-x-0"
-      leave-active-class="transition ease-in duration-100"
-      leave-from-class="opacity-100"
-      leave-to-class="opacity-0"
-    >
-      <PopoverPanel
-        class="absolute translate-x-0 left-10 sm:left-12 top-2 bg-foundation max-h-64 simple-scrollbar overflow-y-auto outline outline-2 outline-primary-muted rounded-lg shadow-lg overflow-hidden flex flex-col space-y-2"
+  <div class="relative z-30">
+    <Popover as="div" class="relative z-30">
+      <PopoverButton v-slot="{ open }" as="template">
+        <ViewerControlsButtonToggle flat secondary :active="open">
+          <SunIcon class="w-5 h-5" />
+        </ViewerControlsButtonToggle>
+      </PopoverButton>
+      <Transition
+        enter-active-class="transform ease-out duration-300 transition"
+        enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+        enter-to-class="translate-y-0 opacity-100 sm:translate-x-0"
+        leave-active-class="transition ease-in duration-100"
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0"
       >
-        <div class="p-2 border-b border-outline flex gap-2 items-center">
-          <div class="scale-90">
-            <FormSwitch v-model="sunlightShadows" />
+        <PopoverPanel
+          class="absolute translate-x-0 left-10 sm:left-12 top-2 bg-foundation max-h-64 simple-scrollbar overflow-y-auto outline outline-2 outline-primary-muted rounded-lg shadow-lg overflow-hidden flex flex-col space-y-2"
+        >
+          <div class="p-2 border-b border-outline flex gap-2 items-center">
+            <div class="scale-90">
+              <FormSwitch v-model="sunlightShadows" />
+            </div>
+            <span class="text-foreground text-sm">Sun Shadows</span>
           </div>
-          <span class="text-foreground text-sm">Sun Shadows</span>
-        </div>
-        <div class="flex items-center gap-1 px-2">
-          <input
-            id="intensity"
-            v-model="intensity"
-            class="w-24 sm:w-32 h-2 mr-2"
-            type="range"
-            name="intensity"
-            min="1"
-            max="10"
-            step="0.05"
-          />
-          <label class="text-xs sm:text-sm text-foreground-2" for="intensity">
-            Intensity
-          </label>
-        </div>
-        <div class="flex items-center gap-1 px-2">
-          <input
-            id="elevation"
-            v-model="elevation"
-            class="w-24 sm:w-32 h-2 mr-2"
-            type="range"
-            name="elevation"
-            min="0"
-            :max="Math.PI"
-            step="0.05"
-          />
-          <label class="text-xs sm:text-sm text-foreground-2" for="elevation">
-            Elevation
-          </label>
-        </div>
-        <div class="flex items-center gap-1 px-2">
-          <input
-            id="azimuth"
-            v-model="azimuth"
-            class="w-24 sm:w-32 h-2 mr-2"
-            type="range"
-            name="azimuth"
-            :min="-Math.PI * 0.5"
-            :max="Math.PI * 0.5"
-            step="0.05"
-          />
-          <label class="text-xs sm:text-sm text-foreground-2" for="azimuth">
-            Azimuth
-          </label>
-        </div>
-        <div class="flex items-center gap-1 px-2 pb-2">
-          <input
-            id="indirect"
-            v-model="indirectLightIntensity"
-            class="w-24 sm:w-32 h-2 mr-2"
-            type="range"
-            name="indirect"
-            min="0"
-            max="5"
-            step="0.05"
-          />
-          <label class="text-xs sm:text-sm text-foreground-2" for="indirect">
-            Indirect
-          </label>
-        </div>
-      </PopoverPanel>
-    </Transition>
-  </Popover>
+          <div class="flex items-center gap-1 px-2">
+            <input
+              id="intensity"
+              v-model="intensity"
+              class="w-24 sm:w-32 h-2 mr-2"
+              type="range"
+              name="intensity"
+              min="1"
+              max="10"
+              step="0.05"
+            />
+            <label class="text-xs sm:text-sm text-foreground-2" for="intensity">
+              Intensity
+            </label>
+          </div>
+          <div class="flex items-center gap-1 px-2">
+            <input
+              id="elevation"
+              v-model="elevation"
+              class="w-24 sm:w-32 h-2 mr-2"
+              type="range"
+              name="elevation"
+              min="0"
+              :max="Math.PI"
+              step="0.05"
+            />
+            <label class="text-xs sm:text-sm text-foreground-2" for="elevation">
+              Elevation
+            </label>
+          </div>
+          <div class="flex items-center gap-1 px-2">
+            <input
+              id="azimuth"
+              v-model="azimuth"
+              class="w-24 sm:w-32 h-2 mr-2"
+              type="range"
+              name="azimuth"
+              :min="-Math.PI * 0.5"
+              :max="Math.PI * 0.5"
+              step="0.05"
+            />
+            <label class="text-xs sm:text-sm text-foreground-2" for="azimuth">
+              Azimuth
+            </label>
+          </div>
+          <div class="flex items-center gap-1 px-2 pb-2">
+            <input
+              id="indirect"
+              v-model="indirectLightIntensity"
+              class="w-24 sm:w-32 h-2 mr-2"
+              type="range"
+              name="indirect"
+              min="0"
+              max="5"
+              step="0.05"
+            />
+            <label class="text-xs sm:text-sm text-foreground-2" for="indirect">
+              Indirect
+            </label>
+          </div>
+        </PopoverPanel>
+      </Transition>
+    </Popover>
+  </div>
 </template>
 <script setup lang="ts">
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
