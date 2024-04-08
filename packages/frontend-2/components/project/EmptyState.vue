debf5373369c0f71f48b5385b96e8a5812db7191
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2024-04-08 12:22:21 +0100

    Remove animation

--- packages/frontend-2/components/project/EmptyState.vue
+++ packages/frontend-2/components/project/EmptyState.vue
@@ -1,15 +1,10 @@
 <template>
-  <div class="flex flex-col items-center max-w-[180px] mx-auto">
+  <div class="flex flex-col gap-1 items-center max-w-[180px] mx-auto">
     <div
       class="relative text-foreground-2 h-32 w-full"
       :class="small ? 'scale-75' : ''"
     >
-      <div class="svg-downward absolute right-0 bottom-0">
-        <slot name="bottom-svg"></slot>
-      </div>
-      <div class="svg-upward absolute left-0 bottom-0 z-10">
-        <slot name="top-svg"></slot>
-      </div>
+      <slot name="image"></slot>
     </div>
     <div class="flex flex-col gap-1 items-center text-center">
       <span class="text-foreground font-bold">{{ title }}</span>
@@ -26,34 +21,3 @@ defineProps<{
   text?: string
 }>()
 </script>
-<style scoped>
-@keyframes move-up {
-  0%,
-  100% {
-    transform: translateY(0);
-  }
-
-  50% {
-    transform: translateY(-5px);
-  }
-}
-
-@keyframes move-down {
-  0%,
-  100% {
-    transform: translateY(0);
-  }
-
-  50% {
-    transform: translateY(5px);
-  }
-}
-
-.svg-upward {
-  animation: move-up 4s linear infinite;
-}
-
-.svg-downward {
-  animation: move-down 4s linear infinite;
-}
-</style>
