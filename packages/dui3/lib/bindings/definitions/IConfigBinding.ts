3697fa0b629896f83740fd42d1b97744575cf7bb
Dimitrie Stefanescu <didimitrie@gmail.com>
2024-04-16 10:35:34 +0100

    feat(dui3): some quick config changes/simplifying things

--- packages/dui3/lib/bindings/definitions/IConfigBinding.ts
+++ packages/dui3/lib/bindings/definitions/IConfigBinding.ts
@@ -1,7 +1,5 @@
-/* eslint-disable @typescript-eslint/require-await */
-
 import { BaseBridge } from '~/lib/bridge/base'
-import { IBinding } from '~~/lib/bindings/definitions/IBinding'
+import { IBinding } from '~/lib/bindings/definitions/IBinding'
 
 /**
  * The name under which this binding will be registered.
@@ -12,109 +10,15 @@ export const IConfigBindingKey = 'configBinding'
  * A test binding interface to ensure compatbility. Ideally all host environments would implement and register it.
  */
 export interface IConfigBinding extends IBinding<IConfigBindingEvents> {
-  getConfig: () => Promise<UiConfig>
-  updateGlobalConfig: (config: GlobalConfig) => Promise<void>
-  updateConnectorConfig: (config: ConnectorConfig) => Promise<void>
+  getConfig: () => Promise<ConnectorConfig>
+  updateConfig: (config: ConnectorConfig) => void
 }
 
-export interface IConfigBindingEvents {
-  void: () => void
-}
-
-export type UiConfigOld = {
-  hostApp: string
-  darkTheme: boolean
-  onboardingCompleted: boolean
-}
-
-export type UiConfig = {
-  global: GlobalConfig
-  connectors: ConnectorConfigDictionary
-}
-
-export type ConnectorConfigDictionary = {
-  [hostAppName: string]: ConnectorConfig
-}
-
-export type GlobalConfig = {
-  onboardingSkipped: boolean
-  onboardings: OnboardingDictionary
-}
+export interface IConfigBindingEvents {}
 
 export type ConnectorConfig = {
-  hostApp: string
   darkTheme: boolean
-  onboardings: OnboardingDictionary
 }
 
-export type OnboardingDictionary = {
-  [id: string]: OnboardingData
-}
-
-export type OnboardingData = {
-  title: string
-  blurb: string
-  completed: boolean
-  page: string
-}
-
-export class MockedConfigBinding extends BaseBridge {
-  constructor() {
-    super()
-  }
-
-  private uiConfig: UiConfig = {
-    global: {
-      onboardingSkipped: false,
-      onboardings: {
-        send: {
-          title: 'Send',
-          blurb: 'Send first model to Speckleverse!',
-          completed: true,
-          page: '/onboarding/send'
-        },
-        receive: {
-          title: 'Receive',
-          blurb: 'Receive first model from Speckleverse!',
-          completed: true,
-          page: '/onboarding/receive'
-        }
-      }
-    },
-    connectors: {
-      mock: {
-        hostApp: 'Mock',
-        darkTheme: false,
-        onboardings: {
-          test: {
-            title: 'Test',
-            blurb: 'Initial Test',
-            completed: true,
-            page: '/onboarding/test'
-          }
-        }
-      }
-    }
-  }
-
-  getConfigOld() {
-    return {
-      darkTheme: false,
-      onboardingCompleted: false
-    }
-  }
-
-  getConfig() {
-    return this.uiConfig
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  updateGlobalConfig(globalConfig: GlobalConfig) {
-    this.uiConfig.global = globalConfig
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  updateConnectorConfig(connectorConfig: ConnectorConfig) {
-    this.uiConfig.connectors[connectorConfig.hostApp.toLowerCase()] = connectorConfig
-  }
-}
+// Useless, but will do for now :)
+export class MockedConfigBinding extends BaseBridge {}
