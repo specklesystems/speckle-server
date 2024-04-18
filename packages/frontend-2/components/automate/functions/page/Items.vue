0e7a5f4acca8643e7c4c1f60c211e5a7a80fbd90
Kristaps Fabians Geikins <fabis94@live.com>
2024-04-18 15:21:33 +0300

    create automation from fns page

--- packages/frontend-2/components/automate/functions/page/Items.vue
+++ packages/frontend-2/components/automate/functions/page/Items.vue
@@ -1,21 +1,32 @@
 <template>
   <div>
     <AutomateFunctionCardView>
-      <AutomateFunctionCard v-for="fn in fns" :key="fn.id" :fn="fn" />
+      <AutomateFunctionCard
+        v-for="fn in fns"
+        :key="fn.id"
+        :fn="fn"
+        @use="() => $emit('createAutomationFrom', fn)"
+      />
     </AutomateFunctionCardView>
   </div>
 </template>
 <script setup lang="ts">
 import { graphql } from '~/lib/common/generated/gql'
 import type { AutomateFunctionsPageItems_QueryFragment } from '~/lib/common/generated/gql/graphql'
+import type { CreateAutomationSelectableFunction } from '~/lib/automate/helpers/automations'
 
 // TODO: Pagination
+
+defineEmits<{
+  createAutomationFrom: [fn: CreateAutomationSelectableFunction]
+}>()
 
 graphql(`
   fragment AutomateFunctionsPageItems_Query on Query {
     automateFunctions(limit: 21, filter: { search: $search }) {
       items {
         ...AutomationsFunctionsCard_AutomateFunction
+        ...AutomateAutomationCreateDialog_AutomateFunction
       }
     }
   }
--- packages/frontend-2/pages/functions/index.vue
+++ packages/frontend-2/pages/functions/index.vue
@@ -7,12 +7,21 @@
       class="mb-8"
     />
     <CommonLoadingBar :loading="loading" />
-    <AutomateFunctionsPageItems v-if="!loading" :functions="result" />
+    <AutomateFunctionsPageItems
+      v-if="!loading"
+      :functions="result"
+      @create-automation-from="openCreateNewAutomation"
+    />
+    <AutomateAutomationCreateDialog
+      v-model:open="showNewAutomationDialog"
+      :preselected-function="newAutomationTargetFn"
+    />
   </div>
 </template>
 <script setup lang="ts">
 import { CommonLoadingBar } from '@speckle/ui-components'
 import { useQuery, useQueryLoading } from '@vue/apollo-composable'
+import type { CreateAutomationSelectableFunction } from '~/lib/automate/helpers/automations'
 import { graphql } from '~/lib/common/generated/gql'
 
 // TODO: Proper search & pagination
@@ -29,4 +38,12 @@ const loading = useQueryLoading()
 const { result } = useQuery(pageQuery, () => ({
   search: search.value?.length ? search.value : null
 }))
+
+const showNewAutomationDialog = ref(false)
+const newAutomationTargetFn = ref<CreateAutomationSelectableFunction>()
+
+const openCreateNewAutomation = (fn: CreateAutomationSelectableFunction) => {
+  newAutomationTargetFn.value = fn
+  showNewAutomationDialog.value = true
+}
 </script>
