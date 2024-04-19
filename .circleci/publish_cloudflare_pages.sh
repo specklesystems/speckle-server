568ba9036472cab15c7e13dd0588304053899e5a
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2024-04-19 15:19:45 +0100

    chore(viewer sandbox): use CircleCI to publish to Cloudflare pages

---
+++ .circleci/publish_cloudflare_pages.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+GIT_ROOT=$( git rev-parse --show-toplevel )
+
+CLOUDFLARE_PAGES_PROJECT_NAME="${CLOUDFLARE_PAGES_PROJECT_NAME:-"viewer"}"
+CLOUDFLARE_PAGES_DIST_DIR="${CLOUDFLARE_PAGES_DIST_DIR:-"${GIT_ROOT}/packages/viewer-sandbox/dist"}"
+
+wrangler pages publish "${GIT_ROOT}/${CLOUDFLARE_PAGES_DIST_DIR}" --project-name="${CLOUDFLARE_PAGES_PROJECT_NAME}"
