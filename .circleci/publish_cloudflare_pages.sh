c02159d5946de953448bb13ee2ac02bf07a0f42a
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2024-04-19 16:36:21 +0100

    fix(circleci): install wrangler as a dev dependency

--- .circleci/publish_cloudflare_pages.sh
+++ .circleci/publish_cloudflare_pages.sh
@@ -4,6 +4,8 @@ set -eo pipefail
 GIT_ROOT=$( git rev-parse --show-toplevel )
 
 CLOUDFLARE_PAGES_PROJECT_NAME="${CLOUDFLARE_PAGES_PROJECT_NAME:-"viewer"}"
-CLOUDFLARE_PAGES_DIST_DIR="${CLOUDFLARE_PAGES_DIST_DIR:-"${GIT_ROOT}/packages/viewer-sandbox/dist"}"
+VIEWER_SANDBOX_DIR_PATH="${VIEWER_SANDBOX_DIR_PATH:-"${GIT_ROOT}/packages/viewer-sandbox/dist"}"
 
-npx wrangler pages publish "${GIT_ROOT}/${CLOUDFLARE_PAGES_DIST_DIR}" --project-name="${CLOUDFLARE_PAGES_PROJECT_NAME}"
+pushd "${GIT_ROOT}/${VIEWER_SANDBOX_DIR_PATH}"
+yarn wrangler pages publish "${GIT_ROOT}/${VIEWER_SANDBOX_DIR_PATH}/dist" --project-name="${CLOUDFLARE_PAGES_PROJECT_NAME}"
+popd
