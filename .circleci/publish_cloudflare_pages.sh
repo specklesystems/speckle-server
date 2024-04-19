78f23e862dfc5dc2a53ae68b1f5365d8cc8a6b5a
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2024-04-19 16:39:20 +0100

    fix path

--- .circleci/publish_cloudflare_pages.sh
+++ .circleci/publish_cloudflare_pages.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 GIT_ROOT=$( git rev-parse --show-toplevel )
 
 CLOUDFLARE_PAGES_PROJECT_NAME="${CLOUDFLARE_PAGES_PROJECT_NAME:-"viewer"}"
-VIEWER_SANDBOX_DIR_PATH="${VIEWER_SANDBOX_DIR_PATH:-"${GIT_ROOT}/packages/viewer-sandbox/dist"}"
+VIEWER_SANDBOX_DIR_PATH="${VIEWER_SANDBOX_DIR_PATH:-"packages/viewer-sandbox"}"
 
 pushd "${GIT_ROOT}/${VIEWER_SANDBOX_DIR_PATH}"
 yarn wrangler pages publish "${GIT_ROOT}/${VIEWER_SANDBOX_DIR_PATH}/dist" --project-name="${CLOUDFLARE_PAGES_PROJECT_NAME}"
