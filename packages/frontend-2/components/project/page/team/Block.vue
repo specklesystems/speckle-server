510fb68552a89c37cc44e93ca41af4f44a535820
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2024-05-08 11:45:01 +0200

    WEB-877 update-collaborators-block

--- packages/frontend-2/components/project/page/team/Block.vue
+++ packages/frontend-2/components/project/page/team/Block.vue
@@ -6,7 +6,7 @@
           <div class="flex items-center gap-1 flex-grow select-none">
             <span class="text-foreground text-sm font-semibold">Collaborators</span>
           </div>
-          <div class="flex items-center text-sm">
+          <div class="flex items-center text-sm capitalize">
             {{ project.role?.split(':').reverse()[0] }}
           </div>
         </div>
@@ -16,27 +16,18 @@
       <div class="flex items-center justify-between mt-1">
         <UserAvatarGroup :users="teamUsers" class="max-w-[104px]" />
         <div v-if="activeUser">
-          <FormButton class="ml-2" :icon-left="UserPlusIcon" @click="onButtonClick">
-            Invite
+          <FormButton class="ml-2" :to="projectCollaboratorsRoute(project.id)">
+            Manage
           </FormButton>
         </div>
       </div>
     </template>
-    <template #default>
-      <ProjectPageInviteDialog
-        v-model:open="dialogOpen"
-        :project="project"
-        :project-id="project.id"
-        :disabled="!isOwner"
-      />
-    </template>
   </ProjectPageStatsBlock>
 </template>
 <script setup lang="ts">
-import { UserPlusIcon } from '@heroicons/vue/24/outline'
-import { useTeamInternals } from '~/lib/projects/composables/team'
 import { useActiveUser } from '~~/lib/auth/composables/activeUser'
 import type { ProjectPageTeamInternals_ProjectFragment } from '~~/lib/common/generated/gql/graphql'
+import { projectCollaboratorsRoute } from '~~/lib/common/helpers/route'
 
 const props = defineProps<{
   project: ProjectPageTeamInternals_ProjectFragment
@@ -44,15 +35,5 @@ const props = defineProps<{
 
 const { activeUser } = useActiveUser()
 
-const projectData = computed(() => props.project)
-
-const { isOwner } = useTeamInternals(projectData)
-
-const dialogOpen = ref(false)
-
 const teamUsers = computed(() => props.project.team.map((t) => t.user))
-
-const onButtonClick = () => {
-  dialogOpen.value = true
-}
 </script>
