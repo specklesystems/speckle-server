7000d7653f137c39e5ca9c7d2b0503b68bf4e42e
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2024-05-10 15:03:34 +0200

    Use showbutton prop instead of useCheckViewerCommentingAccess

--- packages/frontend-2/components/project/page/latest-items/comments/EmptyState.vue
+++ packages/frontend-2/components/project/page/latest-items/comments/EmptyState.vue
@@ -35,7 +35,7 @@
       </svg>
     </template>
     <template #cta>
-      <div v-if="small && canPostComment" class="mt-3">
+      <div v-if="showButton" class="mt-3">
         <FormButton
           size="sm"
           :icon-left="PlusIcon"
@@ -49,7 +49,6 @@
 </template>
 <script setup lang="ts">
 import { PlusIcon } from '@heroicons/vue/24/solid'
-import { useCheckViewerCommentingAccess } from '~/lib/viewer/composables/commentManagement'
 
 defineEmits<{
   (e: 'new-discussion'): void
@@ -57,7 +56,6 @@ defineEmits<{
 
 defineProps<{
   small?: boolean
+  showButton?: boolean
 }>()
-
-const canPostComment = useCheckViewerCommentingAccess()
 </script>
