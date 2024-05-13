68e38cc844f914571e91f741ad69bbe46b7364cc
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2024-05-13 13:37:15 +0200

    Make project & model headers non-editable

---
+++ packages/frontend-2/components/common/TitleDescription.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col gap-2 sm:pr-8">
+    <h2 class="h3">{{ title }}</h2>
+    <p v-if="description" class="text-foreground-2">{{ description }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Nullable } from '@speckle/shared'
+
+defineProps<{
+  title: string
+  description?: Nullable<string>
+}>()
+</script>
