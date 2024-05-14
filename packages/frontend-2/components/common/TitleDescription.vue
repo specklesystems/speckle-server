106499e89676503b6ce3de11dcd160c0d3a30637
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2024-05-14 14:00:30 +0200

    Fixes from PR

--- packages/frontend-2/components/common/TitleDescription.vue
+++ packages/frontend-2/components/common/TitleDescription.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col gap-2 sm:pr-8">
+  <div class="flex flex-col gap-2">
     <h2 class="h3">{{ title }}</h2>
     <p v-if="description" class="text-foreground-2">{{ description }}</p>
   </div>
