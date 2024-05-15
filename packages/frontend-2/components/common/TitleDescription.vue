3f3e5e7e5eeb372b6c8a7a35a2a027d10adcae21
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2024-05-15 10:52:31 +0200

    Add placeholder for description

--- packages/frontend-2/components/common/TitleDescription.vue
+++ packages/frontend-2/components/common/TitleDescription.vue
@@ -2,6 +2,7 @@
   <div class="flex flex-col gap-2">
     <h2 class="h3">{{ title }}</h2>
     <p v-if="description" class="text-foreground-2">{{ description }}</p>
+    <p v-else class="text-foreground-2 italic select-none">No description</p>
   </div>
 </template>
 
