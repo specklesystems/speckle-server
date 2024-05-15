17e489d3d5579d26ecfebd76adf7fa6118b64088
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2024-05-15 14:31:04 +0200

    Update comments

--- packages/frontend-2/middleware/canViewSettings.ts
+++ packages/frontend-2/middleware/canViewSettings.ts
@@ -4,12 +4,12 @@ import { projectRoute } from '~~/lib/common/helpers/route'
 import { projectRoleCheckQuery } from '~~/lib/projects/graphql/queries'
 
 /**
- * Apply this to a page to prevent unauthenticated access to settings and ensure the user is the a collaborator
+ * Apply this to a page to prevent unauthenticated access to settings ensuring the user is a collaborator
  */
 export default defineNuxtRouteMiddleware(async (to) => {
   const client = useApolloClientFromNuxt()
 
-  // Fetch project role data to check if the user is the owner
+  // Fetch project role data to check if the user is a collaborator
   const projectId = to.params.id as string
   const { data } = await client
     .query({
@@ -22,7 +22,7 @@ export default defineNuxtRouteMiddleware(async (to) => {
     return navigateTo(projectRoute(projectId))
   }
 
-  // Check if the user is the owner of the project
+  // Check if the user is a collaborator of the project
   const hasRole = computed(() => data.project.role)
 
   if (!hasRole.value) {
