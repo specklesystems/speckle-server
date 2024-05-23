5ff208a37f04fb76dc7481d765d538d721335682
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2024-05-23 11:09:13 +0200

    Revert "Fix build"
    This reverts commit 9132ddecf3422d05e3776ef46ebed6ef2eec588b.

--- packages/frontend-2/components/viewer/resources/ModelCard.vue
+++ packages/frontend-2/components/viewer/resources/ModelCard.vue
@@ -211,6 +211,10 @@ async function handleVersionChange(versionId: string) {
 
 const onLoadMore = async () => {
   await loadMoreVersions(props.model.id)
+  versions.value = [
+    ...props.model.loadedVersion.items,
+    ...props.model.versions.items
+  ].sort((a, b) => dayjs(b.createdAt).diff(dayjs(a.createdAt)))
 }
 
 async function handleViewChanges(version: ViewerModelVersionCardItemFragment) {
