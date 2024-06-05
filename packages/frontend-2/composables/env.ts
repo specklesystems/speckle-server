7ffd0f7490f50cdad567cd8a94022a64301ea626
Kristaps Fabians Geikins <fabians@speckle.systems>
2024-06-05 13:48:22 +0300

    fix(fe2): using SSR apiOrigin instead of CSR apiOrigin in vue templates (#2326)

--- packages/frontend-2/composables/env.ts
+++ packages/frontend-2/composables/env.ts
@@ -1,3 +1,7 @@
+/**
+ * IMPORTANT: Don't use this directly in Vue templates that may render in SSR, cause this may cause the backend API origin to be rendered instead of the clientside one,
+ * at least until the app finishes hydrating. If people click on links based on this too early, they may end up in the wrong place.
+ */
 export const useApiOrigin = () => {
   const {
     public: { apiOrigin, backendApiOrigin }
