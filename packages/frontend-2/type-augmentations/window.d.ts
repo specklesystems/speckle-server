bd042bd5b339288646a2ff7b8a395f130495b302
Kristaps Fabians Geikins <fabians@speckle.systems>
2024-06-13 12:06:53 +0300

    chore: remove raygun, debugbear etc. code (#2364)

--- packages/frontend-2/type-augmentations/window.d.ts
+++ packages/frontend-2/type-augmentations/window.d.ts
@@ -1,6 +1,5 @@
 declare global {
   interface Window {
-    rg4js?: import('raygun4js').RaygunV2
     DD_RUM?:
       | Pick<import('@datadog/browser-rum').RumGlobal, 'onReady'>
       | import('@datadog/browser-rum').RumGlobal
