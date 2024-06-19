4f592874b7ce4dc8373186eef7b0d89468d3ca6b
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2024-06-19 15:27:23 +0100

    identify type imports

--- packages/server/modules/auth/domain/logic.ts
+++ packages/server/modules/auth/domain/logic.ts
@@ -1,4 +1,4 @@
-import { UserInfo } from '@/modules/auth/domain/types'
+import type { UserInfo } from '@/modules/auth/domain/types'
 
 /* eslint-disable camelcase */
 export function getNameFromUserInfo({
