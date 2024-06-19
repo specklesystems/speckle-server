255f289891374ad382a36c8bf76d7c631e210ef7
Gergő Jedlicska <gergo@jedlicska.com>
2024-06-19 10:59:15 +0200

    feat(serverinvites): extract individual types from repository

--- packages/server/modules/serverinvites/domain/interfaces.ts
+++ packages/server/modules/serverinvites/domain/interfaces.ts
@@ -1,11 +1,11 @@
 import { ServerRoles, StreamRoles } from '@speckle/shared'
-import { UserWithOptionalRole } from '../../core/repositories/users'
-import { ResourceTargets } from '../helpers/inviteHelper'
+import { UserWithOptionalRole } from '@/modules/core/repositories/users'
+import { ResourceTargets } from '@/modules/serverinvites/helpers/inviteHelper'
 import {
   ServerInviteRecord,
   StreamInviteRecord
 } from '@/modules/serverinvites/domain/types'
-import { StreamWithOptionalRole } from '../../core/repositories/streams'
+import { StreamWithOptionalRole } from '@/modules/core/repositories/streams'
 
 export type QueryAllUserStreamInvites = (
   userId: string
@@ -22,51 +22,77 @@ export type FindStreamInvite = (
   args: FindStreamInviteArgs
 ) => Promise<StreamInviteRecord | null>
 
+export type FindUserByTarget = (target: string) => Promise<UserWithOptionalRole | null>
+
+type Invite = {
+  resourceId?: string | null
+  resourceTarget?: typeof ResourceTargets.Streams | null
+}
+
+export type FindResource = (
+  args: Invite
+) => Promise<StreamWithOptionalRole | undefined | null>
+
+type ServerInviteRecordInsertModel = Pick<
+  ServerInviteRecord,
+  | 'id'
+  | 'target'
+  | 'inviterId'
+  | 'message'
+  | 'resourceTarget'
+  | 'resourceId'
+  | 'role'
+  | 'token'
+  | 'serverRole'
+>
+
+export type InsertInviteAndDeleteOld = (
+  args: ServerInviteRecordInsertModel
+) => Promise<number[]>
+
+export type FindServerInvite = (
+  email?: string,
+  token?: string
+) => Promise<ServerInviteRecord | null>
+
+export type QueryAllStreamInvites = (streamId: string) => Promise<StreamInviteRecord[]>
+
+export type DeleteAllStreamInvites = (streamId: string) => Promise<boolean>
+
+export type DeleteServerOnlyInvites = (email?: string) => Promise<number | undefined>
+
+export type UpdateAllInviteTargets = (
+  oldTargets?: string | string[],
+  newTarget?: string
+) => Promise<void>
+
+export type DeleteStreamInvite = (inviteId?: string) => Promise<number | undefined>
+
+export type FindInvite = (inviteId?: string) => Promise<ServerInviteRecord | null>
+
+export type DeleteInvite = (inviteId?: string) => Promise<boolean>
+
+export type DeleteInvitesByTarget = (
+  targets?: string | string[],
+  resourceTarget?: string,
+  resourceId?: string
+) => Promise<boolean>
+
 export interface ServerInvitesRepository {
   queryAllUserStreamInvites: QueryAllUserStreamInvites
-  findStreamInvite: (
-    streamId: string,
-    args: FindStreamInviteArgs
-  ) => Promise<StreamInviteRecord | null>
-  findUserByTarget: (target: string) => Promise<UserWithOptionalRole | null>
-  findResource: (invite: {
-    resourceId?: string | null
-    resourceTarget?: typeof ResourceTargets.Streams | null
-  }) => Promise<StreamWithOptionalRole | undefined | null>
-  insertInviteAndDeleteOld: (
-    invite: Pick<
-      ServerInviteRecord,
-      | 'id'
-      | 'target'
-      | 'inviterId'
-      | 'message'
-      | 'resourceTarget'
-      | 'resourceId'
-      | 'role'
-      | 'token'
-      | 'serverRole'
-    >,
-    alternateTargets: string[]
-  ) => Promise<number[]>
-  findServerInvite: (
-    email?: string,
-    token?: string
-  ) => Promise<ServerInviteRecord | null>
-  queryAllStreamInvites: (streamId: string) => Promise<StreamInviteRecord[]>
-  deleteAllStreamInvites: (streamId: string) => Promise<boolean>
-  deleteServerOnlyInvites: (email?: string) => Promise<number | undefined>
-  updateAllInviteTargets: (
-    oldTargets?: string | string[],
-    newTarget?: string
-  ) => Promise<void>
-  deleteStreamInvite: (inviteId?: string) => Promise<number | undefined>
-  findInvite: (inviteId?: string) => Promise<ServerInviteRecord | null>
-  deleteInvite: (inviteId?: string) => Promise<boolean>
-  deleteInvitesByTarget: (
-    targets?: string | string[],
-    resourceTarget?: string,
-    resourceId?: string
-  ) => Promise<boolean>
+  findStreamInvite: FindStreamInvite
+  findUserByTarget: FindUserByTarget
+  findResource: FindResource
+  insertInviteAndDeleteOld: InsertInviteAndDeleteOld
+  findServerInvite: FindServerInvite
+  queryAllStreamInvites: QueryAllStreamInvites
+  deleteAllStreamInvites: DeleteAllStreamInvites
+  deleteServerOnlyInvites: DeleteServerOnlyInvites
+  updateAllInviteTargets: UpdateAllInviteTargets
+  deleteStreamInvite: DeleteStreamInvite
+  findInvite: FindInvite
+  deleteInvite: DeleteInvite
+  deleteInvitesByTarget: DeleteInvitesByTarget
 }
 
 export interface CreateInviteParams {
