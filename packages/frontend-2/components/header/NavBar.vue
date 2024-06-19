b0f6cbabfc1f1cd8d5c1462a58edc769f24a6d73
andrewwallacespeckle <andrew@speckle.systems>
2024-06-19 13:22:16 +0100

    Revert "TEST COMMIT"
    This reverts commit 1379ba040686a42946ed3412d00d7c5901151b31.

--- packages/frontend-2/components/header/NavBar.vue
+++ packages/frontend-2/components/header/NavBar.vue
@@ -1,5 +1,4 @@
 <template>
-  <!-- Test commit -->
   <div>
     <nav class="fixed z-20 top-0 h-14 bg-foundation shadow hover:shadow-md transition">
       <div class="flex gap-4 items-center justify-between h-full w-screen px-4">
