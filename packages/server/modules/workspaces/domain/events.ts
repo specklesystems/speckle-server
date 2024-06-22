549a38dd5312da63be9e3d99b60634ac79d6882f
Gergő Jedlicska <gergo@jedlicska.com>
2024-06-22 11:32:54 +0200

    feat(workspaces): add domain, graphql and persistent storage dataschema

---
+++ packages/server/modules/workspaces/domain/events.ts
@@ -0,0 +1,11 @@
+export enum WorkspaceEvents {
+  Created = 'created',
+  Deleted = 'deleted',
+  RoleAssigned = 'roleAssigned',
+  RoleRemoved = 'roleRemoved',
+  ProjectAdded = 'projectAdded',
+  ProjectRemoved = 'projectRemoved'
+}
+
+// export type WorkspaceEventsPayloads = {
+// }
