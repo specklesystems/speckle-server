4486ea6066d31c1a40f009aed21f87ac0fd5df35
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2024-06-24 22:11:34 +0100

    port utils/env.js to typescript

---
+++ packages/preview-service/utils/env.ts
@@ -0,0 +1,17 @@
+export const getAppPort = () => process.env.PORT || '3001'
+export const getChromiumExecutablePath = () =>
+  process.env.CHROMIUM_EXECUTABLE_PATH || '/usr/bin/google-chrome-stable'
+export const getHost = () => process.env.HOST || '127.0.0.1'
+export const getLogLevel = () => process.env.LOG_LEVEL || 'info'
+export const getMetricsPort = () => Number(process.env.PROMETHEUS_METRICS_PORT) || 9094
+export const getNodeEnv = () => process.env.NODE_ENV || 'production'
+export const getPostgresConnectionString = () =>
+  process.env.PG_CONNECTION_STRING || 'postgres://speckle:speckle@127.0.0.1/speckle'
+export const getPuppeteerUserDataDir = () =>
+  process.env.USER_DATA_DIR || '/tmp/puppeteer'
+export const isDevelopment = () => getNodeEnv() === 'development'
+export const isLogPretty = () => process.env.LOG_PRETTY === 'true'
+export const isProduction = () => getNodeEnv() === 'production'
+export const isTest = () => getNodeEnv() === 'test'
+export const serviceUrl = () => `http://${getHost()}:${getAppPort()}`
+export const shouldBeHeadless = () => process.env.PREVIEWS_HEADED !== 'true'
