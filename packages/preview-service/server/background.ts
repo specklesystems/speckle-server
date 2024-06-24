44b937d1f30565f46bb64917fe7500fce383f606
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2024-06-24 16:38:25 +0100

    port background.js to typescript

--- packages/preview-service/server/background.ts
+++ packages/preview-service/server/background.ts
@@ -2,13 +2,11 @@
  * @fileoverview Background service for preview service. This service is responsible for generating 360 previews for objects.
  */
 //FIXME this doesn't quite fit in the /server directory, but it's not a service either. It's a background worker.
-'use strict'
+import { initPrometheusMetrics } from '../observability/prometheusMetrics'
+import { logger } from '../observability/logging'
+import { repeatedlyPollForWork, forceExit } from '../services/taskManager'
 
-const { initPrometheusMetrics } = require('../observability/prometheusMetrics')
-const { logger } = require('../observability/logging')
-const { repeatedlyPollForWork, forceExit } = require('../services/taskManager')
-
-async function startPreviewService() {
+export async function startPreviewService() {
   logger.info('📸 Started Preview Service background worker')
 
   process.on('SIGTERM', () => {
@@ -24,5 +22,3 @@ async function startPreviewService() {
   initPrometheusMetrics()
   repeatedlyPollForWork()
 }
-
-module.exports = { startPreviewService }
