dafecd65bb7707fc557138addd9c472304a8eb78
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2024-06-24 16:44:48 +0100

    port clients/puppeteer.js to typescript

--- packages/preview-service/clients/puppeteer.ts
+++ packages/preview-service/clients/puppeteer.ts
@@ -1,8 +1,9 @@
-const puppeteer = require('puppeteer')
-const { isDevelopment } = require('../utils/env')
-const { extendLoggerComponent } = require('../observability/logging')
+import puppeteer, { Browser } from 'puppeteer'
+import { isDevelopment } from '../utils/env'
+import { extendLoggerComponent } from '../observability/logging'
 
-class PuppeteerClient {
+export class PuppeteerClient {
+  browser: Browser | null
   constructor() {
     this.browser = null
   }
@@ -13,11 +14,16 @@ class PuppeteerClient {
     this.browser = await puppeteer.launch(launchParams)
   }
 
-  async loadPageAndEvaluateScript(logger, url, script, ...args) {
+  async loadPageAndEvaluateScript<T>(logger, url, script, ...args): Promise<T> {
     const boundLogger = extendLoggerComponent(
       logger.child({ renderPageUrl: url }),
       'puppeteer'
     )
+    if (!this.browser) {
+      const errorMessage = 'Browser must be initialized using init() before use.'
+      boundLogger.error(errorMessage)
+      throw new Error(errorMessage)
+    }
     boundLogger.info('Loading page from {renderPageUrl}')
     const page = await this.browser.newPage()
 
@@ -47,19 +53,19 @@ class PuppeteerClient {
         boundLogger.info(`${response.status()} ${response.url()}`)
       )
       .on('requestfailed', (request) =>
-        boundLogger.error(`${request.failure().errorText} ${request.url()}`)
+        boundLogger.error(`${request.failure()?.errorText} ${request.url()}`)
       )
-    const evaluationResult = await page.evaluate(script, args)
+
+    //TODO add timeout for page load
+    //TODO parse the response and ensure it's type of T
+    const evaluationResult: T = await page.evaluate(script, args)
 
     boundLogger.info('Page evaluated with Puppeteer script.')
     return evaluationResult
   }
 
   close() {
+    if (!this.browser) return
     this.browser.close()
   }
-}
-
-module.exports = {
-  PuppeteerClient
 }
