c22d94c6da13a7897e567d7b0d1da4fb346b82fa
Alessandro Magionami <alessandro.magionami@gmail.com>
2024-07-03 11:21:58 +0200

    feat(user-emails): create user emails table and type

---
+++ packages/server/modules/user-emails/migrations/20240703084247_user-emails.ts
@@ -0,0 +1,25 @@
+import { Knex } from 'knex'
+
+const USER_EMAILS_TABLE_NAME = 'user_emails'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable(USER_EMAILS_TABLE_NAME, (table) => {
+    table.string('id').notNullable().primary()
+    table.string('email').notNullable()
+    table.boolean('primary').defaultTo(false)
+    table.boolean('verified').defaultTo(false)
+    table.string('userId').notNullable().references('id').inTable('users')
+    table
+      .timestamp('createdAt', { precision: 3, useTz: true })
+      .defaultTo(knex.fn.now())
+      .notNullable()
+    table
+      .timestamp('updatedAt', { precision: 3, useTz: true })
+      .defaultTo(knex.fn.now())
+      .notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists(USER_EMAILS_TABLE_NAME)
+}
