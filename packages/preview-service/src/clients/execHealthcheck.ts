9377ab9ec7e551acb3bb4a972302339ce1428df7
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2024-07-03 12:09:34 +0100

    Convert Date.now to string

--- packages/preview-service/src/clients/execHealthcheck.ts
+++ packages/preview-service/src/clients/execHealthcheck.ts
@@ -3,5 +3,4 @@ import fs from 'fs'
 export type UpdateHealthcheckData = () => void
 export const updateHealthcheckDataFactory =
   (deps: { healthCheckFilePath: string }) => () =>
-    //FIXME we should not deal with file system in this service
-    fs.writeFile(deps.healthCheckFilePath, '' + Date.now(), () => {})
+    fs.writeFile(deps.healthCheckFilePath, Date.now().toLocaleString(), () => {})
