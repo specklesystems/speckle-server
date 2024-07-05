55d51bd84f6eb4af0716fc6d69a96731325657e7
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2024-07-05 15:12:27 +0100

    feat(db monitor): adds a new metric speckle_db_tablesize with db table sizes (#2474)
    * feat(db queries): adds db query scripts for determining db sizes
    
    * Adds a new metric `speckle_db_tablesize` with db table sizes
    
    * lower case all the file types to provide a combined metric
    
    * group file status count by lower cased file type
    
    * reinstate accidentally deleted metric
    
    * Fix developer script

--- utils/monitor-deployment/dev_run.sh
+++ utils/monitor-deployment/dev_run.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -eo pipefail
+
+GIT_ROOT="$(git rev-parse --show-toplevel)"
 
 export PG_CONNECTION_STRING=postgres://speckle:speckle@localhost/speckle
-cd src && python3 -u run.py
+pushd "${GIT_ROOT}/utils/monitor-deployment"
+trap popd EXIT
+pip install --disable-pip-version-check --requirement ./requirements.txt
+python3 -u src/run.py
