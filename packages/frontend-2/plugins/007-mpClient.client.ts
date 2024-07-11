d4d58f81dc79cf59644f9fbdd047c1044fb719d9
Kristaps Fabians Geikins <fabis94@live.com>
2024-07-11 14:40:15 +0300

    fix(fe2): utm not being tracked like it was in fe1

--- packages/frontend-2/plugins/007-mpClient.client.ts
+++ packages/frontend-2/plugins/007-mpClient.client.ts
@@ -1,7 +1,6 @@
 import { LogicError } from '@speckle/ui-components'
-import { provideApolloClient } from '@vue/apollo-composable'
-import { useApolloClientFromNuxt } from '~/lib/common/composables/graphql'
 import { fakeMixpanelClient, type MixpanelClient } from '~/lib/common/helpers/mp'
+import { useClientsideMixpanelClientBuilder } from '~/lib/core/clients/mp'
 
 /**
  * mixpanel-browser only supports being ran on the client-side (hence the name)! So it's only going to be accessible
@@ -10,17 +9,12 @@ import { fakeMixpanelClient, type MixpanelClient } from '~/lib/common/helpers/mp
 
 export default defineNuxtPlugin(async () => {
   const logger = useLogger()
-  const apollo = useApolloClientFromNuxt()
+  const build = useClientsideMixpanelClientBuilder()
 
   let mixpanel: MixpanelClient | undefined = undefined
 
   try {
     // Dynamic import to allow suppressing loading errors that happen because of adblock
-    const builder = (await import('~/lib/core/clients/mp'))
-      .useClientsideMixpanelClientBuilder
-
-    // Not sure why, but apollo client is inaccessible so we have to explicitly provide it
-    const build = provideApolloClient(apollo)(() => builder())
     mixpanel = (await build()) || undefined
   } catch (e) {
     logger.warn(e, 'Failed to load mixpanel in CSR')
