e9b26b553c5e0d3c4619e0cec965de289f4df657
Mike Tasset <mike.tasset@gmail.com>
2024-07-16 00:36:21 +0200

    Remove left-over import

--- packages/frontend-2/plugins/dayjs.ts
+++ packages/frontend-2/plugins/dayjs.ts
@@ -2,13 +2,11 @@ import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import localizedFormat from 'dayjs/plugin/localizedFormat'
 import duration from 'dayjs/plugin/duration.js'
-import updateLocale from 'dayjs/plugin/updateLocale'
 
 export default defineNuxtPlugin(() => {
   dayjs.extend(relativeTime)
   dayjs.extend(localizedFormat)
   dayjs.extend(duration)
-  dayjs.extend(updateLocale)
 
   const customRelativeTime = (date: string): string => {
     const pastDate = dayjs(date)
