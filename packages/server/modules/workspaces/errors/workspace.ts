fcb9375b59039c96379cec0c14783728a482f504
Charles Driesler <chuck@speckle.systems>
2024-07-16 00:53:25 +0100

    feat(workspaces): stencil gql api and resolvers

--- packages/server/modules/workspaces/errors/workspace.ts
+++ packages/server/modules/workspaces/errors/workspace.ts
@@ -5,3 +5,8 @@ export class WorkspaceAdminRequiredError extends BaseError {
   static code = 'WORKSPACE_ADMIN_REQUIRED_ERROR'
   static statusCode = 400
 }
+
+export class WorkspacesNotYetImplementedError extends BaseError {
+  static defaultMessage = 'Not yet implemented'
+  static code = 'WORKSPACES_NOT_YET_IMPLEMENTED_ERROR'
+}
--- packages/server/modules/workspaces/graph/resolvers/workspaces.ts
+++ packages/server/modules/workspaces/graph/resolvers/workspaces.ts
@@ -1,49 +1,82 @@
-import { Resolvers } from "@/modules/core/graph/generated/graphql";
+import { Resolvers } from '@/modules/core/graph/generated/graphql'
+import { getFeatureFlags } from '@/modules/shared/helpers/envHelper'
+import { WorkspacesNotYetImplementedError } from '@/modules/workspaces/errors/workspace'
 
-export = {
-  Query: {
-    workspace: async () => {
-      // Get workspace by id
-    }
-  },
-  WorkspaceMutations: {
-    create: async () => { },
-    delete: async () => { },
-    update: async () => { },
-    updateRole: async () => { },
-    deleteRole: async () => { }
-  },
-  WorkspaceInviteMutations: {
-    create: async () => { },
-    batchCreate: async () => { },
-    use: async () => { },
-    cancel: async () => { }
-  },
-  Workspace: {
-    role: async () => {
-      // Get user id from parent, get role and return
-    },
-    team: async () => {
-      // Get roles for workspace
-    },
-    invitedTeam: async () => {
-      // Get invites
-    },
-    projects: async () => {
-      // Get projects in workspace
-    }
-  },
-  User: {
-    workspaces: async () => {
-      // Get roles for user, get workspaces
-    }
-  },
-  Project: {
-    workspace: async () => {
-      // Get workspaceId from project, get and return workspace data
-    }
-  },
-  AdminQueries: {
-    workspaceList: async () => { }
-  }
-} as Resolvers
+const { FF_WORKSPACES_MODULE_ENABLED } = getFeatureFlags()
+
+export = FF_WORKSPACES_MODULE_ENABLED
+  ? ({
+      Query: {
+        workspace: async () => {
+          // Get workspace by id
+          throw new WorkspacesNotYetImplementedError()
+        }
+      },
+      WorkspaceMutations: {
+        create: async () => {
+          throw new WorkspacesNotYetImplementedError()
+        },
+        delete: async () => {
+          throw new WorkspacesNotYetImplementedError()
+        },
+        update: async () => {
+          throw new WorkspacesNotYetImplementedError()
+        },
+        updateRole: async () => {
+          throw new WorkspacesNotYetImplementedError()
+        },
+        deleteRole: async () => {
+          throw new WorkspacesNotYetImplementedError()
+        }
+      },
+      WorkspaceInviteMutations: {
+        create: async () => {
+          throw new WorkspacesNotYetImplementedError()
+        },
+        batchCreate: async () => {
+          throw new WorkspacesNotYetImplementedError()
+        },
+        use: async () => {
+          throw new WorkspacesNotYetImplementedError()
+        },
+        cancel: async () => {
+          throw new WorkspacesNotYetImplementedError()
+        }
+      },
+      Workspace: {
+        role: async () => {
+          // Get user id from parent, get role and return
+          throw new WorkspacesNotYetImplementedError()
+        },
+        team: async () => {
+          // Get roles for workspace
+          throw new WorkspacesNotYetImplementedError()
+        },
+        invitedTeam: async () => {
+          // Get invites
+          throw new WorkspacesNotYetImplementedError()
+        },
+        projects: async () => {
+          // Get projects in workspace
+          throw new WorkspacesNotYetImplementedError()
+        }
+      },
+      User: {
+        workspaces: async () => {
+          // Get roles for user, get workspaces
+          throw new WorkspacesNotYetImplementedError()
+        }
+      },
+      Project: {
+        workspace: async () => {
+          // Get workspaceId from project, get and return workspace data
+          throw new WorkspacesNotYetImplementedError()
+        }
+      },
+      AdminQueries: {
+        workspaceList: async () => {
+          throw new WorkspacesNotYetImplementedError()
+        }
+      }
+    } as Resolvers)
+  : null
