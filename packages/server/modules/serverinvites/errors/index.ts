f43dcb7e2df4315275e236c5909e94bfb7c36cc1
Kristaps Fabians Geikins <fabis94@live.com>
2024-07-16 14:16:16 +0300

    typescriptifying stuff

--- packages/server/modules/serverinvites/errors/index.ts
+++ packages/server/modules/serverinvites/errors/index.ts
@@ -1,22 +1,16 @@
-const { BaseError } = require('@/modules/shared/errors/base')
+import { BaseError } from '@/modules/shared/errors/base'
 
-class InviteCreateValidationError extends BaseError {
+export class InviteCreateValidationError extends BaseError {
   static code = 'INVITE_CREATE_ERROR'
   static defaultMessage = 'An issue occurred while trying to create an invitation'
 }
 
-class NoInviteFoundError extends BaseError {
+export class NoInviteFoundError extends BaseError {
   static code = 'NO_INVITE_FOUND'
   static defaultMessage = 'No invitation for the related resources was found'
 }
 
-class ResourceNotResolvableError extends BaseError {
+export class ResourceNotResolvableError extends BaseError {
   static code = 'INVITE_RESOURCE_NOT_RESOLVABLE'
   static defaultMessage = "Invite's associated resource could not be resolved"
-}
-
-module.exports = {
-  InviteCreateValidationError,
-  NoInviteFoundError,
-  ResourceNotResolvableError
 }
