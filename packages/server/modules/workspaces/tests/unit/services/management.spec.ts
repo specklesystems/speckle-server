2741fc9fe57a4daf86820cfee0f3baa27a0f6d2b
Charles Driesler <chuck@speckle.systems>
2024-07-18 16:05:45 +0100

    fix(workspaces): some test dry

--- packages/server/modules/workspaces/tests/unit/services/management.spec.ts
+++ packages/server/modules/workspaces/tests/unit/services/management.spec.ts
@@ -11,95 +11,120 @@ import { WorkspaceEvents } from '@/modules/workspacesCore/domain/events'
 import { StreamAclRecord, StreamRecord } from '@/modules/core/helpers/types'
 import { expectToThrow } from '@/test/assertionHelper'
 
+type WorkspaceTestContext = {
+  storedWorkspaces: Workspace[]
+  storedRoles: WorkspaceAcl[]
+  eventData: {
+    isCalled: boolean
+    eventName: string
+    payload: unknown
+  }
+}
+
+const buildCreateWorkspaceWithTestContext = (
+  dependecyOverrides: Partial<Parameters<typeof createWorkspaceFactory>[0]> = {}
+) => {
+  const context: WorkspaceTestContext = {
+    storedWorkspaces: [],
+    storedRoles: [],
+    eventData: {
+      isCalled: false,
+      eventName: '',
+      payload: {}
+    }
+  }
+
+  for (const [key, value] of Object.entries(dependecyOverrides)) {
+    if (typeof value === 'undefined')
+      delete dependecyOverrides[key as keyof typeof dependecyOverrides]
+  }
+
+  const deps: Parameters<typeof createWorkspaceFactory>[0] = {
+    upsertWorkspace: async ({ workspace }: { workspace: Workspace }) => {
+      context.storedWorkspaces.push(workspace)
+    },
+    upsertWorkspaceRole: async (workspaceAcl: WorkspaceAcl) => {
+      context.storedRoles.push(workspaceAcl)
+    },
+    emitWorkspaceEvent: async ({ eventName, payload }) => {
+      context.eventData.isCalled = true
+      context.eventData.eventName = eventName
+      context.eventData.payload = payload
+      return []
+    },
+    storeBlob: async () => cryptoRandomString({ length: 10 }),
+    ...dependecyOverrides
+  }
+
+  const createWorkspace = createWorkspaceFactory(deps)
+
+  return { context, createWorkspace }
+}
+
+const getCreateWorkspaceInput = () => {
+  return {
+    userId: cryptoRandomString({ length: 10 }),
+    workspaceInput: {
+      description: 'foobar',
+      logo: null,
+      name: cryptoRandomString({ length: 6 })
+    }
+  }
+}
+
 describe('Workspace services', () => {
   describe('createWorkspaceFactory creates a function, that', () => {
     it('stores the workspace', async () => {
-      const storedWorkspaces: Workspace[] = []
-      const createWorkspace = createWorkspaceFactory({
-        upsertWorkspace: async ({ workspace }: { workspace: Workspace }) => {
-          storedWorkspaces.push(workspace)
-        },
-        upsertWorkspaceRole: async () => {},
-        emitWorkspaceEvent: async () => [],
-        storeBlob: async () => cryptoRandomString({ length: 10 })
-      })
+      const { context, createWorkspace } = buildCreateWorkspaceWithTestContext()
 
-      const workspaceInput = {
-        description: 'foobar',
-        logo: null,
-        name: cryptoRandomString({ length: 6 })
-      }
-      const workspace = await createWorkspace({
-        userId: cryptoRandomString({ length: 10 }),
-        workspaceInput
-      })
-      expect(storedWorkspaces.length).to.equal(1)
-      expect(storedWorkspaces[0]).to.deep.equal(workspace)
+      const { userId, workspaceInput } = getCreateWorkspaceInput()
+      const workspace = await createWorkspace({ userId, workspaceInput })
+
+      expect(context.storedWorkspaces.length).to.equal(1)
+      expect(context.storedWorkspaces[0]).to.deep.equal(workspace)
     })
     it('makes the workspace creator becomes a workspace:admin', async () => {
-      const storedRole: WorkspaceAcl[] = []
-      const createWorkspace = createWorkspaceFactory({
-        upsertWorkspace: async () => {},
-        upsertWorkspaceRole: async (workspaceAcl: WorkspaceAcl) => {
-          storedRole.push(workspaceAcl)
-        },
-        emitWorkspaceEvent: async () => [],
-        storeBlob: async () => cryptoRandomString({ length: 10 })
-      })
+      const { context, createWorkspace } = buildCreateWorkspaceWithTestContext()
 
-      const workspaceInput = {
-        description: 'foobar',
-        logo: null,
-        name: cryptoRandomString({ length: 6 })
-      }
-      const userId = cryptoRandomString({ length: 10 })
-      const workspace = await createWorkspace({
-        userId,
-        workspaceInput
-      })
-      expect(storedRole.length).to.equal(1)
-      expect(storedRole[0]).to.deep.equal({
+      const { userId, workspaceInput } = getCreateWorkspaceInput()
+      const workspace = await createWorkspace({ userId, workspaceInput })
+
+      expect(context.storedRoles.length).to.equal(1)
+      expect(context.storedRoles[0]).to.deep.equal({
         userId,
         workspaceId: workspace.id,
         role: Roles.Workspace.Admin
       })
     })
     it('emits a workspace created event', async () => {
-      const eventData = {
-        isCalled: false,
-        eventName: '',
-        payload: {}
-      }
-      const createWorkspace = createWorkspaceFactory({
-        upsertWorkspace: async () => {},
-        upsertWorkspaceRole: async () => {},
-        emitWorkspaceEvent: async ({ eventName, payload }) => {
-          eventData.isCalled = true
-          eventData.eventName = eventName
-          eventData.payload = payload
-          return []
-        },
-        storeBlob: async () => cryptoRandomString({ length: 10 })
+      const { context, createWorkspace } = buildCreateWorkspaceWithTestContext()
+
+      const { userId, workspaceInput } = getCreateWorkspaceInput()
+      const workspace = await createWorkspace({ userId, workspaceInput })
+
+      expect(context.eventData.isCalled).to.equal(true)
+      expect(context.eventData.eventName).to.equal(WorkspaceEvents.Created)
+      expect(context.eventData.payload).to.deep.equal({
+        ...workspace,
+        createdByUserId: userId
       })
-
-      const workspaceInput = {
-        description: 'foobar',
-        logo: null,
-        name: cryptoRandomString({ length: 6 })
-      }
-      const userId = cryptoRandomString({ length: 10 })
-
-      const workspace = await createWorkspace({
-        userId,
-        workspaceInput
-      })
-
-      expect(eventData.isCalled).to.equal(true)
-      expect(eventData.eventName).to.equal(WorkspaceEvents.Created)
-      expect(eventData.payload).to.deep.equal({ ...workspace, createdByUserId: userId })
     })
   })
 })
+
+// type WorkspaceRoleTestContext = {
+//   workspaceId: string
+//   workspaceRoles: WorkspaceAcl[],
+//   workspaceProjects: StreamRecord[],
+//   workspaceProjectRoles: StreamAclRecord[],
+//   eventData: {
+//     isCalled: boolean
+//     eventName: string
+//     eventPayload: unknown
+//   }
+// }
+
+// const build
 
 describe('Workspace role services', () => {
   describe('deleteWorkspaceRoleFactory creates a function, that', () => {
