66eb539aa0248c78c966a3eb510997c5ffaedfc9
Chuck Driesler <chuck@speckle.systems>
2024-07-20 00:03:27 +0100

    feat(workspaces): assign project roles for workspace projects (#2499)
    * feat(workspaces): drop createdByUserId from the dataschema
    
    * feat(workspaces): repositories WIP
    
    * merge
    
    * protect against removing last admin in workspace
    
    * quick impl and stub tests
    
    * add tests
    
    * services
    
    * unit tests for role services
    
    * feat(workspaces): authorize project creation if workspace specified
    
    * feat(workspaces): emit project created event
    
    * feat(workspaces): assign roles on project create in workspace
    
    * feat(workspaces): update project roles when user added to workspace
    
    * fix(workspaces): perform automatic project role update in service function
    
    * fix(workspaces): also delete roles
    
    * fix(workspaces): broke tests again oops
    
    * fix(workspaces): update `onProjectCreated` listener to use new repo method
    
    * fix(workspaces): use service function in event listener
    
    * fix(workspaces): get workspace projects via existing stream repo functions
    
    * fix(workspaces): roles mapping in domain, use enum
    
    * fix(workspaces): repair type reference in tests
    
    * fix(workspaces): consolidate files, use different existing stream-getter
    
    * fix(workspaces): more specific error
    
    * fix(workspaces): yield per page
    
    * fix(workspaces): some test dry
    
    * fix(workspaces): superdry
    
    * fix(workspaces): classic
    
    ---------
    
    Co-authored-by: Gergő Jedlicska <gergo@jedlicska.com>

--- packages/server/modules/workspaces/errors/workspace.ts
+++ packages/server/modules/workspaces/errors/workspace.ts
@@ -6,6 +6,16 @@ export class WorkspaceAdminRequiredError extends BaseError {
   static statusCode = 400
 }
 
+export class WorkspaceInvalidRoleError extends BaseError {
+  static defaultMessage = 'Invalid workspace role provided'
+  static code = 'WORKSPACE_INVALID_ROLE_ERROR'
+}
+
+export class WorkspaceQueryError extends BaseError {
+  static defaultMessage = 'Unexpected error during query operation'
+  static code = 'WORKSPACE_QUERY_ERROR'
+}
+
 export class WorkspacesNotYetImplementedError extends BaseError {
   static defaultMessage = 'Not yet implemented'
   static code = 'WORKSPACES_NOT_YET_IMPLEMENTED_ERROR'
