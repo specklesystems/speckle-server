e2081790085828f03c55afb69e5bbee300048d4a
Alessandro Magionami <alessandro.magionami@gmail.com>
2024-07-22 14:47:59 +0200

    chore(useremails): searchUsers use user emails table

--- packages/server/modules/core/services/users.js
+++ packages/server/modules/core/services/users.js
@@ -237,11 +237,21 @@ module.exports = {
    * User search available for normal server users. It's more limited because of the lower access level.
    */
   async searchUsers(searchQuery, limit, cursor, archived = false, emailOnly = false) {
+    const prefixedLimitedUserFields = LIMITED_USER_FIELDS.map(
+      (field) => `users.${field}`
+    )
     const query = Users()
       .join('server_acl', 'users.id', 'server_acl.userId')
-      .select(...LIMITED_USER_FIELDS)
+      .leftJoin(UserEmails.name, UserEmails.col.userId, UsersSchema.col.id)
+      .columns([
+        ...Object.values(omit(UsersSchema.col, ['email', 'verified'])).filter((col) =>
+          prefixedLimitedUserFields.includes(col)
+        ),
+        knex.raw(`(array_agg("user_emails"."verified"))[1] as verified`)
+      ])
+      .groupBy(UsersSchema.col.id)
       .where((queryBuilder) => {
-        queryBuilder.where({ email: searchQuery }) //match full email or partial name
+        queryBuilder.where({ [UserEmails.col.email]: searchQuery }) //match full email or partial name
         if (!emailOnly) queryBuilder.orWhere('name', 'ILIKE', `%${searchQuery}%`)
         if (!archived) queryBuilder.andWhere('role', '!=', Roles.Server.ArchivedUser)
       })
