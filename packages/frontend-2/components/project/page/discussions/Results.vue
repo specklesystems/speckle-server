77168d61f07008ef651b6973b0ec5bc858cd7532
Mike Tasset <mike.tasset@gmail.com>
2024-07-24 10:08:29 +0200

    Added all filters to resolver key

--- packages/frontend-2/components/project/page/discussions/Results.vue
+++ packages/frontend-2/components/project/page/discussions/Results.vue
@@ -43,7 +43,7 @@ const {
     projectId: props.project.id,
     filter: { includeArchived: !!props.includeArchived }
   })),
-  resolveKey: (vars) => [vars.projectId, vars.filter.includeArchived],
+  resolveKey: (vars) => [vars.projectId, vars.filter],
   resolveCurrentResult: (res) => res?.project.commentThreads,
   resolveNextPageVariables: (baseVars, cursor) => ({
     ...baseVars,
