e8c8291c4590312569e68ae1430c7d735775dafe
Kristaps Fabians Geikins <fabis94@live.com>
2024-07-29 13:31:44 +0300

    refactored serverinvites resource migration

--- packages/server/modules/serverinvites/migrations/20240716134617_migrate_to_resources_array.ts
+++ packages/server/modules/serverinvites/migrations/20240716134617_migrate_to_resources_array.ts
@@ -1,5 +1,3 @@
-import { logger } from '@/logging/logging'
-import { Nullable, Roles } from '@speckle/shared'
 import { Knex } from 'knex'
 
 const TABLE_NAME = 'server_invites'
@@ -10,72 +8,27 @@ const OLD_IDX_2 = ['resourceTarget', 'resourceId']
 const NEW_IDX = ['resource']
 const NEW_UNIQUE_IDX = ['target', 'resource']
 
-type InviteResourceTarget = {
-  resourceId: string
-  resourceType: 'project' | 'server'
-  role: string
-  primary: boolean
-}
-
-type ServerInviteRecord = {
-  id: string
-  target: string
-  inviterId: string
-  createdAt: Date
-  message: Nullable<string>
-  resource: InviteResourceTarget
-  resourceTarget: string | null
-  resourceId: Nullable<string>
-  role: Nullable<string>
-  token: string
-  serverRole: Nullable<string>
-}
-
 export async function up(knex: Knex): Promise<void> {
-  // Iterate over all rows and update the resources field w/ empty resource
-  const qStream = knex
-    .select<ServerInviteRecord[]>('*')
-    .where((w1) => {
-      // Where resource JSONB is empty
-      w1.where('resource', '{}').orWhereNull('resource')
-    })
-    .from(TABLE_NAME)
-    .stream()
+  // Update all project invites
+  const updateQ1 = knex.raw(`
+      UPDATE ${TABLE_NAME}
+      SET resource = jsonb_build_object('resourceId', "resourceId", 'resourceType', 'project', 'role',
+                                        coalesce(role, 'stream:contributor'), 'primary', true)
+      WHERE (resource = '{}' OR resource IS NULL)
+        AND "resourceTarget" = 'streams'
+        AND "resourceId" IS NOT NULL
+    `)
+  await updateQ1
 
-  let counter = 1
-  for await (const row of qStream) {
-    let resource: InviteResourceTarget
-
-    // Add stream/project resource
-    if (row.resourceTarget === 'streams' && row.resourceId) {
-      resource = {
-        resourceId: row.resourceId,
-        resourceType: 'project',
-        role: row.role || Roles.Stream.Contributor,
-        primary: true
-      }
-    } else {
-      // Add server resource
-      resource = {
-        resourceId: '',
-        resourceType: 'server',
-        role: row.serverRole || Roles.Server.User,
-        primary: true
-      }
-    }
-
-    const updateQ = knex(TABLE_NAME)
-      .where({ id: row.id })
-      .update({
-        resource: JSON.stringify(resource),
-        resourceTarget: null,
-        resourceId: null,
-        role: null,
-        serverRole: null
-      })
-    await updateQ
-    logger.debug(`Updated #${counter++} row...`)
-  }
+  // Update all remaining (server) invites
+  const updateQ2 = knex.raw(`
+    UPDATE ${TABLE_NAME}
+    SET resource = jsonb_build_object('resourceId', '', 'resourceType', 'server', 'role',
+                                      coalesce("serverRole", 'server:user'), 'primary', true)
+    WHERE (resource = '{}' OR resource IS NULL)
+      AND "resourceTarget" IS NULL
+    `)
+  await updateQ2
 
   // Drop resourceTarget, resourceId, role, serverRole
   await knex.schema.alterTable(TABLE_NAME, (table) => {
@@ -85,11 +38,17 @@ export async function up(knex: Knex): Promise<void> {
     table.dropColumn('serverRole')
 
     table.index(NEW_IDX)
-    table.unique(NEW_UNIQUE_IDX)
+    table.index(NEW_UNIQUE_IDX) // not actually a unique idx, cause i'm not super confident about some duplicates not appearing
   })
 }
 
 export async function down(knex: Knex): Promise<void> {
+  // Remove indices
+  await knex.schema.alterTable(TABLE_NAME, (table) => {
+    table.dropIndex(NEW_IDX)
+    table.dropIndex(NEW_UNIQUE_IDX)
+  })
+
   // Add back columns
   await knex.schema.alterTable(TABLE_NAME, (table) => {
     table.string('resourceTarget', 256)
@@ -98,47 +57,30 @@ export async function down(knex: Knex): Promise<void> {
     table.string('serverRole')
 
     table.index(OLD_IDX_2)
-    table.unique(OLD_UNIQUE_IDX_COLS)
+    table.index(OLD_UNIQUE_IDX_COLS)
   })
 
-  // Iterate over all rows and update the resources field
-  let counter = 1
-  const qStream = knex
-    .select<ServerInviteRecord[]>('*')
-    .where((w1) => {
-      // Where resources JSONB array isn't empty
-      w1.whereNot('resource', '{}').orWhereNotNull('resource')
-    })
-    .from(TABLE_NAME)
-    .stream()
-
-  for await (const row of qStream) {
-    let resourceTarget = null as string | null
-    let resourceId = null as string | null
-    let role = null as string | null
-    let serverRole: string = Roles.Server.User
-
-    const serverResource = row.resource.resourceType === 'server' ? row.resource : null
-    if (serverResource && serverResource.role) {
-      serverRole = serverResource.role
-    }
-
-    const streamResource = row.resource.resourceType === 'project' ? row.resource : null
-    if (streamResource && streamResource.resourceId) {
-      resourceTarget = 'streams'
-      resourceId = streamResource.resourceId
-      role = streamResource.role || Roles.Stream.Contributor
-    }
-
-    await knex(TABLE_NAME)
-      .where({ id: row.id })
-      .update({
-        resource: JSON.stringify({}),
-        resourceTarget,
-        resourceId,
-        role,
-        serverRole
-      })
-    logger.debug(`Updated #${counter++} row...`)
-  }
+  // Move back from resource to old fields
+  const updateQ = knex.raw(`
+    UPDATE ${TABLE_NAME}
+    SET resource         = '{}',
+        "resourceId"     = CASE
+                              WHEN resource ->> 'resourceType' = 'project' THEN resource ->> 'resourceId'
+                              ELSE null
+            END,
+        "role"           = CASE
+                              WHEN resource ->> 'resourceType' = 'project' THEN COALESCE(resource ->> 'role', 'stream:contributor')
+                              ELSE null
+            END,
+        "serverRole"     = CASE
+                              WHEN resource ->> 'resourceType' = 'project' THEN null
+                              ELSE COALESCE(resource ->> 'role', 'server:user')
+            END,
+        "resourceTarget" = CASE
+                              WHEN resource ->> 'resourceType' = 'project' THEN 'streams'
+                              ELSE null
+            END
+    WHERE (NOT (resource = '{}' OR resource IS NULL))
+    `)
+  await updateQ
 }
