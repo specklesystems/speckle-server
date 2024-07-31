9ffe6ddc215c39f668fc752e7394159c24da2071
Alessandro Magionami <alessandro.magionami@gmail.com>
2024-07-31 10:50:55 +0200

    chore(useremails): minor cleanup for users repository

--- packages/server/modules/core/migrations/20240710154658_user_emails_backfill.ts
+++ packages/server/modules/core/migrations/20240710154658_user_emails_backfill.ts
@@ -4,21 +4,21 @@ import { scanTableFactory } from '@/modules/core/helpers/scanTable'
 import { coreLogger } from '@/modules/core/logger'
 
 export async function up(knex: Knex): Promise<void> {
-  coreLogger.info('Migration user_emails_backfill started')
+  coreLogger.debug('Migration user_emails_backfill started')
 
   const batchSize = 1000
   const [countQuery] = await knex('users').count()
   const usersCount = parseInt(countQuery.count.toString())
   const maxLoops = usersCount / batchSize
 
-  coreLogger.info(`Number of loops estimated: ${maxLoops}`)
+  coreLogger.debug(`Number of loops estimated: ${maxLoops}`)
 
   let currentIteration = 1
   for await (const rows of scanTableFactory({ db: knex })({
     tableName: 'users',
     batchSize
   })) {
-    coreLogger.info(`Starting iteration ${currentIteration} with ${rows.length} rows`)
+    coreLogger.debug(`Starting iteration ${currentIteration} with ${rows.length} rows`)
     if (rows.length) {
       await knex('user_emails')
         .insert(
@@ -34,13 +34,13 @@ export async function up(knex: Knex): Promise<void> {
         .ignore()
     }
     currentIteration++
-    coreLogger.info(`Completed iteration ${currentIteration}`)
+    coreLogger.debug(`Completed iteration ${currentIteration}`)
 
     if (currentIteration > maxLoops + 2) {
       throw new Error('Stopping migration to avoid infinite loop')
     }
   }
-  coreLogger.info('Migration user_emails_backfill started')
+  coreLogger.debug('Migration user_emails_backfill started')
 }
 
 export async function down(knex: Knex): Promise<void> {
