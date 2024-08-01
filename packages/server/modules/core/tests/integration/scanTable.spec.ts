fcedff256528f9c964537b5829a0f12a25f42dd4
Alessandro Magionami <alessandro.magionami@gmail.com>
2024-08-01 14:30:25 +0200

    chore(useremails): add failsafe option to scan table

--- packages/server/modules/core/tests/integration/scanTable.spec.ts
+++ packages/server/modules/core/tests/integration/scanTable.spec.ts
@@ -10,6 +10,7 @@ import {
   createRandomPassword
 } from '@/modules/core/helpers/testHelpers'
 import { truncateTables } from '@/test/hooks'
+import { expectToThrow } from '@/test/assertionHelper'
 
 const scanTable = scanTableFactory<UserRecord>({ db })
 
@@ -38,5 +39,26 @@ describe('Helpers scanTable @core', () => {
       }
       offset += 3
     }
+  })
+
+  it('should throw an error when reaching failsafe limit', async () => {
+    const users = Array.from(Array(3)).map(() => ({
+      id: crs({ length: 6 }),
+      email: createRandomEmail(),
+      passwordDigest: createRandomPassword(),
+      name: createRandomPassword()
+    }))
+
+    await db(Users.name).insert(users)
+
+    const err = await expectToThrow(async () => {
+      for await (const rows of scanTable(
+        { tableName: Users.name, batchSize: 1 },
+        { failsafeLimitMultiplier: 1 }
+      )) {
+        expect(rows).to.be.ok
+      }
+    })
+    expect(err.message).to.eq('Never ending loop')
   })
 })
