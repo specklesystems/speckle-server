ad1f97216f37b3179c4b2cc3969c9d3365c05140
Mike <mike.tasset@gmail.com>
2024-08-06 13:39:58 +0200

    Feat: Add new dashboard page (#2580)

---
+++ packages/frontend-2/lib/dashboard/graphql/queries.ts
@@ -0,0 +1,14 @@
+import { graphql } from '~~/lib/common/generated/gql'
+
+export const dashboardProjectsPageQuery = graphql(`
+  query DashboardProjectsPageQuery {
+    activeUser {
+      id
+      projects(limit: 3) {
+        items {
+          ...DashboardProjectCard_Project
+        }
+      }
+    }
+  }
+`)
