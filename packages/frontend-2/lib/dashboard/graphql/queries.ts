4f9128d5146bb392e40b224436cfb86725bb1430
Mike Tasset <mike.tasset@gmail.com>
2024-08-06 12:28:52 +0200

    Addressed merge comments

--- packages/frontend-2/lib/dashboard/graphql/queries.ts
+++ packages/frontend-2/lib/dashboard/graphql/queries.ts
@@ -3,6 +3,7 @@ import { graphql } from '~~/lib/common/generated/gql'
 export const dashboardProjectsPageQuery = graphql(`
   query DashboardProjectsPageQuery {
     activeUser {
+      id
       projects(limit: 3) {
         items {
           ...DashboardProjectCard_Project
