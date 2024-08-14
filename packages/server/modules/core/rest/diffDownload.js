3e223b326ce808125c153ae6c8aa2f4e35fe2f57
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2024-08-14 21:05:11 +0100

    WIP: add logging to diffDownload

--- packages/server/modules/core/rest/diffDownload.js
+++ packages/server/modules/core/rest/diffDownload.js
@@ -56,25 +56,94 @@ module.exports = (app) => {
       }
     )
 
-    const cSize = 1000
+    const databaseStreams = []
+    const cSize = 2
+    let iterationIndex = 0
     try {
       for (let cStart = 0; cStart < childrenList.length; cStart += cSize) {
         const childrenChunk = childrenList.slice(cStart, cStart + cSize)
 
+        req.log.info({iterationIndex}, 'Opening db stream on iteration {iterationIndex}')
         const dbStream = await getObjectsStream({
           streamId: req.params.streamId,
           objectIds: childrenChunk
         })
+        databaseStreams.push(dbStream)
+
+        speckleObjStream.on('error', (err) => {
+          req.log.error(
+            { err, iterationIndex },
+            'Stream error streaming objects on iteration {iterationIndex}'
+          )
+        })
+        speckleObjStream.on('end', (err) => {
+          req.log.info(
+            { err, iterationIndex },
+            'Stream ended on iteration {iterationIndex}'
+          )
+        })
+        speckleObjStream.on('close', (err) => {
+          req.log.info(
+            { err, iterationIndex },
+            'Stream closed on iteration {iterationIndex}'
+          )
+        })
+
+        const numReadableDbStreams = databaseStreams.filter((s) => s.readable).length
+        req.log.info(
+          { numReadableDbStreams, iterationIndex },
+          'Number of readable db streams {numReadableDbStreams} at iteration {iterationIndex}'
+        )
+
+        iterationIndex++
         await new Promise((resolve, reject) => {
           dbStream.pipe(speckleObjStream, { end: false })
-          dbStream.once('end', resolve)
-          dbStream.once('error', reject)
+          dbStream.once('data', (data) => {
+            //HACK force premature close of connection after first iteration
+            if (iterationIndex === 1) res.end()
+            req.log.debug({id: data.id }, 'DB stream data on iteration {iterationIndex} return {id}')
+          })
+          dbStream.once('end', (params) => {
+            req.log.info(
+              { iterationIndex: iterationIndex - 1 },
+              'DB stream ended on iteration {iterationIndex}'
+            )
+            resolve(params)
+          })
+          dbStream.once('close', (params) => {
+            req.log.info(
+              { iterationIndex: iterationIndex - 1 },
+              'DB stream closed on iteration {iterationIndex}'
+            )
+            return resolve(params)
+          })
+          dbStream.once('error', (params) => {
+            req.log.error(
+              { iterationIndex: iterationIndex - 1 },
+              'DB stream error on iteration {iterationIndex}'
+            )
+            return reject(params)
+          })
         })
       }
     } catch (ex) {
       req.log.error(ex, `DB Error streaming objects`)
       speckleObjStream.emit('error', new Error('Database streaming error'))
+    } finally {
+      req.log.warn({iterationIndex}, 'Finally block reached')
     }
+
+    const numReadableDbStreamsBefore = databaseStreams.filter((s) => s.readable).length
+    req.log.warn(
+      { numReadableDbStreams: numReadableDbStreamsBefore, iterationIndex },
+      'Number of readable db streams {numReadableDbStreams} before ending speckle object stream'
+    )
     speckleObjStream.end()
+
+    const numReadableDbStreamsAfter = databaseStreams.filter((s) => s.readable).length
+    req.log.warn(
+      { numReadableDbStreams: numReadableDbStreamsAfter, iterationIndex },
+      'Number of readable db streams {numReadableDbStreams} after ending speckle object stream'
+    )
   })
 }
