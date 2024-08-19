6af159be33d386152bb83e5598777c160bd2ba31
Mike Tasset <mike.tasset@gmail.com>
2024-08-20 00:34:11 +0200

    Remove conditional

--- packages/frontend-2/components/settings/workspaces/General.vue
+++ packages/frontend-2/components/settings/workspaces/General.vue
@@ -6,7 +6,6 @@
       <div class="grid md:grid-cols-2 pt-4">
         <div class="flex items-center justify-center">
           <SettingsWorkspacesGeneralEditAvatar
-            v-if="workspaceResult?.workspace"
             :workspace="workspaceResult?.workspace"
             size="xxl"
           />
