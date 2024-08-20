89d7eb0a2f11efe271188ece01b77f21397c6f50
Charles Driesler <chuck@speckle.systems>
2024-08-20 14:36:06 +0100

    feat(workspaces): add default logo index

--- packages/server/modules/workspaces/services/management.ts
+++ packages/server/modules/workspaces/services/management.ts
@@ -51,6 +51,7 @@ type WorkspaceCreateArgs = {
     name: string
     description: string | null
     logo: string | null
+    defaultLogoIndex: number
   }
   userResourceAccessLimits: MaybeNullOrUndefined<TokenResourceIdentifier[]>
 }
@@ -108,6 +109,7 @@ type WorkspaceUpdateArgs = {
     name?: string | null
     description?: string | null
     logo?: string | null
+    defaultLogoIndex?: number | null
   }
 }
 
