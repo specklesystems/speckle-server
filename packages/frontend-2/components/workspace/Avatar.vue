6d28244ee9c7db496146f1c95e84af408e7a78e2
Mike Tasset <mike.tasset@gmail.com>
2024-08-21 12:27:47 +0200

    Added WorkspaceAvatar component

---
+++ packages/frontend-2/components/workspace/Avatar.vue
@@ -0,0 +1,15 @@
+<template>
+  <UserAvatar logo="test" alt="test" />
+</template>
+
+<script setup lang="ts">
+import { graphql } from '~~/lib/common/generated/gql'
+
+graphql(`
+  fragment WorkspaceAvatar_Workspace on Workspace {
+    id
+    name
+    logo
+    defaultLogoIndex
+`)
+</script>
