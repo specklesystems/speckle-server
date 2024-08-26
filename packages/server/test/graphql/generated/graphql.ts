e1b88ae22efe6aab2c5507d66028784431c170ee
Gergő Jedlicska <gergo@jedlicska.com>
2024-08-26 15:33:20 +0200

    feat(workspaces): allow sending the logo on ws creation

--- packages/server/test/graphql/generated/graphql.ts
+++ packages/server/test/graphql/generated/graphql.ts
@@ -3899,8 +3899,10 @@ export type WorkspaceCollection = {
 };
 
 export type WorkspaceCreateInput = {
-  defaultLogoIndex?: InputMaybe<Scalars['Int']['input']>;
+  defaultLogoIndex: Scalars['Int']['input'];
   description?: InputMaybe<Scalars['String']['input']>;
+  /** Logo image as base64-encoded string */
+  logo?: InputMaybe<Scalars['String']['input']>;
   name: Scalars['String']['input'];
 };
 
