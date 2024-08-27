2181130a75451df0328b393d1b0bd66509ab0ed4
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2024-08-27 12:44:33 +0100

    Add 2024 specklecon banners (#2765)

--- packages/frontend-2/components/projects/DashboardHeader.vue
+++ packages/frontend-2/components/projects/DashboardHeader.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <PromoBannersWrapper v-if="promoBanners.length" :banners="promoBanners" />
     <div class="bg-foundation divide-y divide-outline-3 mb-8 empty:mb-0">
       <ProjectsInviteBanners
         v-if="projectsInvites?.projectInvites?.length"
@@ -13,7 +14,6 @@
         :invites="workspacesInvites"
       />
     </div>
-    <PromoBannersWrapper v-if="promoBanners.length" :banners="promoBanners" />
   </div>
 </template>
 <script setup lang="ts">
@@ -23,6 +23,8 @@ import type {
   ProjectsDashboardHeaderWorkspaces_UserFragment
 } from '~/lib/common/generated/gql/graphql'
 import type { PromoBanner } from '~/lib/promo-banners/types'
+import submitImage from '~/assets/images/banners/submit.gif'
+import earlybirdImage from '~/assets/images/banners/earlybird.gif'
 
 graphql(`
   fragment ProjectsDashboardHeaderProjects_User on User {
@@ -41,5 +43,22 @@ defineProps<{
   workspacesInvites?: ProjectsDashboardHeaderWorkspaces_UserFragment
 }>()
 
-const promoBanners = ref<PromoBanner[]>([])
+const promoBanners = ref<PromoBanner[]>([
+  {
+    primaryText: 'Specklecon - Submit your proposal',
+    url: 'https://conf.speckle.systems/',
+    priority: 1,
+    expiryDate: '2024-09-02',
+    image: submitImage,
+    isBackgroundImage: true
+  },
+  {
+    primaryText: 'Specklecon - Early Bird Tickets',
+    url: 'https://conf.speckle.systems/',
+    priority: 2,
+    expiryDate: '2024-09-15',
+    image: earlybirdImage,
+    isBackgroundImage: true
+  }
+])
 </script>
