0ec76352bd60bbe5bc742038af7a58dccf501277
Mike Tasset <mike.tasset@gmail.com>
2024-09-02 10:10:32 +0200

    Fix: Workspace and project header styling

--- packages/frontend-2/components/common/TitleDescription.vue
+++ packages/frontend-2/components/common/TitleDescription.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col">
     <h2 class="text-heading">{{ title }}</h2>
-    <p class="text-body-sm text-foreground-2">
+    <p class="text-body-sm text-foreground-2 line-clamp-2">
       {{ description ? description : 'No description' }}
     </p>
   </div>
