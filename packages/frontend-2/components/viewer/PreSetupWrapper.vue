93f5eb81b290282a97a67e12db9c73bf64174217
Mike <mike.tasset@gmail.com>
2024-09-04 16:11:33 +0200

    Fix: Remove 'workspaces' from breadcrumbs (#2871)

--- packages/frontend-2/components/viewer/PreSetupWrapper.vue
+++ packages/frontend-2/components/viewer/PreSetupWrapper.vue
@@ -7,13 +7,9 @@
           <ViewerScope :state="state">
             <template v-if="project?.workspace && isWorkspacesEnabled">
               <HeaderNavLink
-                :to="workspacesRoute"
-                name="Workspaces"
-                :separator="false"
-              ></HeaderNavLink>
-              <HeaderNavLink
                 :to="workspaceRoute(project?.workspace.id)"
                 :name="project?.workspace.name"
+                :separator="false"
               ></HeaderNavLink>
             </template>
             <HeaderNavLink
@@ -126,7 +122,7 @@ import { useEmbed } from '~/lib/viewer/composables/setup/embed'
 import { useViewerTour } from '~/lib/viewer/composables/tour'
 import { useFilterUtilities } from '~/lib/viewer/composables/ui'
 import { projectsRoute } from '~~/lib/common/helpers/route'
-import { workspaceRoute, workspacesRoute } from '~/lib/common/helpers/route'
+import { workspaceRoute } from '~/lib/common/helpers/route'
 
 const emit = defineEmits<{
   setup: [InjectableViewerState]
