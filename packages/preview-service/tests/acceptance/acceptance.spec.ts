b4db33b1edcf3a44bda6cfbb9900231f577cc810
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2024-09-05 17:31:51 +0100

    Connect to host.docker.internal

--- packages/preview-service/tests/acceptance/acceptance.spec.ts
+++ packages/preview-service/tests/acceptance/acceptance.spec.ts
@@ -15,11 +15,10 @@ describe.sequential('Acceptance', () => {
       const pgConnString =
         process.env.PG_CONNECTION_STRING ||
         `postgres://preview_service_test:preview_service_test@host.docker.internal:5432/${dbName}`
-      const isHostNetwork = process.env.IS_HOST_NETWORK === 'true'
       //purposefully running in the background without waiting
       void runProcess('docker', [
         'run',
-        isHostNetwork ? '--network="host"' : '',
+        '--add-host=host.docker.internal:host-gateway',
         '--env',
         `PG_CONNECTION_STRING=${pgConnString}`,
         '--rm',
