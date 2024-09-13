27040053d5efafc9c31796b51b5847ddd1828e8f
Mike Tasset <mike.tasset@gmail.com>
2024-09-13 09:23:42 +0200

    Fix banner to render client only due to cookie dependency

--- packages/frontend-2/lib/promo-banners/graphql/queries.ts
+++
@@ -1,9 +0,0 @@
-import { graphql } from '~~/lib/common/generated/gql'
-
-export const promoBannersWorkspaceQuery = graphql(`
-  query PromoBannersWorkspace {
-    activeUser {
-      ...PromoBannersWorkspace_activeUser
-    }
-  }
-`)
