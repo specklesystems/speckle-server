c3c8b8b10fc5aebed488e979ae608a39fe40a292
Alessandro Magionami <alessandro.magionami@gmail.com>
2024-09-17 18:16:13 +0200

    chore(activitystream): getActivity refactor

---
+++ packages/server/modules/activitystream/domain/operations.ts
@@ -0,0 +1,8 @@
+import { StreamScopeActivity } from '@/modules/activitystream/helpers/types'
+
+export type GetActivity = (
+  streamId: string,
+  start: Date,
+  end: Date,
+  filteredUser: string | null
+) => Promise<StreamScopeActivity[]>
