86c1e84c7c3d533a30e91e06682a9a15dafa8423
Alessandro Magionami <alessandro.magionami@gmail.com>
2024-09-27 10:58:48 +0200

    Activitystream IoC 3 createActivitySummary (#3147)
    * chore(activitystream): refactor createActivitySummary
    
    * chore(activitystream): fix imports

--- packages/server/modules/activitystream/services/summary.ts
+++ packages/server/modules/activitystream/services/summary.ts
@@ -1,48 +1,51 @@
-import { getActivityFactory } from '@/modules/activitystream/repositories'
-import { StreamScopeActivity } from '@/modules/activitystream/helpers/types'
 import {
   NotificationPublisher,
   NotificationType
 } from '@/modules/notifications/helpers/types'
-import { StreamRecord, UserRecord } from '@/modules/core/helpers/types'
 import { getUser } from '@/modules/core/repositories/users'
-import { getStream } from '@/modules/core/services/streams'
-import { db } from '@/db/knex'
-import { GetActiveUserStreams } from '@/modules/activitystream/domain/operations'
+import { getStream as getStreamService } from '@/modules/core/services/streams'
+import {
+  CreateActivitySummary,
+  GetActiveUserStreams,
+  GetActivity
+} from '@/modules/activitystream/domain/operations'
 
-export type StreamActivitySummary = {
-  stream: StreamRecord | null
-  activity: StreamScopeActivity[]
-}
-
-export type ActivitySummary = {
-  user: UserRecord
-  streamActivities: StreamActivitySummary[]
-}
-
-export const createActivitySummary = async (
-  userId: string,
-  streamIds: string[],
-  start: Date,
-  end: Date
-): Promise<ActivitySummary | null> => {
-  const streamActivities = (
-    await Promise.all(
-      streamIds.map(async (streamId) => {
-        return {
-          stream: (await getStream({ streamId, userId })) ?? null,
-          activity: await getActivityFactory({ db })(streamId, start, end, null) //userId is null for now, to not filter out any activity
-        }
-      })
-    )
-  ).filter((sa) => sa.activity.length)
-  const user = await getUser(userId)
-  if (!user) return null
-  return {
-    user,
-    streamActivities
+export const createActivitySummaryFactory =
+  ({
+    getStream,
+    getActivity
+  }: {
+    getStream: typeof getStreamService
+    getActivity: GetActivity
+  }): CreateActivitySummary =>
+  async ({
+    userId,
+    streamIds,
+    start,
+    end
+  }: {
+    userId: string
+    streamIds: string[]
+    start: Date
+    end: Date
+  }) => {
+    const streamActivities = (
+      await Promise.all(
+        streamIds.map(async (streamId) => {
+          return {
+            stream: (await getStream({ streamId, userId })) ?? null,
+            activity: await getActivity(streamId, start, end, null) //userId is null for now, to not filter out any activity
+          }
+        })
+      )
+    ).filter((sa) => sa.activity.length)
+    const user = await getUser(userId)
+    if (!user) return null
+    return {
+      user,
+      streamActivities
+    }
   }
-}
 
 export const sendActivityNotificationsFactory =
   ({
