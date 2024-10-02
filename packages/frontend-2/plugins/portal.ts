614fdbb14ef35091c6053fb759c7eac9ff62f890
andrewwallacespeckle <139135120+andrewwallacespeckle@users.noreply.github.com>
2024-10-02 11:07:46 +0100

    fix(fe2): Make toasts clickable when dialogs are open (#3176)
    * Add portal to dialog
    
    * Changes from talk with Fabs

--- packages/frontend-2/plugins/portal.ts
+++ packages/frontend-2/plugins/portal.ts
@@ -2,4 +2,10 @@ import PortalVue from 'portal-vue'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(PortalVue)
+
+  if (import.meta.client) {
+    const div = document.createElement('div')
+    div.id = 'toast-portal'
+    document.body.appendChild(div)
+  }
 })
