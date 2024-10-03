729859d7a749a4485e80235679fb593260ed9b11
andrewwallacespeckle <andrew@speckle.systems>
2024-10-03 11:16:58 +0100

    UI Updates

---
+++ packages/frontend-2/components/specklebot/Window/Question.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="group flex items-center justify-between w-full hover:bg-highlight-1 text-sm"
+  >
+    <button
+      class="flex flex-1 justify-start py-4 px-6 text-foreground"
+      @click="$emit('ask', question.text)"
+    >
+      {{ question.text }}
+    </button>
+    <button
+      class="text-foreground-2 hover:text-foreground focus:outline-none pr-6"
+      @click="$emit('toggleFavorite', question)"
+    >
+      <component
+        :is="question.favorite ? StarIconSolid : StarIconOutline"
+        class="w-5 h-5"
+      />
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { StarIcon as StarIconOutline } from '@heroicons/vue/24/outline'
+import { StarIcon as StarIconSolid } from '@heroicons/vue/24/solid'
+
+interface Question {
+  text: string
+  favorite: boolean
+}
+
+defineProps<{
+  question: Question
+}>()
+
+defineEmits<{
+  (e: 'ask', text: string): void
+  (e: 'toggleFavorite', question: Question): void
+}>()
+</script>
--- packages/frontend-2/components/specklebot/Wrapper.vue
+++ packages/frontend-2/components/specklebot/Wrapper.vue
@@ -1,6 +1,41 @@
 <template>
   <div>
-    <SpecklebotInput />
+    <SpecklebotWindow v-show="isInputVisible" @close="hideInput" />
   </div>
 </template>
-<script setup lang="ts"></script>
+
+<script setup lang="ts">
+import { ModifierKeys, onKeyboardShortcut } from '@speckle/ui-components'
+import { onKeyStroke } from '@vueuse/core'
+
+const isInputVisible = ref(false)
+
+const showInput = () => {
+  isInputVisible.value = true
+}
+
+const hideInput = () => {
+  isInputVisible.value = false
+}
+
+const toggleInput = () => {
+  if (isInputVisible.value) {
+    hideInput()
+  } else {
+    showInput()
+  }
+}
+
+const shortcutModifiers: ModifierKeys[] = [ModifierKeys.CtrlOrCmd]
+const shortcutKey = 'k'
+
+const handleShortcut = () => {
+  toggleInput()
+}
+
+onKeyboardShortcut(shortcutModifiers, shortcutKey, handleShortcut)
+
+onKeyStroke('Escape', () => {
+  hideInput()
+})
+</script>
