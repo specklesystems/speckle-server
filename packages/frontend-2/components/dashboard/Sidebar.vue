fa6c0f34ea6ff9d9e729640bd7a068cebf6eb249
Mike Tasset <mike.tasset@gmail.com>
2024-10-11 08:59:46 +0200

    Remove unused variable

--- packages/frontend-2/components/dashboard/Sidebar.vue
+++ packages/frontend-2/components/dashboard/Sidebar.vue
@@ -96,7 +96,7 @@
 
             <LayoutSidebarMenuGroup title="Resources" collapsible>
               <NuxtLink
-                :to="managerDownloadUrl"
+                :to="downloadManagerUrl"
                 target="_blank"
                 @click="isOpenMobile = false"
               >
@@ -189,7 +189,7 @@ import {
   projectsRoute,
   workspaceRoute,
   workspacesRoute,
-  managerDownloadUrl
+  downloadManagerUrl
 } from '~/lib/common/helpers/route'
 import { useRoute } from 'vue-router'
 import { useActiveUser } from '~~/lib/auth/composables/activeUser'
