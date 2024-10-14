8039446b00f119ee17d437f9016c2b8d12eca775
andrewwallacespeckle <andrew@speckle.systems>
2024-10-14 13:36:21 +0100

    Use visibility

--- packages/frontend-2/components/workspace/header/Header.vue
+++ packages/frontend-2/components/workspace/header/Header.vue
@@ -26,7 +26,7 @@
             v-if="hasOverflow"
             color="subtle"
             size="sm"
-            class="md:opacity-0 group-hover:opacity-100 md:pointer-events-none group-hover:pointer-events-auto items-center text-foreground text-body-2xs"
+            class="md:invisible group-hover:visible items-center text-foreground text-body-2xs"
             @click="showDescriptionDialog"
           >
             Read more
