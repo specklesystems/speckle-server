1ac4c354056a7f5b8e11e613553569b15e98f5b5
andrewwallacespeckle <andrew@speckle.systems>
2024-10-16 14:28:10 +0100

    Improve messaging in discord

--- packages/frontend-2/components/settings/workspaces/General/DeleteDialog.vue
+++ packages/frontend-2/components/settings/workspaces/General/DeleteDialog.vue
@@ -131,8 +131,8 @@ const onDelete = async () => {
     await sendWebhook('https://hooks.zapier.com/hooks/catch/12120532/2m4okri/', {
       userId: activeUser.value?.id ?? '',
       feedback: feedback.value
-        ? `Action: Workspace Deleted - ${feedback.value}`
-        : 'Action: Workspace Deleted - No feedback provided'
+        ? `Action: Workspace Deleted(${props.workspace.name}) - ${feedback.value}`
+        : `Action: Workspace Deleted(${props.workspace.name}) - No feedback provided`
     })
 
     triggerNotification({
