49128192deb003665fbac7f6fbe3f7d90ddc04aa
Gergő Jedlicska <gergo@jedlicska.com>
2024-10-17 12:11:22 +0200

    feat(gatekeeper): require a license for billing

--- packages/server/modules/gatekeeper/index.ts
+++ packages/server/modules/gatekeeper/index.ts
@@ -19,9 +19,19 @@ const gatekeeperModule: SpeckleModule = {
       )
 
     if (isInitial) {
-      if (FF_BILLING_INTEGRATION_ENABLED) app.use(billingRouter)
       // TODO: need to subscribe to the workspaceCreated event and store the workspacePlan as a trial if billing enabled, else store as unlimited
-      // TODO: create a cron job, that removes unused seats from the subscription at the beginning of each workspace plan's billing cycle
+      if (FF_BILLING_INTEGRATION_ENABLED) {
+        app.use(billingRouter)
+
+        const isLicenseValid = await validateModuleLicense({
+          requiredModules: ['billing']
+        })
+        if (!isLicenseValid)
+          throw new Error(
+            'The the billing module needs a valid license to run, contact Speckle to get one.'
+          )
+        // TODO: create a cron job, that removes unused seats from the subscription at the beginning of each workspace plan's billing cycle
+      }
     }
   }
 }
