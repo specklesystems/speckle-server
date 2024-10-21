66fc9ca5117ea5ce1160265d9cc71ad6fe1b1339
Gergő Jedlicska <57442769+gjedlicska@users.noreply.github.com>
2024-10-21 16:53:09 +0200

    feat(gatekeeper): fail fast if the environment is misconfigured for billing (#3357)

--- packages/server/modules/gatekeeper/rest/billing.ts
+++ packages/server/modules/gatekeeper/rest/billing.ts
@@ -44,38 +44,37 @@ import { GetWorkspacePlanPrice } from '@/modules/gatekeeper/domain/billing'
 import { WorkspaceAlreadyPaidError } from '@/modules/gatekeeper/errors/billing'
 import { withTransaction } from '@/modules/shared/helpers/dbHelper'
 
-const workspacePlanPrices = (): Record<
-  WorkspacePricingPlans,
-  Record<WorkspacePlanBillingIntervals, string> & { productId: string }
-> => ({
-  guest: {
-    productId: getStringFromEnv('WORKSPACE_GUEST_SEAT_STRIPE_PRODUCT_ID'),
-    monthly: getStringFromEnv('WORKSPACE_MONTHLY_GUEST_SEAT_STRIPE_PRICE_ID'),
-    yearly: getStringFromEnv('WORKSPACE_YEARLY_GUEST_SEAT_STRIPE_PRICE_ID')
-  },
-  team: {
-    productId: getStringFromEnv('WORKSPACE_TEAM_SEAT_STRIPE_PRODUCT_ID'),
-    monthly: getStringFromEnv('WORKSPACE_MONTHLY_TEAM_SEAT_STRIPE_PRICE_ID'),
-    yearly: getStringFromEnv('WORKSPACE_YEARLY_TEAM_SEAT_STRIPE_PRICE_ID')
-  },
-  pro: {
-    productId: getStringFromEnv('WORKSPACE_PRO_SEAT_STRIPE_PRODUCT_ID'),
-    monthly: getStringFromEnv('WORKSPACE_MONTHLY_PRO_SEAT_STRIPE_PRICE_ID'),
-    yearly: getStringFromEnv('WORKSPACE_YEARLY_PRO_SEAT_STRIPE_PRICE_ID')
-  },
-  business: {
-    productId: getStringFromEnv('WORKSPACE_BUSINESS_SEAT_STRIPE_PRODUCT_ID'),
-    monthly: getStringFromEnv('WORKSPACE_MONTHLY_BUSINESS_SEAT_STRIPE_PRICE_ID'),
-    yearly: getStringFromEnv('WORKSPACE_YEARLY_BUSINESS_SEAT_STRIPE_PRICE_ID')
-  }
-})
-
-const getWorkspacePlanPrice: GetWorkspacePlanPrice = ({
-  workspacePlan,
-  billingInterval
-}) => workspacePlanPrices()[workspacePlan][billingInterval]
-
 export const getBillingRouter = (): Router => {
+  const workspacePlanPrices: Record<
+    WorkspacePricingPlans,
+    Record<WorkspacePlanBillingIntervals, string> & { productId: string }
+  > = {
+    guest: {
+      productId: getStringFromEnv('WORKSPACE_GUEST_SEAT_STRIPE_PRODUCT_ID'),
+      monthly: getStringFromEnv('WORKSPACE_MONTHLY_GUEST_SEAT_STRIPE_PRICE_ID'),
+      yearly: getStringFromEnv('WORKSPACE_YEARLY_GUEST_SEAT_STRIPE_PRICE_ID')
+    },
+    team: {
+      productId: getStringFromEnv('WORKSPACE_TEAM_SEAT_STRIPE_PRODUCT_ID'),
+      monthly: getStringFromEnv('WORKSPACE_MONTHLY_TEAM_SEAT_STRIPE_PRICE_ID'),
+      yearly: getStringFromEnv('WORKSPACE_YEARLY_TEAM_SEAT_STRIPE_PRICE_ID')
+    },
+    pro: {
+      productId: getStringFromEnv('WORKSPACE_PRO_SEAT_STRIPE_PRODUCT_ID'),
+      monthly: getStringFromEnv('WORKSPACE_MONTHLY_PRO_SEAT_STRIPE_PRICE_ID'),
+      yearly: getStringFromEnv('WORKSPACE_YEARLY_PRO_SEAT_STRIPE_PRICE_ID')
+    },
+    business: {
+      productId: getStringFromEnv('WORKSPACE_BUSINESS_SEAT_STRIPE_PRODUCT_ID'),
+      monthly: getStringFromEnv('WORKSPACE_MONTHLY_BUSINESS_SEAT_STRIPE_PRICE_ID'),
+      yearly: getStringFromEnv('WORKSPACE_YEARLY_BUSINESS_SEAT_STRIPE_PRICE_ID')
+    }
+  }
+
+  const getWorkspacePlanPrice: GetWorkspacePlanPrice = ({
+    workspacePlan,
+    billingInterval
+  }) => workspacePlanPrices[workspacePlan][billingInterval]
   const router = Router()
 
   const stripe = new Stripe(getStripeApiKey(), { typescript: true })
