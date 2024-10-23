f2c32308652aeed93ca1b8ecb0814ce6705715ba
oguzhankoral <oguzhankoral@gmail.com>
2024-10-23 03:47:00 +0300

    POC revit view filters

--- packages/dui3/components/filter/ListSelect.vue
+++ packages/dui3/components/filter/ListSelect.vue
@@ -4,35 +4,42 @@
       <FormSelectBase
         v-model="selectedFilterName"
         name="sendFilter"
-        label="Avaialble send filters"
+        label="Selected filter"
         class="w-full"
         fixed-height
+        show-label
         :items="filterNames"
         :allow-unset="false"
         mount-menu-on-body
       >
         <template #something-selected="{ value }">
-          <span class="text-primary text-base">{{ value }}</span>
+          <span class="text-primary text-base text-sm">{{ value }}</span>
         </template>
         <template #option="{ item }">
-          <span class="text-base">{{ item }}</span>
+          <span class="text-base text-sm">{{ item }}</span>
         </template>
       </FormSelectBase>
     </div>
     <div v-if="selectedFilter">
-      <div v-if="selectedFilter.name === 'Everything'">
+      <div v-if="selectedFilter.id === 'everything'">
         <div class="p-4 text-primary bg-blue-500/10 rounded-md text-xs">
           All supported objects will be sent. Depending on the model, this might take a
           while.
         </div>
       </div>
-      <div v-else-if="selectedFilter.name === 'Selection'">
+      <div v-else-if="selectedFilter.id === 'selection'">
         <FilterSelection
           :filter="(selectedFilter as IDirectSelectionSendFilter)"
           @update:filter="(filter : ISendFilter) => (selectedFilter = filter)"
         />
       </div>
-      <div v-else-if="selectedFilter.name === 'Layers'">TODO</div>
+      <div v-else-if="selectedFilter.id === 'layers'">TODO</div>
+      <div v-else-if="selectedFilter.id === 'revitViews'">
+        <FilterRevitViews
+          :filter="(selectedFilter as RevitViewsSendFilter)"
+          @update:filter="(filter : ISendFilter) => (selectedFilter = filter)"
+        />
+      </div>
     </div>
     <div v-if="!!filter" class="text-xs caption rounded p-2 bg-orange-500/10">
       This action will replace the existing
@@ -42,7 +49,11 @@
   </div>
 </template>
 <script setup lang="ts">
-import type { ISendFilter, IDirectSelectionSendFilter } from 'lib/models/card/send'
+import type {
+  ISendFilter,
+  IDirectSelectionSendFilter,
+  RevitViewsSendFilter
+} from 'lib/models/card/send'
 import { useHostAppStore } from '~~/store/hostApp'
 import { storeToRefs } from 'pinia'
 
--- packages/dui3/lib/models/card/send.ts
+++ packages/dui3/lib/models/card/send.ts
@@ -9,6 +9,7 @@ export interface ISenderModelCard extends IModelCard {
 }
 
 export interface ISendFilter extends IDiscriminatedObject {
+  id: string
   name: string
   summary: string
   isDefault: boolean
@@ -17,6 +18,14 @@ export interface ISendFilter extends IDiscriminatedObject {
 
 export interface IDirectSelectionSendFilter extends ISendFilter {
   selectedObjectIds: string[]
+}
+
+export interface RevitViewsSendFilter extends ISendFilter {
+  selectedViewDiscipline: string
+  selectedViewFamily: string
+  selectedView: string
+  availableDisciplines: string[]
+  availableViews: Record<string, string[]>
 }
 
 export class SenderModelCard extends ModelCard implements ISenderModelCard {
