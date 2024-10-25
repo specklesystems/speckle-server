ca61e4f4a1fa6c6acc83c790e06ae70022101a96
Kristaps Fabians Geikins <fabis94@live.com>
2024-10-25 10:51:48 +0300

    ip.ts

--- packages/server/modules/shared/utils/ip.ts
+++ packages/server/modules/shared/utils/ip.ts
@@ -1,15 +1,18 @@
-const getIpFromRequest = (req) => {
-  let ip
+import { Optional } from '@speckle/shared'
+import type express from 'express'
+import type http from 'http'
+
+export const getIpFromRequest = (req: express.Request | http.IncomingMessage) => {
+  let ip: Optional<string> = undefined
   try {
-    ip =
-      req.headers['cf-connecting-ip'] ||
+    ip = (req.headers['cf-connecting-ip'] ||
       req.headers['true-client-ip'] ||
       req.headers['x-real-ip'] ||
       req.headers['x-forwarded-for'] ||
       req.headers['x-original-forwarded-for'] ||
-      req.ip ||
+      ('ip' in req ? req.ip : undefined) ||
       req.connection.remoteAddress ||
-      ''
+      '') as string
   } catch {
     ip = ''
   }
@@ -19,5 +22,3 @@ const getIpFromRequest = (req) => {
     if (ip.startsWith(ipPrefix) || ip === '') return null
   return ip
 }
-
-module.exports = { getIpFromRequest }
