f720ec26c4dbb8903ebc3e6072f3cff867e086e1
andrewwallacespeckle <andrew@speckle.systems>
2024-10-31 14:55:21 +0000

    Remove step ref

--- packages/frontend-2/components/tour/Onboarding.vue
+++ packages/frontend-2/components/tour/Onboarding.vue
@@ -2,27 +2,24 @@
   <div
     class="relative max-w-4xl w-screen h-[100dvh] flex items-center justify-center z-50"
   >
-    <TourSegmentation v-if="showSegmentation && step === 0" @next="step++" />
-    <TourSlideshow v-if="step === 1" @next="step++" />
+    <TourSegmentation v-if="showSegmentation" />
+    <TourSlideshow v-else @next="$emit('complete')" />
   </div>
 </template>
 <script setup lang="ts">
 import { useViewerTour } from '~/lib/viewer/composables/tour'
 import { useMixpanel } from '~~/lib/core/composables/mp'
 
+defineEmits(['complete'])
+
 const { showSegmentation } = useViewerTour()
 
-const step = ref(0)
-
 const mp = useMixpanel()
-watch(step, (val) => {
-  let stepName = 'segmentation'
-  if (val === 1) stepName = 'slideshow'
+watch(showSegmentation, (val) => {
   mp.track('Onboarding Action', {
     type: 'action',
     name: 'step-activation',
-    step: val,
-    stepName
+    stepName: val ? 'slideshow' : 'segmentation'
   })
 })
 </script>
