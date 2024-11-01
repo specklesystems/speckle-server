af7a5cb6efd8e2be5d08ef5d8086c6513848d40f
Gergő Jedlicska <gergo@jedlicska.com>
2024-11-01 10:23:51 +0100

    feat(multiregion): add projectRegion Db client lookup logic

--- packages/server/modules/multiregion/domain/operations.ts
+++ packages/server/modules/multiregion/domain/operations.ts
@@ -1,4 +1,9 @@
-import { MultiRegionConfig, ServerRegion } from '@/modules/multiregion/domain/types'
+import {
+  MultiRegionConfig,
+  ProjectRegion,
+  RegionKey,
+  ServerRegion
+} from '@/modules/multiregion/domain/types'
 import { InsertableRegionRecord } from '@/modules/multiregion/helpers/types'
 import { Optional } from '@speckle/shared'
 
@@ -15,3 +20,26 @@ export type GetFreeRegionKeys = () => Promise<string[]>
 export type CreateAndValidateNewRegion = (params: {
   region: InsertableRegionRecord
 }) => Promise<ServerRegion>
+
+export type RegionKeyLookupArgs = Pick<ProjectRegion, 'projectId'>
+
+// string is the regionKey, null is the main region, undefined is not found
+export type RegionKeyLookupResult = RegionKey
+type MaybeRegionKeyLookupResult = RegionKeyLookupResult | undefined
+
+export type SyncRegionKeyLookup = (
+  args: RegionKeyLookupArgs
+) => MaybeRegionKeyLookupResult
+
+export type CachedRegionKeyLookup = (
+  args: RegionKeyLookupArgs
+) => Promise<MaybeRegionKeyLookupResult>
+
+export type StorageRegionKeyLookup = (
+  args: RegionKeyLookupArgs
+) => Promise<MaybeRegionKeyLookupResult>
+
+type RegionKeyStoreArgs = { projectId: string; regionKey: string | null }
+export type SyncRegionKeyStore = (args: RegionKeyStoreArgs) => void
+// i want this guy to throw, to not care about nulls
+export type AsyncRegionKeyStore = (args: RegionKeyStoreArgs) => Promise<void>
