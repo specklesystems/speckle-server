152cf8800191cdee2d11366bd38d487ee1c4f9b3
AlexandruPopovici <alexandrupopoviciioan@gmail.com>
2024-11-05 22:30:01 +0200

    Fixed and polished the Catalogue extension

--- packages/viewer-sandbox/src/Extensions/Catalogue.ts
+++ packages/viewer-sandbox/src/Extensions/Catalogue.ts
@@ -1,6 +1,5 @@
 import {
   BatchObject,
-  type TreeNode,
   NodeRenderView,
   ObjectLayers,
   SpeckleText,
@@ -29,6 +28,22 @@ interface Animation {
   current: Vector3
   radialEnd: Vector3
   time: number
+}
+
+interface Box {
+  x: number
+  y: number
+  w: number
+  h: number
+}
+
+interface ObjectBox extends Box {
+  object: BatchObject
+}
+
+interface CategoryBox extends Box {
+  category: string
+  boxes: Array<ObjectBox>
 }
 
 export class Catalogue extends Extension {
@@ -80,7 +95,9 @@ export class Catalogue extends Extension {
     }
 
     /** If any animations updated, request a render */
-    if (animCount) this.viewer.requestRender()
+    if (animCount) {
+      this.viewer.requestRender()
+    }
   }
 
   public onRender() {
@@ -105,198 +122,174 @@ export class Catalogue extends Extension {
   }
 
   public wipe() {
-    this.viewer.getRenderer().scene.remove(this.textGroup)
+    this.animations = []
   }
 
   /** Example's main function */
   public async categorize(input: Array<{ ids: Array<string>; value: string }>) {
-    this.wipe()
+    if (this.animations.length) return
 
-    const categories: { [id: string]: TreeNode[] } = {}
-    this.textGroup = new Group()
+    const padding = 0.5
+    const categoryPadding = 10
+    const origin = new Vector3(0, 0, 0)
+
+    const objectBoxes: { [id: string]: ObjectBox } = {}
+    const categories: { [categoryName: string]: CategoryBox } = {}
 
     for (const cat of input) {
+      const boxes: ObjectBox[] = []
       for (let k = 0; k < cat.ids.length; k++) {
         const nodes = this.viewer.getWorldTree().findId(cat.ids[k])
         if (!nodes) continue
+
+        /** Just get the first node */
         const node = nodes[0]
-        // if (!node.model.atomic || this.viewer.getWorldTree().isRoot(node)) continue
-        if (!categories[cat.value]) categories[cat.value] = []
-        categories[cat.value].push(node)
-      }
-    }
 
-    /** We go over each category */
-    const finalBoxes = Array<{
-      category: string
-      obj: BatchObject
-      box: Box3
-    }>()
-    const categoryBoxes = []
-    const origin = new Vector3(0, 0, 0)
-    const padding = 0.5
-    const categoryPadding = 5
-    for (const cat in categories) {
-      const group = categories[cat]
-      const groupBoxes: {
-        obj: BatchObject
-        w: number
-        h: number
-        x: number
-        y: number
-      }[] = []
-
-      /** We go over each node in the category */
-      for (let k = 0; k < group.length; k++) {
-        const node = group[k]
-        /** Get the render views */
         const rvs = this.viewer
           .getWorldTree()
           .getRenderTree()
           .getRenderViewsForNode(node)
 
-        /** Get the batch objects which we'll animate */
-        const objects = rvs
+        const objects: BatchObject[] = rvs
           .map((rv: NodeRenderView) => {
             return this.viewer.getRenderer().getObject(rv)
           })
-          .filter((value) => value)
+          .filter((value: BatchObject | null) => {
+            return value && !objectBoxes[value.renderView.renderData.id]
+          }) as BatchObject[]
 
-        /** Compute the union of all the batch objects bounds in the node */
-        objects.forEach((obj: BatchObject | null) => {
-          if (!obj) return
-          const aabbSize = obj?.aabb.getSize(new Vector3())
-          groupBoxes.push({
-            obj,
+        objects.forEach((object: BatchObject) => {
+          if (!object) return
+          const aabbSize = object?.aabb.getSize(new Vector3())
+          const box = {
+            object,
             w: aabbSize.x + padding,
             h: aabbSize.y + padding,
             x: 0,
             y: 0
-          })
+          }
+          objectBoxes[object.renderView.renderData.id] = box
+          boxes.push(box)
         })
-      }
-      const { w, h, fill } = potpack(groupBoxes)
-      categoryBoxes.push({
-        category: cat,
-        w: w + categoryPadding,
-        h: h + categoryPadding,
-        x: 0,
-        y: 0
-      })
-      console.warn('Groups size -> ', cat, ':', w, h, fill)
-      for (let k = 0; k < groupBoxes.length; k++) {
-        const box = new Box3(
-          new Vector3(groupBoxes[k].x + origin.x, groupBoxes[k].y + origin.y, 0),
-          new Vector3(
-            groupBoxes[k].x + origin.x + groupBoxes[k].w,
 
-            groupBoxes[k].y + origin.y + groupBoxes[k].h,
+        if (!objects.length) continue
+
+        const { w, h } = potpack(boxes)
+        categories[cat.value] = {
+          category: cat.value,
+          boxes,
+          w: w + categoryPadding,
+          h: h + categoryPadding,
+          x: 0,
+          y: 0
+        }
+      }
+      potpack(Object.values(categories))
+      console.log(categories)
+    }
+
+    for (const k in categories) {
+      for (let i = 0; i < categories[k].boxes.length; i++) {
+        const objectBox = categories[k].boxes[i]
+        const box3 = new Box3(
+          new Vector3(objectBox.x + origin.x, objectBox.y + origin.y, 0),
+          new Vector3(
+            objectBox.x + origin.x + objectBox.w,
+            objectBox.y + origin.y + objectBox.h,
             0
           )
+        ).applyMatrix4(
+          new Matrix4().makeTranslation(categories[k].x, categories[k].y, 0)
         )
-        finalBoxes.push({
-          category: cat,
-          obj: groupBoxes[k].obj,
-          box
+
+        const boxHelper = new Box3Helper(box3, new Color(0x047efb))
+        /** Set the layers to PROPS, so that AO and interactions will ignore them */
+        boxHelper.layers.set(ObjectLayers.OVERLAY)
+        boxHelper.frustumCulled = false
+        /** Add the BoxHelper to the scene */
+        // this.viewer.getRenderer().scene.add(boxHelper)
+
+        const bObj = objectBox.object
+        const boxCenter = box3.getCenter(new Vector3())
+        const aabbCenter = bObj.aabb.getCenter(new Vector3())
+        const aabbSize = bObj.aabb.getSize(new Vector3())
+        const finalPos = new Vector3()
+          .copy(boxCenter)
+          .sub(aabbCenter.sub(new Vector3(0, 0, aabbSize.z * 0.5)))
+
+        const theta = Math.random() * 2 * Math.PI
+        const radius = Math.random() * 150
+        const x = radius * Math.cos(theta)
+        const y = radius * Math.sin(theta)
+
+        const finalRadial = boxCenter.sub(new Vector3(x, y, 0))
+        this.animations.push({
+          target: bObj,
+          start: new Vector3(),
+          end: finalPos,
+          current: new Vector3(),
+          time: 0,
+          radialEnd: finalRadial
         })
       }
-      //   offset += w + 5
     }
-    potpack(categoryBoxes)
 
-    for (let k = 0; k < finalBoxes.length; k++) {
-      const categoryBox = categoryBoxes.find(
-        (value) => value.category === finalBoxes[k].category
-      )
-      if (!categoryBox) return
+    for (const categoryBox in categories) {
+      /** Create a speckle text object */
+      const text = new SpeckleText('test-text', ObjectLayers.OVERLAY)
 
-      const box = finalBoxes[k].box.applyMatrix4(
-        new Matrix4().makeTranslation(categoryBox.x, categoryBox.y, 0)
+      /** Simple text material */
+      const material = new SpeckleTextMaterial(
+        {
+          color: 0x1a1a1a,
+          opacity: 1,
+          side: DoubleSide
+        },
+        ['USE_RTE', 'BILLBOARD_FIXED']
       )
-      const boxHelper = new Box3Helper(box, new Color(0x047efb))
+      material.toneMapped = false
+      material.color.convertSRGBToLinear()
+      material.opacity = 1
+      material.transparent = false
+      material.depthTest = false
+      material.billboardPixelHeight = 20
+      material.userData.billboardPos.value.copy(text.position)
+      ;(text.textMesh as unknown as Mesh).material = material.getDerivedMaterial()
+
+      if (text.backgroundMesh) text.backgroundMesh.renderOrder = 3
+      ;(text.textMesh as unknown as Mesh).renderOrder = 4
+
       /** Set the layers to PROPS, so that AO and interactions will ignore them */
-      boxHelper.layers.set(ObjectLayers.OVERLAY)
-      boxHelper.frustumCulled = false
-      /** Add the BoxHelper to the scene */
-      //   this.viewer.getRenderer().scene.add(boxHelper)
-
-      const bObj = finalBoxes[k].obj
-      const boxCenter = box.getCenter(new Vector3())
-      const aabbCenter = bObj.aabb.getCenter(new Vector3())
-      const aabbSize = bObj.aabb.getSize(new Vector3())
-      const finalPos = new Vector3()
-        .copy(boxCenter)
-        .sub(aabbCenter.sub(new Vector3(0, 0, aabbSize.z * 0.5)))
-
-      const theta = Math.random() * 2 * Math.PI
-      const radius = Math.random() * 150
-      const x = radius * Math.cos(theta)
-      const y = radius * Math.sin(theta)
-
-      const finalRadial = boxCenter.sub(new Vector3(x, y, 0))
-      this.animations.push({
-        target: finalBoxes[k].obj,
-        start: new Vector3(),
-        end: finalPos,
-        current: new Vector3(),
-        time: 0,
-        radialEnd: finalRadial
-      })
+      text.layers.set(ObjectLayers.OVERLAY)
+      ;(text.textMesh as unknown as Mesh).layers.set(ObjectLayers.OVERLAY)
+      /** Update the text with the cateogry name, size and anchor */
+      await text
+        .update({
+          textValue: categories[categoryBox].category,
+          height: 1,
+          anchorX: '50%',
+          anchorY: '43%'
+        })
+        .then(() => {
+          text.style = {
+            textColor: new Color(0x1a1a1a),
+            backgroundColor: new Color(0xffffff),
+            billboard: true,
+            backgroundPixelHeight: 20
+          }
+          /** Move the text to the bottom center of the category box */
+          text.setTransform(
+            new Vector3(
+              origin.x + categories[categoryBox].x,
+              origin.y + categories[categoryBox].y,
+              0
+            )
+          )
+        })
+      /** Add the text to the scene */
+      this.textGroup.add(text)
     }
-
-    // for (const categoryBox of categoryBoxes) {
-    //   /** Create a speckle text object */
-    //   const text = new SpeckleText('test-text', ObjectLayers.OVERLAY)
-
-    //   /** Simple text material */
-    //   const material = new SpeckleTextMaterial(
-    //     {
-    //       color: 0x1a1a1a,
-    //       opacity: 1,
-    //       side: DoubleSide
-    //     },
-    //     ['USE_RTE', 'BILLBOARD_FIXED']
-    //   )
-    //   material.toneMapped = false
-    //   material.color.convertSRGBToLinear()
-    //   material.opacity = 1
-    //   material.transparent = false
-    //   material.depthTest = false
-    //   material.billboardPixelHeight = 20
-    //   material.userData.billboardPos.value.copy(text.position)(
-    //     text.textMesh as unknown as Mesh
-    //   ).material = material.getDerivedMaterial()
-
-    //   if (text.backgroundMesh) text.backgroundMesh.renderOrder = 3
-    //   ;(text.textMesh as unknown as Mesh).renderOrder = 4
-
-    //   /** Set the layers to PROPS, so that AO and interactions will ignore them */
-    //   text.layers.set(ObjectLayers.OVERLAY)
-    //   ;(text.textMesh as unknown as Mesh).layers.set(ObjectLayers.OVERLAY)
-    //   /** Update the text with the cateogry name, size and anchor */
-    //   await text
-    //     .update({
-    //       textValue: categoryBox.category,
-    //       height: 1,
-    //       anchorX: '50%',
-    //       anchorY: '43%'
-    //     })
-    //     .then(() => {
-    //       text.style = {
-    //         textColor: new Color(0x1a1a1a),
-    //         backgroundColor: new Color(0xffffff),
-    //         billboard: true,
-    //         backgroundPixelHeight: 20
-    //       }
-    //       /** Move the text to the bottom center of the category box */
-    //       text.setTransform(
-    //         new Vector3(origin.x + categoryBox.x, origin.y + categoryBox.y, 0)
-    //       )
-    //     })
-    //   /** Add the text to the scene */
-    //   this.textGroup.add(text)
-    // }
-    // this.viewer.getRenderer().scene.add(this.textGroup)
+    this.viewer.getRenderer().scene.add(this.textGroup)
   }
 
   private easeOutQuart(x: number): number {
