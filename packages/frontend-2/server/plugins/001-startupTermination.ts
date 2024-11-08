3e1cdfb558e50b724799227898e96d89e70d0fa2
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2024-11-08 17:48:07 +0000

    feat(frontend-2/logging): log the public runtime config on app initialization

--- packages/frontend-2/server/plugins/001-startupTermination.ts
+++ packages/frontend-2/server/plugins/001-startupTermination.ts
@@ -1,6 +1,9 @@
 export default defineNitroPlugin((nitroApp) => {
   const logger = useLogger()
-  logger.info('Starting up the server, hello!')
+  logger.info(
+    { publicRuntimeConfig: useRuntimeConfig().public },
+    'Starting up the server, hello!'
+  )
 
   nitroApp.hooks.hook('close', () => {
     logger.warn('Closing down the server, bye bye!')
