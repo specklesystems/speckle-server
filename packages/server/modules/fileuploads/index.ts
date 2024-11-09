ffe9b028141605f38b37584b8422c83b3552952f
Gergő Jedlicska <gergo@jedlicska.com>
2024-11-09 16:29:39 +0100

    feat(fileuploads, blobs): multi region

--- packages/server/modules/fileuploads/index.ts
+++ packages/server/modules/fileuploads/index.ts
@@ -3,7 +3,11 @@ import { insertNewUploadAndNotifyFactory } from '@/modules/fileuploads/services/
 import request from 'request'
 import { authMiddlewareCreator } from '@/modules/shared/middleware'
 import { moduleLogger } from '@/logging/logging'
-import { listenForImportUpdatesFactory } from '@/modules/fileuploads/services/resultListener'
+import {
+  onFileImportProcessedFactory,
+  onFileProcessingFactory,
+  parseMessagePayload
+} from '@/modules/fileuploads/services/resultListener'
 import {
   getFileInfoFactory,
   saveUploadFileFactory
@@ -19,43 +23,8 @@ import { getStreamFactory } from '@/modules/core/repositories/streams'
 import { addBranchCreatedActivityFactory } from '@/modules/activitystream/services/branchActivity'
 import { saveActivityFactory } from '@/modules/activitystream/repositories'
 import { getPort } from '@/modules/shared/helpers/envHelper'
-
-const insertNewUploadAndNotify = insertNewUploadAndNotifyFactory({
-  getStreamBranchByName: getStreamBranchByNameFactory({ db }),
-  saveUploadFile: saveUploadFileFactory({ db }),
-  publish
-})
-const getStream = getStreamFactory({ db })
-
-const saveFileUploads = async ({
-  userId,
-  streamId,
-  branchName,
-  uploadResults
-}: {
-  userId: string
-  streamId: string
-  branchName: string
-  uploadResults: Array<{
-    blobId: string
-    fileName: string
-    fileSize: number
-  }>
-}) => {
-  await Promise.all(
-    uploadResults.map(async (upload) => {
-      await insertNewUploadAndNotify({
-        fileId: upload.blobId,
-        streamId,
-        branchName,
-        userId,
-        fileName: upload.fileName,
-        fileType: upload.fileName.split('.').pop()!,
-        fileSize: upload.fileSize
-      })
-    })
-  )
-}
+import { getProjectDbClient } from '@/modules/multiregion/dbSelector'
+import { listenFor } from '@/modules/core/utils/dbNotificationListener'
 
 export const init: SpeckleModule['init'] = async (app, isInitial) => {
   if (process.env.DISABLE_FILE_UPLOADS) {
@@ -67,13 +36,16 @@ export const init: SpeckleModule['init'] = async (app, isInitial) => {
 
   app.post(
     '/api/file/:fileType/:streamId/:branchName?',
-    authMiddlewareCreator(
-      streamWritePermissionsPipelineFactory({
-        getRoles: getRolesFactory({ db }),
-        getStream,
-        getAutomationProject: getAutomationProjectFactory({ db })
-      })
-    ),
+    async (req, res, next) => {
+      const projectDb = await getProjectDbClient({ projectId: req.params.streamId })
+      await authMiddlewareCreator(
+        streamWritePermissionsPipelineFactory({
+          getRoles: getRolesFactory({ db }),
+          getStream: getStreamFactory({ db: projectDb }),
+          getAutomationProject: getAutomationProjectFactory({ db: projectDb })
+        })
+      )(req, res, next)
+    },
     async (req, res) => {
       const branchName = req.params.branchName || 'main'
       req.log = req.log.child({
@@ -81,54 +53,103 @@ export const init: SpeckleModule['init'] = async (app, isInitial) => {
         userId: req.context.userId,
         branchName
       })
-      req.pipe(
-        //TODO refactor packages/server/modules/blobstorage/index.js to use the service pattern, and then refactor this to call the service directly from here without the http overhead
-        request(
-          // we call this same server on localhost (IPv4) to upload the blob and do not make an external call
-          `http://127.0.0.1:${getPort()}/api/stream/${req.params.streamId}/blob`,
-          async (err, response, body) => {
-            if (err) {
-              res.log.error(err, 'Error while uploading blob.')
-              res.status(500).send(err.message)
-              return
-            }
-            if (response.statusCode === 201) {
-              const { uploadResults } = JSON.parse(body)
-              await saveFileUploads({
-                userId: req.context.userId!,
-                streamId: req.params.streamId,
-                branchName,
-                uploadResults
-              })
-            } else {
-              res.log.error(
-                {
-                  statusCode: response.statusCode,
-                  path: `http://127.0.0.1:${getPort()}/api/stream/${
-                    req.params.streamId
-                  }/blob`
-                },
-                'Error while uploading file.'
-              )
-            }
-            res.status(response.statusCode).send(body)
-          }
+
+      const projectDb = await getProjectDbClient({ projectId: req.params.streamId })
+      const insertNewUploadAndNotify = insertNewUploadAndNotifyFactory({
+        getStreamBranchByName: getStreamBranchByNameFactory({ db: projectDb }),
+        saveUploadFile: saveUploadFileFactory({ db: projectDb }),
+        publish
+      })
+      const saveFileUploads = async ({
+        userId,
+        streamId,
+        branchName,
+        uploadResults
+      }: {
+        userId: string
+        streamId: string
+        branchName: string
+        uploadResults: Array<{
+          blobId: string
+          fileName: string
+          fileSize: number
+        }>
+      }) => {
+        await Promise.all(
+          uploadResults.map(async (upload) => {
+            await insertNewUploadAndNotify({
+              fileId: upload.blobId,
+              streamId,
+              branchName,
+              userId,
+              fileName: upload.fileName,
+              fileType: upload.fileName.split('.').pop()!,
+              fileSize: upload.fileSize
+            })
+          })
         )
+      }
+      //TODO refactor packages/server/modules/blobstorage/index.js to use the service pattern, and then refactor this to call the service directly from here without the http overhead
+      const pipedReq = request(
+        // we call this same server on localhost (IPv4) to upload the blob and do not make an external call
+        `http://127.0.0.1:${getPort()}/api/stream/${req.params.streamId}/blob`,
+        async (err, response, body) => {
+          if (err) {
+            res.log.error(err, 'Error while uploading blob.')
+            res.status(500).send(err.message)
+            return
+          }
+          if (response.statusCode === 201) {
+            const { uploadResults } = JSON.parse(body)
+            await saveFileUploads({
+              userId: req.context.userId!,
+              streamId: req.params.streamId,
+              branchName,
+              uploadResults
+            })
+          } else {
+            res.log.error(
+              {
+                statusCode: response.statusCode,
+                path: `http://127.0.0.1:${getPort()}/api/stream/${
+                  req.params.streamId
+                }/blob`
+              },
+              'Error while uploading file.'
+            )
+          }
+          res.status(response.statusCode).send(body)
+        }
       )
+
+      // @ts-expect-error for some reason, buffer types don't fully match
+      req.pipe(pipedReq)
     }
   )
 
   if (isInitial) {
-    const listenForImportUpdates = listenForImportUpdatesFactory({
-      getFileInfo: getFileInfoFactory({ db }),
-      publish,
-      getStreamBranchByName: getStreamBranchByNameFactory({ db }),
-      addBranchCreatedActivity: addBranchCreatedActivityFactory({
+    listenFor('file_import_update', async (msg) => {
+      const parsedMessage = parseMessagePayload(msg.payload)
+      if (!parsedMessage.streamId) return
+      const projectDb = await getProjectDbClient({ projectId: parsedMessage.streamId })
+      await onFileImportProcessedFactory({
+        getFileInfo: getFileInfoFactory({ db: projectDb }),
         publish,
-        saveActivity: saveActivityFactory({ db })
-      })
+        getStreamBranchByName: getStreamBranchByNameFactory({ db: projectDb }),
+        addBranchCreatedActivity: addBranchCreatedActivityFactory({
+          publish,
+          saveActivity: saveActivityFactory({ db })
+        })
+      })(parsedMessage)
     })
-
-    listenForImportUpdates()
+    listenFor('file_import_started', async (msg) => {
+      const parsedMessage = parseMessagePayload(msg.payload)
+      if (!parsedMessage.streamId) return
+      const projectDb = await getProjectDbClient({ projectId: parsedMessage.streamId })
+      await onFileProcessingFactory({
+        getFileInfo: getFileInfoFactory({ db: projectDb }),
+        publish
+      })(parsedMessage)
+    })
   }
 }
