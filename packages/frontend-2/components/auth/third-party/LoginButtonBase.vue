1f846dac4c958b8b0cba1290efe3309c92c77bfa
andrewwallacespeckle <andrew@speckle.systems>
2024-11-13 13:10:31 +0000

    Remove FormButton from LoginButtonBase

--- packages/frontend-2/components/auth/third-party/LoginButtonBase.vue
+++ packages/frontend-2/components/auth/third-party/LoginButtonBase.vue
@@ -1,10 +1,10 @@
 <template>
   <NuxtLink :to="to">
-    <FormButton color="outline" size="lg" full-width class="group">
-      <div class="flex items-center justify-center">
-        <slot />
-      </div>
-    </FormButton>
+    <div
+      class="group inline-flex justify-center items-center text-center select-none whitespace-nowrap outline outline-2 outline-transparent transition duration-200 ease-in-out focus-visible:outline-outline-4 rounded-md border w-full h-10 text-body-sm bg-foundation border-outline-2 text-foreground font-medium hover:bg-primary-muted disabled:hover:bg-foundation focus-visible:border-foundation px-6 py-2"
+    >
+      <slot />
+    </div>
   </NuxtLink>
 </template>
 <script setup lang="ts">
