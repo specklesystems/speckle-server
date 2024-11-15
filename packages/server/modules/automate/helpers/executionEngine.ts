d1b7dfc24e825e50e199314b3c4a7b2e0f8dde01
Charles Driesler <chuck@speckle.systems>
2024-11-15 18:24:56 +0000

    fix(automate): drop specklecon function templates

--- packages/server/modules/automate/helpers/executionEngine.ts
+++ packages/server/modules/automate/helpers/executionEngine.ts
@@ -47,18 +47,6 @@ export const functionTemplateRepos = <const>[
     title: '.NET / C#',
     url: 'https://github.com/specklesystems/SpeckleAutomateDotnetExample',
     logo: '/images/functions/dotnet.svg'
-  },
-  {
-    id: AutomateFunctionTemplateLanguage.Demonstration,
-    title: 'SpeckleCon 2024 - Thermal',
-    url: 'https://github.com/specklesystems/Thermal-Resistance-Checker-Template',
-    logo: '/images/functions/specklecon.svg'
-  },
-  {
-    id: AutomateFunctionTemplateLanguage.Demonstrationpython,
-    title: 'SpeckleCon 2024 - QAQC',
-    url: 'https://github.com/specklesystems/SpeckleConWorkshop-QAQC',
-    logo: '/images/functions/specklecon.svg'
   }
 ]
 
