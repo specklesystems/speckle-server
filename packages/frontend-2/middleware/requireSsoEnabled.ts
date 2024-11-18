9ecd4bf115bf15879c9568edff74153c90299d52
andrewwallacespeckle <andrew@speckle.systems>
2024-11-18 14:47:03 +0000

    Skip middleware during auth flow

--- packages/frontend-2/middleware/requireSsoEnabled.ts
+++ packages/frontend-2/middleware/requireSsoEnabled.ts
@@ -5,6 +5,10 @@ import { useWorkspacePublicSsoCheck } from '~/lib/workspaces/composables/sso'
  * Used to validate that the workspace has SSO enabled, redirects to workspace page if not
  */
 export default defineNuxtRouteMiddleware(async (to) => {
+  if (to.query.access_code) {
+    return
+  }
+
   const workspaceSlug = to.params.slug as string
   if (!workspaceSlug) return
 
