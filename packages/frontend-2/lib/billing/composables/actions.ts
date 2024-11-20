8c8f3d8a903af9c8a531c32e366214ffd4dde10e
Mike <mike.tasset@gmail.com>
2024-11-20 09:05:42 +0100

    Added billig mixpanel events (#3521)

--- packages/frontend-2/lib/billing/composables/actions.ts
+++ packages/frontend-2/lib/billing/composables/actions.ts
@@ -5,14 +5,21 @@ import type {
   BillingInterval
 } from '~/lib/common/generated/gql/graphql'
 import { settingsBillingCancelCheckoutSessionMutation } from '~/lib/settings/graphql/mutations'
+import { useMixpanel } from '~/lib/core/composables/mp'
 
 export const useBillingActions = () => {
+  const mixpanel = useMixpanel()
   const { client: apollo } = useApolloClient()
   const { mutate: cancelCheckoutSessionMutation } = useMutation(
     settingsBillingCancelCheckoutSessionMutation
   )
 
   const billingPortalRedirect = async (workspaceId: string) => {
+    mixpanel.track('Billing Portal Button Clicked', {
+      // eslint-disable-next-line camelcase
+      workspace_id: workspaceId
+    })
+
     // We need to fetch this on click because the link expires very quickly
     const result = await apollo.query({
       query: settingsWorkspaceBillingCustomerPortalQuery,
@@ -31,12 +38,23 @@ export const useBillingActions = () => {
     workspaceId: string
   }) => {
     const { plan, cycle, workspaceId } = args
+    mixpanel.track('Upgrade Button Clicked', {
+      plan,
+      cycle,
+      // eslint-disable-next-line camelcase
+      workspace_id: workspaceId
+    })
     window.location.href = `/api/v1/billing/workspaces/${workspaceId}/checkout-session/${plan}/${cycle}`
   }
 
   const cancelCheckoutSession = async (sessionId: string, workspaceId: string) => {
     await cancelCheckoutSessionMutation({
       input: { sessionId, workspaceId }
+    })
+
+    mixpanel.track('Checkout Session Cancelled', {
+      // eslint-disable-next-line camelcase
+      workspace_id: workspaceId
     })
   }
 
