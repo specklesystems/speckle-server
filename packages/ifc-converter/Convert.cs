c3fd0e0dab632a0840ce52f33b8319f2be5dc674
Adam Hathcock <adam@hathcock.uk>
2024-11-20 10:43:42 +0000

    some updates

--- packages/ifc-converter/Convert.cs
+++ packages/ifc-converter/Convert.cs
@@ -37,3 +37,33 @@ public static class Convert
     await operations.Send((Base)null, transport, true, null);
   }
 }
+
+public class ConsoleProgress : IProgress<ProgressArgs>
+{
+  private readonly TimeSpan DEBOUNCE = TimeSpan.FromSeconds(1);
+  private DateTime _lastTime = DateTime.UtcNow;
+
+  private long _totalBytes;
+
+  public void Report(ProgressArgs value)
+  {
+    if (value.ProgressEvent == ProgressEvent.DownloadBytes)
+    {
+      Interlocked.Add(ref _totalBytes, value.Count);
+    }
+    var now = DateTime.UtcNow;
+    if (now - _lastTime >= DEBOUNCE)
+    {
+      if (value.ProgressEvent == ProgressEvent.DownloadBytes)
+      {
+        Console.WriteLine(value.ProgressEvent + " t " + _totalBytes);
+      }
+      else
+      {
+        Console.WriteLine(value.ProgressEvent + " c " + value.Count + " t " + value.Total);
+      }
+
+      _lastTime = now;
+    }
+  }
+}
--- packages/ifc-converter/Program.cs
+++ packages/ifc-converter/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System.CommandLine;
+using Microsoft.Extensions.DependencyInjection;
 using Speckle.Converter;
+using Speckle.Sdk.Common;
+using Speckle.Sdk.Credentials;
+using Speckle.WebIfc.Importer;
 
+/*
 var filePathArgument = new Argument<string>
 (name: "filePath");
 var userIdArgument = new Argument<string>
@@ -25,4 +30,15 @@ rootCommand.SetHandler( (filePath, userId, streamId, branchName, commitMessage, 
 {
   Speckle.Converter.Convert.Ifc(filePath, userId, streamId, branchName, commitMessage, token);
 }, filePathArgument, userIdArgument, streamIdArgument, branchNameArgument, commitMessageArgument, tokenArgument);
-await rootCommand.InvokeAsync(args);
+await rootCommand.InvokeAsync(args);*/
+
+
+
+var serviceProvider = Import.GetServiceProvider();
+var  account = serviceProvider.GetRequiredService<IAccountManager>().GetDefaultAccount().NotNull();
+var url = "https://latest.speckle.systems/";
+var streamId = "aa44fe8d6a";
+var modelId = "8f4b7fa648";
+var filePath = "C:\\Users\\adam\\Git\\IFC-toolkit\\test-files\\ISSUE_068_ARK_NUS_skolebygg.ifc";
+
+await  Import.Ifc(serviceProvider, url, filePath, streamId, modelId, "ifc test", account.token);
