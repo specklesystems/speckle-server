563375d8f8dbe12f1a31ab43b7848a5de13d5ddc
andrewwallacespeckle <andrew@speckle.systems>
2024-11-21 11:49:36 +0000

    Small fix to index

--- packages/frontend-2/pages/workspaces/[slug]/sso/index.vue
+++ packages/frontend-2/pages/workspaces/[slug]/sso/index.vue
@@ -72,7 +72,7 @@ const mixpanel = useMixpanel()
 
 const workspaceSlug = computed(() => route.params.slug as string)
 const { isSsoAuthenticated } = useWorkspaceSsoStatus({
-  workspaceSlug: computed(() => workspaceSlug.value)
+  workspaceSlug
 })
 
 const { result } = useQuery(authRegisterPanelQuery, {
