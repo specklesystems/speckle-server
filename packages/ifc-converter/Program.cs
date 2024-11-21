e60d7b980e44d091a8d6ca3de49bfd1eb154ed72
adamhathcock <adam@speckle.systems>
2024-11-21 12:49:08 +0000

    update importer to output stuff

--- packages/ifc-converter/Program.cs
+++ packages/ifc-converter/Program.cs
@@ -1,40 +1,46 @@
 ﻿using System.CommandLine;
+using System.Text.Json;
 using Speckle.Sdk.Common;
 using Speckle.WebIfc.Importer;
 
 var filePathArgument = new Argument<string>(name: "filePath");
-var userIdArgument = new Argument<string>("userId");
+var outputPathArgument = new Argument<string>("outputPath");
 var streamIdArgument = new Argument<string>("streamId");
-var branchNameArgument = new Argument<string>("branchName");
 var commitMessageArgument = new Argument<string>("commitMessage");
-var fileIdArgument = new Argument<string>("fileId");
 var modelIdArgument = new Argument<string>("modelId");
 var regionNameArgument = new Argument<string>("regionName");
 
 var rootCommand = new RootCommand
 {
   filePathArgument,
-  userIdArgument,
+  outputPathArgument,
   streamIdArgument,
-  branchNameArgument,
   commitMessageArgument,
-  fileIdArgument,
   modelIdArgument,
   regionNameArgument,
 };
 rootCommand.SetHandler(
-  async (filePath, _, streamId, _, commitMessage, _, modelId, _) =>
+  async (filePath, outputPath, streamId, commitMessage, modelId, _) =>
   {
-    var token = Environment.GetEnvironmentVariable("USER_TOKEN").NotNull("USER_TOKEN is missing");
-    var url = Environment.GetEnvironmentVariable("SPECKLE_SERVER_URL") ?? "http://127.0.0.1:3000";
-    await Import.Ifc(url, filePath, streamId, modelId, commitMessage, token);
+    try
+    {
+      var token = Environment.GetEnvironmentVariable("USER_TOKEN").NotNull("USER_TOKEN is missing");
+      var url = Environment.GetEnvironmentVariable("SPECKLE_SERVER_URL") ?? "http://127.0.0.1:3000";
+      var commitId = await Import.Ifc(url, filePath, streamId, modelId, commitMessage, token);
+      File.WriteAllText(outputPath, JsonSerializer.Serialize(new { success = true, commitId }));
+    }
+    catch (Exception e)
+    {
+      File.WriteAllText(
+        outputPath,
+        JsonSerializer.Serialize(new { success = false, error = e.ToString() })
+      );
+    }
   },
   filePathArgument,
-  userIdArgument,
+  outputPathArgument,
   streamIdArgument,
-  branchNameArgument,
   commitMessageArgument,
-  fileIdArgument,
   modelIdArgument,
   regionNameArgument
 );
