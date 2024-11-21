a37a7fc88a75223310453fb950eeb0c9a3dc1f96
Mike <mike.tasset@gmail.com>
2024-11-21 15:32:13 +0100

    Fix: Invite banners borders (#3539)

--- packages/frontend-2/lib/projects/graphql/queries.ts
+++ packages/frontend-2/lib/projects/graphql/queries.ts
@@ -33,7 +33,6 @@ export const projectsDashboardQuery = graphql(`
           ...ProjectDashboardItem
         }
       }
-      ...ProjectsInviteBanners
       ...ProjectsDashboardHeaderProjects_User
     }
   }
