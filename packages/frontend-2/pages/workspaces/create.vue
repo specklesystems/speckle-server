ec701c54adf1cab86145d417686d20e11cecaa74
Mike Tasset <mike.tasset@gmail.com>
2024-11-28 10:37:12 +0100

    Updated layout

--- packages/frontend-2/pages/workspaces/create.vue
+++ packages/frontend-2/pages/workspaces/create.vue
@@ -1,11 +1,26 @@
 <template>
-  <div>
-    <WorkspaceWizard />
+  <div class="bg-gradient-to-b from-white to-purple-50">
+    <nav
+      class="fixed top-0 h-14 bg-foundation w-full shadow flex items-center justify-between px-2"
+    >
+      <HeaderLogoBlock :active="false" class="mr-0" no-link />
+      <FormButton color="outline">Cancel</FormButton>
+    </nav>
+    <div class="h-dvh w-dvh overflow-hidden flex flex-col">
+      <!-- Static Spacer to allow for absolutely positioned HeaderNavBar  -->
+      <div class="h-12 w-full shrink-0"></div>
+      <main class="w-full h-full overflow-y-auto simple-scrollbar pt-8 pb-16">
+        <div class="container mx-auto px-6 md:px-12">
+          <WorkspaceWizard />
+        </div>
+      </main>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
 definePageMeta({
-  middleware: ['requires-workspaces-enabled', 'auth']
+  middleware: ['requires-workspaces-enabled', 'auth'],
+  layout: 'empty'
 })
 useHead({
   title: 'Create a workspace'
