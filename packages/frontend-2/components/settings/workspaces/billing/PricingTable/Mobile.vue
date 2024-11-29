0f13b8d2cad496037cb7edfc0273fe1e14ab217b
Mike <mike.tasset@gmail.com>
2024-11-29 17:23:28 +0100

    Fix: Updated button text conditional (#3584)

--- packages/frontend-2/components/settings/workspaces/billing/PricingTable/Mobile.vue
+++ packages/frontend-2/components/settings/workspaces/billing/PricingTable/Mobile.vue
@@ -5,7 +5,11 @@
       :key="`mobile-${plan.name}`"
       class="border border-outline-3 bg-foundation rounded-lg p-4 pb-2"
     >
-      <SettingsWorkspacesBillingPricingTableHeader :plan="plan" v-bind="$props" />
+      <SettingsWorkspacesBillingPricingTableHeader
+        :plan="plan"
+        v-bind="$props"
+        @on-cta-click="$emit('on-cta-click', $event)"
+      />
       <ul class="flex flex-col gap-y-2 mt-6">
         <li
           v-for="feature in features"
@@ -25,17 +29,31 @@
 </template>
 
 <script setup lang="ts">
-import type { WorkspacePlan, BillingInterval } from '~/lib/common/generated/gql/graphql'
+import type {
+  WorkspacePlan,
+  BillingInterval,
+  WorkspacePlans
+} from '~/lib/common/generated/gql/graphql'
 import { pricingPlansConfig } from '~/lib/billing/helpers/constants'
 import type { PlanFeaturesList } from '~/lib/billing/helpers/types'
 import { XMarkIcon } from '@heroicons/vue/24/outline'
 import type { MaybeNullOrUndefined } from '@speckle/shared'
 
+defineEmits<{
+  (
+    e: 'on-cta-click',
+    v: {
+      plan: WorkspacePlans
+      billingInterval: BillingInterval
+    }
+  ): void
+}>()
+
 defineProps<{
   isYearlyPlan: boolean
-  currentPlan: MaybeNullOrUndefined<WorkspacePlan>
-  workspaceId: string
-  isAdmin: boolean
+  currentPlan?: MaybeNullOrUndefined<WorkspacePlan>
+  workspaceId?: string
+  isAdmin?: boolean
   activeBillingInterval?: BillingInterval
 }>()
 
