5f1c1f7d72764087d0652a28dbc8f7086cddd3e3
Mike Tasset <mike.tasset@gmail.com>
2024-12-02 22:30:20 +0100

    WIP

--- packages/frontend-2/lib/workspaces/composables/wizard.ts
+++ packages/frontend-2/lib/workspaces/composables/wizard.ts
@@ -1,7 +1,8 @@
 import { nanoid } from 'nanoid'
 import { PaidWorkspacePlans } from '~/lib/common/generated/gql/graphql'
 import { type WorkspaceWizardState, WizardSteps } from '~/lib/workspaces/helpers/types'
-
+import { useCreateWorkspace } from '~/lib/workspaces/composables/management'
+import { useWorkspacesAvatar } from '~/lib/workspaces/composables/avatar'
 const state = ref<WorkspaceWizardState>({
   name: '',
   slug: '',
@@ -25,6 +26,9 @@ const stepComponents = shallowRef<Record<number, WizardSteps>>({
 const currentStep = computed(() => stepComponents.value[currentStepIndex.value])
 
 export const useWorkspacesWizard = () => {
+  const createWorkspace = useCreateWorkspace()
+  const { generateDefaultLogoIndex } = useWorkspacesAvatar()
+
   const setState = (initialState: WorkspaceWizardState) => {
     state.value = initialState
   }
@@ -47,8 +51,30 @@ export const useWorkspacesWizard = () => {
     currentStepIndex.value--
   }
 
-  const completeWizard = () => {
-    // console.log('completeWizard')
+  /**
+   * This will complete the wizard and create the workspace.
+   * We have to do a few things here:
+   * - Create the workspace
+   * - Add the ID and a param to the URL, in case the use comes back from Stripe
+   * - Redirect to Stripe if the plan is paid
+   */
+  const completeWizard = async () => {
+    // Starter plan doesn't need checkout
+    const needsCheckout = state.value.plan !== PaidWorkspacePlans.Starter
+    const newWorkspace = await createWorkspace(
+      {
+        name: state.value.name,
+        slug: state.value.slug,
+        defaultLogoIndex: generateDefaultLogoIndex() // We will get rid of this
+      },
+      { navigateOnSuccess: !needsCheckout },
+      { source: 'wizard' }
+    )
+
+    // If there
+    if (newWorkspace && !newWorkspace?.errors && needsCheckout) {
+      // Go to Stripe
+    }
   }
 
   return {
