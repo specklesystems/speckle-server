39ad74e7b11862d3c346f05ce74e70bc4402b73c
Gergő Jedlicska <gergo@jedlicska.com>
2024-12-02 19:42:09 +0100

    feat(workspaces): all workspaces start trial now

---
+++ packages/server/modules/workspacesCore/migrations/20241202183039_workspace_start_trial.ts
@@ -0,0 +1,17 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  // there aren't that many workspaces.
+  const workspaceIds = await knex<{ id: string }>('workspaces').select('id')
+  const createdAt = new Date()
+  const workspacePlans = workspaceIds.map(({ id }) => ({
+    workspaceId: id,
+    name: 'starter',
+    status: 'trial',
+    createdAt
+  }))
+
+  await knex('workspace_plans').insert(workspacePlans).onConflict().ignore()
+}
+
+export async function down(): Promise<void> {}
