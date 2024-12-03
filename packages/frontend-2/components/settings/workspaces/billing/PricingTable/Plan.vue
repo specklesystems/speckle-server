256a43f93dd0daca96c78cd7f7f7d196c17dd4d4
Benjamin Ottensten <benjamin@speckle.systems>
2024-12-03 11:19:29 +0100

    Explain features in the plan component (#3606)
    * Explain features in the plan component
    
    * Update copy and correct the guest prices
    
    * Make description function
    
    * Changed to only guest
    
    ---------
    
    Co-authored-by: Mike Tasset <mike.tasset@gmail.com>

--- packages/frontend-2/components/settings/workspaces/billing/PricingTable/Plan.vue
+++ packages/frontend-2/components/settings/workspaces/billing/PricingTable/Plan.vue
@@ -57,6 +57,14 @@
         />
         <XMarkIcon v-else class="w-4 h-4 mx-2 text-danger" />
         <span
+          v-tippy="
+            feature.description(
+              yearlyIntervalSelected
+                ? plan.cost.yearly[Roles.Workspace.Guest]
+                : plan.cost.monthly[Roles.Workspace.Guest]
+            )
+          "
+          class="underline decoration-outline-5 decoration-dashed underline-offset-4 cursor-help"
           :class="{
             'text-foreground-2': !plan.features.includes(feature.name as PlanFeaturesList)
           }"
