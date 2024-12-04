2efed981f428b32bc4960ffa267bf2687b507344
Benjamin Ottensten <benjamin.ottensten@gmail.com>
2024-12-04 09:32:25 +0100

    Track embed views

--- packages/frontend-2/components/viewer/PreSetupWrapper.vue
+++ packages/frontend-2/components/viewer/PreSetupWrapper.vue
@@ -123,6 +123,7 @@ import { useViewerTour } from '~/lib/viewer/composables/tour'
 import { useFilterUtilities } from '~/lib/viewer/composables/ui'
 import { projectsRoute } from '~~/lib/common/helpers/route'
 import { workspaceRoute } from '~/lib/common/helpers/route'
+import { useMixpanel } from '~/lib/core/composables/mp'
 
 const emit = defineEmits<{
   setup: [InjectableViewerState]
@@ -198,4 +199,11 @@ const lastUpdate = computed(() => {
 })
 
 useHead({ title })
+
+const mp = useMixpanel()
+onMounted(() => {
+  if (isEmbedEnabled.value) {
+    mp.track('Embedded Model Load')
+  }
+})
 </script>
