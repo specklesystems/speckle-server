3d636328f8285d0fcf7eea758194b3cd8d010fe9
root <root@Clynelish.>
2024-12-13 14:54:31 +0000

    add changes

--- packages/dui3/lib/bindings/definitions/IHostAppTestBinding.ts
+++ packages/dui3/lib/bindings/definitions/IHostAppTestBinding.ts
@@ -6,11 +6,24 @@ import type {
 export const IHostAppTestBindingKey = 'hostAppTestBiding'
 
 export interface IHostAppTestBinding extends IBinding<IHostAppTestBindingEvents> {
-  getTests: () => Promise<string[]>
+  getLoadedModel: () => Promise<string>
+  getTests: () => Promise<ModelTest[]>
+  getTestsResults: () => Promise<ModelTestResult[]>
   executeTest: (testName: string) => Promise<void>
   executeAllTests: () => Promise<void>
 }
 
 export interface IHostAppTestBindingEvents extends IBindingSharedEvents {
   setTestResult: (args: { testName: string; result: Record<string, unknown> }) => void
+}
+
+export type ModelTest = {
+  name: string
+  status: string
+}
+
+export type ModelTestResult = {
+  name: string
+  status: string
+  timeStamp: string
 }
--- packages/dui3/pages/hostapptests.vue
+++ packages/dui3/pages/hostapptests.vue
@@ -1,24 +1,53 @@
 <template>
-  <div class="flex flex-col space-y-2 px-4">
-    <div v-for="testName in testNames" :key="testName" class="space-y-2">
-      <FormButton full-width color="outline" @click="invokeTest(testName)">
-        Run: {{ testName }}
-      </FormButton>
-      <div class="text-xs">
-        <pre>todo: test results</pre>
+  <div>
+    <div class="flex flex-col space-y-2 px-4">
+      <div :key="loadedModel" class="text-xs">
+        {{ loadedModel }}
+      </div>
+    </div>
+    <div class="flex flex-col space-y-2 px-4">
+      <div v-for="test in modelTests" :key="test.name" class="space-y-2">
+        <div class="text-xs">
+          <pre>{{ test.status }} - {{ test.name }}</pre>
+        </div>
+      </div>
+    </div>
+    <div v-if="modelTests.length > 0" class="flex flex-col space-y-2 px-4">
+      <FormButton full-width color="outline" @click="invokeTests()">Run All</FormButton>
+    </div>
+    <div class="flex flex-col space-y-2 px-4">
+      <div v-for="result in modelResultsTests" :key="result.name" class="space-y-2">
+        <div class="text-xs">
+          <pre>{{ result.status }} - {{ result.name }} - {{ result.timeStamp }}</pre>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const app = useNuxtApp()
-const hostAppTestBinding = app.$hostAppTestBiding
+import type {
+  ModelTest,
+  ModelTestResult
+} from 'lib/bindings/definitions/IHostAppTestBinding'
 
-const testNames = ref<string[]>(await hostAppTestBinding.getTests())
-const invokeTest = async (testName: string) => {
-  await hostAppTestBinding.executeTest(testName)
+const app = useNuxtApp()
+
+const hostAppTestBinding = app.$hostAppTestBiding
+let loadedModel = await hostAppTestBinding.getLoadedModel()
+let tests: ModelTest[] = []
+if (loadedModel?.length > 0) {
+  tests = await hostAppTestBinding.getTests()
+} else {
+  loadedModel = '<No Loaded File>'
 }
+const modelTests = ref<ModelTest[]>(tests)
+
+let resultsTests: ModelTestResult[] = []
+const invokeTests = async () => {
+  resultsTests = await hostAppTestBinding.getTestsResults()
+}
+const modelResultsTests = ref<ModelTestResult[]>(resultsTests)
 
 hostAppTestBinding.on('setTestResult', (result) => {
   console.log(result)
