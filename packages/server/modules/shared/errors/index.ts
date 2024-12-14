a2dff93f6d021c60020e70fcc3df6a092e8d868b
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2024-12-14 13:19:32 +0000

    chore(server): errors for database append additional context

--- packages/server/modules/shared/errors/index.ts
+++ packages/server/modules/shared/errors/index.ts
@@ -1,4 +1,10 @@
-import { BaseError, Info } from '@/modules/shared/errors/base'
+import {
+  BaseError,
+  type ExtendedOptions,
+  type Info
+} from '@/modules/shared/errors/base'
+import type { Knex } from 'knex'
+import { numberOfFreeConnections } from '@/modules/shared/helpers/dbHelper'
 
 /**
  * Use this to throw when the request has auth credentials, but they are not sufficient
@@ -106,10 +112,59 @@ export class EnvironmentResourceError extends BaseError {
   static statusCode = 502
 }
 
-export class DatabaseError extends EnvironmentResourceError {
+export class DatabaseError<I extends Info = Info> extends EnvironmentResourceError {
   static code = 'DATABASE_ERROR'
   static defaultMessage = 'An error occurred while trying to access the database.'
   static statusCode = 502
+
+  constructor(
+    message?: string | null | undefined,
+    dbClient?: Knex | undefined,
+    options: ExtendedOptions<I> | Error | undefined = undefined
+  ) {
+    const additionalInfo: Record<string, unknown> = {}
+    if (dbClient) {
+      const dbClientClient = dbClient.client
+      try {
+        // get more info about the connection string (without exposing the password!)
+        const connectionURL = new URL(
+          dbClientClient.config?.connection?.connectionString
+        )
+        additionalInfo.databaseHost = connectionURL.hostname
+        additionalInfo.databasePort = connectionURL.port
+        additionalInfo.databaseUser = connectionURL.username
+        additionalInfo.databaseOrConnectionPoolName = connectionURL.pathname
+          .split('/')
+          .pop()
+      } catch {
+        // ignore problems if we can't get additional info and log whatever we have
+      }
+      try {
+        // get more info about the state of the connection pool
+        const connPool = dbClientClient?.pool
+        additionalInfo.databasePoolConnectionsFree = connPool.numFree()
+        additionalInfo.databasePoolConnectionsUsed = connPool.numUsed()
+        additionalInfo.databasePoolConnectionsPendingAcquires =
+          connPool.numPendingAcquires()
+        additionalInfo.databasePoolConnectionsPendingCreates =
+          connPool.numPendingCreates()
+        additionalInfo.databasePoolConnectionsPendingValidations =
+          connPool.numPendingValidations()
+        additionalInfo.databasePoolConnectionsRemainingCapacity =
+          numberOfFreeConnections(dbClient.client)
+      } catch {
+        // ignore problems if we can't get additional info and log whatever we have
+      }
+    }
+
+    super(message, {
+      ...options,
+      info:
+        options && 'info' in options
+          ? { ...options?.info, ...additionalInfo }
+          : additionalInfo
+    })
+  }
 }
 
 export { BaseError }
