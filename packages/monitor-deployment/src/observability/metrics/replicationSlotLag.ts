ad1813575ffd9e767ea84bd0353f0bef8f8448b4
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2024-12-16 16:11:02 +0000

    tidying up new metrics

--- packages/monitor-deployment/src/observability/metrics/replicationSlotLag.ts
+++ packages/monitor-deployment/src/observability/metrics/replicationSlotLag.ts
@@ -7,16 +7,16 @@ export const init: MetricInitializer = (config) => {
   const promMetric = new prometheusClient.Gauge({
     name: join([namePrefix, 'db_replication_slot_lag'], '_'),
     help: 'Lag of replication slots in bytes',
-    labelNames: ['region', ...labelNames]
+    labelNames: ['region', 'slotname', ...labelNames]
   })
   return async (params) => {
     const { dbClients, labels } = params
     await Promise.all(
       dbClients.map(async ({ client, regionKey }) => {
         const queryResults = await client.raw<{
-          rows: [{ slot_lag_bytes: number }]
+          rows: [{ slot_name: string; slot_lag_bytes: string }]
         }>(`
-            SELECT pg_current_wal_lsn() - confirmed_flush_lsn AS slot_lag_bytes
+            SELECT slot_name, pg_current_wal_lsn() - confirmed_flush_lsn AS slot_lag_bytes
               FROM pg_replication_slots;
           `)
         if (!queryResults.rows.length) {
@@ -26,10 +26,12 @@ export const init: MetricInitializer = (config) => {
           )
           return
         }
-        promMetric.set(
-          { ...labels, region: regionKey },
-          queryResults.rows[0].slot_lag_bytes
-        )
+        for (const row of queryResults.rows) {
+          promMetric.set(
+            { ...labels, region: regionKey, slotname: row.slot_name },
+            parseInt(row.slot_lag_bytes)
+          )
+        }
       })
     )
   }
--- packages/monitor-deployment/src/observability/metrics/replicationWorkerLag.ts
+++ packages/monitor-deployment/src/observability/metrics/replicationWorkerLag.ts
@@ -7,18 +7,26 @@ export const init: MetricInitializer = (config) => {
   const promMetric = new prometheusClient.Gauge({
     name: join([namePrefix, 'db_replication_worker_lag'], '_'),
     help: 'Lag of replication workers, by type of lag',
-    labelNames: ['region', 'lagtype', ...labelNames]
+    labelNames: ['region', 'lagtype', 'name', ...labelNames]
   })
   return async (params) => {
     const { dbClients, labels } = params
     await Promise.all(
       dbClients.map(async ({ client, regionKey }) => {
         const queryResults = await client.raw<{
-          rows: [{ write_lag: number; flush_lag: number; replay_lag: number }]
+          rows: [
+            {
+              write_lag: string
+              flush_lag: string
+              replay_lag: string
+              application_name: string
+            }
+          ]
         }>(`
           SELECT write_lsn - sent_lsn AS write_lag,
             flush_lsn - write_lsn AS flush_lag,
-            replay_lsn - flush_lsn AS replay_lag
+            replay_lsn - flush_lsn AS replay_lag,
+            application_name
             FROM pg_stat_replication;
           `)
         if (!queryResults.rows.length) {
@@ -28,18 +36,35 @@ export const init: MetricInitializer = (config) => {
           )
           return
         }
-        promMetric.set(
-          { ...labels, region: regionKey, lagtype: 'write' },
-          queryResults.rows[0].write_lag
-        )
-        promMetric.set(
-          { ...labels, region: regionKey, lagtype: 'flush' },
-          queryResults.rows[0].flush_lag
-        )
-        promMetric.set(
-          { ...labels, region: regionKey, lagtype: 'replay' },
-          queryResults.rows[0].replay_lag
-        )
+        for (const row of queryResults.rows) {
+          promMetric.set(
+            {
+              ...labels,
+              region: regionKey,
+              lagtype: 'write',
+              name: row.application_name
+            },
+            parseInt(row.write_lag)
+          )
+          promMetric.set(
+            {
+              ...labels,
+              region: regionKey,
+              lagtype: 'flush',
+              name: row.application_name
+            },
+            parseInt(row.flush_lag)
+          )
+          promMetric.set(
+            {
+              ...labels,
+              region: regionKey,
+              lagtype: 'replay',
+              name: row.application_name
+            },
+            parseInt(row.replay_lag)
+          )
+        }
       })
     )
   }
