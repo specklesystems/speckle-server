e0ebee9ac0b3600cdd0d138eeb18c2b09eee2ce3
Mike Tasset <mike.tasset@gmail.com>
2024-12-20 14:22:57 +0100

    Feat: Let is snow

--- packages/frontend-2/lib/viewer/composables/ui.ts
+++ packages/frontend-2/lib/viewer/composables/ui.ts
@@ -27,7 +27,7 @@ import type {
   ViewerShortcutAction
 } from '~/lib/viewer/helpers/shortcuts/types'
 import { useActiveElement } from '@vueuse/core'
-import { SnowPipeline } from './../../../../viewer-sandbox/src/Pipelines/Snow/SnowPipeline'
+import { SnowPipeline } from '@speckle/viewer-sandbox/src/Pipelines/Snow/SnowPipeline'
 
 export function useSectionBoxUtilities() {
   const { instance } = useInjectedViewer()
