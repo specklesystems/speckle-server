d635cb203cb8ce08137f5dbc066590a94a78224d
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2025-01-08 20:01:57 +0000

    improve parsing of postgres interval

--- packages/monitor-deployment/src/observability/metrics/currentConnections.ts
+++ packages/monitor-deployment/src/observability/metrics/currentConnections.ts
@@ -1,6 +1,7 @@
 import prometheusClient from 'prom-client'
 import { join } from 'lodash-es'
 import type { MetricInitializer } from '@/observability/types.js'
+import Interval from 'postgres-interval'
 
 export const init: MetricInitializer = (config) => {
   const { labelNames, namePrefix, logger } = config
@@ -22,14 +23,18 @@ export const init: MetricInitializer = (config) => {
                 FROM pg_stat_activity
                 WHERE state <> 'idle'
                     AND query NOT LIKE '% FROM pg_stat_activity %'
+                    AND query NOT LIKE 'START_REPLICATION SLOT %'
+                    AND query NOT LIKE '<insufficient privilege>'
                 ORDER BY interval DESC
-                LIMIT 20;
+                LIMIT 100;
             `
           )
           for (const row of currentConnectionResults.rows) {
+            const interval = Interval(row.interval)
+
             currentConnections.set(
               { ...labels, query: row.query, region: regionKey },
-              parseInt(row.interval)
+              intervalToMilliseconds(interval)
             )
           }
         } catch (err) {
@@ -41,4 +46,16 @@ export const init: MetricInitializer = (config) => {
       })
     )
   }
+}
+
+const intervalToMilliseconds = (interval: Interval.IPostgresInterval) => {
+  return (
+    interval.years * 31536000000 + //assumes 365 days exactly
+    interval.months * 2592000000 + //assumes 30 days
+    interval.days * 86400000 +
+    interval.hours * 3600000 +
+    interval.minutes * 60000 +
+    interval.seconds * 1000 +
+    interval.milliseconds
+  )
 }
