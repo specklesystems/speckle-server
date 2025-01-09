c611272ad86dd4fa86d3106c118a314e68149023
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2025-01-09 16:24:03 +0000

    feat(server/core): optimise serverInfo database query - cache returned value in Redis - allow config to be queried separately

--- packages/server/modules/core/repositories/server.ts
+++ packages/server/modules/core/repositories/server.ts
@@ -17,6 +17,7 @@ import {
   getServerOrigin,
   getServerVersion
 } from '@/modules/shared/helpers/envHelper'
+import Redis from 'ioredis'
 import { Knex } from 'knex'
 
 const ServerConfig = buildTableHelper('server_config', [
@@ -38,15 +39,47 @@ const tables = {
   scopes: (db: Knex) => db<ScopeRecord>(Scopes.name)
 }
 
+const SERVER_CONFIG_CACHE_KEY = 'server_config'
+
+export type GetServerConfig = (params: {
+  bustCache?: boolean
+}) => Promise<ServerConfigRecord>
+
+export const getServerConfigFactory =
+  (deps: { db: Knex; cache?: Redis }): GetServerConfig =>
+  async (params) => {
+    const { cache, db } = deps
+    const { bustCache } = params
+    if (cache && !bustCache) {
+      const cachedResult = await cache.get(SERVER_CONFIG_CACHE_KEY)
+      if (cachedResult) return JSON.parse(cachedResult) as ServerConfigRecord
+    }
+    if (cache && bustCache) {
+      await cache.del(SERVER_CONFIG_CACHE_KEY)
+    }
+    const result = await tables.serverConfig(db).select('*').first()
+    if (cache) {
+      await cache.setex(
+        SERVER_CONFIG_CACHE_KEY,
+        60 /* seconds */,
+        JSON.stringify(result)
+      )
+    }
+    // An entry should always exist as it is placed there by database migrations
+    return result!
+  }
+
 export const getServerInfoFactory =
-  (deps: { db: Knex }): GetServerInfo =>
+  (deps: { getServerConfig: GetServerConfig }): GetServerInfo =>
   async () => {
     const movedTo = getServerMovedTo()
     const movedFrom = getServerMovedFrom()
 
+    const serverConfig = await deps.getServerConfig({ bustCache: false })
+
     // An entry should always exist from migrations
     const serverInfo: ServerInfo = {
-      ...(await tables.serverConfig(deps.db).select('*').first())!,
+      ...serverConfig,
       version: getServerVersion(),
       canonicalUrl: getServerOrigin(),
       configuration: {
