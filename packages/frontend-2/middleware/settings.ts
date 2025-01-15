584cb51053a82cadebc536d1ccefa8b3b951e1c4
Mike Tasset <mike.tasset@gmail.com>
2025-01-15 12:32:32 +0100

    Add middleware check for server settings routes

--- packages/frontend-2/middleware/settings.ts
+++ packages/frontend-2/middleware/settings.ts
@@ -1,15 +1,16 @@
 import { useSettingsMenuState } from '~/lib/settings/composables/menu'
 import { settingsRoutes, homeRoute } from '~/lib/common/helpers/route'
+import { Roles } from '@speckle/shared'
 
 export default defineNuxtRouteMiddleware((to, from) => {
   const settingsMenuState = useSettingsMenuState()
-  const { isAdmin } = useActiveUser()
+  const { activeUser } = useActiveUser()
 
   const serverRoutes = [...Object.values(settingsRoutes.server)].map(
     (section) => section.route
   )
 
-  if (serverRoutes.includes(to.path) && !isAdmin.value) {
+  if (serverRoutes.includes(to.path) && activeUser.value?.role !== Roles.Server.Admin) {
     return navigateTo(homeRoute)
   }
 
