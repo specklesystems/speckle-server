abcee783818d35ee1ea9d7995e53ad7a99771975
Mike Tasset <mike.tasset@gmail.com>
2025-01-15 12:30:07 +0100

    Add middleware check for server settings routes

--- packages/frontend-2/middleware/settings.ts
+++ packages/frontend-2/middleware/settings.ts
@@ -1,9 +1,19 @@
 import { useSettingsMenuState } from '~/lib/settings/composables/menu'
+import { settingsRoutes, homeRoute } from '~/lib/common/helpers/route'
 
 export default defineNuxtRouteMiddleware((to, from) => {
   const settingsMenuState = useSettingsMenuState()
+  const { isAdmin } = useActiveUser()
+
+  const serverRoutes = [...Object.values(settingsRoutes.server)].map(
+    (section) => section.route
+  )
+
+  if (serverRoutes.includes(to.path) && !isAdmin.value) {
+    return navigateTo(homeRoute)
+  }
 
   if (to.path.startsWith('/settings') && !from.path.startsWith('/settings')) {
-    settingsMenuState.value.previousRoute = from.fullPath
+    settingsMenuState.value.previousRoute = from.path
   }
 })
