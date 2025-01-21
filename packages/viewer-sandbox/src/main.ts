32497eeb5ac3410a2de397984d595f4bd022d70a
Alexandru Popovici <alexandrupopoviciioan@gmail.com>
2025-01-21 18:20:45 +0200

    fix(viewer-lib): Handles WEB-2491. (#3856)
    - Section outlines are no longer visible behind other geometry. They are depth tested now for all view modes
    - Shaded mode aka BastiMode now properly updates the section planes

--- packages/viewer-sandbox/src/main.ts
+++ packages/viewer-sandbox/src/main.ts
@@ -113,7 +113,7 @@ const getStream = () => {
     // prettier-ignore
     // 'https://app.speckle.systems/streams/da9e320dad/commits/5388ef24b8?c=%5B-7.66134,10.82932,6.41935,-0.07739,-13.88552,1.8697,0,1%5D'
     // Revit sample house (good for bim-like stuff with many display meshes)
-    // 'https://app.speckle.systems/streams/da9e320dad/commits/5388ef24b8'
+    'https://app.speckle.systems/streams/da9e320dad/commits/5388ef24b8'
     // 'https://latest.speckle.systems/streams/c1faab5c62/commits/ab1a1ab2b6'
     // 'https://app.speckle.systems/streams/da9e320dad/commits/5388ef24b8'
     // 'https://latest.speckle.systems/streams/58b5648c4d/commits/60371ecb2d'
@@ -469,7 +469,7 @@ const getStream = () => {
     // 'https://app.speckle.systems/projects/e89b61b65c/models/2a0995f124'
 
     // 'https://latest.speckle.systems/projects/3fe1880c36/models/65bb4287a8'
-    'https://latest.speckle.systems/projects/db06488e1c/models/21f3930771'
+    // 'https://latest.speckle.systems/projects/db06488e1c/models/21f3930771'
   )
 }
 
