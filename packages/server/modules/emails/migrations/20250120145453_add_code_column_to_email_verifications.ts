dcf33fdab4764c95c7e0892ab7779dc62c99ebf5
Alessandro Magionami <alessandro.magionami@gmail.com>
2025-01-23 17:11:58 +0100

    chore(emails): use custom transaction in migration

--- packages/server/modules/emails/migrations/20250120145453_add_code_column_to_email_verifications.ts
+++ packages/server/modules/emails/migrations/20250120145453_add_code_column_to_email_verifications.ts
@@ -1,16 +1,34 @@
+import { coreLogger } from '@/modules/core/logger'
 import { Knex } from 'knex'
 
 const TABLE_NAME = 'email_verifications'
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.alterTable(TABLE_NAME, (table) => {
-    table.string('code')
-    table.unique('email')
-  })
+  const trx = await knex.transaction()
+  try {
+    await trx.schema.alterTable(TABLE_NAME, (table) => {
+      table.string('code')
+      table.unique('email')
+    })
+  } catch (error) {
+    coreLogger.debug('Failed to apply unique constraint to email_verifications table')
+  } finally {
+    await trx.commit()
+  }
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.alterTable(TABLE_NAME, (table) => {
-    table.dropColumn('code')
-  })
+  const trx = await knex.transaction()
+  try {
+    await trx.schema.alterTable(TABLE_NAME, (table) => {
+      table.dropColumn('code')
+      table.dropUnique(['email'])
+    })
+  } catch (error) {
+    coreLogger.debug('Failed to drop unique constraint to email_verifications table')
+  } finally {
+    await trx.commit()
+  }
 }
+
+export const config = { transaction: false }
--- packages/server/modules/emails/migrations/20250123133749_remove_unique_email_verifications.ts
+++ packages/server/modules/emails/migrations/20250123133749_remove_unique_email_verifications.ts
@@ -1,15 +1,21 @@
+import { coreLogger } from '@/modules/core/logger'
 import { Knex } from 'knex'
 
 const TABLE_NAME = 'email_verifications'
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.alterTable(TABLE_NAME, (table) => {
-    table.dropUnique(['email'])
-  })
+  const trx = await knex.transaction()
+  try {
+    await trx.schema.alterTable(TABLE_NAME, (table) => {
+      table.dropUnique(['email'])
+    })
+  } catch (error) {
+    coreLogger.debug('Failed to drop unique constraint to email_verifications table')
+  } finally {
+    await trx.commit()
+  }
 }
 
-export async function down(knex: Knex): Promise<void> {
-  await knex.schema.alterTable(TABLE_NAME, (table) => {
-    table.dropColumn('code')
-  })
-}
+export async function down(): Promise<void> {}
+
+export const config = { transaction: false }
