2acbfc67d090a109aa95b2507f981cc7e120d72d
oguzhankoral <oguzhankoral@gmaill.com>
2025-01-24 11:34:39 +0300

    Ugly fix for etabs

--- packages/dui3/components/filter/Selection.vue
+++ packages/dui3/components/filter/Selection.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="space-y-2 p-2 text-primary bg-blue-500/10 rounded-md text-xs">
-    <div v-if="selectionStore.selectionInfo.selectedObjectIds?.length === 0">
-      No objects selected, go ahead and select some!
+    <div v-if="isEtabs">
+      <div v-if="selectionStore.selectionInfo.selectedObjectIds?.length === 0">
+        Set selection manually since ETABS can not dynamically detect selections.
+      </div>
+      <div v-else>{{ selectionStore.selectionInfo.summary }}</div>
     </div>
-    <div v-else>{{ selectionStore.selectionInfo.summary }}</div>
+    <div v-else>
+      <div v-if="selectionStore.selectionInfo.selectedObjectIds?.length === 0">
+        No objects selected, go ahead and select some!
+      </div>
+      <div v-else>{{ selectionStore.selectionInfo.summary }}</div>
+    </div>
+  </div>
+  <div v-if="isEtabs" class="flex justify-between items-center mt-2">
+    <FormButton full-width @click="app.$selectionBinding.getSelection()">
+      Set Selection
+    </FormButton>
   </div>
 </template>
 <script setup lang="ts">
@@ -17,10 +30,13 @@ const emit = defineEmits<{
 }>()
 
 const store = useHostAppStore()
+const app = useNuxtApp()
 const { selectionFilter } = storeToRefs(store)
 
 const selectionStore = useSelectionStore()
 const { selectionInfo } = storeToRefs(selectionStore)
+
+const isEtabs = computed(() => store.hostAppName === 'etabs')
 
 defineProps<{
   filter: IDirectSelectionSendFilter
