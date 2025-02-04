8934179e365c6fee07687fca2e3dd4bc452144c8
Mike <mike.tasset@gmail.com>
2025-02-04 16:47:27 +0100

    Fix: Show workspace ID for workspace admins as well (#3933)

--- packages/frontend-2/pages/settings/workspaces/[slug]/general.vue
+++ packages/frontend-2/pages/settings/workspaces/[slug]/general.vue
@@ -126,7 +126,7 @@
           </div>
         </div>
       </template>
-      <template v-if="isServerAdmin">
+      <template v-if="isServerAdmin || isAdmin">
         <hr class="mb-6 mt-8 border-outline-2" />
         <p class="text-body-2xs text-foreground-2">
           Workspace ID: #{{ workspaceResult?.workspaceBySlug?.id }}
