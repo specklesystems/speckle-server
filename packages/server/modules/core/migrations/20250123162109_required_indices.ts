7ccbcf11e1de31a451d405651b1741de49f5635e
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2025-02-06 11:16:59 +0000

    use knex helpers even if index is built sequentially

--- packages/server/modules/core/migrations/20250123162109_required_indices.ts
+++ packages/server/modules/core/migrations/20250123162109_required_indices.ts
@@ -1,103 +1,85 @@
 import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-  //stream_commits
-  await knex.raw(
-    'CREATE INDEX CONCURRENTLY IF NOT EXISTS stream_commits_commitid_index ON public.stream_commits USING btree ("commitId")'
-  )
+  await knex.schema.alterTable('stream_commits', (table) => {
+    table.index('commitId')
+  })
 
-  //branch_commits
-  await knex.raw(
-    'CREATE INDEX CONCURRENTLY IF NOT EXISTS branch_commits_commitid_index ON public.branch_commits USING btree ("commitId")'
-  )
+  await knex.schema.alterTable('branch_commits', (table) => {
+    table.index('commitId')
+  })
 
-  //refresh_tokens
-  await knex.raw(
-    'CREATE INDEX CONCURRENTLY IF NOT EXISTS refresh_tokens_userid_index ON public.refresh_tokens USING btree ("userId")'
-  )
+  await knex.schema.alterTable('refresh_tokens', (table) => {
+    table.index('userId')
+  })
 
-  //commits
-  await knex.raw(
-    'CREATE INDEX CONCURRENTLY IF NOT EXISTS commits_author_index ON public.commits USING btree ("author")'
-  )
-  await knex.raw(
-    'CREATE INDEX CONCURRENTLY IF NOT EXISTS commits_sourceapplication_index ON public.commits USING btree ("sourceApplication")'
-  )
-  await knex.raw(
-    'CREATE INDEX CONCURRENTLY IF NOT EXISTS commits_referencedobject_index ON public.commits USING btree ("referencedObject")'
-  )
-  await knex.raw(
-    'CREATE INDEX CONCURRENTLY IF NOT EXISTS commits_createdat_index ON public.commits USING btree ("createdAt")'
-  )
+  await knex.schema.alterTable('commits', (table) => {
+    table.index('author')
+    table.index('sourceApplication')
+    table.index('referencedObject')
+    table.index('createdAt')
+  })
 
-  //file_uploads
-  await knex.raw(
-    'DROP INDEX CONCURRENTLY IF EXISTS file_uploads_streamid_index' // Drop the old index on streamid alone
-  )
-  await knex.raw(
-    // create new composite index
-    'CREATE INDEX CONCURRENTLY IF NOT EXISTS file_uploads_streamid_convertedstatus_index ON public.file_uploads USING btree ("streamId", "convertedStatus")'
-  )
-  await knex.raw(
-    // create new composite index
-    'CREATE INDEX CONCURRENTLY IF NOT EXISTS file_uploads_streamid_uploaddate_index ON public.file_uploads USING btree ("streamId", "uploadDate")'
-  )
-  await knex.raw(
-    // create new composite index
-    'CREATE INDEX CONCURRENTLY IF NOT EXISTS file_uploads_streamid_branchname_index ON public.file_uploads USING btree ("streamId", "branchName")'
-  )
+  await knex.schema.alterTable('file_uploads', (table) => {
+    table.dropIndex('streamId') // Drop the old index on streamid alone as it's always used in conjunction with other columns
+    table.index(['streamId', 'convertedStatus'])
+    table.index(['streamId', 'uploadDate'])
+    table.index(['streamId', 'branchName'])
+  })
 
-  //server_invites
-  await knex.raw(
-    'CREATE INDEX CONCURRENTLY IF NOT EXISTS server_invites_updatedat_index ON public.server_invites USING btree ("updatedAt")'
-  )
-  await knex.raw(
-    'CREATE INDEX CONCURRENTLY IF NOT EXISTS server_invites_target_createdat_index ON public.server_invites USING btree ("target", "createdAt")'
-  )
+  await knex.schema.alterTable('server_invites', (table) => {
+    table.index('updatedAt')
+    table.index(['target', 'createdAt'])
+  })
 
-  //gendo_ai_renders
-  await knex.raw(
-    'CREATE INDEX CONCURRENTLY IF NOT EXISTS gendo_ai_renders_id_versionid_createdat_index ON public.gendo_ai_renders USING btree ("id", "versionId", "createdAt")'
-  )
+  await knex.schema.alterTable('gendo_ai_renders', (table) => {
+    table.index(['id', 'versionId', 'createdAt'])
+  })
 
-  //pwdreset_tokens
-  await knex.raw(
-    'CREATE INDEX CONCURRENTLY IF NOT EXISTS pwdreset_tokens_email_createdat_index ON public.pwdreset_tokens USING btree ("email", "createdAt")'
-  )
+  await knex.schema.alterTable('pwdreset_tokens', (table) => {
+    table.index(['email', 'createdAt'])
+  })
 
-  //automation_triggers
-  await knex.raw(
-    'CREATE INDEX CONCURRENTLY IF NOT EXISTS automation_triggers_triggeringid_triggertype_index ON public.automation_triggers USING btree ("triggeringId", "triggerType")'
-  )
-  await knex.raw(
-    'CREATE INDEX CONCURRENTLY IF NOT EXISTS automation_triggers_automationrevisionid_triggeringid_index ON public.automation_triggers USING btree ("automationRevisionId", "triggeringId")'
-  )
+  await knex.schema.alterTable('automation_triggers', (table) => {
+    table.index(['triggeringId', 'triggerType'])
+    table.index(['automationRevisionId', 'triggeringId'])
+  })
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.raw('DROP INDEX CONCURRENTLY stream_commits_commitid_index')
-  await knex.raw('DROP INDEX CONCURRENTLY branch_commits_commitid_index')
-  await knex.raw('DROP INDEX CONCURRENTLY refresh_tokens_userid_index')
-  await knex.raw('DROP INDEX CONCURRENTLY commits_author_index')
-  await knex.raw('DROP INDEX CONCURRENTLY commits_sourceapplication_index')
-  await knex.raw('DROP INDEX CONCURRENTLY commits_referencedobject_index')
-  await knex.raw('DROP INDEX CONCURRENTLY commits_createdat_index')
-  await knex.raw('DROP INDEX CONCURRENTLY file_uploads_streamid_convertedstatus_index')
-  await knex.raw('DROP INDEX CONCURRENTLY file_uploads_streamid_uploaddate_index')
-  await knex.raw('DROP INDEX CONCURRENTLY file_uploads_streamid_branchname_index')
-  await knex.raw('DROP INDEX CONCURRENTLY server_invites_updatedat_index')
-  await knex.raw('DROP INDEX CONCURRENTLY server_invites_target_createdat_index')
-  await knex.raw(
-    'DROP INDEX CONCURRENTLY gendo_ai_renders_id_versionid_createdat_index'
-  )
-  await knex.raw('DROP INDEX CONCURRENTLY pwdreset_tokens_email_createdat_index')
-  await knex.raw(
-    'DROP INDEX CONCURRENTLY automation_triggers_triggeringid_triggertype_index'
-  )
-  await knex.raw(
-    'DROP INDEX CONCURRENTLY automation_triggers_automationrevisionid_triggeringid_index'
-  )
+  await knex.schema.alterTable('stream_commits', (table) => {
+    table.dropIndex('commitId')
+  })
+  await knex.schema.alterTable('branch_commits', (table) => {
+    table.dropIndex('commitId')
+  })
+  await knex.schema.alterTable('refresh_tokens', (table) => {
+    table.dropIndex('userId')
+  })
+  await knex.schema.alterTable('commits', (table) => {
+    table.dropIndex('author')
+    table.dropIndex('sourceApplication')
+    table.dropIndex('referencedObject')
+    table.dropIndex('createdAt')
+  })
+  await knex.schema.alterTable('file_uploads', (table) => {
+    table.index('streamId')
+    table.dropIndex(['streamId', 'convertedStatus'])
+    table.dropIndex(['streamId', 'uploadDate'])
+    table.dropIndex(['streamId', 'branchName'])
+  })
+  await knex.schema.alterTable('server_invites', (table) => {
+    table.dropIndex('updatedAt')
+    table.dropIndex(['target', 'createdAt'])
+  })
+  await knex.schema.alterTable('gendo_ai_renders', (table) => {
+    table.dropIndex(['id', 'versionId', 'createdAt'])
+  })
+  await knex.schema.alterTable('pwdreset_tokens', (table) => {
+    table.dropIndex(['email', 'createdAt'])
+  })
+  await knex.schema.alterTable('automation_triggers', (table) => {
+    table.dropIndex(['triggeringId', 'triggerType'])
+    table.dropIndex(['automationRevisionId', 'triggeringId'])
+  })
 }
-
-// This migration is not transactional because it uses CREATE INDEX CONCURRENTLY
-export const config = { transaction: false }
