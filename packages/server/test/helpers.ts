ff297368aacbfdd3048f983fb221e31e054f8160
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2025-02-07 22:26:55 +0000

    changed my mind on whether we should be importing this logic from @core into the test - probably not, we shouldn't be relying on the logic under test in the assertions

--- packages/server/test/helpers.ts
+++ packages/server/test/helpers.ts
@@ -1,4 +1,5 @@
-import { calculateObjectHash } from '@/modules/core/services/objects/management'
+import crypto from 'crypto'
+import { omit } from 'lodash'
 
 export function generateManyObjects(shitTon: number, noise: unknown) {
   shitTon = shitTon || 10000
@@ -74,7 +75,12 @@ export function createManyObjects(num: number, noise?: unknown) {
  * @description Mutates the object in place to have an id. If the object already has an id, it will be left as is. If it does not, an id will be generated based on the object's contents (excluding the id field).
  */
 export function getAnIdForThisOnePlease(obj: { id: string } & Record<string, unknown>) {
-  obj.id = obj.id || calculateObjectHash(obj)
+  obj.id =
+    obj.id ||
+    crypto
+      .createHash('md5')
+      .update(JSON.stringify(omit(obj, 'id'))) // omitting id field from hash, so we don't get different values depending on whether the id is present & empty, or not present at all.
+      .digest('hex')
 }
 
 export const sleep = (ms: number) => {
