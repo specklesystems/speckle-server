e6a1c52d4ee528fbaff3a4679b879823c5300a4a
Gergő Jedlicska <gergo@jedlicska.com>
2025-02-07 08:51:45 +0100

    feat(core): only drop closure table if it exists

--- packages/server/modules/core/migrations/20250127172429_drop_closures.ts
+++ packages/server/modules/core/migrations/20250127172429_drop_closures.ts
@@ -1,7 +1,7 @@
 import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.dropTable('object_children_closure')
+  await knex.schema.dropTableIfExists('object_children_closure')
 }
 
 export async function down(knex: Knex): Promise<void> {
