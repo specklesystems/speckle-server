6a42ff844e7f767339963287f881eca22bd1a7f5
andrewwallacespeckle <andrew@speckle.systems>
2025-02-11 13:30:25 +0000

    Hide header on embed

--- packages/frontend-2/layouts/viewer.vue
+++ packages/frontend-2/layouts/viewer.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="relative min-h-full">
     <ClientOnly>
-      <HeaderNavBar class="relative z-20" />
+      <HeaderNavBar v-if="!isEmbedEnabled" class="relative z-20" />
     </ClientOnly>
     <main class="absolute top-0 left-0 z-10 h-[100dvh] w-screen">
       <slot />
     </main>
   </div>
 </template>
+<script setup lang="ts">
+import { useEmbed } from '~/lib/viewer/composables/setup/embed'
+
+const { isEnabled: isEmbedEnabled } = useEmbed()
+</script>
