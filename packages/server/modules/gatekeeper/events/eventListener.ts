5b0bb3b65dc54a94ffc46537bf40c73abfbd0080
Gergő Jedlicska <57442769+gjedlicska@users.noreply.github.com>
2025-02-21 14:49:04 +0100

    gergo/web 2635 set up new plans in the backend (#4031)
    * feat(gatekeeper): add new free plan option as an unpaid workspace plan
    
    * feat(workspaces): add feature flag for new plans
    
    * feat(gatekeeper): add free plan option to workspace plans
    
    * Added button in FE
    
    * Make button more beautifuller
    
    * feat(gatekeeper): enable creating workspaces on a free plan
    
    * Fix
    
    * Fix FE linting
    
    ---------
    
    Co-authored-by: Mike Tasset <mike.tasset@gmail.com>

--- packages/server/modules/gatekeeper/events/eventListener.ts
+++ packages/server/modules/gatekeeper/events/eventListener.ts
@@ -2,18 +2,22 @@ import { reconcileWorkspaceSubscriptionFactory } from '@/modules/gatekeeper/clie
 import {
   getWorkspacePlanFactory,
   getWorkspaceSubscriptionFactory,
-  upsertTrialWorkspacePlanFactory
+  upsertTrialWorkspacePlanFactory,
+  upsertUnpaidWorkspacePlanFactory
 } from '@/modules/gatekeeper/repositories/billing'
 import { addWorkspaceSubscriptionSeatIfNeededFactory } from '@/modules/gatekeeper/services/subscriptions'
 import {
   getWorkspacePlanPrice,
   getWorkspacePlanProductId
 } from '@/modules/gatekeeper/stripe'
+import { getFeatureFlags } from '@/modules/shared/helpers/envHelper'
 import { getEventBus } from '@/modules/shared/services/eventBus'
 import { countWorkspaceRoleWithOptionalProjectRoleFactory } from '@/modules/workspaces/repositories/workspaces'
 import { WorkspaceEvents } from '@/modules/workspacesCore/domain/events'
 import { Knex } from 'knex'
 import Stripe from 'stripe'
+
+const { FF_GATEKEEPER_FORCE_FREE_PLAN } = getFeatureFlags()
 
 export const initializeEventListenersFactory =
   ({ db, stripe }: { db: Knex; stripe: Stripe }) =>
@@ -36,14 +40,26 @@ export const initializeEventListenersFactory =
         await addWorkspaceSubscriptionSeatIfNeeded(payload)
       }),
       eventBus.listen(WorkspaceEvents.Created, async ({ payload }) => {
-        await upsertTrialWorkspacePlanFactory({ db })({
-          workspacePlan: {
-            name: 'starter',
-            status: 'trial',
-            workspaceId: payload.workspace.id,
-            createdAt: new Date()
-          }
-        })
+        // TODO: based on a feature flag, we can force new workspaces into the free plan here
+        if (FF_GATEKEEPER_FORCE_FREE_PLAN) {
+          await upsertUnpaidWorkspacePlanFactory({ db })({
+            workspacePlan: {
+              name: 'free',
+              status: 'valid',
+              workspaceId: payload.workspace.id,
+              createdAt: new Date()
+            }
+          })
+        } else {
+          await upsertTrialWorkspacePlanFactory({ db })({
+            workspacePlan: {
+              name: 'starter',
+              status: 'trial',
+              workspaceId: payload.workspace.id,
+              createdAt: new Date()
+            }
+          })
+        }
       })
     ]
 
