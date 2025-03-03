51eecb5b0b342383c38c920adc9319b97972f505
oguzhankoral <oguzhankoral@gmaill.com>
2025-03-03 17:11:32 +0300

    Feat: conditional margin on home screen + added version info to bottom

--- packages/dui3/components/header/NavBar.vue
+++ packages/dui3/components/header/NavBar.vue
@@ -31,86 +31,15 @@
           >
             <span class="">Update</span>
           </FormButton>
-
-          <div v-if="uiConfigStore.isDevMode">
-            <!-- ONLY FOR TESTS FOR NOW-->
-            <div v-if="testSettings && hasConfigBindings" class="flex justify-end">
-              <FormButton
-                v-tippy="'Settings'"
-                class="px-1"
-                text
-                hide-text
-                :icon-left="Cog6ToothIcon"
-                @click="openConfigDialog = true"
-              ></FormButton>
-              <LayoutDialog v-model:open="openConfigDialog" fullscreen="none">
-                <ConfigDialog @close="openConfigDialog = false" />
-              </LayoutDialog>
-            </div>
-            <div>
-              <HeaderUserMenu />
-            </div>
-          </div>
-          <div v-else>
-            <FormButton
-              v-tippy="isDarkTheme ? 'Switch to light theme' : 'Switch to dark theme'"
-              text
-              @click="uiConfigStore.toggleTheme()"
-            >
-              <Component :is="isDarkTheme ? SunIcon : MoonIcon" class="w-4" />
-            </FormButton>
-          </div>
+          <HeaderUserMenu />
         </div>
       </div>
     </div>
   </nav>
-  <div v-else class="fixed top-1 right-2 z-100 flex items-center space-x-2">
-    <FormButton
-      v-tippy="isDarkTheme ? 'Switch to light theme' : 'Switch to dark theme'"
-      text
-      @click="uiConfigStore.toggleTheme()"
-    >
-      <Component :is="isDarkTheme ? SunIcon : MoonIcon" class="w-4" />
-    </FormButton>
-    <div v-if="isDev">
-      <NuxtLink
-        v-tippy="'Test page'"
-        to="test"
-        class="text-xs text-foreground-2 hover:text-primary"
-        exact-active-class="hidden"
-      >
-        <WrenchScrewdriverIcon class="w-4" />
-      </NuxtLink>
-      <NuxtLink
-        v-tippy="'Home'"
-        to="/"
-        class="text-xs text-foreground-2 hover:text-primary"
-        exact-active-class="hidden"
-      >
-        <HomeIcon class="w-4" />
-      </NuxtLink>
-    </div>
-  </div>
 </template>
 <script setup lang="ts">
-import { storeToRefs } from 'pinia'
-import { useConfigStore } from '~/store/config'
-import {
-  Cog6ToothIcon,
-  WrenchScrewdriverIcon,
-  HomeIcon,
-  MoonIcon,
-  SunIcon
-} from '@heroicons/vue/24/solid'
 import { ArrowUpCircleIcon } from '@heroicons/vue/24/outline'
 import { useHostAppStore } from '~/store/hostApp'
-const isDev = ref(import.meta.dev)
-const openConfigDialog = ref(false)
-// NOTE: make it true to test settings, it might be removed later. TBD
-const testSettings = ref(false)
-
-const uiConfigStore = useConfigStore()
-const { isDarkTheme, hasConfigBindings } = storeToRefs(uiConfigStore)
 
 const hostAppStore = useHostAppStore()
 const hasNoModelCards = computed(() => hostAppStore.projectModelGroups.length === 0)
--- packages/dui3/layouts/default.vue
+++ packages/dui3/layouts/default.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="relative min-h-screen flex flex-col">
     <HeaderNavBar />
-    <main class="flex-1 mt-10 px-1 max-[275px]:px-0">
+    <main class="flex-1 px-1 max-[275px]:px-0" :class="hasNoModelCards ? '' : 'mt-10'">
       <slot />
     </main>
+    <div
+      v-if="hasNoModelCards"
+      class="px-3 pt-1 text-tiny text-foreground-2 text-center"
+    >
+      Version {{ hostApp.connectorVersion }}
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { useHostAppStore } from '~/store/hostApp'
+
+const hostApp = useHostAppStore()
+const hasNoModelCards = computed(() => hostApp.projectModelGroups.length === 0)
+</script>
