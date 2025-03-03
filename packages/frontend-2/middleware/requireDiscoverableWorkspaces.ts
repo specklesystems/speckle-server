a8b75d756decc5fcdeb0a06b0cdbaec26c575800
andrewwallacespeckle <andrew@speckle.systems>
2025-03-03 14:34:41 +0000

    Redirect users with no discoverable workspaces to create

---
+++ packages/frontend-2/middleware/requireDiscoverableWorkspaces.ts
@@ -0,0 +1,27 @@
+import { activeUserQuery } from '~~/lib/auth/composables/activeUser'
+import { useApolloClientFromNuxt } from '~~/lib/common/composables/graphql'
+import { convertThrowIntoFetchResult } from '~~/lib/common/helpers/graphql'
+import { workspaceCreateRoute } from '~~/lib/common/helpers/route'
+
+/**
+ * Redirect user to /workspaces/create, if they have no discoverable workspaces
+ */
+export default defineNuxtRouteMiddleware(async (to) => {
+  const client = useApolloClientFromNuxt()
+  const { data } = await client
+    .query({
+      query: activeUserQuery,
+      fetchPolicy: 'network-only'
+    })
+    .catch(convertThrowIntoFetchResult)
+
+  const hasDiscoverableWorkspaces =
+    (data?.activeUser?.discoverableWorkspaces?.length ?? 0) > 0 ||
+    (data?.activeUser?.workspaceJoinRequests?.items?.length ?? 0) > 0
+
+  const isGoingToCreateWorkspace = to.path === workspaceCreateRoute()
+
+  if (!hasDiscoverableWorkspaces && !isGoingToCreateWorkspace) {
+    return navigateTo(workspaceCreateRoute())
+  }
+})
