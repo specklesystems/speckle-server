18f47790d16d2c0df8b5f5acbfda6509269cf95b
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2025-03-06 12:33:35 +0000

    znv allows description to be added which are shown in error messages, which will help debug issues

--- packages/shared/src/environment/index.ts
+++ packages/shared/src/environment/index.ts
@@ -16,107 +16,93 @@ const parseFeatureFlags = () => {
   //INFO
   // As a convention all feature flags should be prefixed with a FF_
   const res = parseEnv(process.env, {
-    // Enables the automate module.
-    // Requires FF_GATEKEEPER_MODULE_ENABLED and FF_WORKSPACES_MODULE_ENABLED to be true
-    // This requires a valid Speckle Enterprise Edition license in order to be enabled.
-    // See https://github.com/specklesystems/speckle-server?tab=License-1-ov-file#readme
     FF_AUTOMATE_MODULE_ENABLED: {
       schema: z.boolean(),
+      description:
+        'Enables the Automate module. Requires FF_GATEKEEPER_MODULE_ENABLED and FF_WORKSPACES_MODULE_ENABLED to be true. This requires a valid Speckle Enterprise Edition license in order to be enabled, see https://github.com/specklesystems/speckle-server?tab=License-1-ov-file#readme',
       defaults: { _: false }
     },
-    // Enables the gendo ai integration
     FF_GENDOAI_MODULE_ENABLED: {
       schema: z.boolean(),
+      description: 'Enables the gendo ai integration',
       defaults: { _: false }
     },
-    // Enables the workspaces module
-    // Requires FF_GATEKEEPER_MODULE_ENABLED to be true
-    // This requires a valid Speckle Enterprise Edition license in order to be enabled.
-    // See https://github.com/specklesystems/speckle-server?tab=License-1-ov-file#readme
+    //
     FF_WORKSPACES_MODULE_ENABLED: {
       schema: z.boolean(),
+      description:
+        'Enables the workspaces module. Requires FF_GATEKEEPER_MODULE_ENABLED to be true. This requires a valid Speckle Enterprise Edition license in order to be enabled, see https://github.com/specklesystems/speckle-server?tab=License-1-ov-file#readme',
       defaults: { _: false }
     },
-    // Enables the new (under development as of Q1 2025) plans for workspaces
-    // Requires FF_GATEKEEPER_MODULE_ENABLED and FF_WORKSPACES_MODULE_ENABLED to be true
     // This is expected to be a temporary flag, to be removed once the new plans are stable
     // See https://linear.app/speckle/initiative/new-pricing-and-packaging-f3c6c8ada825/overview
-    // This requires a valid Speckle Enterprise Edition license in order to be enabled.
-    // See https://github.com/specklesystems/speckle-server?tab=License-1-ov-file#readme
     FF_WORKSPACES_NEW_PLANS_ENABLED: {
       schema: z.boolean(),
+      description:
+        'Enables the new (under development as of Q1 2025) plans for workspaces. Requires FF_GATEKEEPER_MODULE_ENABLED and FF_WORKSPACES_MODULE_ENABLED to be true. This requires a valid Speckle Enterprise Edition license in order to be enabled, see https://github.com/specklesystems/speckle-server?tab=License-1-ov-file#readme',
       defaults: { _: false }
     },
-    // Enables free plans for workspaces
-    // Requires FF_GATEKEEPER_MODULE_ENABLED and FF_WORKSPACES_MODULE_ENABLED to be true
     // This is expected to be a temporary flag, to be removed once the new plans are stable
     // See https://linear.app/speckle/initiative/new-pricing-and-packaging-f3c6c8ada825/overview
-    // This requires a valid Speckle Enterprise Edition license in order to be enabled.
-    // See https://github.com/specklesystems/speckle-server?tab=License-1-ov-file#readme
     FF_GATEKEEPER_FORCE_FREE_PLAN: {
       schema: z.boolean(),
+      description:
+        'Enables free plans for workspaces. Requires FF_GATEKEEPER_MODULE_ENABLED and FF_WORKSPACES_MODULE_ENABLED to be true. This requires a valid Speckle Enterprise Edition license in order to be enabled, ee https://github.com/specklesystems/speckle-server?tab=License-1-ov-file#readme',
       defaults: { _: false }
     },
-    // Enables the 'gatekeeper', required for enabling licensed features
-    // This requires a valid Speckle Enterprise Edition license in order to be enabled.
-    // See https://github.com/specklesystems/speckle-server?tab=License-1-ov-file#readme
     FF_GATEKEEPER_MODULE_ENABLED: {
       schema: z.boolean(),
+      description:
+        "Enables the 'gatekeeper', required for enabling licensed features. This requires a valid Speckle Enterprise Edition license in order to be enabled, see https://github.com/specklesystems/speckle-server?tab=License-1-ov-file#readme",
       defaults: { _: false }
     },
-    // Enables Stripe billing integration
     // This is expected to be disabled in Enterprise and self-hosted deployments, but enabled in app.speckle.systems
-    // Requires FF_GATEKEEPER_MODULE_ENABLED and FF_WORKSPACES_MODULE_ENABLED to be true
-    // This requires a valid Speckle Enterprise Edition license in order to be enabled.
-    // See https://github.com/specklesystems/speckle-server?tab=License-1-ov-file#readme
     FF_BILLING_INTEGRATION_ENABLED: {
       schema: z.boolean(),
+      description:
+        'Enables Stripe billing integration. Requires FF_GATEKEEPER_MODULE_ENABLED and FF_WORKSPACES_MODULE_ENABLED to be true. This requires a valid Speckle Enterprise Edition license in order to be enabled, see https://github.com/specklesystems/speckle-server?tab=License-1-ov-file#readme',
       defaults: { _: false }
     },
-    // Enables using dynamic SSO on a per workspace basis
-    // Requires FF_GATEKEEPER_MODULE_ENABLED and FF_WORKSPACES_MODULE_ENABLED to be true
-    // This requires a valid Speckle Enterprise Edition license in order to be enabled.
-    // See https://github.com/specklesystems/speckle-server?tab=License-1-ov-file#readme
     FF_WORKSPACES_SSO_ENABLED: {
       schema: z.boolean(),
+      description:
+        'Enables using dynamic SSO on a per workspace basis. Requires FF_GATEKEEPER_MODULE_ENABLED and FF_WORKSPACES_MODULE_ENABLED to be true. This requires a valid Speckle Enterprise Edition license in order to be enabled, see https://github.com/specklesystems/speckle-server?tab=License-1-ov-file#readme',
       defaults: { _: false }
     },
-    // Enables the multiple emails module
     FF_MULTIPLE_EMAILS_MODULE_ENABLED: {
       schema: z.boolean(),
+      description:
+        'Allows multiple email addresses to be associated with a single user',
       defaults: { _: false }
     },
-    // Enables workspaces multi region DB support
-    // Requires FF_GATEKEEPER_MODULE_ENABLED and FF_WORKSPACES_MODULE_ENABLED to be true
-    // This requires a valid Speckle Enterprise Edition license in order to be enabled.
-    // See https://github.com/specklesystems/speckle-server?tab=License-1-ov-file#readme
     FF_WORKSPACES_MULTI_REGION_ENABLED: {
       schema: z.boolean(),
+      description:
+        'Enables workspaces multi region DB support. Requires FF_GATEKEEPER_MODULE_ENABLED and FF_WORKSPACES_MODULE_ENABLED to be true. This requires a valid Speckle Enterprise Edition license in order to be enabled.  See https://github.com/specklesystems/speckle-server?tab=License-1-ov-file#readme',
       defaults: { _: false }
     },
-    // Toggles IFC parsing with experimental .Net parser
-    // Development project as of Q1 2025. Intention is to evaluate the performance of the .Net parser and remove this flag once the parser is stable.
+    // Development. Intention is to evaluate the performance of the .Net parser and remove this flag once the parser is stable.
     // See https://linear.app/speckle/project/ifc-ecaffd5eb1c2/overview
     FF_FILEIMPORT_IFC_DOTNET_ENABLED: {
       schema: z.boolean(),
+      description: 'Toggles IFC parsing with experimental .Net parser',
       defaults: { _: false }
     },
-    // Forces onboarding for all users
     FF_FORCE_ONBOARDING: {
       schema: z.boolean(),
+      description: 'Forces onboarding flow for all users',
       defaults: { _: false }
     },
-    // Enable to not allow personal emails
     FF_NO_PERSONAL_EMAILS_ENABLED: {
       schema: z.boolean(),
+      description: 'Enable to not allow personal emails',
       defaults: { _: false }
     },
-    // Enables endpoint(s) for updating a project's region
-    // Requires FF_WORKSPACES_MULTI_REGION_ENABLED to be true (which indirectly requires FF_GATEKEEPER_MODULE_ENABLED and FF_WORKSPACES_MODULE_ENABLED to be true)
-    // This requires a valid Speckle Enterprise Edition license in order to be enabled.
-    // See https://github.com/specklesystems/speckle-server?tab=License-1-ov-file#readme
+    //
     FF_MOVE_PROJECT_REGION_ENABLED: {
       schema: z.boolean(),
+      description:
+        "Enables endpoint(s) for updating a project's region. Requires FF_WORKSPACES_MULTI_REGION_ENABLED to be true (which indirectly requires FF_GATEKEEPER_MODULE_ENABLED and FF_WORKSPACES_MODULE_ENABLED to be true. This requires a valid Speckle Enterprise Edition license in order to be enabled, see https://github.com/specklesystems/speckle-server?tab=License-1-ov-file#readme",
       defaults: { _: false }
     }
   })
