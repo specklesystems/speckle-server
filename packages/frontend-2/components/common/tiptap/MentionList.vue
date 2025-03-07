a623485d8e78d866422d517a9e2198d5412d585d
Mike Tasset <mike.tasset@gmail.com>
2025-03-07 10:14:07 +0100

    Allow spaces in mentions

--- packages/frontend-2/components/common/tiptap/MentionList.vue
+++ packages/frontend-2/components/common/tiptap/MentionList.vue
@@ -14,7 +14,7 @@
         </li>
       </template>
       <template v-else>
-        <li class="p-2 text-body-2xs text-foreground-2">Couldn't find anyone</li>
+        <li class="p-2 text-body-2xs text-foreground-2">No matching users found</li>
       </template>
     </ul>
   </div>
