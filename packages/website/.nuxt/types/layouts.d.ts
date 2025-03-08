79ef5b5ec68072bd0c02863567ed0abe4f48635a
Dimitrie Stefanescu <didimitrie@gmail.com>
2025-03-08 07:24:36 +0000

    feat: removes .nuxt folder

--- packages/website/.nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import type { ComputedRef, MaybeRef } from 'vue'
-export type LayoutKey = "default"
-declare module "../../node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: MaybeRef<LayoutKey | false> | ComputedRef<LayoutKey | false>
-  }
-}
