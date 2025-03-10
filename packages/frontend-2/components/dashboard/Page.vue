24c9c61d34d645157b0f3b5ec59ff01c0c77e717
Dimitrie Stefanescu <didimitrie@gmail.com>
2025-03-10 12:29:35 +0000

    feat: removes docs link, adds usecases quickstart card

--- packages/frontend-2/components/dashboard/Page.vue
+++ packages/frontend-2/components/dashboard/Page.vue
@@ -15,7 +15,7 @@
       <div class="flex flex-col-reverse lg:flex-col gap-y-12">
         <section>
           <h2 class="text-heading-sm text-foreground-2">Quickstart</h2>
-          <div class="grid grid-cols-1 md:grid-cols-3 gap-3 pt-5">
+          <div class="grid grid-cols-1 md:grid-cols-2 gap-3 pt-5">
             <CommonCard
               v-for="quickStartItem in quickStartItems"
               :key="quickStartItem.title"
@@ -24,6 +24,42 @@
               :buttons="quickStartItem.buttons"
               :is-external-route="quickStartItem.isExternalRoute"
             />
+          </div>
+          <div class="pt-5">
+            <CommonCard title="Use cases and workflows">
+              <!-- <h2 class="text-heading-sm text-foreground-2">
+              Get started with these use cases
+            </h2> -->
+              <div
+                class="grid md:grid-cols-2 md:gap-y-6 lg:grid-cols-4 gap-3 mt-4 lg:divide-x divide-outline-2 -ml-3"
+              >
+                <div
+                  v-for="useCase in useCaseItems"
+                  :key="useCase.title"
+                  :title="useCase.title"
+                  :description="useCase.description"
+                  class="pl-4 flex flex-col justify-between"
+                >
+                  <div>
+                    <p class="text-heading-sm text-foreground-2">{{ useCase.title }}</p>
+                    <p class="text-body-xs text-foreground-2 py-2">
+                      {{ useCase.description }}
+                    </p>
+                  </div>
+                  <div class="">
+                    <FormButton
+                      :to="useCase.url"
+                      target="_blank"
+                      external
+                      color="outline"
+                      size="sm"
+                    >
+                      Open documentation
+                    </FormButton>
+                  </div>
+                </div>
+              </div>
+            </CommonCard>
           </div>
         </section>
         <!-- <section>
@@ -88,7 +124,6 @@ import type { QuickStartItem } from '~~/lib/dashboard/helpers/types'
 import { useQuery } from '@vue/apollo-composable'
 // import { useMixpanel } from '~~/lib/core/composables/mp'
 import {
-  docsPageUrl,
   forumPageUrl,
   homeRoute,
   connectorsRoute,
@@ -120,6 +155,42 @@ useUserProjectsUpdatedTracking()
 
 const promoBanners = ref<PromoBanner[]>()
 const openNewProject = ref(false)
+
+const useCaseItems = shallowRef([
+  {
+    title: 'Design Coordination',
+    url: 'https://www.speckle.systems/use-cases/design-coordination',
+    imgSrc:
+      'https://cdn.prod.website-files.com/66c31b5a50432200dc753cc4/67c0302b6d2fda823c50d927_design%20coordination%20header%20image.png',
+    description:
+      'The smoothest design coordination for AEC! Ditch files. Share only what’s needed and catch changes instantly.'
+  },
+  {
+    title: 'Business Intelligence',
+    url: 'https://www.speckle.systems/use-cases/business-intelligence',
+    imgSrc:
+      'https://cdn.prod.website-files.com/66c31b5a50432200dc753cc4/67c822c3139c3aba5c8c3003_Header%20final%20BI.png',
+    description:
+      'Get from boring BIM data to insightful dashboards! Swap guesswork for informed decisions.'
+  },
+  {
+    title: 'Online Collaboration',
+    url: 'https://www.speckle.systems/use-cases/online-collaboration',
+    imgSrc:
+      'https://cdn.prod.website-files.com/66c31b5a50432200dc753cc4/67c574e6ef8841d296e989cc_viwer%20header.png',
+    description:
+      'View, share, and brainstorm on 3D models online! Share with anyone—no desktop apps, no licenses, no hassle.'
+  },
+  {
+    title: 'Automation',
+    url: 'https://www.speckle.systems/use-cases/automate',
+    imgSrc:
+      'https://cdn.prod.website-files.com/66c31b5a50432200dc753cc4/67c0302b6d2fda823c50d927_design%20coordination%20header%20image.png',
+    description:
+      'Goodbye, repetitive tasks! Kick into high gear with pre-built automations for your workflows.'
+  }
+])
+
 const quickStartItems = shallowRef<QuickStartItem[]>([
   {
     title: 'Install Connectors',
@@ -133,16 +204,16 @@ const quickStartItems = shallowRef<QuickStartItem[]>([
     ],
     isExternalRoute: false
   },
-  {
-    title: "Don't know where to start?",
-    description: "We'll walk you through some of most common usage scenarios.",
-    buttons: [
-      {
-        text: 'Open documentation',
-        props: { to: docsPageUrl }
-      }
-    ]
-  },
+  // {
+  //   title: "Don't know where to start?",
+  //   description: "We'll walk you through some of most common usage scenarios.",
+  //   buttons: [
+  //     {
+  //       text: 'Open documentation',
+  //       props: { to: docsPageUrl }
+  //     }
+  //   ]
+  // },
   {
     title: 'Have a question you need answered?',
     description: 'Submit your question on the forum and get help from the community.',
