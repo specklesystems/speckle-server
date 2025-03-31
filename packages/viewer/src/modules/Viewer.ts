4460f1295b30cf7077c4328f6cffdd208dbe0962
AlexandruPopovici <alexandrupopoviciioan@gmail.com>
2025-03-31 22:44:01 +0300

    Revert "feat(viewer-lib): Implemented WebXrViewer which wraps three's xr functionality"
    This reverts commit 8798f265b7c0d0c654e588e16f0aefd4ecd5508c.

--- packages/viewer/src/modules/Viewer.ts
+++ packages/viewer/src/modules/Viewer.ts
@@ -196,12 +196,12 @@ export class Viewer extends EventEmitter implements IViewer {
     }
   }
 
-  protected frame() {
+  private frame() {
     this.update()
     this.render()
   }
 
-  protected update() {
+  private update() {
     const delta = this.clock.getDelta() * 1000 // turn to miliseconds
     const extensions = Object.values(this.extensions)
     extensions.forEach((ext: Extension) => {
@@ -215,8 +215,8 @@ export class Viewer extends EventEmitter implements IViewer {
     requestAnimationFrame(this.frame.bind(this))
   }
 
-  protected render() {
-    this.speckleRenderer.render()
+  private render() {
+    // this.speckleRenderer.render()
     Object.values(this.extensions).forEach((ext: Extension) => {
       ext.onRender()
     })
