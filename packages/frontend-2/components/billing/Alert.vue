c267c983ffd7806b98d1b97ecb91f18b4f6f7726
Mike Tasset <mike.tasset@gmail.com>
2025-04-02 17:01:07 +0200

    Feat: Clean up old sidebar and trial related logic

--- packages/frontend-2/components/billing/Alert.vue
+++ packages/frontend-2/components/billing/Alert.vue
@@ -1,21 +1,7 @@
 <template>
   <div>
     <template v-if="!hasValidPlan">
-      <div
-        v-if="condensed"
-        class="flex items-center justify-between rounded-md p-2 pl-3 text-body-3xs font-medium bg-info-lighter text-primary-focus dark:text-foreground gap-x-2"
-      >
-        {{ title }}
-        <FormButton
-          v-if="actions.length > 0"
-          size="sm"
-          :disabled="actions[0].disabled"
-          @click="actions[0].onClick"
-        >
-          {{ actions[0].title }}
-        </FormButton>
-      </div>
-      <CommonAlert v-else :color="alertColor" :actions="actions">
+      <CommonAlert :color="alertColor" :actions="actions">
         <template #title>
           {{ title }}
         </template>
@@ -28,7 +14,6 @@
 </template>
 
 <script setup lang="ts">
-import dayjs from 'dayjs'
 import { graphql } from '~/lib/common/generated/gql'
 import {
   type BillingAlert_WorkspaceFragment,
@@ -61,43 +46,18 @@ const props = defineProps<{
 const { billingPortalRedirect } = useBillingActions()
 
 const planStatus = computed(() => props.workspace.plan?.status)
-// If there is no plan status, we assume it's a trial
-const isTrial = computed(
-  () => !planStatus.value || planStatus.value === WorkspacePlanStatuses.Trial
-)
 const isPaymentFailed = computed(
   () => planStatus.value === WorkspacePlanStatuses.PaymentFailed
 )
 const isScheduledForCancelation = computed(
   () => planStatus.value === WorkspacePlanStatuses.CancelationScheduled
 )
-const trialDaysLeft = computed(() => {
-  const createdAt = props.workspace.plan?.createdAt
-  const trialEndDate = dayjs(createdAt).add(31, 'days')
-  const diffDays = trialEndDate.diff(dayjs(), 'day')
-  return Math.max(0, diffDays)
-})
 const title = computed(() => {
-  if (isTrial.value) {
-    if (trialDaysLeft.value === 0) {
-      return 'Final day of free trial'
-    }
-    if (props.condensed) {
-      return `${trialDaysLeft.value} day${
-        trialDaysLeft.value !== 1 ? 's' : ''
-      } left in trial`
-    } else
-      return `You have ${trialDaysLeft.value} day${
-        trialDaysLeft.value !== 1 ? 's' : ''
-      } left on your free trial`
-  }
   switch (planStatus.value) {
     case WorkspacePlanStatuses.CancelationScheduled:
       return `Your workspace subscription is scheduled for cancellation`
     case WorkspacePlanStatuses.Canceled:
       return `Your workspace subscription has been cancelled`
-    case WorkspacePlanStatuses.Expired:
-      return `Your free trial has ended`
     case WorkspacePlanStatuses.PaymentFailed:
       return "Your last payment didn't go through"
     default:
@@ -105,18 +65,11 @@ const title = computed(() => {
   }
 })
 const description = computed(() => {
-  if (isTrial.value) {
-    return trialDaysLeft.value === 0
-      ? 'Upgrade to a paid plan to continue using your workspace'
-      : 'Upgrade to a paid plan to start your subscription'
-  }
   switch (planStatus.value) {
     case WorkspacePlanStatuses.CancelationScheduled:
       return 'Once the current billing cycle ends your workspace will enter read-only mode. Renew your subscription to undo.'
     case WorkspacePlanStatuses.Canceled:
       return 'Your workspace has been cancelled and is in read-only mode. Subscribe to a plan to regain full access.'
-    case WorkspacePlanStatuses.Expired:
-      return "The workspace is in a read-only locked state until there's an active subscription. Subscribe to a plan to regain full access."
     case WorkspacePlanStatuses.PaymentFailed:
       return "Update your payment information now to ensure your workspace doesn't go into maintenance mode."
     default:
@@ -131,7 +84,6 @@ const alertColor = computed<AlertColor>(() => {
     case WorkspacePlanStatuses.Canceled:
       return 'danger'
     case WorkspacePlanStatuses.CancelationScheduled:
-    case WorkspacePlanStatuses.Expired:
       return 'warning'
     default:
       return 'neutral'
