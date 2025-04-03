80845f0acf325cb8a9f1e0057aa06aa70f78d253
Chuck Driesler <chuck@speckle.systems>
2025-04-03 13:42:41 +0200

    chore(workspaces): adjust move project tests

--- packages/server/modules/workspaces/tests/integration/projects.graph.spec.ts
+++ packages/server/modules/workspaces/tests/integration/projects.graph.spec.ts
@@ -392,7 +392,7 @@ describe('Workspace project GQL CRUD', () => {
       expect(workspaceId).to.equal(targetWorkspace.id)
     })
 
-    it('should preserve project roles for project members', async () => {
+    it('should preserve project roles for project members with editor seats', async () => {
       const res = await apollo.execute(MoveProjectToWorkspaceDocument, {
         projectId: testProject.id,
         workspaceId: targetWorkspace.id
@@ -401,10 +401,22 @@ describe('Workspace project GQL CRUD', () => {
       const { team } = res.data?.workspaceMutations.projects.moveToWorkspace ?? {}
 
       const adminProjectRole = team?.find((role) => role.id === serverAdminUser.id)
-      const memberProjectRole = team?.find((role) => role.id === serverMemberUser.id)
 
       expect(res).to.not.haveGraphQLErrors()
       expect(adminProjectRole?.role).to.equal(Roles.Stream.Owner)
+    })
+
+    it('should demote users with editor project roles and workspace viewer seats', async () => {
+      const res = await apollo.execute(MoveProjectToWorkspaceDocument, {
+        projectId: testProject.id,
+        workspaceId: targetWorkspace.id
+      })
+
+      const { team } = res.data?.workspaceMutations.projects.moveToWorkspace ?? {}
+
+      const memberProjectRole = team?.find((role) => role.id === serverMemberUser.id)
+
+      expect(res).to.not.haveGraphQLErrors()
       expect(memberProjectRole?.role).to.equal(Roles.Stream.Contributor)
     })
 
