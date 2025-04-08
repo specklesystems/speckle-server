df0669bc2d1d759d1ec91669a8006f645c503082
Gergő Jedlicska <gergo@jedlicska.com>
2025-04-08 13:07:38 +0200

    feat(shared): add plan helpers

--- packages/shared/src/workspaces/helpers/plans.spec.ts
+++ packages/shared/src/workspaces/helpers/plans.spec.ts
@@ -1,28 +1,95 @@
 import { describe, expect, it } from 'vitest'
-import { isNewWorkspacePlan, WorkspacePlans } from './plans.js'
+import {
+  doesPlanIncludeThePaidUnlimitedProjectsAddon,
+  isNewWorkspacePlan,
+  isSelfServerAvailablePlan,
+  WorkspacePlans
+} from './plans.js'
 
-describe('isNewWorkspacePlan', () => {
-  const planCases: {
-    [P in WorkspacePlans]: boolean
-  } = <const>{
-    business: false,
-    businessInvoiced: false,
-    plus: false,
-    plusInvoiced: false,
-    starter: false,
-    starterInvoiced: false,
-    free: true,
-    academia: true,
-    unlimited: true,
-    pro: true,
-    proUnlimited: true,
-    proUnlimitedInvoiced: true,
-    team: true,
-    teamUnlimited: true,
-    teamUnlimitedInvoiced: true
-  }
-  it.each(Object.entries(planCases))('plan %s is new type -> %s', (plan, isNew) => {
-    const result = isNewWorkspacePlan(plan as WorkspacePlans)
-    expect(result).toStrictEqual(isNew)
+describe('plan helpers', () => {
+  describe('isNewWorkspacePlan', () => {
+    const planCases: {
+      [P in WorkspacePlans]: boolean
+    } = <const>{
+      business: false,
+      businessInvoiced: false,
+      plus: false,
+      plusInvoiced: false,
+      starter: false,
+      starterInvoiced: false,
+      free: true,
+      academia: true,
+      unlimited: true,
+      pro: true,
+      proUnlimited: true,
+      proUnlimitedInvoiced: true,
+      team: true,
+      teamUnlimited: true,
+      teamUnlimitedInvoiced: true
+    }
+    it.each(Object.entries(planCases))('plan %s is new type -> %s', (plan, isNew) => {
+      const result = isNewWorkspacePlan(plan as WorkspacePlans)
+      expect(result).toStrictEqual(isNew)
+    })
+  })
+
+  describe('doesPlanIncludeThePaidUnlimitedProjectsAddon', () => {
+    const planCases: {
+      [P in WorkspacePlans]: boolean
+    } = <const>{
+      business: false,
+      businessInvoiced: false,
+      plus: false,
+      plusInvoiced: false,
+      starter: false,
+      starterInvoiced: false,
+      free: false,
+      academia: false,
+      unlimited: false,
+      pro: false,
+      proUnlimited: true,
+      proUnlimitedInvoiced: false,
+      team: false,
+      teamUnlimited: true,
+      teamUnlimitedInvoiced: false
+    }
+    it.each(Object.entries(planCases))(
+      'plan %s include the paid unlimited projects addon -> %s',
+      (plan, isNew) => {
+        const result = doesPlanIncludeThePaidUnlimitedProjectsAddon(
+          plan as WorkspacePlans
+        )
+        expect(result).toStrictEqual(isNew)
+      }
+    )
+  })
+
+  describe('isSelfServerAvailablePlan', () => {
+    const planCases: {
+      [P in WorkspacePlans]: boolean
+    } = <const>{
+      business: false,
+      businessInvoiced: false,
+      plus: false,
+      plusInvoiced: false,
+      starter: false,
+      starterInvoiced: false,
+      free: true,
+      academia: false,
+      unlimited: false,
+      pro: true,
+      proUnlimited: true,
+      proUnlimitedInvoiced: false,
+      team: true,
+      teamUnlimited: true,
+      teamUnlimitedInvoiced: false
+    }
+    it.each(Object.entries(planCases))(
+      'is plan %s available self served -> %s',
+      (plan, isNew) => {
+        const result = isSelfServerAvailablePlan(plan as WorkspacePlans)
+        expect(result).toStrictEqual(isNew)
+      }
+    )
   })
 })
