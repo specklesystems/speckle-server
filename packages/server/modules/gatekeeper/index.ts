b71f0bd5c4741b1ab4659c2930cec708672f33e1
Gergő Jedlicska <57442769+gjedlicska@users.noreply.github.com>
2025-04-08 20:00:18 +0200

    feat(gatekeeper): re-enable workspace plan migrations (#4352)

--- packages/server/modules/gatekeeper/index.ts
+++ packages/server/modules/gatekeeper/index.ts
@@ -111,17 +111,18 @@ const scheduleWorkspaceSubscriptionDownscale = ({
   )
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const scheduleWorkspacePlanMigrations = (scheduleExecution: ScheduleExecution) => {
   let isMigrationComplete = false
+  let isMigrationRunning = false
   const cronExpression = '*/5 * * * * *' // every 5 seconds
   return scheduleExecution(
     cronExpression,
     'WorkspaceNewPlanMigration',
     async (_scheduledTime, { logger }) => {
-      if (isMigrationComplete) return
+      if (isMigrationComplete || isMigrationRunning) return
+      isMigrationRunning = true
       await migrateOldWorkspacePlans({ db, stripe: getStripeClient(), logger })()
-
+      isMigrationRunning = false
       isMigrationComplete = true
     }
   )
@@ -248,8 +249,8 @@ const gatekeeperModule: SpeckleModule = {
         scheduledTasks = [
           scheduleWorkspaceSubscriptionDownscale({ scheduleExecution }),
           scheduleWorkspaceTrialEmails({ scheduleExecution }),
-          scheduleWorkspaceTrialExpiry({ scheduleExecution, emit: eventBus.emit })
-          // scheduleWorkspacePlanMigrations(scheduleExecution)
+          scheduleWorkspaceTrialExpiry({ scheduleExecution, emit: eventBus.emit }),
+          scheduleWorkspacePlanMigrations(scheduleExecution)
         ]
 
         quitListeners = initializeEventListenersFactory({
