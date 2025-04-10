96698452f21a45818038901c2851b43fad3ba886
Alessandro Magionami <alessandro.magionami@gmail.com>
2025-04-10 10:04:43 +0200

    feat(core): limit returned commits in streams and breanches

--- packages/server/modules/core/tests/unit/services/versions.spec.ts
+++ packages/server/modules/core/tests/unit/services/versions.spec.ts
@@ -1,14 +1,50 @@
 import { createRandomString } from '@/modules/core/helpers/testHelpers'
-import { getLimitedReferencedObjectFactory } from '@/modules/core/services/versions/limits'
+import {
+  getDateFromLimitsFactory,
+  getLimitedReferencedObjectFactory
+} from '@/modules/core/services/versions/limits'
 import { GetWorkspaceLimits } from '@speckle/shared/dist/commonjs/authz/domain/workspaces/operations'
 import { expect } from 'chai'
+import dayjs from 'dayjs'
 
 describe('Module @core', () => {
   describe('Services versions', () => {
+    describe('getDateFromLimits', () => {
+      it('should return null if workspace has no versionHistory limits', async () => {
+        const getWorkspaceLimits = async () => null
+        const workspaceId = createRandomString()
+
+        expect(
+          await getDateFromLimitsFactory({
+            getWorkspaceLimits,
+            environment: { personalProjectsLimitEnabled: false }
+          })({ workspaceId })
+        ).to.eq(null)
+      })
+      it('should return date in workspace versionHistory limits', async () => {
+        const getWorkspaceLimits = async () => ({
+          projectCount: null,
+          modelCount: null,
+          versionsHistory: { value: 1, unit: 'month' as const }
+        })
+        const workspaceId = createRandomString()
+
+        expect(
+          (
+            await getDateFromLimitsFactory({
+              getWorkspaceLimits,
+              environment: { personalProjectsLimitEnabled: false }
+            })({ workspaceId })
+          )
+            ?.toISOString()
+            .slice(-5)
+        ).to.eq(dayjs().subtract(1, 'month').toDate().toISOString().slice(-5))
+      })
+    })
     describe('getLimitedReferencedObjectFactory returns a function that, ', () => {
       it('should return the version referencedObject if project workspace has no limits', async () => {
         const getWorkspaceLimits = (() => null) as unknown as GetWorkspaceLimits
-        const project = { workspaceId: createRandomString() }
+        const workspaceId = createRandomString()
         const version = {
           referencedObject: createRandomString(),
           createdAt: new Date()
@@ -17,14 +53,14 @@ describe('Module @core', () => {
           await getLimitedReferencedObjectFactory({
             environment: { personalProjectsLimitEnabled: false },
             getWorkspaceLimits
-          })({ version, project })
+          })({ version, workspaceId })
         ).to.eq(version.referencedObject)
       })
       it('should return null if version is outside of workspace limit', async () => {
         const getWorkspaceLimits = (() => ({
           versionsHistory: { value: 1, unit: 'week' }
         })) as unknown as GetWorkspaceLimits
-        const project = { workspaceId: createRandomString() }
+        const workspaceId = createRandomString()
         const tenDaysAgo = new Date()
         tenDaysAgo.setDate(new Date().getDate() - 10)
         const version = {
@@ -35,14 +71,14 @@ describe('Module @core', () => {
           await getLimitedReferencedObjectFactory({
             environment: { personalProjectsLimitEnabled: false },
             getWorkspaceLimits
-          })({ version, project })
+          })({ version, workspaceId })
         ).to.eq(null)
       })
       it('should return version referencedObject if version is inside of workspace limit', async () => {
         const getWorkspaceLimits = (() => ({
           versionsHistory: { value: 1, unit: 'week' }
         })) as unknown as GetWorkspaceLimits
-        const project = { workspaceId: createRandomString() }
+        const workspaceId = createRandomString()
         const twoDaysAgo = new Date()
         twoDaysAgo.setDate(new Date().getDate() - 2)
         const version = {
@@ -53,13 +89,13 @@ describe('Module @core', () => {
           await getLimitedReferencedObjectFactory({
             environment: { personalProjectsLimitEnabled: false },
             getWorkspaceLimits
-          })({ version, project })
+          })({ version, workspaceId })
         ).to.eq(version.referencedObject)
       })
       it('should return version referencedObject if project is not in a workspace and personalProjectsLimits is not enabled', async () => {
         const getWorkspaceLimits = (() =>
           expect.fail()) as unknown as GetWorkspaceLimits
-        const project = { workspaceId: null }
+        const workspaceId = null
         const tenDaysAgo = new Date()
         tenDaysAgo.setDate(new Date().getDate() - 10)
         const version = {
@@ -70,13 +106,13 @@ describe('Module @core', () => {
           await getLimitedReferencedObjectFactory({
             environment: { personalProjectsLimitEnabled: false },
             getWorkspaceLimits
-          })({ version, project })
+          })({ version, workspaceId })
         ).to.eq(version.referencedObject)
       })
       it('should return null if project is not in a workspace and personalProjectsLimits is enabled and version is outside of limits', async () => {
         const getWorkspaceLimits = (() =>
           expect.fail()) as unknown as GetWorkspaceLimits
-        const project = { workspaceId: null }
+        const workspaceId = null
         const tenDaysAgo = new Date()
         tenDaysAgo.setDate(new Date().getDate() - 10)
         const version = {
@@ -87,13 +123,13 @@ describe('Module @core', () => {
           await getLimitedReferencedObjectFactory({
             environment: { personalProjectsLimitEnabled: true },
             getWorkspaceLimits
-          })({ version, project })
+          })({ version, workspaceId })
         ).to.eq(null)
       })
       it('should return version referencedObject if project is not in a workspace and personalProjectsLimits is enabled and version is inside limits', async () => {
         const getWorkspaceLimits = (() =>
           expect.fail()) as unknown as GetWorkspaceLimits
-        const project = { workspaceId: null }
+        const workspaceId = null
         const version = {
           referencedObject: createRandomString(),
           createdAt: new Date()
@@ -102,7 +138,7 @@ describe('Module @core', () => {
           await getLimitedReferencedObjectFactory({
             environment: { personalProjectsLimitEnabled: true },
             getWorkspaceLimits
-          })({ version, project })
+          })({ version, workspaceId })
         ).to.eq(version.referencedObject)
       })
     })
