d96726318ff6c63e5f94e02e390326e82d2c3db6
Kristaps Fabians Geikins <fabis94@live.com>
2025-04-23 14:41:01 +0300

    fix(server): cache logger not respecting level

--- packages/server/modules/shared/middleware/index.ts
+++ packages/server/modules/shared/middleware/index.ts
@@ -47,7 +47,6 @@ import { getUserRoleFactory } from '@/modules/core/repositories/users'
 import { UserInputError } from '@/modules/core/errors/userinput'
 import compression from 'compression'
 import { moduleAuthLoaders } from '@/modules'
-import { getRequestLogger } from '@/observability/components/express/requestContext'
 
 export const authMiddlewareCreator = (
   steps: AuthPipelineFunction[]
@@ -198,14 +197,6 @@ export async function buildContext(params?: {
     authContext ||
     req?.context ||
     (await createAuthContextFromToken(token ?? getTokenFromRequest(req), validateToken))
-
-  // Update req.log w/ userId
-  const reqLogger = getRequestLogger()
-  if (reqLogger) {
-    reqLogger.setBindings({
-      userId: ctx.userId || null
-    })
-  }
 
   const log = Observability.extendLoggerComponent(
     req?.log || subscriptionLogger,
