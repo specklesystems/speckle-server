720eec6a5bc65ee97098e397a8dc336040774e09
Daniel Gak Anagrov <daniel@speckle.systems>
2025-05-20 18:39:10 +0200

    fix: get_version

--- .github/workflows/scripts/common.sh
+++ .github/workflows/scripts/common.sh
@@ -3,33 +3,17 @@ set -eo pipefail
 
 DOCKER_IMAGE_TAG="speckle/speckle-${SPECKLE_SERVER_PACKAGE}"
 
-echo "1 ${DOCKER_IMAGE_TAG}"
-
 # shellcheck disable=SC2034,SC2086
 IMAGE_VERSION_TAG="${IMAGE_VERSION_TAG:-${GITHUB_SHA}}"
-
-echo "2 ${IMAGE_VERSION_TAG}"
-
-echo "3 $(git rev-list --tags)"
-echo "3.2 $(git describe --always --tags $(git rev-list --tags))"
-echo "3.3 $(git describe --always --tags $(git rev-list --tags) | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$')"
 
 # shellcheck disable=SC2068,SC2046
 LAST_RELEASE="$(git describe --always --tags $(git rev-list --tags) | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' | head -n 1)" # get the last release tag. FIXME: Fails if a commit is tagged with more than one tag: https://stackoverflow.com/questions/8089002/git-describe-with-two-tags-on-the-same-commit/56039163#56039163
 
-echo "4 ${LAST_RELEASE}"
-
 # shellcheck disable=SC2034
 NEXT_RELEASE="$(echo "${LAST_RELEASE}" | awk -F. -v OFS=. '{$NF += 1 ; print}')"
-
-echo "5 ${NEXT_RELEASE}"
 
 # shellcheck disable=SC2034
 BRANCH_NAME_TRUNCATED="$(echo "${GITHUB_HEAD_REF}" | cut -c -50 | sed 's/[^a-zA-Z0-9.-]/-/g')" # docker has a 128 character tag limit, so ensuring the branch name will be short enough
 
-echo "6 ${BRANCH_NAME_TRUNCATED}"
-
 # shellcheck disable=SC2034
 COMMIT_SHA1_TRUNCATED="$(echo "${GITHUB_SHA}" | cut -c -7)"
-
-echo "7 ${COMMIT_SHA1_TRUNCATED}"
--- .github/workflows/scripts/get_version.sh
+++ .github/workflows/scripts/get_version.sh
@@ -5,29 +5,21 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # shellcheck disable=SC1090,SC1091
 source "${SCRIPT_DIR}/common.sh"
 
-echo "10 ${SCRIPT_DIR}"
-
 if [[ "${GITHUB_REF}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     echo "${GITHUB_REF}"
     exit 0
 fi
-
-echo "11 ${GITHUB_REF}"
 
 if [[ "${GITHUB_HEAD_REF}" == "main" ]]; then
     echo "${NEXT_RELEASE}-alpha.${GITHUB_RUN_ID}"
     exit 0
 fi
 
-echo "12 ${GITHUB_HEAD_REF}"
-
 # if branch name truncated contains an underscore, we should exit
 if [[ "${BRANCH_NAME_TRUNCATED}" =~ "_" ]]; then
     echo "Branch name contains an underscore, exiting"
     exit 1
 fi
-
-echo "13 ${BRANCH_NAME_TRUNCATED}"
 
 echo "${NEXT_RELEASE}-branch.${BRANCH_NAME_TRUNCATED}.${GITHUB_RUN_ID}-${COMMIT_SHA1_TRUNCATED}"
 exit 0
