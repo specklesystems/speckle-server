7e600cd9109624f090f7d64b50ee909742cb86ba
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2025-05-20 08:30:40 +0100

    Incorporate PR comments

--- packages/server/observability/utils/requestContext.ts
+++ packages/server/observability/utils/requestContext.ts
@@ -1,40 +1,62 @@
 import { asyncRequestContextEnabled } from '@/modules/shared/helpers/envHelper'
 import { AsyncLocalStorage } from 'node:async_hooks'
-import type { Logger } from 'pino'
+import type { Logger } from '@/observability/logging'
 
-type StorageType = {
-  requestId?: string
-  taskId?: string
-  taskName?: string
+type StorageTypeBase = {
   dbMetrics: {
     totalDuration: number
     totalCount: number
   }
   logger: Logger
 }
+type RequestStorageType = { requestId: string } & StorageTypeBase
+type TaskStorageType = { taskId: string; taskName: string } & StorageTypeBase
+
+type StorageType = RequestStorageType | TaskStorageType
+
+const isRequestStorage = (store: unknown): store is RequestStorageType => {
+  return typeof store === 'object' && store !== null && 'requestId' in store
+}
+const isTaskStorage = (store: unknown): store is TaskStorageType => {
+  return (
+    typeof store === 'object' &&
+    store !== null &&
+    'taskId' in store &&
+    'taskName' in store
+  )
+}
 
 const storage = asyncRequestContextEnabled()
   ? new AsyncLocalStorage<StorageType>()
   : undefined
 
-export const enterNewRequestContext = (params: {
-  reqId?: string
-  taskId?: string
-  taskName?: string
-  logger: Logger
-}) => {
-  const { reqId, logger } = params
-  const store: StorageType = {
-    requestId: reqId,
-    taskId: params.taskId,
-    taskName: params.taskName,
+export const enterNewRequestContext = (
+  params: ({ reqId: string } | { taskId: string; taskName: string }) & {
+    logger: Logger
+  }
+) => {
+  const { logger } = params
+  const base: StorageTypeBase = {
     dbMetrics: {
       totalCount: 0,
       totalDuration: 0
     },
     logger
   }
-  storage?.enterWith(store)
+  if ('reqId' in params) {
+    const store: RequestStorageType = {
+      ...base,
+      requestId: params.reqId
+    }
+    storage?.enterWith(store)
+  } else if ('taskId' in params) {
+    const store: TaskStorageType = {
+      ...base,
+      taskId: params.taskId,
+      taskName: params.taskName
+    }
+    storage?.enterWith(store)
+  }
 }
 
 export const getRequestContext = () => storage?.getStore()
@@ -43,10 +65,20 @@ export const getRequestLogger = () => getRequestContext()?.logger
 export const loggerWithMaybeContext = ({ logger }: { logger: Logger }) => {
   const reqCtx = getRequestContext()
   if (!reqCtx) return logger
-  return logger.child({
-    req: reqCtx.requestId ? { id: reqCtx.requestId } : undefined,
-    taskId: reqCtx.taskId,
-    taskName: reqCtx.taskName,
-    dbMetrics: reqCtx.dbMetrics
-  })
+
+  if (isTaskStorage(reqCtx)) {
+    return logger.child({
+      taskId: reqCtx.taskId,
+      taskName: reqCtx.taskName,
+      dbMetrics: reqCtx.dbMetrics
+    })
+  }
+
+  if (isRequestStorage(reqCtx)) {
+    return logger.child({
+      req: reqCtx.requestId ? { id: reqCtx.requestId } : undefined,
+      dbMetrics: reqCtx.dbMetrics
+    })
+  }
+  return logger
 }
