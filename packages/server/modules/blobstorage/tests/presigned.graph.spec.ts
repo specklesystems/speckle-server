46c8bb87d04651d0cc292218068abfde6d7aa7a1
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2025-06-04 09:49:45 +0100

    fix tests so they run locally

--- packages/server/modules/blobstorage/tests/presigned.graph.spec.ts
+++ packages/server/modules/blobstorage/tests/presigned.graph.spec.ts
@@ -4,7 +4,6 @@ import { beforeEachContext } from '@/test/hooks'
 import { createProject, grantPermissionsOnProject } from '@/test/projectHelper'
 import { Roles } from '@speckle/shared'
 import { expect } from 'chai'
-import { describe, it } from 'mocha'
 import cryptoRandomString from 'crypto-random-string'
 import gql from 'graphql-tag'
 import { SetNonNullable } from 'type-fest'
@@ -75,39 +74,90 @@ const generateUploadUrl = async (params: TestContext) => {
 }
 
 describe('Presigned @blobstorage', async () => {
-  const serverAdmin = await createTestUser({ role: Roles.Server.Admin })
-  const regularServerUser = await createTestUser({ role: Roles.Server.User })
-  const archivedUser = await createTestUser({ role: Roles.Server.ArchivedUser })
-  const unaffiliatedUser = await createTestUser({ role: Roles.Server.Guest })
+  const serverAdmin = { id: '', name: 'server admin', role: Roles.Server.Admin }
+  const regularServerUser = {
+    id: '',
+    name: 'regular server user',
+    role: Roles.Server.User
+  }
+  const archivedUser = {
+    id: '',
+    name: 'archived user',
+    role: Roles.Server.ArchivedUser
+  }
+  const unaffiliatedUser = {
+    id: '',
+    name: 'unaffiliated user',
+    role: Roles.Server.Guest
+  }
 
-  const ownedProject = await createProject({
+  const ownedProject = {
+    id: '',
     name: 'owned stream',
-    isPublic: false,
-    ownerId: serverAdmin.id
-  })
-
-  const contributorProject = await createProject({
+    isPublic: false
+  }
+  const contributorProject = {
+    id: '',
     name: 'contributions are welcome',
-    isPublic: false,
-    ownerId: serverAdmin.id
-  })
-
-  const reviewerProject = await createProject({
+    isPublic: false
+  }
+  const reviewerProject = {
+    id: '',
     name: 'reviewer stream',
-    isPublic: false,
-    ownerId: serverAdmin.id
-  })
-
-  const noAccessProject = await createProject({
+    isPublic: false
+  }
+  const noAccessProject = {
+    id: '',
     name: 'cannot touch this',
-    isPublic: false,
-    ownerId: serverAdmin.id
-  })
-
-  const publicProject = await createProject({
+    isPublic: false
+  }
+  const publicProject = {
+    id: '',
     name: 'everyone can look',
-    isPublic: true,
-    ownerId: serverAdmin.id
+    isPublic: true
+  }
+
+  before(async () => {
+    await beforeEachContext()
+    serverAdmin.id = (await createTestUser(serverAdmin)).id
+    regularServerUser.id = (await createTestUser(regularServerUser)).id
+    archivedUser.id = (await createTestUser(archivedUser)).id
+    unaffiliatedUser.id = (await createTestUser(unaffiliatedUser)).id
+
+    ownedProject.id = (
+      await createProject({
+        ...ownedProject,
+        ownerId: serverAdmin.id
+      })
+    ).id
+
+    contributorProject.id = (
+      await createProject({
+        ...contributorProject,
+        ownerId: serverAdmin.id
+      })
+    ).id
+
+    reviewerProject.id = (
+      await createProject({
+        ...reviewerProject,
+        ownerId: serverAdmin.id
+      })
+    ).id
+
+    noAccessProject.id = (
+      await createProject({
+        ...noAccessProject,
+        ownerId: serverAdmin.id
+      })
+    ).id
+
+    publicProject.id = (
+      await createProject({
+        ...publicProject,
+        ownerId: serverAdmin.id
+      })
+    ).id
   })
 
   const testData = <const>[
@@ -200,20 +250,50 @@ describe('Presigned @blobstorage', async () => {
           cases: [[generateUploadUrl, false]]
         }
       ]
+    },
+    {
+      user: null,
+      projectData: [
+        {
+          project: ownedProject,
+          projectRole: null,
+          cases: [[generateUploadUrl, false]]
+        },
+        {
+          project: contributorProject,
+          projectRole: null,
+          cases: [[generateUploadUrl, false]]
+        },
+        {
+          project: reviewerProject,
+          projectRole: null,
+          cases: [[generateUploadUrl, false]]
+        },
+        {
+          project: noAccessProject,
+          projectRole: null,
+          cases: [[generateUploadUrl, false]]
+        },
+        {
+          project: publicProject,
+          projectRole: null,
+          cases: [[generateUploadUrl, false]]
+        }
+      ]
     }
   ]
 
-  before(async () => {
-    await beforeEachContext()
-  })
-
-  testData.forEach((userContext) => {
+  testData.forEach(async (userContext) => {
     const testUser = userContext.user
+
     describe(`User: ${testUser?.name ?? 'Anonymous'} as a ${
       testUser?.role ?? 'anonymous user'
     }`, async () => {
-      const apollo = await testApolloServer({
-        authUserId: testUser?.id
+      let apollo: Awaited<ReturnType<typeof testApolloServer>>
+      before(async () => {
+        apollo = await testApolloServer({
+          authUserId: testUser?.id
+        })
       })
 
       userContext.projectData.forEach((projectContext) => {
@@ -222,7 +302,7 @@ describe('Presigned @blobstorage', async () => {
 
         describe(`testing ${projectContext.cases.length} cases for project "${
           project.name
-        }" where I, "${testUser.name}", ${
+        }" where I, "${testUser?.name ?? 'anonymous'}", ${
           testUser && projectRole ? `have the role of ${projectRole}` : 'have no role'
         }`, () => {
           before(async () => {
