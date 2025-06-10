8f0290f8d70194739dc18d321704b4511e60f2ca
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2025-06-10 12:15:07 +0100

    Use the registry mutable tags to keep track of latest & released (`2`) versions

--- .circleci/publish_helm_chart_oci.sh
+++ .circleci/publish_helm_chart_oci.sh
@@ -17,3 +17,18 @@ yq e -i ".docker_image_tag = \"${RELEASE_VERSION}\"" "${GIT_REPO}/utils/helm/spe
 echo "${DOCKER_REG_PASS}" | helm registry login "${DOCKER_HELM_REG_URL}" --username "${DOCKER_REG_USER}" --password-stdin
 helm package "${GIT_REPO}/utils/helm/speckle-server" --version "${RELEASE_VERSION}" --app-version "${RELEASE_VERSION}" --destination "/tmp"
 helm push "/tmp/${CHART_NAME}-${RELEASE_VERSION}.tgz" "oci://${DOCKER_HELM_REG_URL}/speckle"
+rm "/tmp/${CHART_NAME}-${RELEASE_VERSION}.tgz"
+
+if [[ "${IMAGE_VERSION_TAG}" =~ ^[0-9]+\.[0-9]+\.[0-9]+(-alpha\.[0-9]+)?$ ]]; then
+  echo "🏷 Tagging and pushing helm chart as 'speckle/${CHART_NAME}:latest'"
+  helm package "${GIT_REPO}/utils/helm/speckle-server" --version "latest" --app-version "${RELEASE_VERSION}" --destination "/tmp"
+  helm push "/tmp/${CHART_NAME}-latest.tgz" "oci://${DOCKER_HELM_REG_URL}/speckle"
+  rm "/tmp/${CHART_NAME}-latest.tgz"
+
+  if [[ "${IMAGE_VERSION_TAG}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "🏷 Tagging and pushing helm chart as 'speckle/${CHART_NAME}:2'"
+    helm package "${GIT_REPO}/utils/helm/speckle-server" --version "2" --app-version "${RELEASE_VERSION}" --destination "/tmp"
+    helm push "/tmp/${CHART_NAME}-2.tgz" "oci://${DOCKER_HELM_REG_URL}/speckle"
+    rm "/tmp/${CHART_NAME}-2.tgz"
+  fi
+fi
