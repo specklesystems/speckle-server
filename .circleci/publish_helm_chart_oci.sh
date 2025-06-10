9604c7e4db8e4b737ada6b57e535cf5b4506ce19
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2025-06-10 14:28:03 +0100

    fix(helm chart): prefix OCI helm chart tag with `chart.` - prevent conflicts with docker image tags

--- .circleci/publish_helm_chart_oci.sh
+++ .circleci/publish_helm_chart_oci.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # shellcheck disable=SC1090,SC1091
 source "${SCRIPT_DIR}/common.sh"
 
-RELEASE_VERSION="${IMAGE_VERSION_TAG}"
+RELEASE_VERSION="chart.${IMAGE_VERSION_TAG}"
 HELM_STABLE_BRANCH="${HELM_STABLE_BRANCH:-"main"}"
 
 echo "Releasing Helm Chart version ${RELEASE_VERSION}"
@@ -17,18 +17,9 @@ yq e -i ".docker_image_tag = \"${RELEASE_VERSION}\"" "${GIT_REPO}/utils/helm/spe
 echo "${DOCKER_REG_PASS}" | helm registry login "${DOCKER_HELM_REG_URL}" --username "${DOCKER_REG_USER}" --password-stdin
 helm package "${GIT_REPO}/utils/helm/speckle-server" --version "${RELEASE_VERSION}" --app-version "${RELEASE_VERSION}" --destination "/tmp"
 helm push "/tmp/${CHART_NAME}-${RELEASE_VERSION}.tgz" "oci://${DOCKER_HELM_REG_URL}/speckle"
-rm "/tmp/${CHART_NAME}-${RELEASE_VERSION}.tgz"
 
 if [[ "${IMAGE_VERSION_TAG}" =~ ^[0-9]+\.[0-9]+\.[0-9]+(-alpha\.[0-9]+)?$ ]]; then
-  echo "🏷 Tagging and pushing helm chart as 'speckle/${CHART_NAME}:latest'"
-  helm package "${GIT_REPO}/utils/helm/speckle-server" --version "latest" --app-version "${RELEASE_VERSION}" --destination "/tmp"
-  helm push "/tmp/${CHART_NAME}-latest.tgz" "oci://${DOCKER_HELM_REG_URL}/speckle"
-  rm "/tmp/${CHART_NAME}-latest.tgz"
-
-  if [[ "${IMAGE_VERSION_TAG}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    echo "🏷 Tagging and pushing helm chart as 'speckle/${CHART_NAME}:2'"
-    helm package "${GIT_REPO}/utils/helm/speckle-server" --version "2" --app-version "${RELEASE_VERSION}" --destination "/tmp"
-    helm push "/tmp/${CHART_NAME}-2.tgz" "oci://${DOCKER_HELM_REG_URL}/speckle"
-    rm "/tmp/${CHART_NAME}-2.tgz"
-  fi
+  echo "🏷 Tagging and pushing helm chart as 'speckle/${CHART_NAME}:chart.latest'"
+  helm package "${GIT_REPO}/utils/helm/speckle-server" --version "chart.latest" --app-version "${RELEASE_VERSION}" --destination "/tmp"
+  helm push "/tmp/${CHART_NAME}-chart-latest.tgz" "oci://${DOCKER_HELM_REG_URL}/speckle"
 fi
