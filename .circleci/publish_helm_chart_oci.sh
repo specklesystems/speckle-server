ae6aa8b686d43d9667dd68329aaf40b5f4c2b81b
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2025-06-10 12:02:12 +0100

    ci(publish helm chart oci): version guard not required

--- .circleci/publish_helm_chart_oci.sh
+++ .circleci/publish_helm_chart_oci.sh
@@ -14,23 +14,6 @@ echo "Releasing Helm Chart version ${RELEASE_VERSION}"
 
 yq e -i ".docker_image_tag = \"${RELEASE_VERSION}\"" "${GIT_REPO}/utils/helm/speckle-server/values.yaml"
 
-if [[ -n "${CIRCLE_TAG}" || "${CIRCLE_BRANCH}" == "${HELM_STABLE_BRANCH}" ]]; then
-  # before overwriting the chart with the build version, check if the current chart version
-  # is not newer than the currently build one
-
-  helm pull "oci://${DOCKER_HELM_REG_URL}/speckle/${CHART_NAME}" --destination "/tmp/old-version" --untar --untardir "untar"
-  CURRENT_VERSION="$(yq .version "/tmp/old-version/untar/${CHART_NAME}/Chart.yaml")"
-  echo "${CURRENT_VERSION}"
-  rm -rf "/tmp/old-version"
-
-  .circleci/check_version.py "${CURRENT_VERSION}" "${RELEASE_VERSION}"
-  if [ $? -eq 1 ]
-  then
-    echo "The current helm chart version '${CURRENT_VERSION}' is newer than the version '${RELEASE_VERSION}' we are attempting to publish. Exiting"
-    exit 1
-  fi
-fi
-
 echo "${DOCKER_REG_PASS}" | helm registry login "${DOCKER_HELM_REG_URL}" --username "${DOCKER_REG_USER}" --password-stdin
 helm package "${GIT_REPO}/utils/helm/speckle-server" --version "${RELEASE_VERSION}" --app-version "${RELEASE_VERSION}" --destination "/tmp"
 helm push "/tmp/${CHART_NAME}-${RELEASE_VERSION}.tgz" "oci://${DOCKER_HELM_REG_URL}/speckle"
