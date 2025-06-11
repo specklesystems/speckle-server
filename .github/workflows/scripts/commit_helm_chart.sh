a1471daac48240e22a9ed9854ad39f78786e194e
Daniel Gak Anagrov <daniel@speckle.systems>
2025-06-11 10:56:00 +0200

    updated script

--- .github/workflows/scripts/commit_helm_chart.sh
+++ .github/workflows/scripts/commit_helm_chart.sh
@@ -4,7 +4,8 @@ set -eo pipefail
 
 echo "🏷️ Setting envs"
 
-GIT_REPO=$( pwd )
+GIT_ROOT="$(git rev-parse --show-toplevel)"
+GIT_HELM="$(dirname "$GIT_ROOT")/helm"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # shellcheck disable=SC1090,SC1091
 source "${SCRIPT_DIR}/common.sh"
@@ -12,27 +13,28 @@ source "${SCRIPT_DIR}/common.sh"
 RELEASE_VERSION="${IMAGE_VERSION_TAG}"
 HELM_STABLE_BRANCH="${HELM_STABLE_BRANCH:-"main"}"
 
-if [ ! -d "${HOME}/helm" ]; then
-  echo "${HOME}/helm dir not found"
+
+if [ ! -d "${GIT_HELM}/.git" & ]; then
+  echo "helm repo not found at ${GIT_HELM} "
   exit 1
 fi
-if [ ! -d "${GIT_REPO}" ]; then
-  echo "${GIT_REPO} speckle repo not found"
+if [ ! -d "${GIT_ROOT}/.git" ]; then
+  echo "speckle repo not found at ${GIT_ROOT}"
   exit 1
 fi
 
 echo "✏️ Editing Helm Chart version ${RELEASE_VERSION}"
 
-yq e -i ".version = \"${RELEASE_VERSION}\"" "${GIT_REPO}/utils/helm/speckle-server/Chart.yaml"
-yq e -i ".appVersion = \"${RELEASE_VERSION}\"" "${GIT_REPO}/utils/helm/speckle-server/Chart.yaml"
-yq e -i ".docker_image_tag = \"${RELEASE_VERSION}\"" "${GIT_REPO}/utils/helm/speckle-server/values.yaml"
+yq e -i ".version = \"${RELEASE_VERSION}\"" "${GIT_ROOT}/utils/helm/speckle-server/Chart.yaml"
+yq e -i ".appVersion = \"${RELEASE_VERSION}\"" "${GIT_ROOT}/utils/helm/speckle-server/Chart.yaml"
+yq e -i ".docker_image_tag = \"${RELEASE_VERSION}\"" "${GIT_ROOT}/utils/helm/speckle-server/values.yaml"
 
 if [[ "${GITHUB_REF}" == refs/tags/* || "${GITHUB_REF_NAME}" == "${HELM_STABLE_BRANCH}" ]]; then
   echo "⚠️ prod release ${RELEASE_VERSION}"
   # before overwriting the chart with the build version, check if the current chart version
   # is not newer than the currently build one
 
-  CURRENT_VERSION="$(grep ^version "${HOME}/helm/charts/speckle-server/Chart.yaml"  | grep -o '2\..*')"
+  CURRENT_VERSION="$(grep ^version "${GIT_HELM}/charts/speckle-server/Chart.yaml"  | grep -o '2\..*')"
   echo "ℹ️ Current version ${CURRENT_VERSION}"
 
   .github/workflows/scripts/check_version.py "${CURRENT_VERSION}" "${RELEASE_VERSION}"
@@ -41,20 +43,20 @@ if [[ "${GITHUB_REF}" == refs/tags/* || "${GITHUB_REF_NAME}" == "${HELM_STABLE_B
     echo "The current helm chart version '${CURRENT_VERSION}' is newer than the version '${RELEASE_VERSION}' we are attempting to publish. Exiting"
     exit 1
   fi
-  rm -rf "${HOME}/helm/charts/speckle-server"
-  cp -r "${GIT_REPO}/utils/helm/speckle-server" "${HOME}/helm/charts/speckle-server"
+  rm -rf "${GIT_HELM}/charts/speckle-server"
+  cp -r "${GIT_ROOT}/utils/helm/speckle-server" "${GIT_HELM}/charts/speckle-server"
 else
   # overwrite the name of the chart
-  yq e -i ".name = \"speckle-server-branch-${BRANCH_NAME_TRUNCATED}\"" "${GIT_REPO}/utils/helm/speckle-server/Chart.yaml"
-  rm -rf "${HOME}/helm/charts/speckle-server-branch-${BRANCH_NAME_TRUNCATED}"
-  cp -r "${GIT_REPO}/utils/helm/speckle-server" "${HOME}/helm/charts/speckle-server-branch-${BRANCH_NAME_TRUNCATED}"
+  yq e -i ".name = \"speckle-server-branch-${BRANCH_NAME_TRUNCATED}\"" "${GIT_ROOT}/utils/helm/speckle-server/Chart.yaml"
+  rm -rf "${GIT_HELM}/charts/speckle-server-branch-${BRANCH_NAME_TRUNCATED}"
+  cp -r "${GIT_ROOT}/utils/helm/speckle-server" "${GIT_HELM}/charts/speckle-server-branch-${BRANCH_NAME_TRUNCATED}"
 fi
 
 echo "💾 Pushing commit"
 
-cd ~/helm
+cd ${GIT_HELM}
 
-cat ${HOME}/helm/charts/speckle-server-branch-${BRANCH_NAME_TRUNCATED}
+cat ./charts/speckle-server-branch-${BRANCH_NAME_TRUNCATED}
 
 #git add .
 #git -c user.email="devops+circleci@speckle.systems" -c user.name="CI" commit -m "CircleCI commit for version '${RELEASE_VERSION}'"
