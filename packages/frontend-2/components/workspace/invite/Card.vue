31bc8b4d93d0321c1019a5f4449061f39e33fba2
andrewwallacespeckle <andrew@speckle.systems>
2025-06-18 16:12:13 +0200

    Show invites in card list

---
+++ packages/frontend-2/components/workspace/invite/Card.vue
@@ -0,0 +1,68 @@
+<template>
+  <WorkspaceCard
+    :logo="''"
+    :name="invite.workspaceName"
+    :class="isAccepted ? '' : 'bg-foundation'"
+    :banner-text="`${invite.invitedBy.name} invited you to join this workspace`"
+  >
+    <template #actions>
+      <FormButton
+        v-if="isAccepted"
+        color="outline"
+        size="sm"
+        :icon-left="CheckIcon"
+        disabled
+      >
+        Workspace joined
+      </FormButton>
+      <div v-else class="flex flex-col gap-2 sm:items-end">
+        <FormButton color="primary" size="sm" :disabled="loading" @click="onAccept">
+          Accept invitation
+        </FormButton>
+      </div>
+    </template>
+  </WorkspaceCard>
+</template>
+
+<script setup lang="ts">
+import { graphql } from '~/lib/common/generated/gql'
+import type { WorkspaceInviteCard_PendingWorkspaceCollaboratorFragment } from '~/lib/common/generated/gql/graphql'
+import { useWorkspaceInviteManager } from '~/lib/workspaces/composables/management'
+import { CheckIcon } from '@heroicons/vue/20/solid'
+
+graphql(`
+  fragment WorkspaceInviteCard_PendingWorkspaceCollaborator on PendingWorkspaceCollaborator {
+    id
+    workspaceId
+    workspaceSlug
+    workspaceName
+    invitedBy {
+      id
+      name
+    }
+  }
+`)
+
+const props = defineProps<{
+  invite: WorkspaceInviteCard_PendingWorkspaceCollaboratorFragment
+  isAccepted?: boolean
+}>()
+
+const emit = defineEmits<{
+  (e: 'accepted', inviteId: string): void
+}>()
+
+const { loading, accept } = useWorkspaceInviteManager(
+  {
+    invite: computed(() => props.invite)
+  },
+  {
+    preventRedirect: true
+  }
+)
+
+const onAccept = async () => {
+  emit('accepted', props.invite.id)
+  await accept()
+}
+</script>
