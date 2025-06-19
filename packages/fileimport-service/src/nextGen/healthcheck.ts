09cb93469e30614481189160d9fd57823e72f3b0
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2025-06-19 15:44:07 +0100

    fix(fileimport service): health check for next gen importer (#4963)

---
+++ packages/fileimport-service/src/nextGen/healthcheck.ts
@@ -0,0 +1,21 @@
+import http from 'node:http'
+import { Logger } from 'pino'
+
+export const startHealthCheckServer = (params: { logger: Logger }) => {
+  const { logger } = params
+  const server = http.createServer((req, res) => {
+    if (req.url === '/healthz' && req.method === 'GET') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
+      res.end('OK')
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' })
+      res.end('Not Found')
+    }
+  })
+
+  server.listen(9080, 'localhost', () => {
+    logger.info('Server running at http://localhost with endpoint /healthz')
+  })
+
+  return server
+}
