a983a35ca1a965f25d80622ed022d0f4565a7b9f
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2025-06-19 12:21:58 +0100

    Revert "Amend nix shell to match example"
    This reverts commit 4177c5bc4b81aab09ad8af8fe79a7b7b99bdd556.

--- tests/deployment/helm/helm-chart-shell.nix
+++ tests/deployment/helm/helm-chart-shell.nix
@@ -1,10 +1,6 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/63dacb46bf939521bdc93981b4cbb7ecb58427a0.tar.gz") {} }:
+
 let
-  nixpkgs = builtins.fetchTarball {
-    url    = "https://github.com/NixOS/nixpkgs/archive/63dacb46bf939521bdc93981b4cbb7ecb58427a0.tar.gz";
-  };
-
-  pkgs = import nixpkgs { config = {}; };
-
   corepack = pkgs.stdenv.mkDerivation {
     name = "corepack";
     buildInputs = [ pkgs.nodejs_22 ];
