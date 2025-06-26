c5967a96166fa72786503b263139f050b86fa3cf
Adam Hathcock <adamhathcock@users.noreply.github.com>
2025-06-26 13:28:50 +0100

    (OL2) move files around to make more sense (#4950)
    * Rename to saveBatch
    
    * forgot a file
    
    * first pass of cacheReader
    
    * OL2 tests have infinite timeout
    
    * OL2 refactor works
    
    * fix for tests
    
    * moved/removed types to make a more logical structure
    
    * fixed imports
    
    * rework loop to be in async generator for the expected count
    
    * get rid of pumps and fix test
    
    * lint fix
    
    * redo mermaid diagrams
    
    * add readme section on deferment
    
    * always return root first
    
    * fix linting
    
    * revert the counting
    
    * merge fixes
    
    * remove unused var

--- packages/objectloader2/src/types/types.ts
+++ packages/objectloader2/src/types/types.ts
@@ -1,10 +1,3 @@
-export type CustomLogger = (message?: string, ...optionalParams: unknown[]) => void
-
-export type Fetcher = (
-  input: string | URL | Request,
-  init?: RequestInit
-) => Promise<Response>
-
 export interface Item {
   baseId: string
   base?: Base
@@ -25,47 +18,4 @@ export interface Reference {
 
 export interface DataChunk extends Base {
   data?: Base[]
-}
-
-export function isBase(maybeBase?: unknown): maybeBase is Base {
-  return (
-    maybeBase !== null &&
-    typeof maybeBase === 'object' &&
-    'id' in maybeBase &&
-    typeof maybeBase.id === 'string'
-  )
-}
-
-export function isReference(maybeRef?: unknown): maybeRef is Reference {
-  return (
-    maybeRef !== null &&
-    typeof maybeRef === 'object' &&
-    'referencedId' in maybeRef &&
-    typeof maybeRef.referencedId === 'string'
-  )
-}
-
-export function isScalar(
-  value: unknown
-): value is string | number | boolean | bigint | symbol | undefined {
-  const type = typeof value
-  return (
-    value === null ||
-    type === 'string' ||
-    type === 'number' ||
-    type === 'boolean' ||
-    type === 'bigint' ||
-    type === 'symbol' ||
-    type === 'undefined'
-  )
-}
-
-export function take<T>(it: Iterator<T>, count: number): T[] {
-  const result: T[] = []
-  for (let i = 0; i < count; i++) {
-    const itr = it.next()
-    if (itr.done) break
-    result.push(itr.value)
-  }
-  return result
 }
