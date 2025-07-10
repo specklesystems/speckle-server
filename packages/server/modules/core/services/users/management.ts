c2a28f8674a22734af3a47ea884e25c2e6567807
Daniel Gak Anagrov <dani_gak@hotmail.com>
2025-07-10 11:45:32 +0200

    fix(#5018): avatar can be removed (#5022)
    * fix: avatar can be removed
    * feat: added tests for activeUser updates

--- packages/server/modules/core/services/users/management.ts
+++ packages/server/modules/core/services/users/management.ts
@@ -85,6 +85,11 @@ export const updateUserAndNotifyFactory =
       const key = entry[0] as keyof typeof update
       let val = entry[1]
 
+      if (key === 'avatar' && val === '') {
+        filteredUpdate[key] = null // avatar removal
+        continue
+      }
+
       if (key === 'avatar') {
         val = sanitizeImageUrl(val)
       }
