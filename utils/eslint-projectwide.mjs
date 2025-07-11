c40160d1838300f9221a3219d53890c72f10121a
Gergő Jedlicska <gergo@jedlicska.com>
2025-07-11 16:21:14 +0200

    feat(eslint): ignore package from eslint

--- utils/eslint-projectwide.mjs
+++ utils/eslint-projectwide.mjs
@@ -24,9 +24,12 @@ const getFileNames = () => {
 }
 
 const resolvePackageContexts = async (absoluteFileNames) => {
+  const ignoredPackageNames = ['ifc-importer']
   const allPackages = [
     ...(await fs.readdir(path.join(rootDir, './packages'), { withFileTypes: true }))
-      .filter((d) => d.isDirectory())
+      .filter((d) => {
+        return d.isDirectory() && !ignoredPackageNames.includes(d.name)
+      })
       .map((d) => ({
         absolutePath: path.join(rootDir, './packages', d.name)
       })),
