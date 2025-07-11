a37c9c69ec3c609f1c17bf565b7f65c20985752e
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2025-07-11 09:14:54 +0100

    rearrange the new index

--- packages/server/modules/core/migrations/20250701164407_object_preview_attempts.ts
+++ packages/server/modules/core/migrations/20250701164407_object_preview_attempts.ts
@@ -3,20 +3,23 @@ import { Knex } from 'knex'
 const tableName = 'object_preview'
 const attemptsCol = 'attempts'
 
+/**
+ * In getPaginatedObjectsPreviewsBaseQueryFactory we filter by previewStatus and attempts.
+ * We then sort by lastUpdate and objectId.
+ */
+const indexCols = ['previewStatus', attemptsCol, 'lastUpdate', 'objectId']
+
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable(tableName, (table) => {
     table.integer(attemptsCol).defaultTo(0).notNullable()
-
-    table.dropIndex(['previewStatus', 'priority', 'lastUpdate'])
-    table.index(['previewStatus', 'priority', 'lastUpdate', attemptsCol])
+    table.index(indexCols)
   })
 }
 
 export async function down(knex: Knex): Promise<void> {
   // drop in a two step process to first remove the index then the column
   await knex.schema.alterTable(tableName, (table) => {
-    table.dropIndex(['previewStatus', 'priority', 'lastUpdate', attemptsCol])
-    table.index(['previewStatus', 'priority', 'lastUpdate'])
+    table.dropIndex(indexCols)
   })
 
   await knex.schema.alterTable(tableName, (table) => {
