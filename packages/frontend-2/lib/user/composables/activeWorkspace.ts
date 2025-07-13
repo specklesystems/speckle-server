e2dc60fa9bec94e193c575877a9804c771fdfe0b
Mike Tasset <mike.tasset@gmail.com>
2025-07-13 21:55:37 +0200

    WIP

--- packages/frontend-2/lib/user/composables/activeWorkspace.ts
+++ packages/frontend-2/lib/user/composables/activeWorkspace.ts
@@ -1,7 +1,6 @@
 import { userActiveWorkspaceQuery } from '~/lib/user/graphql/queries'
 import { useQuery, useMutation } from '@vue/apollo-composable'
 import { setActiveWorkspaceMutation } from '~/lib/user/graphql/mutations'
-import { modifyObjectField, ROOT_QUERY } from '~/lib/common/helpers/graphql'
 
 export const useActiveWorkspace = () => {
   const { result } = useQuery(userActiveWorkspaceQuery)
@@ -17,26 +16,30 @@ export const useActiveWorkspace = () => {
 
 export const useSetActiveWorkspace = () => {
   const { mutate } = useMutation(setActiveWorkspaceMutation)
-  const { activeUser } = useActiveUser()
 
   const setActiveWorkspace = async (workspaceSlug: string) => {
     const result = await mutate(
       { slug: workspaceSlug },
       {
         update: (cache, { data }) => {
-          if (data?.activeUserMutations?.setActiveWorkspace && activeUser.value?.id) {
-            // Evict the activeUser field to force a refetch with the new active workspace
-            modifyObjectField(
-              cache,
-              ROOT_QUERY,
-              'activeUser',
-              ({ variables, helpers: { evict } }) => {
-                if (!variables || Object.keys(variables).length === 0) {
-                  return evict()
+          if (data?.activeUserMutations?.setActiveWorkspace) {
+            // Update the cache immediately with the new active workspace
+            cache.modify({
+              id: 'ROOT_QUERY',
+              fields: {
+                activeUser(existingActiveUser) {
+                  if (!existingActiveUser) return existingActiveUser
+
+                  return {
+                    ...existingActiveUser,
+                    activeWorkspace: {
+                      __typename: 'Workspace',
+                      slug: workspaceSlug
+                    }
+                  }
                 }
-                return undefined
               }
-            )
+            })
           }
         }
       }
