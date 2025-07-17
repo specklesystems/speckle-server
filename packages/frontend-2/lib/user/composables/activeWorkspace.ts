943c98de5454c1359aec401e75417bcbd7f14c9d
Mike Tasset <mike.tasset@gmail.com>
2025-07-17 14:37:46 +0200

    Update mutate

--- packages/frontend-2/lib/user/composables/activeWorkspace.ts
+++ packages/frontend-2/lib/user/composables/activeWorkspace.ts
@@ -1,6 +1,7 @@
 import { userActiveWorkspaceQuery } from '~/lib/user/graphql/queries'
 import { useQuery, useMutation } from '@vue/apollo-composable'
 import { setActiveWorkspaceMutation } from '~/lib/user/graphql/mutations'
+import { modifyObjectField, getCacheId } from '~/lib/common/helpers/graphql'
 
 export const useActiveWorkspace = () => {
   const { result } = useQuery(userActiveWorkspaceQuery)
@@ -15,7 +16,23 @@ export const useActiveWorkspace = () => {
 }
 
 export const useSetActiveWorkspace = () => {
-  const { mutate } = useMutation(setActiveWorkspaceMutation)
+  const { activeUser } = useActiveUser()
+  const { mutate } = useMutation(setActiveWorkspaceMutation, {
+    update: (cache, { data }) => {
+      if (!data?.activeUserMutations?.setActiveWorkspace || !activeUser.value) return
+
+      const newWorkspace = data.activeUserMutations.setActiveWorkspace
+
+      modifyObjectField(
+        cache,
+        getCacheId('User', activeUser.value.id),
+        'activeWorkspace',
+        ({ helpers: { ref } }) => {
+          return ref('LimitedWorkspace', newWorkspace.id)
+        }
+      )
+    }
+  })
 
   const setActiveWorkspace = async (workspaceSlug: string) => {
     return await mutate({ slug: workspaceSlug })
