e503b29a50e0ec4e422187348c1b45c7af3ef559
Mike Tasset <mike.tasset@gmail.com>
2025-07-21 22:11:17 +0200

    Feat: Remove book a demo page

--- packages/frontend-2/lib/common/helpers/route.ts
+++ packages/frontend-2/lib/common/helpers/route.ts
@@ -21,7 +21,6 @@ export const forumPageUrl = 'https://speckle.community/'
 export const defaultZapierWebhookUrl =
   'https://hooks.zapier.com/hooks/catch/12120532/2m4okri/'
 export const guideBillingUrl = 'https://docs.speckle.systems/workspaces/billing'
-export const bookDemoRoute = '/book-a-demo'
 export const onboardingRoute = '/onboarding'
 
 export const settingsUserRoutes = {
