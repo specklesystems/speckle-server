65521ffb54aa32960d334a26e8b915ef88d3f32b
Mike Tasset <mike.tasset@gmail.com>
2025-07-22 12:42:28 +0200

    Update type

--- packages/frontend-2/lib/auth/composables/auth.ts
+++ packages/frontend-2/lib/auth/composables/auth.ts
@@ -206,7 +206,7 @@ export const useAuthManager = (
   /**
    * Token used for embedding
    */
-  const embedToken = computed(() => route.query.embedToken)
+  const embedToken = computed(() => route.query.embedToke as Optional<string>)
 
   /**
    * Get the effective auth token (embed token takes precedence)
