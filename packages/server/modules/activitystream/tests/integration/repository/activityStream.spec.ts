5c3c4279afcdebea6df2dd86a14a4325ffe66a88
Daniel Gak Anagrov <daniel@speckle.systems>
2025-07-28 09:14:40 +0100

    fix: protect message of activity stream beeing to long

---
+++ packages/server/modules/activitystream/tests/integration/repository/activityStream.spec.ts
@@ -0,0 +1,70 @@
+import { db } from '@/db/knex'
+import { saveStreamActivityFactory } from '@/modules/activitystream/repositories'
+import { StreamActivity } from '@/modules/core/dbSchema'
+import { expect } from 'chai'
+import cryptoRandomString from 'crypto-random-string'
+
+describe('Stream activity repository @activitystream', () => {
+  const saveStreamActivity = saveStreamActivityFactory({ db })
+  const streamId = cryptoRandomString({ length: 10 })
+  const resourceId = cryptoRandomString({ length: 10 })
+  const userId = cryptoRandomString({ length: 10 })
+  const exampleActivity = {
+    streamId,
+    resourceType: 'user' as const,
+    resourceId,
+    userId,
+    actionType: 'user_update' as const,
+    info: {
+      new: {
+        name: 'user',
+        field: 'a'
+      },
+      old: {
+        name: 'user2',
+        field: 'b'
+      }
+    },
+    message: 'User plan updated'
+  }
+
+  it('stores an activity', async () => {
+    await saveStreamActivity(exampleActivity)
+
+    const activity = await db
+      .table(StreamActivity.name)
+      .select(StreamActivity.cols)
+      .first()
+
+    expect(activity).to.nested.include({
+      streamId,
+      resourceType: 'user',
+      resourceId,
+      userId,
+      actionType: 'user_update',
+      message: 'User plan updated'
+    })
+    expect(activity).to.have.a.property('time').that.is.a('Date')
+  })
+
+  it('trims the message in case its too long', async () => {
+    await saveStreamActivity({
+      ...exampleActivity,
+      message: cryptoRandomString({ length: 1000 })
+    })
+
+    const activity = await db
+      .table(StreamActivity.name)
+      .select(StreamActivity.cols)
+      .first()
+
+    expect(activity).to.nested.include({
+      streamId,
+      resourceType: 'user',
+      resourceId,
+      userId,
+      actionType: 'user_update'
+    })
+    expect(activity).to.have.a.property('time').that.is.a('Date')
+  })
+})
