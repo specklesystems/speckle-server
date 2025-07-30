0f72b137593355d9c41d671806a4c25ddb7939ab
michalspeckle <michal@speckle.systems>
2025-07-30 09:18:29 +0200

    updated tippy conf and added new theme

--- packages/frontend-2/plugins/001-tippy.ts
+++ packages/frontend-2/plugins/001-tippy.ts
@@ -1,10 +1,16 @@
 import VueTippy from 'vue-tippy'
 import 'tippy.js/dist/tippy.css'
+import 'assets/css/vtippy.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueTippy, {
     defaultProps: {
-      arrow: true
+      arrow: false,
+      delay: [1000, 0],
+      duration: [300, 0],
+      animation: 'fade',
+      theme: 'speckleTooltip',
+      offset: [0, 4]
     },
     flipDuration: 0
   })
