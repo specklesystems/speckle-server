c87956ac8af443f01ffba6af3a9ebcc905c1b7e1
andrewwallacespeckle <andrew@speckle.systems>
2025-08-05 14:50:07 +0100

    Version - wrap in div

--- packages/frontend-2/components/viewer/models/versions/Versions.vue
+++ packages/frontend-2/components/viewer/models/versions/Versions.vue
@@ -1,56 +1,58 @@
 <template>
-  <ViewerCompareChangesPanel
-    v-if="showDiff"
-    :clear-on-back="false"
-    @close="handleDiffClose"
-  />
-  <ViewerLayoutSidePanel v-else>
-    <template #title>
-      <div class="flex items-center gap-x-1">
-        <FormButton
-          :icon-left="ChevronLeftIcon"
-          color="subtle"
-          class="-ml-3"
-          hide-text
-          size="sm"
-          @click="handleClose"
-        >
-          Exit versions
-        </FormButton>
-        Versions
-      </div>
-    </template>
-
-    <div class="flex flex-col h-full">
-      <template v-if="resourceItems.length">
-        <!-- Versions with single scroll container for sticky headers -->
-        <div class="flex-1 overflow-y-auto">
-          <div
-            v-for="({ model, versionId }, index) in modelsAndVersionIds"
-            :key="model.id"
+  <div>
+    <ViewerCompareChangesPanel
+      v-if="showDiff"
+      :clear-on-back="false"
+      @close="handleDiffClose"
+    />
+    <ViewerLayoutSidePanel v-else>
+      <template #title>
+        <div class="flex items-center gap-x-1">
+          <FormButton
+            :icon-left="ChevronLeftIcon"
+            color="subtle"
+            class="-ml-3"
+            hide-text
+            size="sm"
+            @click="handleClose"
           >
-            <ViewerModelsVersionsCard
-              :model="model"
-              :version-id="versionId"
-              :last="index === modelsAndVersionIds.length - 1"
-              :initially-expanded="
-                props.expandedModelId === model.id || modelsAndVersionIds.length === 1
-              "
-            />
-          </div>
-          <template v-if="objects.length !== 0">
-            <ViewerResourcesObjectCard
-              v-for="object in objects"
-              :key="object.objectId"
-              :object="object"
-              :show-remove="false"
-              @remove="(id: string) => removeModel(id)"
-            />
-          </template>
+            Exit versions
+          </FormButton>
+          Versions
         </div>
       </template>
-    </div>
-  </ViewerLayoutSidePanel>
+
+      <div class="flex flex-col h-full">
+        <template v-if="resourceItems.length">
+          <!-- Versions with single scroll container for sticky headers -->
+          <div class="flex-1 overflow-y-auto">
+            <div
+              v-for="({ model, versionId }, index) in modelsAndVersionIds"
+              :key="model.id"
+            >
+              <ViewerModelsVersionsCard
+                :model="model"
+                :version-id="versionId"
+                :last="index === modelsAndVersionIds.length - 1"
+                :initially-expanded="
+                  props.expandedModelId === model.id || modelsAndVersionIds.length === 1
+                "
+              />
+            </div>
+            <template v-if="objects.length !== 0">
+              <ViewerResourcesObjectCard
+                v-for="object in objects"
+                :key="object.objectId"
+                :object="object"
+                :show-remove="false"
+                @remove="(id: string) => removeModel(id)"
+              />
+            </template>
+          </div>
+        </template>
+      </div>
+    </ViewerLayoutSidePanel>
+  </div>
 </template>
 
 <script setup lang="ts">
