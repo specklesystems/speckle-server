82b114f1a049df5e68757c11d573efe04a2bdb7a
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2025-08-05 12:06:22 +0100

    Proposed-changes

--- packages/shared/src/blobs/index.ts
+++ packages/shared/src/blobs/index.ts
@@ -1,4 +1,63 @@
-export const acceptedFileExtensions = [
+// This is a full list of all the file extensions supported by Rhino
+// For some of them, the utility to connect them with Speckle is unfounded
+
+export const rhinoImporterSupportedFileExtensions = new Set([
+  '3dm',
+  '3dmbak',
+  'rws',
+  '3mf',
+  '3ds',
+  'amf',
+  'ai',
+  'dwg',
+  'dxf',
+  'x',
+  'e57',
+  'dst',
+  'exp',
+  'dst',
+  'exp',
+  'off',
+  'gf',
+  'gft',
+  'gltf',
+  'glb',
+  'gts',
+  'igs',
+  'iges',
+  'lwo',
+  'dgn',
+  'fbx',
+  'scn',
+  'obj',
+  'pdf',
+  'ply',
+  'asc',
+  'csv',
+  'xyz',
+  'pts',
+  'cgo_ascii',
+  'cgo_asci',
+  'txt',
+  'raw',
+  'm',
+  'pts',
+  'svg',
+  'skp',
+  'slc',
+  'sldprt',
+  'sldasm',
+  'stp',
+  'step',
+  'stl',
+  'vda',
+  'wrl',
+  'vrml',
+  'iv',
+  'gdf'
+])
+
+export const acceptedFileExtensions = new Set([
   'pdf',
   'zip',
   '7z',
@@ -29,8 +88,9 @@ export const acceptedFileExtensions = [
   'eps',
   'gwb',
   'skp',
-  'pbix'
-]
+  'pbix',
+  ...rhinoImporterSupportedFileExtensions
+])
 
 export const BlobUploadStatus = <const>{
   Pending: 0,
