f7562331700764a2a69247ecc940b01449c097bb
andrewwallacespeckle <andrew@speckle.systems>
2025-08-05 13:00:21 +0100

    Accessibility

--- packages/frontend-2/components/viewer/VisibilityButton.vue
+++ packages/frontend-2/components/viewer/VisibilityButton.vue
@@ -1,11 +1,9 @@
 <template>
   <button
     v-tippy="tooltip"
+    :aria-label="isHidden ? 'Show' : 'Hide'"
     class="group-hover:opacity-100 hover:bg-highlight-3 rounded-md h-6 w-6 flex items-center justify-center"
-    :class="{
-      'opacity-100': isHidden || forceVisible,
-      'sm:opacity-0': !isHidden && !forceVisible
-    }"
+    :class="buttonClasses"
     @click.stop="$emit('click', $event)"
   >
     <IconEyeClosed v-if="isHidden" class="w-4 h-4" />
@@ -14,13 +12,22 @@
 </template>
 
 <script setup lang="ts">
-defineProps<{
+import type { Nullable } from '@speckle/shared'
+
+const props = defineProps<{
   isHidden: boolean
   forceVisible?: boolean
-  tooltip?: object | string | null
+  tooltip?: Nullable<object | string>
 }>()
 
 defineEmits<{
   click: [event: Event]
 }>()
+
+const buttonClasses = computed(() => {
+  return {
+    'opacity-100': props.isHidden || props.forceVisible,
+    'sm:opacity-0': !props.isHidden && !props.forceVisible
+  }
+})
 </script>
