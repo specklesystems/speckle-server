7612f8cfcddfdd7ce6bc90f3e636a86ee5952cec
andrewwallacespeckle <andrew@speckle.systems>
2025-08-06 13:46:11 +0100

    Button dot styling

--- packages/frontend-2/components/viewer/controls/ButtonToggle.vue
+++ packages/frontend-2/components/viewer/controls/ButtonToggle.vue
@@ -13,7 +13,7 @@
     <slot />
     <div
       v-if="dot"
-      class="absolute -top-1 -right-1 w-3 h-3 bg-primary rounded-full border-2 border-foundation"
+      class="absolute top-0.5 right-0.5 w-2.5 h-2.5 bg-primary rounded-full border-2 border-foundation"
     />
   </button>
 </template>
