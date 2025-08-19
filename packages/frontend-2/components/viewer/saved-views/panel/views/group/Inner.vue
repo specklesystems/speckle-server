c0f9ae69b3deb1ce2812f45adf34b8062f51684e
Jack <jack@speckle.systems>
2025-08-19 16:21:36 +0100

    styling of empty group span and tweaks to v-tippy props for dateUpdated

--- packages/frontend-2/components/viewer/saved-views/panel/views/group/Inner.vue
+++ packages/frontend-2/components/viewer/saved-views/panel/views/group/Inner.vue
@@ -23,7 +23,11 @@
         />
       </template>
       <template v-else>
-        <span>No views in group</span>
+        <span
+          class="min-w-full flex justify-center items-center bg-foundation-page text-body-xs rounded-md text-foreground-2 border border-dashed border-outline-2 w-full text-center mx-auto my-2 px-4 h-10"
+        >
+          No views in group
+        </span>
       </template>
     </div>
   </div>
