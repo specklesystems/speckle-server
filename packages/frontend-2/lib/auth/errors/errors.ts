4bffcfce0b1e725dbb0bd42e2bcec41195dfe34f
Kristaps Fabians Geikins <fabis94@live.com>
2025-08-21 09:46:34 +0300

    lint fix

--- packages/frontend-2/lib/auth/errors/errors.ts
+++ packages/frontend-2/lib/auth/errors/errors.ts
@@ -1,21 +1,21 @@
 import { BaseError } from '~~/lib/core/errors/base'
 
 export class InvalidLoginParametersError extends BaseError {
-  static defaultMessage = 'Invalid parameters for logging in!'
+  static override defaultMessage = 'Invalid parameters for logging in!'
 }
 
 export class AuthFailedError extends BaseError {
-  static defaultMessage = 'Logging in failed!'
+  static override defaultMessage = 'Logging in failed!'
 }
 
 export class InvalidRegisterParametersError extends BaseError {
-  static defaultMessage = 'Invalid parameters for signing up!'
+  static override defaultMessage = 'Invalid parameters for signing up!'
 }
 
 export class PasswordResetError extends BaseError {
-  static defaultMessage = 'Something went wrong while resetting password'
+  static override defaultMessage = 'Something went wrong while resetting password'
 }
 
 export class OnboardingError extends BaseError {
-  static defaultMessage = 'Something went wrong while onboarding the user'
+  static override defaultMessage = 'Something went wrong while onboarding the user'
 }
