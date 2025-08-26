a8f4bae180a66f46c28a2c1417a7528aa71ca0d9
Chuck Driesler <chuck@speckle.systems>
2025-08-26 15:11:47 +0100

    fix(dashboards): export default

--- packages/server/modules/dashboards/index.ts
+++ packages/server/modules/dashboards/index.ts
@@ -1,8 +1,10 @@
 import type { SpeckleModule } from '@/modules/shared/helpers/typeHelper'
 import { moduleLogger } from '@/observability/logging'
 
-export const dashboardsModule: SpeckleModule = {
+const dashboardsModule: SpeckleModule = {
   init: async ({ isInitial }) => {
     moduleLogger.info('🧢 Init dashboards module')
   }
 }
+
+export default dashboardsModule
