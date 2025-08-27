eff00e85086e9f84a95a66feaefb80fb51aed445
Mike Tasset <mike.tasset@gmail.com>
2025-08-27 09:55:33 +0100

    Make share not blue and big

--- packages/frontend-2/layouts/dashboard.vue
+++ packages/frontend-2/layouts/dashboard.vue
@@ -1,19 +1,18 @@
 <template>
   <div>
-    <HeaderNavBar>
-      <template #secondary-actions>
-        <FormButton
-          v-tippy="'Toggle fullscreen'"
-          size="sm"
-          color="outline"
-          :icon-right="Fullscreen"
-          hide-text
-          @click="toggleFullScreen()"
-        >
-          Fullscreen
-        </FormButton>
-      </template>
-    </HeaderNavBar>
+    <HeaderNavBar />
+    <Portal to="primary-actions">
+      <FormButton
+        v-tippy="'Toggle fullscreen'"
+        size="sm"
+        color="outline"
+        :icon-right="Fullscreen"
+        hide-text
+        @click="toggleFullScreen()"
+      >
+        Fullscreen
+      </FormButton>
+    </Portal>
     <div class="h-dvh w-dvh overflow-hidden flex flex-col">
       <!-- Static Spacer to allow for absolutely positioned HeaderNavBar  -->
       <div class="h-12 w-full shrink-0"></div>
