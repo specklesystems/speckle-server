4f5efb253d2f717a2e6f16ec7174f89c3abdca1c
Daniel Gak Anagrov <daniel@speckle.systems>
2025-08-28 14:16:57 +0100

    scope 2pc monitor specs to multiregion only

--- packages/server/modules/multiregion/tests/integration/repositories/transactions.spec.ts
+++ packages/server/modules/multiregion/tests/integration/repositories/transactions.spec.ts
@@ -8,41 +8,44 @@ import {
 import { wait } from '@speckle/shared'
 import { expect } from 'chai'
 import cryptoRandomString from 'crypto-random-string'
+import { isMultiRegionTestMode } from '@/test/speckle-helpers/regions'
 
-describe('prepared transaction repository functions', () => {
-  describe('getStalePreparedTransactionsFactory returns a function, that', () => {
-    let trx: Knex.Transaction
-    let transactionId: string = ''
+isMultiRegionTestMode()
+  ? describe('prepared transaction repository functions @multiregion', () => {
+      describe('getStalePreparedTransactionsFactory returns a function, that', () => {
+        let trx: Knex.Transaction
+        let transactionId: string = ''
 
-    beforeEach(async () => {
-      trx = await db.transaction()
-      transactionId = cryptoRandomString({ length: 10 })
+        beforeEach(async () => {
+          trx = await db.transaction()
+          transactionId = cryptoRandomString({ length: 10 })
 
-      await prepareTransaction(trx, transactionId)
-      try {
-        await trx.commit()
-      } catch {}
-    })
+          await prepareTransaction(trx, transactionId)
+          try {
+            await trx.commit()
+          } catch {}
+        })
 
-    afterEach(async () => {
-      await rollbackPreparedTransaction(db, transactionId)
-      try {
-        await trx.rollback()
-      } catch {}
-    })
+        afterEach(async () => {
+          await rollbackPreparedTransaction(db, transactionId)
+          try {
+            await trx.rollback()
+          } catch {}
+        })
 
-    it('returns prepared transactions older than a given time interval', async () => {
-      await wait(2000)
-      const result = await getStalePreparedTransactionsFactory({ db })({
-        interval: '1 second'
+        it('returns prepared transactions older than a given time interval', async () => {
+          await wait(2000)
+          const result = await getStalePreparedTransactionsFactory({ db })({
+            interval: '1 second'
+          })
+          expect(result.length).to.equal(1)
+          expect(result.at(0)?.gid).to.equal(transactionId)
+        })
+
+        it('does not return recently prepared transactions', async () => {
+          const result = await getStalePreparedTransactionsFactory({ db })({})
+          expect(result.length).to.equal(0)
+        })
       })
-      expect(result.length).to.equal(1)
-      expect(result.at(0)?.gid).to.equal(transactionId)
     })
-
-    it('does not return recently prepared transactions', async () => {
-      const result = await getStalePreparedTransactionsFactory({ db })({})
-      expect(result.length).to.equal(0)
-    })
-  })
-})
+  : null
