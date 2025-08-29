6a41c928f35adcdbdb75279f6abe7d300183256c
Mike Tasset <mike.tasset@gmail.com>
2025-08-29 22:03:24 +0200

    Feat: Add Intelligence SU Promo banner

---
+++ packages/frontend-2/components/dashboard/IntelligencePromo.vue
@@ -0,0 +1,56 @@
+<template>
+  <CommonCard class="relative !px-2 !py-2.5 bg-foundation shadow-sm">
+    <FormButton
+      class="absolute top-1 right-1"
+      size="sm"
+      color="subtle"
+      :icon-right="XMarkIcon"
+      hide-text
+      @click="dismissBanner"
+    >
+      <span class="sr-only">Close</span>
+    </FormButton>
+    <div class="flex flex-col gap-y-2 text-foreground">
+      <span class="text-[10px] font-mono uppercase tracking-widest">
+        Upcoming event
+      </span>
+      <h3 class="text-body-xs font-semibold leading-tight tracking-tight">
+        See Speckle Intelligence in action
+      </h3>
+      <p class="text-body-3xs leading-tight">
+        Join our online
+        <br />
+        Community StandUp on September 10th.
+      </p>
+      <NuxtLink
+        to="https://streamyard.com/watch/RhTZBgkzRcRe"
+        target="_blank"
+        external
+        class="flex gap-1 items-center border-b border-transparent hover:border-highlight-3 max-w-max -mb-0.5"
+        @click="onCTAClick"
+      >
+        <span class="text-body-3xs font-semibold">Register</span>
+        <ArrowUpRightIcon class="h-2 w-2 mt-px stroke-2 stroke-foreground" />
+      </NuxtLink>
+    </div>
+  </CommonCard>
+</template>
+
+<script setup lang="ts">
+import { ArrowUpRightIcon, XMarkIcon } from '@heroicons/vue/24/solid'
+import { useMixpanel } from '~~/lib/core/composables/mp'
+import { useActiveUserMeta } from '~~/lib/user/composables/meta'
+
+const mixpanel = useMixpanel()
+const { updateIntelligenceCommunityStandUpBannerDismissed } = useActiveUserMeta()
+
+const onCTAClick = () => {
+  mixpanel.track('SpeckleCON CTA Clicked', {
+    location: 'sidebar'
+  })
+}
+
+const dismissBanner = async () => {
+  await updateIntelligenceCommunityStandUpBannerDismissed(true)
+}
+</script>
--- packages/frontend-2/lib/common/generated/gql/gql.ts
+++ packages/frontend-2/lib/common/generated/gql/gql.ts
@@ -396,8 +396,9 @@ type Documents = {
     "\n  fragment AppAuthorAvatar on AppAuthor {\n    id\n    name\n    avatar\n  }\n": typeof types.AppAuthorAvatarFragmentDoc,
     "\n  fragment LimitedUserAvatar on LimitedUser {\n    id\n    name\n    avatar\n  }\n": typeof types.LimitedUserAvatarFragmentDoc,
     "\n  fragment ActiveUserAvatar on User {\n    id\n    name\n    avatar\n  }\n": typeof types.ActiveUserAvatarFragmentDoc,
-    "\n  query ActiveUserMeta {\n    activeUser {\n      meta {\n        legacyProjectsExplainerCollapsed\n      }\n    }\n  }\n": typeof types.ActiveUserMetaDocument,
+    "\n  query ActiveUserMeta {\n    activeUser {\n      meta {\n        legacyProjectsExplainerCollapsed\n        intelligenceCommunityStandUpBannerDismissed\n      }\n    }\n  }\n": typeof types.ActiveUserMetaDocument,
     "\n  mutation UpdateLegacyProjectsExplainer($value: Boolean!) {\n    activeUserMutations {\n      meta {\n        setLegacyProjectsExplainerCollapsed(value: $value)\n      }\n    }\n  }\n": typeof types.UpdateLegacyProjectsExplainerDocument,
+    "\n  mutation UpdateIntelligenceCommunityStandUpBannerDismissed($value: Boolean!) {\n    activeUserMutations {\n      meta {\n        setIntelligenceCommunityStandUpBannerDismissed(value: $value)\n      }\n    }\n  }\n": typeof types.UpdateIntelligenceCommunityStandUpBannerDismissedDocument,
     "\n      subscription OnUserProjectsUpdate {\n        userProjectsUpdated {\n          type\n          id\n          project {\n            ...ProjectDashboardItem\n            workspaceId\n          }\n        }\n      }\n    ": typeof types.OnUserProjectsUpdateDocument,
     "\n  mutation UpdateUser($input: UserUpdateInput!) {\n    activeUserMutations {\n      update(user: $input) {\n        id\n        name\n        bio\n        company\n        avatar\n      }\n    }\n  }\n": typeof types.UpdateUserDocument,
     "\n  mutation UpdateNotificationPreferences($input: JSONObject!) {\n    userNotificationPreferencesUpdate(preferences: $input)\n  }\n": typeof types.UpdateNotificationPreferencesDocument,
@@ -901,8 +902,9 @@ const documents: Documents = {
     "\n  fragment AppAuthorAvatar on AppAuthor {\n    id\n    name\n    avatar\n  }\n": types.AppAuthorAvatarFragmentDoc,
     "\n  fragment LimitedUserAvatar on LimitedUser {\n    id\n    name\n    avatar\n  }\n": types.LimitedUserAvatarFragmentDoc,
     "\n  fragment ActiveUserAvatar on User {\n    id\n    name\n    avatar\n  }\n": types.ActiveUserAvatarFragmentDoc,
-    "\n  query ActiveUserMeta {\n    activeUser {\n      meta {\n        legacyProjectsExplainerCollapsed\n      }\n    }\n  }\n": types.ActiveUserMetaDocument,
+    "\n  query ActiveUserMeta {\n    activeUser {\n      meta {\n        legacyProjectsExplainerCollapsed\n        intelligenceCommunityStandUpBannerDismissed\n      }\n    }\n  }\n": types.ActiveUserMetaDocument,
     "\n  mutation UpdateLegacyProjectsExplainer($value: Boolean!) {\n    activeUserMutations {\n      meta {\n        setLegacyProjectsExplainerCollapsed(value: $value)\n      }\n    }\n  }\n": types.UpdateLegacyProjectsExplainerDocument,
+    "\n  mutation UpdateIntelligenceCommunityStandUpBannerDismissed($value: Boolean!) {\n    activeUserMutations {\n      meta {\n        setIntelligenceCommunityStandUpBannerDismissed(value: $value)\n      }\n    }\n  }\n": types.UpdateIntelligenceCommunityStandUpBannerDismissedDocument,
     "\n      subscription OnUserProjectsUpdate {\n        userProjectsUpdated {\n          type\n          id\n          project {\n            ...ProjectDashboardItem\n            workspaceId\n          }\n        }\n      }\n    ": types.OnUserProjectsUpdateDocument,
     "\n  mutation UpdateUser($input: UserUpdateInput!) {\n    activeUserMutations {\n      update(user: $input) {\n        id\n        name\n        bio\n        company\n        avatar\n      }\n    }\n  }\n": types.UpdateUserDocument,
     "\n  mutation UpdateNotificationPreferences($input: JSONObject!) {\n    userNotificationPreferencesUpdate(preferences: $input)\n  }\n": types.UpdateNotificationPreferencesDocument,
@@ -2569,11 +2571,15 @@ export function graphql(source: "\n  fragment ActiveUserAvatar on User {\n    id
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "\n  query ActiveUserMeta {\n    activeUser {\n      meta {\n        legacyProjectsExplainerCollapsed\n      }\n    }\n  }\n"): (typeof documents)["\n  query ActiveUserMeta {\n    activeUser {\n      meta {\n        legacyProjectsExplainerCollapsed\n      }\n    }\n  }\n"];
+export function graphql(source: "\n  query ActiveUserMeta {\n    activeUser {\n      meta {\n        legacyProjectsExplainerCollapsed\n        intelligenceCommunityStandUpBannerDismissed\n      }\n    }\n  }\n"): (typeof documents)["\n  query ActiveUserMeta {\n    activeUser {\n      meta {\n        legacyProjectsExplainerCollapsed\n        intelligenceCommunityStandUpBannerDismissed\n      }\n    }\n  }\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
 export function graphql(source: "\n  mutation UpdateLegacyProjectsExplainer($value: Boolean!) {\n    activeUserMutations {\n      meta {\n        setLegacyProjectsExplainerCollapsed(value: $value)\n      }\n    }\n  }\n"): (typeof documents)["\n  mutation UpdateLegacyProjectsExplainer($value: Boolean!) {\n    activeUserMutations {\n      meta {\n        setLegacyProjectsExplainerCollapsed(value: $value)\n      }\n    }\n  }\n"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n  mutation UpdateIntelligenceCommunityStandUpBannerDismissed($value: Boolean!) {\n    activeUserMutations {\n      meta {\n        setIntelligenceCommunityStandUpBannerDismissed(value: $value)\n      }\n    }\n  }\n"): (typeof documents)["\n  mutation UpdateIntelligenceCommunityStandUpBannerDismissed($value: Boolean!) {\n    activeUserMutations {\n      meta {\n        setIntelligenceCommunityStandUpBannerDismissed(value: $value)\n      }\n    }\n  }\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
