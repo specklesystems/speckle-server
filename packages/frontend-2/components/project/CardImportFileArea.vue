e9062ae8c8ad3b40e3c7c4959ba82d8197464f22
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2025-09-01 21:08:46 +0100

    frontend follow up, of sorts

--- packages/frontend-2/components/project/CardImportFileArea.vue
+++ packages/frontend-2/components/project/CardImportFileArea.vue
@@ -50,8 +50,12 @@
               <span class="underline">connectors</span>
             </NuxtLink>
             to publish a {{ modelName ? '' : 'new model' }} version to
-            {{ modelName ? 'this model' : 'this project' }}, or drag and drop a
-            IFC/OBJ/STL{{ isNextGenFileImporterEnabled ? '/SKP' : '' }} file here.
+            {{ modelName ? 'this model' : 'this project' }}, or drag and drop a IFC{{
+              isRhinoFileImporterEnabled
+                ? `, ${Array.from(rhinoImporterSupportedFileExtensions).join(', ')}`
+                : ''
+            }}
+            file here.
           </p>
           <div v-if="showEmptyState && !isDisabled" :class="buttonsClasses">
             <FormButton :to="connectorsRoute" size="sm" color="outline">
@@ -88,7 +92,7 @@ import type {
   ProjectPageLatestItemsModelItemFragment
 } from '~/lib/common/generated/gql/graphql'
 import type { FileAreaUploadingPayload } from '~/lib/form/helpers/fileUpload'
-import { useIsNextGenFileImporterEnabled } from '~/composables/globals'
+import { rhinoImporterSupportedFileExtensions } from '@speckle/shared/blobs'
 
 type EmptyStateVariants = 'modelGrid' | 'modelList' | 'modelsSection'
 
@@ -131,7 +135,7 @@ const props = defineProps<{
   emptyStateVariant?: EmptyStateVariants
 }>()
 
-const isNextGenFileImporterEnabled = useIsNextGenFileImporterEnabled()
+const isRhinoFileImporterEnabled = useIsRhinoFileImporterEnabled()
 const { addFailedJob } = useGlobalFileImportManager()
 const {
   maxSizeInBytes,
