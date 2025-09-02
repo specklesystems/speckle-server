21e897741b445f1a18c1cd66a135c6ccfffc276c
michalspeckle <michal@speckle.systems>
2025-09-02 15:23:39 +0200

    header adjustment

--- packages/frontend-2/components/viewer/filters/property-selection/Header.vue
+++ packages/frontend-2/components/viewer/filters/property-selection/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="px-2.5 flex items-center h-9">
-    <div class="text-body-3xs font-medium text-foreground-2 tracking-wide">
+    <div class="text-body-3xs font-semibold text-foreground-3">
       {{ title }}
     </div>
   </div>
