878a58b780b91f76739ef60ff1a0c0990c5a6dea
Mike Tasset <mike.tasset@gmail.com>
2025-09-04 23:51:10 +0200

    WIP MVP base

---
+++ packages/frontend-2/components/presentation/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="bg-foundation border border-outline-3 rounded-xl py-3 pl-1 pr-4 shadow-md h-10 flex items-center"
+  >
+    <div class="flex items-center justify-between space-x-2">
+      <button
+        class="size-8 flex items-center justify-center bg-foundation rounded-xl hover:bg-info-lighter hover:text-primary-focus"
+        :class="{ 'bg-info-lighter text-primary-focus': isSidebarOpen }"
+        @click="emit('toggleSidebar')"
+      >
+        <IconSidebar v-if="!isSidebarOpen" class="size-4" />
+        <IconSidebarClose v-else class="size-4" />
+      </button>
+      <h1 v-if="title" class="text-body-xs font-medium text-foreground leading-none">
+        {{ title }}
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { MaybeNullOrUndefined } from '@speckle/shared'
+
+const emit = defineEmits<{
+  (e: 'toggleSidebar'): void
+}>()
+
+defineProps<{
+  title: MaybeNullOrUndefined<string>
+}>()
+
+const isSidebarOpen = defineModel<boolean>('is-sidebar-open')
+</script>
---
+++ packages/frontend-2/pages/projects/[id]/presentations/[presentationId]/index.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="relative">
+    <PresentationHeader
+      v-model:is-sidebar-open="isSlidesSidebarOpen"
+      class="absolute top-4 left-4"
+      :title="presentation?.title"
+      :class="{ 'left-[15.75rem]': isSlidesSidebarOpen }"
+      @toggle-sidebar="isSlidesSidebarOpen = !isSlidesSidebarOpen"
+    />
+    <PresentationActions
+      v-model:is-sidebar-open="isInfoSidebarOpen"
+      class="absolute top-4 right-4"
+      :class="{ 'right-[21rem]': isInfoSidebarOpen }"
+      @toggle-sidebar="isInfoSidebarOpen = !isInfoSidebarOpen"
+    />
+
+    <PresentationSlideIndicator
+      class="absolute left-4 top-1/2 -translate-y-1/2"
+      :current-slide-index="currentViewIndex"
+      :slide-count="viewsCount || 0"
+      :class="{ 'left-[15.75rem]': isSlidesSidebarOpen }"
+    />
+
+    <div class="h-screen w-screen flex">
+      <PresentationSlidesSidebar
+        v-if="isSlidesSidebarOpen"
+        class="flex-shrink-0"
+        :slides="presentation"
+        :workspace-logo="workspace?.logo"
+        :workspace-name="workspace?.name"
+        :current-slide-index="currentViewIndex"
+        @select-slide="currentViewIndex = $event"
+      />
+
+      <div class="flex-1">
+        <img
+          :src="currentView?.screenshot"
+          alt="Current view"
+          class="h-full w-full object-cover"
+        />
+      </div>
+
+      <PresentationInfoSidebar
+        v-if="isInfoSidebarOpen"
+        class="flex-shrink-0"
+        :title="currentView?.name"
+      />
+    </div>
+
+    <PresentationControls
+      class="absolute bottom-4 right-1/2 -translate-x-1/2"
+      :disable-previous="disablePrevious"
+      :disable-next="disableNext"
+      @on-previous="onPrevious"
+      @on-next="onNext"
+    />
+
+    <div
+      class="bg-foundation border border-outline-3 rounded-xl shadow-md h-10 flex items-center absolute right-4 bottom-4 p-1"
+      :class="{ 'right-[21rem]': isInfoSidebarOpen }"
+    >
+      <button
+        class="size-8 flex items-center justify-center bg-foundation rounded-xl hover:bg-info-lighter hover:text-primary-focus"
+      >
+        <LucideFullscreen class="size-4" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { graphql } from '~~/lib/common/generated/gql'
+import { useQuery } from '@vue/apollo-composable'
+import { LucideFullscreen } from 'lucide-vue-next'
+
+graphql(`
+  fragment ProjectPresentations_SavedViewGroup on SavedViewGroup {
+    id
+  }
+`)
+
+const projectPresentationPageQuery = graphql(`
+  query ProjectPresentationPage(
+    $input: SavedViewGroupViewsInput!
+    $savedViewGroupId: ID!
+    $projectId: String!
+  ) {
+    project(id: $projectId) {
+      workspace {
+        id
+        name
+        logo
+      }
+      savedViewGroup(id: $savedViewGroupId) {
+        id
+        title
+        ...PresentationSlidesSidebar_SavedViewGroup
+        views(input: $input) {
+          totalCount
+          items {
+            name
+            id
+            screenshot
+          }
+        }
+      }
+    }
+  }
+`)
+
+definePageMeta({
+  layout: 'empty'
+})
+
+const route = useRoute()
+const projectId = computed(() => route.params.id as string)
+const savedViewGroupId = computed(() => route.params.presentationId as string)
+const { result } = useQuery(projectPresentationPageQuery, () => ({
+  projectId: projectId.value,
+  savedViewGroupId: savedViewGroupId.value,
+  input: {
+    limit: 100
+  }
+}))
+
+const currentViewIndex = ref(0)
+const isInfoSidebarOpen = ref(false)
+const isSlidesSidebarOpen = ref(false)
+
+const workspace = computed(() => result.value?.project.workspace)
+const currentView = computed(
+  () => result.value?.project.savedViewGroup.views.items[currentViewIndex.value]
+)
+const viewsCount = computed(() => result.value?.project.savedViewGroup.views.totalCount)
+const presentation = computed(() => result.value?.project.savedViewGroup)
+const disablePrevious = computed(() => currentViewIndex.value === 0)
+const disableNext = computed(() =>
+  viewsCount.value ? currentViewIndex.value === viewsCount.value - 1 : false
+)
+const onPrevious = () => {
+  currentViewIndex.value--
+}
+
+const onNext = () => {
+  currentViewIndex.value++
+}
+</script>
