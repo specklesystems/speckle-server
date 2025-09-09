a4ebb9fe124c466447e056b251afbdb2b486215f
Gergo Jedlicska <gergo@jedlicska.com>
2025-09-09 16:50:38 +0200

    WIP: streaming masking endpoint

--- packages/server/modules/core/rest/diffDownload.ts
+++ packages/server/modules/core/rest/diffDownload.ts
@@ -1,17 +1,33 @@
 import zlib from 'zlib'
 import { corsMiddlewareFactory } from '@/modules/core/configs/cors'
 import type { Application } from 'express'
-import { SpeckleObjectsStream } from '@/modules/core/rest/speckleObjectsStream'
-import { pipeline, PassThrough } from 'stream'
-import { getObjectsStreamFactory } from '@/modules/core/repositories/objects'
+import {
+  objectDataTransformFactory,
+  SpeckleObjectsStream
+} from '@/modules/core/rest/speckleObjectsStream'
+import { pipeline, PassThrough, Readable } from 'stream'
+import {
+  getObjectsStreamFactory,
+  getProjectObjectStreamFactory
+} from '@/modules/core/repositories/objects'
 import { db } from '@/db/knex'
 import { validatePermissionsReadStreamFactory } from '@/modules/core/services/streams/auth'
 import { getStreamFactory } from '@/modules/core/repositories/streams'
 import { authorizeResolver, validateScopes } from '@/modules/shared'
 import { getProjectDbClient } from '@/modules/multiregion/utils/dbSelector'
 import { UserInputError } from '@/modules/core/errors/userinput'
-import { ensureError } from '@speckle/shared'
+import { ensureError, Roles, Scopes } from '@speckle/shared'
 import { DatabaseError } from '@/modules/shared/errors'
+import { validateRequest } from 'zod-express'
+import { z } from 'zod'
+import { authMiddlewareCreator } from '@/modules/shared/middleware'
+import {
+  streamReadPermissionsPipelineFactory,
+  validateScope,
+  validateServerRoleBuilderFactory
+} from '@/modules/shared/authz'
+import { getRolesFactory } from '@/modules/shared/repositories/roles'
+import { chunk, constant, flatten, times } from 'lodash-es'
 
 export default (app: Application) => {
   const validatePermissionsReadStream = validatePermissionsReadStreamFactory({
@@ -120,4 +136,83 @@ export default (app: Application) => {
       speckleObjStream.end()
     }
   })
+
+  app.options('/api/v2/project/:streamId/object-stream', corsMiddlewareFactory())
+  app.post(
+    '/api/v2/project/:streamId/createObjectStream',
+    corsMiddlewareFactory(),
+    authMiddlewareCreator([
+      ...streamReadPermissionsPipelineFactory({
+        getStream: getStreamFactory({ db })
+      })
+    ]),
+    validateRequest({
+      body: z.object({
+        objectIds: z.string().array().min(1),
+        attributeMask: z
+          .union([
+            z.object({ include: z.string().array().min(1) }),
+            z.object({ exclude: z.string().array().min(1) })
+          ])
+          .optional()
+      })
+    }),
+    async ({ body: { objectIds, attributeMask }, params: { streamId }, log }, res) => {
+      const projectId = streamId
+      const projectDb = await getProjectDbClient({ projectId })
+
+      const streamObjectsFromDb = getProjectObjectStreamFactory({ db: projectDb })
+
+      res.writeHead(200, {
+        'Content-Encoding': 'gzip',
+        'Content-Type': 'text/plain; charset=UTF-8'
+      })
+      const objectDataTransform = objectDataTransformFactory({ attributeMask })
+
+      const gzipStream = zlib.createGzip()
+      pipeline(
+        objectDataTransform,
+        gzipStream,
+        new PassThrough({ highWaterMark: 16384 * 31 }),
+        res,
+        (err) => {
+          if (err) {
+            switch (err.code) {
+              case 'ERR_STREAM_PREMATURE_CLOSE':
+                log.debug({ err }, 'Stream to client has prematurely closed')
+                break
+              default:
+                log.error(err, 'App error streaming objects')
+                break
+            }
+            return
+          }
+          log.info(
+            {
+              childCount: objectIds.length,
+              mbWritten: gzipStream.bytesWritten / 1000000
+            },
+            'Streamed {childCount} objects (size: {mbWritten} MB)'
+          )
+        }
+      )
+      const maxBatchSize = 1000
+
+      // TODO, this could potentially be sped up a bit, if we concurrently
+      // pipe multiple db streams into the transform
+      for (const objectIdChunk of chunk(objectIds, maxBatchSize)) {
+        const objectStream = streamObjectsFromDb({
+          projectId,
+          objectIds: objectIdChunk
+        })
+        await new Promise((resolve, reject) => {
+          objectStream.once('end', resolve)
+          objectStream.once('error', reject)
+          // this is here, to make sure event handlers are registerd before piping the stream
+          objectStream.pipe(objectDataTransform, { end: false })
+        })
+      }
+      objectDataTransform.end()
+    }
+  )
 }
