9800411a425549647e5c2fe26288021d1a3c4a5b
Mike Tasset <mike.tasset@gmail.com>
2025-09-12 10:10:12 +0200

    Add link sharing

--- packages/frontend-2/components/presentation/Actions.vue
+++ packages/frontend-2/components/presentation/Actions.vue
@@ -10,9 +10,23 @@
       >
         Present
       </FormButton>
+
+      <LayoutMenu
+        v-model:open="showMenu"
+        :items="menuItems"
+        :menu-id="menuId"
+        mount-menu-on-body
+        @chosen="onActionChosen"
+      >
+        <PresentationFloatingPanelButton @click="showMenu = !showMenu">
+          <LucideEllipsis class="size-4" />
+        </PresentationFloatingPanelButton>
+      </LayoutMenu>
+
       <PresentationFloatingPanelButton v-if="isPresentMode" @click="toggleFullscreen">
         <LucideFullscreen class="size-4" />
       </PresentationFloatingPanelButton>
+
       <PresentationFloatingPanelButton
         :is-active="isSidebarOpen"
         @click="emit('toggleSidebar')"
@@ -24,7 +38,26 @@
 </template>
 
 <script setup lang="ts">
-import { LucideInfo, LucideFullscreen, LucidePlay } from 'lucide-vue-next'
+import {
+  LucideInfo,
+  LucideFullscreen,
+  LucidePlay,
+  LucideEllipsis
+} from 'lucide-vue-next'
+import type { LayoutMenuItem } from '~~/lib/layout/helpers/components'
+import { presentationRoute } from '~/lib/common/helpers/route'
+
+const { copy } = useClipboard()
+const { triggerNotification } = useGlobalToast()
+
+const props = defineProps<{
+  presentationId: string
+}>()
+
+enum MenuItems {
+  CopyLink = 'copy-link',
+  CopyPublicLink = 'copy-public-link'
+}
 
 const emit = defineEmits<{
   (e: 'toggleSidebar'): void
@@ -34,11 +67,84 @@ const emit = defineEmits<{
 const isSidebarOpen = defineModel<boolean>('is-sidebar-open')
 const isPresentMode = defineModel<boolean>('is-present-mode')
 
+const showMenu = ref(false)
+const menuId = useId()
+
+const menuItems = computed<LayoutMenuItem[][]>(() => [
+  [
+    {
+      title: 'Copy link',
+      id: MenuItems.CopyLink
+    }
+  ],
+  [
+    {
+      title: 'Copy public link',
+      id: MenuItems.CopyPublicLink
+    }
+  ]
+])
+
+const onActionChosen = async (params: { item: LayoutMenuItem }) => {
+  const { item } = params
+
+  switch (item.id) {
+    case MenuItems.CopyLink:
+      onCopyLink()
+      break
+    case MenuItems.CopyPublicLink:
+      onCopyPublicLink()
+      break
+  }
+}
+
 const toggleFullscreen = () => {
   if (!document.fullscreenElement) {
     document.documentElement.requestFullscreen()
   } else {
     document.exitFullscreen()
   }
+}
+
+const onCopyLink = async () => {
+  if (import.meta.server) return
+  const url = presentationRoute(props.presentationId)
+  if (!url) return
+
+  try {
+    await copy(new URL(url, window.location.origin).toString())
+  } catch (e) {
+    triggerNotification({
+      type: ToastNotificationType.Danger,
+      title: 'Link copy failed'
+    })
+    throw e
+  }
+
+  triggerNotification({
+    type: ToastNotificationType.Info,
+    title: 'Copied link'
+  })
+}
+
+const onCopyPublicLink = async () => {
+  if (import.meta.server) return
+  const url = presentationRoute(props.presentationId)
+  if (!url) return
+
+  try {
+    await copy(new URL(url, window.location.origin).toString())
+  } catch (e) {
+    triggerNotification({
+      type: ToastNotificationType.Danger,
+      title: 'Public link copy failed'
+    })
+    throw e
+  }
+
+  triggerNotification({
+    type: ToastNotificationType.Info,
+    title: 'Copied public link'
+  })
 }
 </script>
