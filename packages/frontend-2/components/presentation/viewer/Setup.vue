e6968f97fd80e68e9efc6e74e3ac859f6328c5ab
Mike Tasset <mike.tasset@gmail.com>
2025-09-18 21:45:11 +0200

    Disable highlighting and selection in presentation mode

--- packages/frontend-2/components/presentation/viewer/Setup.vue
+++ packages/frontend-2/components/presentation/viewer/Setup.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="presentation-viewer-setup h-full">
-    <ViewerCoreSetup viewer-host-classes="h-full" />
+    <ViewerCoreSetup
+      viewer-host-classes="h-full"
+      :disable-selection="disableSelection"
+    />
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps<{
+  disableSelection?: boolean
+}>()
+</script>
