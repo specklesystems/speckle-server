04bceb65e375a6f6d40633916bbba59f7d89dc82
Mike Tasset <mike.tasset@gmail.com>
2025-09-22 11:53:45 +0200

    Fix: Minor CSS fix

--- packages/frontend-2/components/presentation/InfoSidebar.vue
+++ packages/frontend-2/components/presentation/InfoSidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <aside
-    class="bg-foundation h-48 md:h-screen w-full md:w-64 xl:w-80 border-t md:border-t-0 md:border-l border-outline-3 py-5 px-4"
+    class="bg-foundation h-48 md:h-dvh w-full md:w-64 xl:w-80 border-t md:border-t-0 md:border-l border-outline-3 py-5 px-4"
   >
     <div class="hidden md:flex items-center justify-end space-x-0.5">
       <FormButton
