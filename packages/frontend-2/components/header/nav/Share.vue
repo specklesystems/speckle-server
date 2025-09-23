f46908975bd3c1a2dd31baad0abf494f9eb249db
Mike <mike.tasset@gmail.com>
2025-09-23 18:46:13 +0200

    Fix: Remove share menu transition (#5531)

--- packages/frontend-2/components/header/nav/Share.vue
+++ packages/frontend-2/components/header/nav/Share.vue
@@ -21,55 +21,48 @@
         Share
       </FormButton>
     </MenuButton>
-    <Transition
-      enter-active-class="transition ease-out duration-200"
-      enter-from-class="transform opacity-0 scale-95"
-      enter-to-class="transform opacity-100 scale-100"
-      leave-active-class="transition ease-in duration-75"
-      leave-from-class="transform opacity-100 scale-100"
-      leave-to-class="transform opacity-0 scale-95"
+
+    <MenuItems
+      class="absolute z-50 flex flex-col gap-1 right-0 top-11 min-w-max w-full sm:w-32 py-1 origin-top-right bg-foundation outline outline-1 outline-primary-muted rounded-md shadow-lg overflow-hidden mt-1"
     >
-      <MenuItems
-        class="absolute z-50 flex flex-col gap-1 right-0 top-11 min-w-max w-full sm:w-32 py-1 origin-top-right bg-foundation outline outline-1 outline-primary-muted rounded-md shadow-lg overflow-hidden mt-1"
-      >
-        <MenuItem v-slot="{ active }">
-          <div
-            :class="[
-              active ? 'bg-highlight-1' : '',
-              'text-body-xs flex px-2 py-1 text-foreground cursor-pointer transition mx-1 rounded'
-            ]"
-            @click="handleCopyLink"
-            @keypress="keyboardClick(handleCopyLink)"
-          >
-            Copy link
-          </div>
-        </MenuItem>
-        <MenuItem v-if="!isFederated" v-slot="{ active }">
-          <div
-            :class="[
-              active ? 'bg-highlight-1' : '',
-              'text-body-xs flex px-2 py-1 text-foreground cursor-pointer transition mx-1 rounded'
-            ]"
-            @click="handleCopyId"
-            @keypress="keyboardClick(handleCopyId)"
-          >
-            Copy ID
-          </div>
-        </MenuItem>
-        <MenuItem v-slot="{ active }">
-          <div
-            :class="[
-              active ? 'bg-highlight-1' : '',
-              'text-body-xs flex px-2 py-1 text-foreground cursor-pointer transition mx-1 rounded'
-            ]"
-            @click="handleEmbed"
-            @keypress="keyboardClick(handleEmbed)"
-          >
-            Embed model
-          </div>
-        </MenuItem>
-      </MenuItems>
-    </Transition>
+      <MenuItem v-slot="{ active }">
+        <div
+          :class="[
+            active ? 'bg-highlight-1' : '',
+            'text-body-xs flex px-2 py-1 text-foreground cursor-pointer transition mx-1 rounded'
+          ]"
+          @click="handleCopyLink"
+          @keypress="keyboardClick(handleCopyLink)"
+        >
+          Copy link
+        </div>
+      </MenuItem>
+      <MenuItem v-if="!isFederated" v-slot="{ active }">
+        <div
+          :class="[
+            active ? 'bg-highlight-1' : '',
+            'text-body-xs flex px-2 py-1 text-foreground cursor-pointer transition mx-1 rounded'
+          ]"
+          @click="handleCopyId"
+          @keypress="keyboardClick(handleCopyId)"
+        >
+          Copy ID
+        </div>
+      </MenuItem>
+      <MenuItem v-slot="{ active }">
+        <div
+          :class="[
+            active ? 'bg-highlight-1' : '',
+            'text-body-xs flex px-2 py-1 text-foreground cursor-pointer transition mx-1 rounded'
+          ]"
+          @click="handleEmbed"
+          @keypress="keyboardClick(handleEmbed)"
+        >
+          Embed model
+        </div>
+      </MenuItem>
+    </MenuItems>
+
     <ProjectModelPageDialogEmbed v-model:open="embedDialogOpen" :project="project" />
   </Menu>
 </template>
