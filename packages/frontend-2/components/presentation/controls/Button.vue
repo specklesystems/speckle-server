322566950cb534b0895beaf0caef0dfd2ad987f8
Mike Tasset <mike.tasset@gmail.com>
2025-10-02 14:00:55 +0200

    tooltip

--- packages/frontend-2/components/presentation/controls/Button.vue
+++ packages/frontend-2/components/presentation/controls/Button.vue
@@ -1,5 +1,6 @@
 <template>
   <button
+    v-tippy="tooltip"
     :disabled="disabled"
     class="bg-foundation size-8 rounded-md flex items-center justify-center hover:bg-primary-muted disabled:hover:bg-transparent text-foreground disabled:text-foreground-3"
   >
@@ -14,5 +15,6 @@ import type { ConcreteComponent } from 'vue'
 defineProps<{
   icon: ConcreteComponent
   disabled?: boolean
+  tooltip?: string
 }>()
 </script>
