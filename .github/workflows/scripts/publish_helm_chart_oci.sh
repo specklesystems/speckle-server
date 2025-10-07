862f6347c52f2db10a0f16ce7b5457018cb7de15
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2025-10-07 14:13:32 +0100

    ci(helm chart): debug by printing obfuscated password (#5692)

--- .github/workflows/scripts/publish_helm_chart_oci.sh
+++ .github/workflows/scripts/publish_helm_chart_oci.sh
@@ -55,7 +55,7 @@ fi
 yq e -i ".docker_image_tag = \"${IMAGE_VERSION_TAG}\"" "${GIT_REPO}/utils/helm/speckle-server/values.yaml"
 yq e -i ".name = \"${CHART_NAME}\"" "${GIT_REPO}/utils/helm/speckle-server/Chart.yaml"
 
-echo "Logging in to Helm registry ${HELM_REGISTRY_DOMAIN} with user ${REGISTRY_USERNAME}"
+echo "Logging in to Helm registry ${HELM_REGISTRY_DOMAIN} with user ${REGISTRY_USERNAME}. Password: ****${REGISTRY_PASSWORD: -3}"
 echo "${REGISTRY_PASSWORD}" | helm registry login "${HELM_REGISTRY_DOMAIN}" --username "${REGISTRY_USERNAME}" --password-stdin
 helm package "${GIT_REPO}/utils/helm/speckle-server" --version "${RELEASE_VERSION}" --app-version "${IMAGE_VERSION_TAG}" --destination "/tmp"
 echo "Publishing chart to oci://${HELM_REGISTRY_DOMAIN}/${HELM_REPOSITORY_PATH}/${CHART_NAME}"
