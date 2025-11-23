879a9df701983d3c62ddc0cb8d383b34dc6e1137
Charles Driesler <chuck@speckle.systems>
2025-11-23 18:03:09 +0000

    do not skip comments

--- packages/server/scripts/moveProjectsBetweenServers.ts
+++ packages/server/scripts/moveProjectsBetweenServers.ts
@@ -521,41 +521,32 @@ const main = async () => {
         for await (const commentBatch of getBatchedStreamCommentsFactory({
           db: sourceDb
         })(sourceProject.id)) {
-          try {
-            const commentsRemapped = commentBatch
-              .map((c) => {
-                if (c.text)
-                  return {
-                    ...c,
-                    authorId:
-                      userIdMapping?.[c.authorId] ?? TARGET_WORKSPACE_ROOT_ADMIN_USER_ID
-                  }
-              })
-              .filter((c) => c !== undefined)
-            // TODO: this borks the createdAt date !!!!!
-            // TODO: why is the text null in the return object?
-            if (commentsRemapped.length) {
-              // @ts-expect-error comments are always text
-              await insertCommentsFactory({ db: regionTrx })(commentsRemapped)
-              commentIds.push(...commentsRemapped.map((comment) => comment.id))
-            }
-            console.log(`${logKey} ${commentIds.length} comments moved`)
-          } catch {
-            console.log(`${logKey} Skipped comments`)
+          const commentsRemapped = commentBatch
+            .map((c) => {
+              if (c.text)
+                return {
+                  ...c,
+                  authorId:
+                    userIdMapping?.[c.authorId] ?? TARGET_WORKSPACE_ROOT_ADMIN_USER_ID
+                }
+            })
+            .filter((c) => c !== undefined)
+          // TODO: this borks the createdAt date !!!!!
+          // TODO: why is the text null in the return object?
+          if (commentsRemapped.length) {
+            // @ts-expect-error comments are always text
+            await insertCommentsFactory({ db: regionTrx })(commentsRemapped)
+            commentIds.push(...commentsRemapped.map((comment) => comment.id))
           }
-
+          console.log(`${logKey} ${commentIds.length} comments moved`)
         }
 
         // comment links
         if (commentIds.length) {
-          try {
-            const commentLinks = await getCommentLinksFactory({ db: sourceDb })(
-              commentIds
-            )
-            await insertCommentLinksFactory({ db: regionTrx })(commentLinks)
-          } catch {
-
-          }
+          const commentLinks = await getCommentLinksFactory({ db: sourceDb })(
+            commentIds
+          )
+          await insertCommentLinksFactory({ db: regionTrx })(commentLinks)
         }
 
         // skipping file uploads and blobs, there is none of that in the current source
